7e7d90356aa8a2288dbeae7cba3716cc896d4f56
Graham McNicoll <graham@growthbook.io>
2021-06-03 15:44:03 -0700

    Abstracted the onboarding welcome frame into new component, and updated the create org modal to use it (#15)
    * Abstracted onboarding welcome frame into new component, and moved the welcome flow and create org to this component

--- packages/front-end/components/Auth/CreateOrganization.tsx
+++ packages/front-end/components/Auth/CreateOrganization.tsx
@@ -1,8 +1,8 @@
-import { ReactElement, useContext } from "react";
+import { ReactElement, useContext, useState } from "react";
 import useForm from "../../hooks/useForm";
 import { useAuth } from "../../services/auth";
 import track from "../../services/track";
-import Modal from "../Modal";
+import WelcomeFrame from "./WelcomeFrame";
 import { UserContext } from "../ProtectedPage";
 
 export default function CreateOrganization(): ReactElement {
@@ -10,48 +10,75 @@ export default function CreateOrganization(): ReactElement {
     company: "",
   });
 
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
   const { apiCall } = useAuth();
   const { update } = useContext(UserContext);
 
-  return (
-    <Modal
-      header={
-        <img
-          alt="Growth Book"
-          src="/logo/growthbook-logo.png"
-          style={{ height: 40 }}
-        />
-      }
-      solidOverlay={true}
-      open={true}
-      submit={async () => {
-        await apiCall("/organization", {
-          method: "POST",
-          body: JSON.stringify({
-            company: value.company,
-          }),
-        });
-        track("Create Organization");
-        update();
-      }}
-      cta={"Submit"}
-    >
-      <h3>Create Organization</h3>
-      <p>
-        It looks like you aren&apos;t part of an organization yet. Create a new
-        one below.
-      </p>
+  const submit = async () => {
+    await apiCall("/organization", {
+      method: "POST",
+      body: JSON.stringify({
+        company: value.company,
+      }),
+    });
+    track("Create Organization");
+    update();
+  };
 
-      <div className="form-group">
-        Company Name
-        <input
-          required
-          minLength={3}
-          type="text"
-          {...inputProps.company}
-          className="form-control"
-        />
-      </div>
-    </Modal>
+  const leftside = (
+    <>
+      <h1 className="title h1">Welcome to Growth&nbsp;Book</h1>
+      <p>
+        you aren&apos;t part of an organization yet. <br />
+        Create a new one here.
+      </p>
+    </>
+  );
+  return (
+    <>
+      <WelcomeFrame leftside={leftside} loading={loading}>
+        <form
+          onSubmit={async (e) => {
+            e.preventDefault();
+            if (loading) return;
+            setError(null);
+            setLoading(true);
+            try {
+              await submit();
+              setLoading(false);
+            } catch (e) {
+              setError(e.message);
+              setLoading(false);
+            }
+          }}
+        >
+          <div>
+            <h3 className="h2">Create organization</h3>
+            <p className="text-muted">You can edit this at any time.</p>
+          </div>
+
+          <div className="form-group">
+            Company name
+            <input
+              required
+              type="text"
+              name="companyname"
+              autoFocus
+              autoComplete="companyname"
+              minLength={3}
+              {...inputProps.company}
+              className="form-control"
+            />
+          </div>
+
+          {error && <div className="alert alert-danger mr-auto">{error}</div>}
+          <button className={`btn btn-primary btn-block btn-lg`} type="submit">
+            Create organization
+          </button>
+        </form>
+      </WelcomeFrame>
+    </>
   );
 }
