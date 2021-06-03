caddd6fc8df08131446b12b32b4a42edfc2d9205
Graham McNicoll <graham@growthbook.io>
2021-06-03 15:00:52 -0700

    Abstracted onboarding frame into new component, and moved the welcome flow and create org to new style

--- packages/front-end/components/Auth/Welcome.tsx
+++ packages/front-end/components/Auth/Welcome.tsx
@@ -2,9 +2,9 @@ import { ReactElement, useState } from "react";
 import useForm from "../../hooks/useForm";
 import track from "../../services/track";
 import { getApiHost } from "../../services/env";
-import LoadingOverlay from "../../components/LoadingOverlay";
+import WelcomeFrame from "./WelcomeFrame";
 
-export default function Auth({
+export default function Welcome({
   onSuccess,
   firstTime = false,
 }: {
@@ -102,231 +102,196 @@ export default function Auth({
     ) : (
       <p>Let&apos;s get started with some experimentation</p>
     );
+
+  const leftside = (
+    <>
+      <h1 className="title h1">{welcomeMsg[welcomeMsgIndex]}</h1>
+      {welcomeContent}
+    </>
+  );
   return (
     <>
-      <div className="welcome container-fluid">
-        {loading && <LoadingOverlay />}
-        <div className="row full-height align-items-stretch d-flex flex-fill d-flex justify-content-start">
-          <div className="col-sm-5 intro-side ">
-            <div className="ghosted-logo"></div>
-            <div className="p-sm-1 p-md-3 pt-3 pt-sm-3 pt-md-5 d-flex align-items-center justify-content-center h-100">
-              <div className="text-center">
-                <h1 className="title h1">{welcomeMsg[welcomeMsgIndex]}</h1>
-                {welcomeContent}
+      <WelcomeFrame leftside={leftside} loading={loading}>
+        <form
+          onSubmit={async (e) => {
+            e.preventDefault();
+            if (loading) return;
+            setError(null);
+            setLoading(true);
+            try {
+              await submit();
+              setLoading(false);
+            } catch (e) {
+              setError(e.message);
+              setLoading(false);
+            }
+          }}
+        >
+          {state === "register" && (
+            <div>
+              <h3 className="h2">Register</h3>
+              <p>
+                Already have an account?{" "}
+                <a
+                  href="#"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setState("login");
+                  }}
+                >
+                  Log In
+                </a>
+              </p>
+            </div>
+          )}
+          {state === "firsttime" && (
+            <div>
+              <h3 className="h2">Set up your first account</h3>
+              <p>
+                This information stays on your servers and is never shared.{" "}
+                <br />
+                You can invite the rest of your team later.
+              </p>
+            </div>
+          )}
+          {state === "login" && (
+            <div>
+              <h3 className="h2">Log In</h3>
+              <p>
+                Don&apos;t have an account yet?{" "}
+                <a
+                  href="#"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setState("register");
+                  }}
+                >
+                  Register
+                </a>
+              </p>
+            </div>
+          )}
+          {state === "forgot" && (
+            <div>
+              <h3 className="h2">Forgot Password</h3>
+              <p>
+                <a
+                  href="#"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setState("login");
+                  }}
+                >
+                  Go back to Log In
+                </a>
+              </p>
+            </div>
+          )}
+          {state === "forgotSuccess" && (
+            <div>
+              <h3 className="h2">Forgot Password</h3>
+              <div className="alert alert-success">
+                Password reset link sent to <strong>{value.email}</strong>.
               </div>
+              <p>Click the link in the email to reset your password.</p>
+              <p>
+                Sent to the wrong email or need to resend?{" "}
+                <a
+                  href="#"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setState("forgot");
+                  }}
+                >
+                  Go Back
+                </a>
+              </p>
             </div>
-            <div className="logo">
-              <a
-                href="https://www.growthbook.io"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img
-                  src="/logo/growth-book-logo-white.png"
-                  style={{ maxWidth: "150px" }}
-                />
-              </a>
+          )}
+          {state === "firsttime" && (
+            <div className="form-group">
+              Company name
+              <input
+                required
+                type="text"
+                name="companyname"
+                autoFocus
+                autoComplete="companyname"
+                minLength={2}
+                {...inputProps.companyname}
+                className="form-control"
+              />
             </div>
-          </div>
-          <div className="col-sm-7 form-side p-0">
-            <div className="welcomemodal p-4 h-100">
-              <div className="h-100 align-items-center justify-content-center d-flex pr-2">
-                <div className="formwrap">
-                  <form
-                    onSubmit={async (e) => {
+          )}
+          {(state === "register" || state === "firsttime") && (
+            <div className="form-group">
+              Name
+              <input
+                required
+                type="text"
+                name="name"
+                autoFocus={state === "register"}
+                autoComplete="name"
+                minLength={2}
+                {...inputProps.name}
+                className="form-control"
+              />
+            </div>
+          )}
+          {(state === "login" ||
+            state === "register" ||
+            state === "forgot" ||
+            state === "firsttime") && (
+            <div className="form-group">
+              Email Address
+              <input
+                required
+                type="email"
+                name="email"
+                autoFocus={state === "login" || state === "forgot"}
+                autoComplete="username"
+                {...inputProps.email}
+                className="form-control"
+              />
+            </div>
+          )}
+          {(state === "login" ||
+            state === "register" ||
+            state === "firsttime") && (
+            <div className="form-group">
+              Password
+              <input
+                required
+                type="password"
+                name="password"
+                autoComplete={
+                  state === "login" ? "current-password" : "new-password"
+                }
+                minLength={8}
+                {...inputProps.password}
+                className="form-control"
+              />
+              {state === "login" && (
+                <small className="form-text text-muted">
+                  <a
+                    href="#"
+                    onClick={(e) => {
                       e.preventDefault();
-                      if (loading) return;
-                      setError(null);
-                      setLoading(true);
-                      try {
-                        await submit();
-                        setLoading(false);
-                      } catch (e) {
-                        setError(e.message);
-                        setLoading(false);
-                      }
+                      setState("forgot");
                     }}
                   >
-                    {state === "register" && (
-                      <div>
-                        <h3 className="h2">Register</h3>
-                        <p>
-                          Already have an account?{" "}
-                          <a
-                            href="#"
-                            onClick={(e) => {
-                              e.preventDefault();
-                              setState("login");
-                            }}
-                          >
-                            Log In
-                          </a>
-                        </p>
-                      </div>
-                    )}
-                    {state === "firsttime" && (
-                      <div>
-                        <h3 className="h2">Set up your first account</h3>
-                        <p>
-                          This information stays on your servers and is never
-                          shared. <br />
-                          You can invite the rest of your team later.
-                        </p>
-                      </div>
-                    )}
-                    {state === "login" && (
-                      <div>
-                        <h3 className="h2">Log In</h3>
-                        <p>
-                          Don&apos;t have an account yet?{" "}
-                          <a
-                            href="#"
-                            onClick={(e) => {
-                              e.preventDefault();
-                              setState("register");
-                            }}
-                          >
-                            Register
-                          </a>
-                        </p>
-                      </div>
-                    )}
-                    {state === "forgot" && (
-                      <div>
-                        <h3 className="h2">Forgot Password</h3>
-                        <p>
-                          <a
-                            href="#"
-                            onClick={(e) => {
-                              e.preventDefault();
-                              setState("login");
-                            }}
-                          >
-                            Go back to Log In
-                          </a>
-                        </p>
-                      </div>
-                    )}
-                    {state === "forgotSuccess" && (
-                      <div>
-                        <h3 className="h2">Forgot Password</h3>
-                        <div className="alert alert-success">
-                          Password reset link sent to{" "}
-                          <strong>{value.email}</strong>.
-                        </div>
-                        <p>
-                          Click the link in the email to reset your password.
-                        </p>
-                        <p>
-                          Sent to the wrong email or need to resend?{" "}
-                          <a
-                            href="#"
-                            onClick={(e) => {
-                              e.preventDefault();
-                              setState("forgot");
-                            }}
-                          >
-                            Go Back
-                          </a>
-                        </p>
-                      </div>
-                    )}
-                    {state === "firsttime" && (
-                      <div className="form-group">
-                        Company name
-                        <input
-                          required
-                          type="text"
-                          name="companyname"
-                          autoFocus
-                          autoComplete="companyname"
-                          minLength={2}
-                          {...inputProps.companyname}
-                          className="form-control"
-                        />
-                      </div>
-                    )}
-                    {(state === "register" || state === "firsttime") && (
-                      <div className="form-group">
-                        Name
-                        <input
-                          required
-                          type="text"
-                          name="name"
-                          autoFocus={state === "register"}
-                          autoComplete="name"
-                          minLength={2}
-                          {...inputProps.name}
-                          className="form-control"
-                        />
-                      </div>
-                    )}
-                    {(state === "login" ||
-                      state === "register" ||
-                      state === "forgot" ||
-                      state === "firsttime") && (
-                      <div className="form-group">
-                        Email Address
-                        <input
-                          required
-                          type="email"
-                          name="email"
-                          autoFocus={state === "login" || state === "forgot"}
-                          autoComplete="username"
-                          {...inputProps.email}
-                          className="form-control"
-                        />
-                      </div>
-                    )}
-                    {(state === "login" ||
-                      state === "register" ||
-                      state === "firsttime") && (
-                      <div className="form-group">
-                        Password
-                        <input
-                          required
-                          type="password"
-                          name="password"
-                          autoComplete={
-                            state === "login"
-                              ? "current-password"
-                              : "new-password"
-                          }
-                          minLength={8}
-                          {...inputProps.password}
-                          className="form-control"
-                        />
-                        {state === "login" && (
-                          <small className="form-text text-muted">
-                            <a
-                              href="#"
-                              onClick={(e) => {
-                                e.preventDefault();
-                                setState("forgot");
-                              }}
-                            >
-                              Forgot Password?
-                            </a>
-                          </small>
-                        )}
-                      </div>
-                    )}
-                    {error && (
-                      <div className="alert alert-danger mr-auto">{error}</div>
-                    )}
-                    <button
-                      className={`btn btn-primary btn-block btn-lg`}
-                      type="submit"
-                    >
-                      {cta}
-                    </button>
-                  </form>
-                </div>
-              </div>
+                    Forgot Password?
+                  </a>
+                </small>
+              )}
             </div>
-          </div>
-        </div>
-      </div>
+          )}
+          {error && <div className="alert alert-danger mr-auto">{error}</div>}
+          <button className={`btn btn-primary btn-block btn-lg`} type="submit">
+            {cta}
+          </button>
+        </form>
+      </WelcomeFrame>
     </>
   );
 }
---
+++ packages/front-end/components/Auth/WelcomeFrame.tsx
@@ -0,0 +1,51 @@
+import { FC, ReactElement, useState } from "react";
+import LoadingOverlay from "../../components/LoadingOverlay";
+
+type WelcomeFrameProps = {
+  leftside: string | ReactElement | boolean;
+  loading?: boolean;
+};
+const WelcomeFrame: FC<WelcomeFrameProps> = ({
+  leftside,
+  children,
+  loading: loadingState,
+}) => {
+  const [loading] = useState(loadingState);
+
+  return (
+    <>
+      <div className="welcome container-fluid">
+        {loading && <LoadingOverlay />}
+        <div className="row full-height align-items-stretch d-flex flex-fill d-flex justify-content-start">
+          <div className="col-sm-5 intro-side ">
+            <div className="ghosted-logo"></div>
+            <div className="p-sm-1 p-md-3 pt-3 pt-sm-3 pt-md-5 d-flex align-items-center justify-content-center h-100">
+              <div className="text-center">{leftside}</div>
+            </div>
+            <div className="logo">
+              <a
+                href="https://www.growthbook.io"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src="/logo/growth-book-logo-white.png"
+                  style={{ maxWidth: "150px" }}
+                />
+              </a>
+            </div>
+          </div>
+          <div className="col-sm-7 form-side p-0">
+            <div className="welcomemodal p-4 h-100">
+              <div className="h-100 align-items-center justify-content-center d-flex pr-2">
+                <div className="formwrap">{children}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default WelcomeFrame;
