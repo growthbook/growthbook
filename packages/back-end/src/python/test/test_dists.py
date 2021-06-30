4c81559ceab15f50ce4cac7831959fd1029f4d9e
itamarf <itamar.faran@gmail.com>
2021-07-01 01:01:38 +0300

    add tests

---
+++ packages/back-end/src/python/test/test_dists.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+
+
+class TestBeta(TestCase):
+    def test_posterior(self):
+        self.fail()
+
+    def test_moments(self):
+        self.fail()
+
+    def test_gq(self):
+        self.fail()
+
+
+class TestNorm(TestCase):
+    def test_posterior(self):
+        self.fail()
+
+    def test_moments(self):
+        self.fail()
+
+    def test_gq(self):
+        self.fail()
---
+++ packages/back-end/src/python/test/test_main.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+
+class TestBinom(TestCase):
+    def test_binomial_ab_test(self):
+        self.fail()
+
+
+class TestNorm(TestCase):
+    def test_gaussian_ab_test(self):
+        self.fail()
