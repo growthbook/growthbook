462a80bab058cf925fe53047096859cc9869c5bf
Jeremy Dorn <jeremy@jeremydorn.com>
2021-07-02 08:33:18 -0500

    Improve typography of the docs

--- packages/docs/tailwind.config.js
+++ packages/docs/tailwind.config.js
@@ -1,11 +1,69 @@
+const round = (num) =>
+  num
+    .toFixed(7)
+    .replace(/(\.[0-9]+?)0+$/, "$1")
+    .replace(/\.0$/, "");
+const em = (px, base) => `${round(px / base)}em`;
+
 module.exports = {
   purge: {
     content: ["./**/*.{jsx,tsx,mdx}"],
+    safelist: ["border", "dark:border-gray-700"],
   },
   theme: {
     extend: {
       typography(theme) {
         return {
+          DEFAULT: {
+            css: {
+              lineHeight: "1.5",
+              h2: {
+                marginTop: "1.5em",
+              },
+              ul: {
+                marginTop: "0.75em",
+                marginBottom: "1.25em",
+              },
+              "> ul > li > *:first-child": {
+                marginTop: em(12, 16),
+              },
+              "> ul > li > *:last-child": {
+                marginBottom: em(12, 16),
+              },
+              code: {
+                color: theme("colors.pink.600"),
+                borderColor: theme("colors.gray.200"),
+                padding: "3px 6px",
+                borderWidth: 1,
+                fontWeight: 400,
+                borderRadius: 5,
+              },
+              "code::before": {
+                content: '""',
+              },
+              "code::after": {
+                content: '""',
+              },
+            },
+          },
+          lg: {
+            css: {
+              lineHeight: "1.5",
+              h2: {
+                marginTop: "1.5em",
+              },
+              ul: {
+                marginTop: "0.75em",
+                marginBottom: "1.25em",
+              },
+              "> ul > li > *:first-child": {
+                marginTop: em(12, 16),
+              },
+              "> ul > li > *:last-child": {
+                marginBottom: em(12, 16),
+              },
+            },
+          },
           dark: {
             css: {
               color: theme("colors.gray.300"),
@@ -22,11 +80,17 @@ module.exports = {
               h2: { color: theme("colors.gray.100") },
               h3: { color: theme("colors.gray.100") },
               h4: { color: theme("colors.gray.100") },
-              code: { color: theme("colors.gray.100") },
+              code: {
+                color: theme("colors.green.300"),
+                borderColor: theme("colors.gray.700"),
+              },
               "a code": { color: theme("colors.gray.100") },
               pre: {
                 color: theme("colors.gray.200"),
                 backgroundColor: theme("colors.gray.800"),
+              },
+              "pre code": {
+                color: "inherit",
               },
               thead: {
                 color: theme("colors.gray.100"),
