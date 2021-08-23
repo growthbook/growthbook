938aa604eaca9102910d9db8b5eba382c17286ba
Graham McNicoll <graham@growthbook.io>
2021-08-23 14:56:23 -0700

    Added custom risk thresholds per metric (#53)
    * Added custom risk thresholds per metric

--- packages/front-end/components/Metrics/MetricForm.tsx
+++ packages/front-end/components/Metrics/MetricForm.tsx
@@ -12,6 +12,10 @@ import { useEffect } from "react";
 import Code from "../Code";
 import TagsInput from "../TagsInput";
 import { getDefaultConversionWindowHours } from "../../services/env";
+import {
+  defaultLoseRiskThreshold,
+  defaultWinRiskThreshold,
+} from "../../services/metrics";
 
 const weekAgo = new Date();
 weekAgo.setDate(weekAgo.getDate() - 7);
@@ -140,6 +144,8 @@ const MetricForm: FC<MetricFormProps> = ({
       userIdType: current.userIdType || "either",
       timestampColumn: current.timestampColumn || "",
       tags: current.tags || [],
+      winRisk: current.winRisk || defaultWinRiskThreshold * 100,
+      loseRisk: current.loseRisk || defaultLoseRiskThreshold * 100,
     },
     current.id || "new"
   );
@@ -194,8 +200,15 @@ const MetricForm: FC<MetricFormProps> = ({
   };
 
   const onSubmit = async () => {
+    const sendValue = { ...value };
+    //correct decimal/percent:
+    if (sendValue?.winRisk) sendValue.winRisk = sendValue.winRisk / 100;
+    if (sendValue?.loseRisk) sendValue.loseRisk = sendValue.loseRisk / 100;
+
+    if (value.loseRisk < value.winRisk) return;
+
     const body = JSON.stringify({
-      ...value,
+      sendValue,
       sql: sqlInput ? value.sql : "",
     });
 
@@ -263,6 +276,11 @@ const MetricForm: FC<MetricFormProps> = ({
     sqlPreviewData.column =
       ",\n  " + sqlPreviewData.column.replace(/\{\s*alias\s*\}\./g, "");
   }
+
+  const riskError =
+    value.loseRisk < value.winRisk
+      ? "The acceptable risk percentage cannot be higher than the too risky percentage"
+      : "";
 
   return (
     <PagedModal
@@ -690,6 +708,114 @@ GROUP BY
             </select>
           </div>
         )}
+        <div className="form-group">
+          Risk thresholds
+          <div className="riskbar row align-items-center pt-3">
+            <div className="col green-bar pr-0">
+              <span
+                style={{
+                  position: "absolute",
+                  top: "-20px",
+                  color: "#009a6d",
+                  fontSize: "0.75rem",
+                }}
+              >
+                acceptable risk under {value.winRisk}%
+              </span>
+              <div
+                style={{
+                  height: "10px",
+                  backgroundColor: "#009a6d",
+                  borderRadius: "5px 0 0 5px",
+                }}
+              ></div>
+            </div>
+            <div className="col-2 px-0">
+              <span
+                style={{
+                  position: "absolute",
+                  right: "4px",
+                  top: "6px",
+                  color: "#888",
+                }}
+              >
+                %
+              </span>
+              <input
+                className="form-control winrisk text-center"
+                type="number"
+                step="any"
+                min="0"
+                max="100"
+                value={value.winRisk}
+                onChange={(e) => {
+                  let newRisk = parseFloat(e.target.value);
+                  if (isNaN(newRisk)) newRisk = 0;
+                  manualUpdate({ winRisk: newRisk });
+                }}
+              />
+            </div>
+            <div className="col yellow-bar px-0">
+              <div
+                style={{
+                  height: "10px",
+                  backgroundColor: "#dfd700",
+                }}
+              ></div>
+            </div>
+            <div className="col-2 px-0">
+              <span
+                style={{
+                  position: "absolute",
+                  right: "4px",
+                  top: "6px",
+                  color: "#888",
+                }}
+              >
+                %
+              </span>
+              <input
+                className="form-control loserisk text-center"
+                type="number"
+                step="any"
+                min="0"
+                max="100"
+                value={value.loseRisk}
+                onChange={(e) => {
+                  let newRisk = parseFloat(e.target.value);
+                  if (isNaN(newRisk)) newRisk = 0;
+                  manualUpdate({ loseRisk: newRisk });
+                }}
+              />
+            </div>
+            <div className="col red-bar pl-0">
+              <span
+                style={{
+                  position: "absolute",
+                  top: "-20px",
+                  right: "15px",
+                  color: "#c50f0f",
+                  fontSize: "0.75rem",
+                }}
+              >
+                too much risk over {value.loseRisk}%
+              </span>
+              <div
+                style={{
+                  height: "10px",
+                  backgroundColor: "#c50f0f",
+                  borderRadius: "0 5px 5px 0",
+                }}
+              ></div>
+            </div>
+          </div>
+          {riskError && <div className="text-danger">{riskError}</div>}
+          <small className="text-muted">
+            Set the threasholds for risk for this metric. This is used when
+            determining metric signigicance, highlighting the risk value as
+            green, yellow, or red.
+          </small>
+        </div>
       </Page>
     </PagedModal>
   );
