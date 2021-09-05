84835ff670194844552332a10e67513b025e21e7
Jeremy Dorn <jeremy@jeremydorn.com>
2021-09-05 08:00:38 -0500

    Dynamically create portal div instead of adding to initial html output

--- packages/front-end/components/Modal/Portal.tsx
+++ packages/front-end/components/Modal/Portal.tsx
@@ -1,12 +1,20 @@
 import { FC, useEffect, useRef, useState } from "react";
 import { createPortal } from "react-dom";
 
+const PORTAL_CONTAINER_ID = "modal";
+
 const Portal: FC = ({ children }) => {
   const ref = useRef<HTMLElement>();
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
-    ref.current = document.getElementById("modal");
+    let el = document.getElementById(PORTAL_CONTAINER_ID);
+    if (!el) {
+      el = document.createElement("div");
+      el.id = PORTAL_CONTAINER_ID;
+      document.body.append(el);
+    }
+    ref.current = el;
     setMounted(true);
   }, []);
 
