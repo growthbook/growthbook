e0676e2b742b0b03ee384aabdfba9cd77ff8dcb4
Jeremy Dorn <jeremy@jeremydorn.com>
2022-01-11 22:31:55 -0600

    Add test suites for experiments and FNV hashes

--- packages/sdk-js/test/json.test.ts
+++ packages/sdk-js/test/json.test.ts
@@ -1,43 +1,58 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { Context, FeatureResult, GrowthBook } from "../src";
+import { Context, Experiment, FeatureResult, GrowthBook } from "../src";
 import { evalCondition } from "../src/mongrule";
+import { hashFnv32a } from "../src/util";
 import features from "./cases/features.json";
 import conditions from "./cases/conditions.json";
+import fnv from "./cases/fnv.json";
+import experiments from "./cases/experiments.json";
 
-type FeatureCasesArray = [string, Context, string, Partial<FeatureResult>][];
-
-// 5th optional array element specifies if a debug error is expected or not
-type ConditionCasesArray =
-  | [string, boolean, any, any]
-  | [string, boolean, any, any, boolean];
+// Name, context, feature key, result
+type FeatureCasesArray = [string, Context, string, FeatureResult][];
+// Name, result, condition, attributes
+type ConditionCasesArray = [string, boolean, any, any];
+// Value, result
+type FnvCasesArray = [string, number];
+// Name, context, experiment, result.value, result.inExperiment
+type ExperimentsCasesArray = [string, Context, Experiment<any>, any, boolean];
 
 describe("json test suite", () => {
   it.each(features as FeatureCasesArray)(
     "features.json[%#] %s",
     (name, ctx, key, result) => {
       const growthbook = new GrowthBook(ctx);
-      const res = growthbook.feature(key);
-
-      expect(res).toEqual({
-        on: !!result.value,
-        off: !result.value,
-        ...result,
-      });
-
+      expect(growthbook.feature(key)).toEqual(result);
       growthbook.destroy();
     }
   );
 
   it.each(conditions as ConditionCasesArray)(
     "conditions.json[%#] %s",
-    (name, expected, condition, value, expectError = false) => {
+    (name, expected, condition, value) => {
       const consoleErrorMock = jest
         .spyOn(console, "error")
         .mockImplementation();
       expect(evalCondition(value, condition)).toEqual(expected);
-      expect(consoleErrorMock).toHaveBeenCalledTimes(expectError ? 1 : 0);
       consoleErrorMock.mockRestore();
+    }
+  );
+
+  it.each((fnv as unknown) as FnvCasesArray)(
+    "fnv.json[%#] %s",
+    (value, expected) => {
+      expect(hashFnv32a(value as string) % 1000).toEqual(expected);
+    }
+  );
+
+  it.each(experiments as ExperimentsCasesArray)(
+    "experiments.json[%#] %s",
+    (name, ctx, exp, value, inExperiment) => {
+      const growthbook = new GrowthBook(ctx);
+      const res = growthbook.run(exp);
+      expect(res.value).toEqual(value);
+      expect(res.inExperiment).toEqual(inExperiment);
+      growthbook.destroy();
     }
   );
 });
