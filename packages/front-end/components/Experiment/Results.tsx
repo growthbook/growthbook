c99035d5d77f35778247376dd55e28a2f5793c8c
Jeremy Dorn <jeremy@jeremydorn.com>
2022-01-27 07:22:48 -0600

    Auto-fix mismatched variation ids. (#225)
    -  When there are unknown variation ids in experiment/report results, add CTA to fix it which opens a simple modal form.
    -  Allow re-ordering variations for an experiment
    -  When importing an experiment, show the variation ids and allow changing them

--- packages/front-end/components/Experiment/Results.tsx
+++ packages/front-end/components/Experiment/Results.tsx
@@ -16,6 +16,7 @@ import DateResults from "./DateResults";
 import AnalysisSettingsBar from "./AnalysisSettingsBar";
 import ExperimentReportsList from "./ExperimentReportsList";
 import usePermissions from "../../hooks/usePermissions";
+import { useAuth } from "../../services/auth";
 
 const BreakDownResults = dynamic(() => import("./BreakDownResults"));
 const CompactResults = dynamic(() => import("./CompactResults"));
@@ -27,6 +28,8 @@ const Results: FC<{
   mutateExperiment: () => void;
 }> = ({ experiment, editMetrics, editResult, mutateExperiment }) => {
   const { dimensions, getMetricById, getDatasourceById } = useDefinitions();
+
+  const { apiCall } = useAuth();
 
   const [phase, setPhase] = useState(experiment.phases.length - 1);
   const [dimension, setDimension] = useState("");
@@ -215,6 +218,37 @@ const Results: FC<{
             variations={variations}
             isUpdating={status === "running"}
             editMetrics={editMetrics}
+            setVariationIds={async (ids) => {
+              // Don't do anything if the query is currently running
+              if (status === "running") {
+                throw new Error("Cancel running query first");
+              }
+
+              // Update variation ids
+              await apiCall(`/experiment/${experiment.id}`, {
+                method: "POST",
+                body: JSON.stringify({
+                  variations: experiment.variations.map((v, i) => {
+                    return {
+                      ...v,
+                      key: ids[i] ?? v.key,
+                    };
+                  }),
+                }),
+              });
+
+              // Fetch results again
+              await apiCall(`/experiment/${experiment.id}/snapshot`, {
+                method: "POST",
+                body: JSON.stringify({
+                  phase,
+                  dimension,
+                }),
+              });
+
+              mutateExperiment();
+              mutate();
+            }}
           />
           {experiment.guardrails?.length > 0 && (
             <div className="mb-3 p-3">
