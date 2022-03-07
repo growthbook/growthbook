330df9ea959c71c2f2220e7f740a14ce7236b333
Jeremy Dorn <jeremy@jeremydorn.com>
2022-03-07 07:45:38 -0600

    Remove deprecated SQL group by column number syntax

--- packages/back-end/src/integrations/SqlIntegration.ts
+++ packages/back-end/src/integrations/SqlIntegration.ts
@@ -215,7 +215,9 @@ export default abstract class SqlIntegration
         params.from
       )}
         GROUP BY
-          1, 2, 3
+          experiment_id,
+          variation_id,
+          ${this.dateTrunc(this.castUserDateCol("timestamp"))}
       ),
       __userThresholds as (
         SELECT
@@ -1093,7 +1095,7 @@ export default abstract class SqlIntegration
           FROM
             (${replaceDateVars(join.query, from, to)}) i
           GROUP BY
-            1, 2
+            ${id1}, ${id2}
           `;
         }
       }
@@ -1116,7 +1118,7 @@ export default abstract class SqlIntegration
           ${timestampColumn} >= ${this.toTimestamp(from)}
           ${to ? `AND ${timestampColumn} <= ${this.toTimestamp(to)}` : ""}
         GROUP BY
-          1, 2
+          user_id, anonymous_id
         `;
       }
     }
