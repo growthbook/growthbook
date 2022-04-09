420762bec69111524013ed9233d93de3d426416f
Jeremy Dorn <jeremy@jeremydorn.com>
2022-04-09 18:51:23 -0500

    Simplify checkSrm implementation

--- packages/back-end/src/services/stats.ts
+++ packages/back-end/src/services/stats.ts
@@ -277,29 +277,26 @@ export async function analyzeExperimentResults(
 
 export function checkSrm(users: number[], weights: number[]) {
   // Skip variations with weight=0 or users=0
-  const realUsers: number[] = [];
-  const realWeights: number[] = [];
+  const data: [number, number][] = [];
   let totalUsers = 0;
   let totalWeight = 0;
   for (let i = 0; i < weights.length; i++) {
-    if (!weights[i]) continue;
-    if (!users[i]) continue;
-    realUsers.push(users[i]);
-    realWeights.push(weights[i]);
+    if (!weights[i] || !users[i]) continue;
+    data.push([users[i], weights[i]]);
     totalUsers += users[i];
     totalWeight += weights[i];
   }
 
   // Skip SRM calculation if there aren't enough valid variations
-  if (realUsers.length < 2) {
+  if (data.length < 2) {
     return 1;
   }
 
   // Calculate and return SRM p-value using a ChiSquare test
   let x = 0;
-  realUsers.forEach((o, i) => {
-    const e = (realWeights[i] / totalWeight) * totalUsers;
+  data.forEach(([o, e]) => {
+    e = (e / totalWeight) * totalUsers;
     x += Math.pow(o - e, 2) / e;
   });
-  return 1 - jStat.chisquare.cdf(x, realUsers.length - 1);
+  return 1 - jStat.chisquare.cdf(x, data.length - 1);
 }
