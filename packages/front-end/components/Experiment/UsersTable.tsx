e960babe1a15cd06b38dffe6fa12d6998c94e3dd
Jeremy Dorn <jeremy@jeremydorn.com>
2022-04-09 18:38:24 -0500

    Add missing file

---
+++ packages/front-end/components/Experiment/UsersTable.tsx
@@ -0,0 +1,125 @@
+import { FC, useMemo, useState } from "react";
+import { FaExclamationTriangle, FaQuestionCircle } from "react-icons/fa";
+import { useDefinitions } from "../../services/DefinitionsContext";
+import Tooltip from "../Tooltip";
+import {
+  ExperimentReportResultDimension,
+  ExperimentReportVariation,
+} from "back-end/types/report";
+import { formatTrafficSplit } from "../../services/utils";
+
+const numberFormatter = new Intl.NumberFormat();
+
+const SRM_THRESHOLD = 0.001;
+
+const UsersTable: FC<{
+  results: ExperimentReportResultDimension[];
+  variations: ExperimentReportVariation[];
+  dimensionId: string;
+}> = ({ dimensionId, results, variations }) => {
+  const { getDimensionById } = useDefinitions();
+
+  const [expand, setExpand] = useState(false);
+
+  const dimension = useMemo(() => {
+    return getDimensionById(dimensionId)?.name || "Dimension";
+  }, [getDimensionById, dimensionId]);
+
+  const hasSrm = (results || []).find((row) => row.srm < SRM_THRESHOLD);
+
+  if (!hasSrm && !expand) {
+    return (
+      <div>
+        <a
+          href="#"
+          onClick={(e) => {
+            e.preventDefault();
+            setExpand(true);
+          }}
+        >
+          Show traffic allocation
+        </a>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h2>
+        Users{" "}
+        {!hasSrm && (
+          <a
+            href="#"
+            style={{ fontSize: ".8rem", fontWeight: "normal" }}
+            className="ml-2"
+            onClick={(e) => {
+              e.preventDefault();
+              setExpand(false);
+            }}
+          >
+            hide
+          </a>
+        )}
+      </h2>
+      {hasSrm && (
+        <div className="alert alert-danger">
+          One or more dimensions has a Sample Ratio Mismatch (SRM)
+        </div>
+      )}
+      <table className="table w-auto table-bordered mb-5">
+        <thead>
+          <tr>
+            <th>{dimension}</th>
+            {variations.map((v, i) => (
+              <th key={i}>{v.name}</th>
+            ))}
+            <th>Expected</th>
+            <th>Actual</th>
+            <th>
+              SRM P-Value{" "}
+              <Tooltip
+                text={`Sample Ratio Mismatch (SRM) occurs when the actual traffic split is not what we expect. A small value (<${SRM_THRESHOLD}) indicates a likely bug.`}
+              >
+                <FaQuestionCircle />
+              </Tooltip>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {(results || []).map((r, i) => (
+            <tr key={i}>
+              <td>{r.name || <em>unknown</em>}</td>
+              {variations.map((v, i) => (
+                <td key={i}>
+                  {numberFormatter.format(r.variations[i]?.users || 0)}
+                </td>
+              ))}
+              <td>
+                {formatTrafficSplit(
+                  variations.map((v) => v.weight),
+                  1
+                )}
+              </td>
+              <td>
+                {formatTrafficSplit(
+                  variations.map((v, i) => r.variations[i]?.users || 0),
+                  1
+                )}
+              </td>
+              {r.srm < SRM_THRESHOLD ? (
+                <td className="bg-danger text-light">
+                  <FaExclamationTriangle className="mr-1" />
+                  {(r.srm || 0).toFixed(6)}
+                </td>
+              ) : (
+                <td>{(r.srm || 0).toFixed(6)}</td>
+              )}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <h2>Metrics</h2>
+    </div>
+  );
+};
+export default UsersTable;
