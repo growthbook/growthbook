06b491310167e132584170e1a3d033f09ed4b657
Gabriel Sotomayor <66047479+mr-gabe49@users.noreply.github.com>
2022-04-19 23:59:24 -0500

    Add tooltip to dashboard graph (#314)
    * Fix lint warning
    
    * Restructure code and install visx/event
    
    * Small name improvement
    
    * Create div and move onMOuseLeave inside div

--- packages/front-end/components/Experiment/ExperimentGraph.tsx
+++ packages/front-end/components/Experiment/ExperimentGraph.tsx
@@ -11,6 +11,9 @@ import format from "date-fns/format";
 import { useDefinitions } from "../../services/DefinitionsContext";
 import { getValidDate } from "../../services/dates";
 import { ExperimentStatus } from "back-end/types/experiment";
+import { TooltipWithBounds, useTooltip } from "@visx/tooltip";
+import styles from "./ExperimentGraph.module.scss";
+import { localPoint } from "@visx/event";
 
 export default function ExperimentGraph({
   resolution = "month",
@@ -24,6 +27,15 @@ export default function ExperimentGraph({
   height?: number;
 }): React.ReactElement {
   const { project } = useDefinitions();
+
+  const {
+    showTooltip,
+    hideTooltip,
+    tooltipOpen,
+    tooltipData,
+    tooltipLeft = 0,
+    tooltipTop = 0,
+  } = useTooltip();
 
   const { data, error } = useApi<{
     data: {
@@ -78,61 +90,106 @@ export default function ExperimentGraph({
           round: true,
         });
 
-        return (
-          <svg width={width} height={height}>
-            <Group left={margin[3]} top={margin[0]}>
-              <GridRows
-                scale={yScale}
-                numTicks={Math.min(maxYValue, 5)}
-                width={xMax + barWidth / 2}
-              />
-              {graphData.map((d, i) => {
-                const barX = xScale(getValidDate(d.name)) - barWidth / 2;
-                let barHeight = yMax - (yScale(d.numExp) ?? 0);
-                // if there are no experiments this month, show a little nub for design reasons.
-                if (barHeight === 0) barHeight = 6;
-                const barY = yMax - barHeight;
-                return (
-                  <BarRounded
-                    key={d.name + i}
-                    x={barX + 5}
-                    y={barY}
-                    width={Math.max(10, barWidth - 10)}
-                    height={barHeight}
-                    fill="#73D1F0"
-                    top
-                    radius={6}
-                  />
-                );
-              })}
+        const handlePointer = (event: React.MouseEvent<SVGElement>) => {
+          const coords = localPoint(event);
+          const xCoord = coords.x - barWidth;
 
-              <AxisBottom
-                top={yMax}
-                scale={xScale}
-                numTicks={
-                  width > 567
-                    ? graphData.length
-                    : Math.ceil(graphData.length / 2)
-                }
-                hideAxisLine={false}
-                stroke={"#C2C5D6"}
-                hideTicks={true}
-                rangePadding={barWidth / 2}
-                tickFormat={(v) => {
-                  return format(v as Date, "LLL yyyy");
-                }}
-              />
-              <AxisLeft
-                scale={yScale}
-                numTicks={Math.min(maxYValue, 5)}
-                stroke={"#C2C5D6"}
-                hideTicks={true}
-                tickFormat={(v) => {
-                  return Math.round(v as number) + "";
-                }}
-              />
-            </Group>
-          </svg>
+          const barData = graphData.map((d) => {
+            return { xcord: xScale(getValidDate(d.name)), numExp: d.numExp };
+          });
+
+          const closestBar = barData.reduce((prev, curr) =>
+            Math.abs(curr.xcord - xCoord) < Math.abs(prev.xcord - xCoord)
+              ? curr
+              : prev
+          );
+          let barHeight = yMax - (yScale(closestBar.numExp) ?? 0);
+          if (barHeight === 0) barHeight = 6;
+          const barY = yMax - barHeight;
+
+          showTooltip({
+            tooltipTop: barY - 25,
+            tooltipLeft: closestBar.xcord,
+            tooltipData: closestBar.numExp,
+          });
+        };
+
+        return (
+          <div
+            onMouseLeave={() => {
+              window.setTimeout(() => {
+                hideTooltip();
+              }, 100);
+            }}
+          >
+            <div style={{ position: "relative" }}>
+              {tooltipOpen && (
+                <TooltipWithBounds
+                  left={tooltipLeft}
+                  top={tooltipTop}
+                  className={styles.tooltip}
+                  unstyled={true}
+                >
+                  {tooltipData} ex
+                </TooltipWithBounds>
+              )}
+            </div>
+            <svg width={width} height={height} onMouseMove={handlePointer}>
+              <Group left={margin[3]} top={margin[0]}>
+                <GridRows
+                  scale={yScale}
+                  numTicks={Math.min(maxYValue, 5)}
+                  width={xMax + barWidth / 2}
+                />
+                {graphData.map((d, i) => {
+                  const barX = xScale(getValidDate(d.name)) - barWidth / 2;
+                  let barHeight = yMax - (yScale(d.numExp) ?? 0);
+                  // if there are no experiments this month, show a little nub for design reasons.
+                  if (barHeight === 0) barHeight = 6;
+                  const barY = yMax - barHeight;
+                  return (
+                    <BarRounded
+                      key={d.name + i}
+                      x={barX + 5}
+                      y={barY}
+                      width={Math.max(10, barWidth - 10)}
+                      height={barHeight}
+                      fill={"#73D1F0"}
+                      top
+                      radius={6}
+                      className={styles.barHov}
+                    />
+                  );
+                })}
+
+                <AxisBottom
+                  top={yMax}
+                  scale={xScale}
+                  numTicks={
+                    width > 567
+                      ? graphData.length
+                      : Math.ceil(graphData.length / 2)
+                  }
+                  hideAxisLine={false}
+                  stroke={"#C2C5D6"}
+                  hideTicks={true}
+                  rangePadding={barWidth / 2}
+                  tickFormat={(v) => {
+                    return format(v as Date, "LLL yyyy");
+                  }}
+                />
+                <AxisLeft
+                  scale={yScale}
+                  numTicks={Math.min(maxYValue, 5)}
+                  stroke={"#C2C5D6"}
+                  hideTicks={true}
+                  tickFormat={(v) => {
+                    return Math.round(v as number) + "";
+                  }}
+                />
+              </Group>
+            </svg>
+          </div>
         );
       }}
     </ParentSizeModern>
