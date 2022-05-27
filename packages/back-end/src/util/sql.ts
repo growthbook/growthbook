dc5cbb60f27d1a2b870306d8e94ba6282e8556cd
Jeremy Dorn <jeremy@jeremydorn.com>
2022-05-27 17:16:36 -0500

    Support Mixpanel metric conditions with boolean/number comparisons

--- packages/back-end/src/util/sql.ts
+++ packages/back-end/src/util/sql.ts
@@ -1,4 +1,5 @@
 import { format as sqlFormat } from "sql-formatter";
+import { Condition } from "../../types/metric";
 
 function getBaseIdType(objects: string[][], forcedBaseIdType?: string) {
   // If a specific id type is already chosen as the base, return it
@@ -96,4 +97,37 @@ export function format(sql: string) {
       // Fix Snowflate syntax for flatten function
       .replace(/ = > /g, " => ")
   );
+}
+
+export function getMixpanelPropertyColumn(col: string) {
+  // Use the column directly if it contains a reference to `event`
+  if (col.match(/\bevent\b/)) {
+    return col;
+  }
+
+  const colAccess = col.split(".").map((part) => {
+    if (part.substr(0, 1) !== "[") return `["${part}"]`;
+    return part;
+  });
+  return `event.properties${colAccess.join("")}`;
+}
+
+export function conditionToJavascript({ operator, value, column }: Condition) {
+  const col = getMixpanelPropertyColumn(column);
+  const encoded = JSON.stringify(value);
+
+  // Some operators map to special javascript syntax
+  if (operator === "~") {
+    return `${col}.match(/${value}/)`;
+  } else if (operator === "!~") {
+    return `!${col}.match(/${value}/)`;
+  } else if (operator === "=") {
+    return `${col}+'' == ${encoded}`;
+  } else {
+    // If the value is a number, don't use the JSON encoded version for comparison
+    const comp = !value || isNaN(Number(value)) ? encoded : value;
+
+    // All the other operators exactly match the javascript syntax so we can use them directly
+    return `${col}+'' ${operator} ${comp}`;
+  }
 }
