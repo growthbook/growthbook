630d69905bc370ab27b49ed8435bcdf7edb69507
Mira Bali <mirabali@Miras-MacBook-Pro.local>
2022-06-14 12:35:33 -0700

    Added functionality for discussion threads for features

--- packages/front-end/pages/features/[fid].tsx
+++ packages/front-end/pages/features/[fid].tsx
@@ -37,6 +37,7 @@ import DraftModal from "../../components/Features/DraftModal";
 import { FaExclamationTriangle } from "react-icons/fa";
 import RevisionDropdown from "../../components/Features/RevisionDropdown";
 import usePermissions from "../../hooks/usePermissions";
+import DiscussionThread from "../../components/DiscussionThread";
 
 export default function FeaturePage() {
   const router = useRouter();
@@ -378,6 +379,11 @@ export default function FeaturePage() {
           );
         })}
       </ControlledTabs>
+
+      <div className="mb-4">
+        <h3>Comments</h3>
+        <DiscussionThread type="feature" id={data.feature.id} />
+      </div>
 
       {permissions.createFeatureDrafts && (
         <div className="row">
