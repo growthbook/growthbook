72e2c289d48b7b4b20756fdae2427d1b3d147200
kelvan <kelvanb97@gmail.com>
2022-08-01 10:09:45 -0700

    save point

--- .devcontainer/dockerBuild.sh
+++ .devcontainer/dockerBuild.sh
@@ -1,7 +1,8 @@
-# add poetry to PATH
-export PATH="/root/.poetry/bin:$PATH"
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
+export PATH="$PATH:$HOME/.poetry/bin"
 
 #yarn related
+sudo chmod -R a+rwx /workspace
 yarn
 yarn setup
 yarn dev
