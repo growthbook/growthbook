e3bb5d597cc9db82394bcc3247cebaa8a37a38a9
kelvan <kelvanb97@gmail.com>
2022-08-01 18:41:31 -0700

    uses the poetry virtual environment as the main python environment to reference gbstats. Updates temporary path so the initialization session has access the the correct /home/kelvan/.poetry/bin:/home/kelvan/.local/bin:/home/kelvan/.yarn/bin:/home/kelvan/.config/yarn/global/node_modules/.bin:/home/kelvan/.poetry/bin:/home/kelvan/.local/bin:/home/kelvan/.yarn/bin:/home/kelvan/.config/yarn/global/node_modules/.bin:/home/kelvan/.poetry/bin:/home/kelvan/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin

--- .devcontainer/dockerBuild.sh
+++ .devcontainer/dockerBuild.sh
@@ -1,7 +1,13 @@
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
-export PATH="$PATH:$HOME/.poetry/bin"
+#environment setup
+sudo chmod -R a+rwx /etc/bash.bashrc
+echo "export PATH=\"/home/node/.cache/pypoetry/virtualenvs/gbstats-vabhrsvx-py3.7/bin:$PATH:$HOME/.poetry/bin\"" >> /etc/bash.bashrc
 
-#yarn related
+#poetry installation
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
+
+#needed for the current shell session
+export PATH="/home/node/.cache/pypoetry/virtualenvs/gbstats-vabhrsvx-py3.7/bin:$PATH:$HOME/.poetry/bin"
+
 sudo chmod -R a+rwx /workspace
 yarn
 yarn setup
