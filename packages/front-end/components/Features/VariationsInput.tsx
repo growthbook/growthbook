5ae9965fb4ac2fd0ce53b287eac18bccce10fd26
kelvan <kelvanb97@gmail.com>
2022-08-02 14:55:26 -0700

    added back coverageTooltip

--- packages/front-end/components/Features/VariationsInput.tsx
+++ packages/front-end/components/Features/VariationsInput.tsx
@@ -168,7 +168,7 @@ export default function VariationsInput({
         <div className="p-3 pb-0 border-bottom">
           <label>
             Percent of traffic included in this experiment{" "}
-            <Tooltip body="Users not included in the experiment will skip this rule." />
+            <Tooltip body={coverageTooltip} />
           </label>
           <div className="row align-items-center pb-3">
             <div className="col">
