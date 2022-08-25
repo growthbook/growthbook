b4d08ce32aa1ab88fa2f2b4f94348bcdfc5577bd
Michael Knowlton <michael@growthbook.io>
2022-08-25 11:03:05 -0400

    Checking in my original files just in case we want to use them down the road.

--- packages/front-end/components/GuidedGetStarted.tsx
+++ packages/front-end/components/GuidedGetStarted.tsx
@@ -1,46 +1,28 @@
-import { DimensionInterface } from "back-end/types/dimension";
 import { ExperimentInterfaceStringDates } from "back-end/types/experiment";
 import { FeatureInterface } from "back-end/types/feature";
 import { MetricInterface } from "back-end/types/metric";
-import { SegmentInterface } from "back-end/types/segment";
 import router from "next/router";
 import React, { useEffect, useMemo, useState } from "react";
-import useApi from "../hooks/useApi";
 import useOrgSettings from "../hooks/useOrgSettings";
-import { SettingsApiResponse } from "../pages/settings";
-import { useAuth } from "../services/auth";
-import { useDefinitions } from "../services/DefinitionsContext";
-import { hasFileConfig } from "../services/env";
-import { useAttributeSchema } from "../services/features";
-import track from "../services/track";
-import DimensionForm from "./Dimensions/DimensionForm";
 import ImportExperimentModal from "./Experiment/ImportExperimentModal";
 import CodeSnippetModal from "./Features/CodeSnippetModal";
-import EditAttributesModal from "./Features/EditAttributesModal";
 import FeatureModal from "./Features/FeatureModal";
-import GetStartedVideoModal from "./Features/GetStartedVideoModal";
-import { GetHelpDiv } from "./GetHelpDiv";
 import GetStartedSteps from "./GetStartedSteps";
-import SetupGuide from "./HomePage/SetupGuide";
-import { ExpandableDrawer } from "./Layout/ExpandableDrawer";
 import MetricForm from "./Metrics/MetricForm";
-import SegmentForm from "./Segments/SegmentForm";
 import DataSourceForm from "./Settings/DataSourceForm";
-import EditDataSourceSettingsForm from "./Settings/EditDataSourceSettingsForm";
+import ReactPlayer from "react-player";
+import Link from "next/link";
+import LoadingOverlay from "./LoadingOverlay";
+import { useDefinitions } from "../services/DefinitionsContext";
 
 export type Task = {
-  title: string;
+  titleOne: string;
+  titleTwo: string;
   text: string;
   cta: string;
   learnMoreLink?: string;
   link?: string;
   completed: boolean;
-  onClick: (
-    value: boolean | null
-  ) =>
-    | void
-    | Promise<void>
-    | ((value: null | Partial<DimensionInterface>) => void);
 };
 
 type Props = {
@@ -48,53 +30,23 @@ type Props = {
     experiments: ExperimentInterfaceStringDates[];
   };
   features: FeatureInterface[];
-  data: {
+  data?: {
     metrics: MetricInterface[];
   };
-  mutate: () => void;
 };
 
-export default function GuidedGetStarted({
+export default function GuidedGetStarted2({
   experiments,
   features,
   data,
-  mutate,
 }: Props) {
-  const { apiCall } = useAuth();
+  const [currentStep, setCurrentStep] = useState(null);
   const settings = useOrgSettings();
-  const [
-    initialTasksPercentComplete,
-    setInitialTasksPercentComplete,
-  ] = useState(null);
-  const [
-    advancedTasksPercentComplete,
-    setAdvancedTasksPercentComplete,
-  ] = useState(null);
-  const [dismissedSteps, setDismissedSteps] = useState(
+  const [dismissedSteps] = useState(
     settings.dismissedGettingStartedSteps || {}
   );
-  const [featureModalOpen, setFeatureModalOpen] = useState(false);
-  const [videoModalOpen, setVideoModalOpen] = useState(false);
-  const [codeModalOpen, setCodeModalOpen] = useState(false);
-  const [dataSourceModalOpen, setDataSourceModalOpen] = useState(false);
-  const [dataSourceQueriesOpen, setDataSourceQueriesOpen] = useState(false);
-  const [metricsOpen, setMetricsOpen] = useState(false);
-  const [experimentsModalOpen, setExperimentsModalOpen] = useState(false);
-  const [attributesModalOpen, setAttributesModalOpen] = useState(false);
-  const [dimensionForm] = useState<null | Partial<DimensionInterface>>({});
-  const [dimensionFormModalOpen, setDimensionFormModalOpen] = useState(false);
-  const [segmentForm] = useState<null | Partial<SegmentInterface>>({});
-  const [segmentFormModalOpen, setSegmentFormModalOpen] = useState(false);
 
-  const attributeSchema = useAttributeSchema();
-
-  const {
-    dimensions,
-    segments,
-    datasources,
-    mutateDefinitions,
-    metrics,
-  } = useDefinitions();
+  const { datasources } = useDefinitions();
 
   // If this is coming from a feature experiment rule
   const featureExperiment = useMemo(() => {
@@ -113,91 +65,61 @@ export default function GuidedGetStarted({
     }
   }, [router?.query?.featureExperiment]);
 
-  const org = useApi<SettingsApiResponse>(`/organization`);
-
-  const hasDataSource = datasources.length > 0;
-
-  const hasMetrics =
-    metrics.filter((m) => !m.id.match(/^met_sample/)).length > 0;
-
-  const hasExperiments =
-    experiments?.experiments.filter((m) => !m.id.match(/^exp_sample/)).length >
-    0;
-
-  const allowImport = !(hasMetrics || hasExperiments) && !hasFileConfig();
-
-  const hasSampleExperiment = experiments?.experiments.filter((m) =>
-    m.id.match(/^exp_sample/)
-  )[0];
-
-  const importSampleData = (source: string) => async () => {
-    const res = await apiCall<{
-      experiment: string;
-    }>(`/organization/sample-data`, {
-      method: "POST",
-    });
-    await mutateDefinitions();
-    await mutate();
-    track("Add Sample Data", {
-      source,
-    });
-    await router.push("/experiment/" + res.experiment);
-  };
-
-  const initialTasks: Task[] = [
+  const steps: Task[] = [
     {
-      title: "Video: Growthbook 101",
+      titleOne: "Welcome to ",
+      titleTwo: "GrowthBook!",
       text:
-        "A very brief introduction to GrowthBook - the open-source feature flagging and A/B testing platform.",
+        "This quick start guide is designed to get you up and running with GrowthBook in ~15 minutes!",
       completed:
         settings?.videoInstructionsViewed ||
         dismissedSteps["Video: Growthbook 101"],
       cta: "Watch Video",
-      onClick: setVideoModalOpen,
     },
     {
-      title: "Install SDK",
+      titleOne: "Install an ",
+      titleTwo: "SDK",
       text:
         "Integrate GrowthBook into your Javascript, React, Golang, Ruby, PHP, Python, or Android application. More languages and frameworks coming soon!",
       cta: "View Instructions",
       learnMoreLink: "Learn more about our SDKs.",
       link: "https://docs.growthbook.io/lib",
-      onClick: setCodeModalOpen,
       completed:
         settings?.sdkInstructionsViewed || dismissedSteps["Install SDK"],
     },
     {
-      title: "Create a Feature Flag",
+      titleOne: "Create a ",
+      titleTwo: "Feature Flag",
       text:
         "Create a feature flag within GrowthBook. Use feature flags to toggle app behavior, do gradual rollouts, and run A/B tests.",
       cta: "Create Feature Flag",
       learnMoreLink: "Learn more about how to use feature flags.",
       link: "https://docs.growthbook.io/app/features",
-      onClick: setFeatureModalOpen,
       completed: features.length > 0 || dismissedSteps["Create a Feature Flag"],
     },
     {
-      title: "Add a Data Source",
+      titleOne: "Add a ",
+      titleTwo: "Data Source",
       text:
         "GrowthBook needs read access to where your experiment and metric data lives. We support Mixpanel, Snowflake, Redshift, BigQuery, Google Analytics, and more. If you don't see yours, let us know or open a GitHub issue.",
       cta: "Add Data Source",
       learnMoreLink: "Learn more about how to connect to a data source.",
       link: "https://docs.growthbook.io/app/datasources",
       completed: datasources.length > 0 || dismissedSteps["Add a Data Source"],
-      onClick: setDataSourceModalOpen,
     },
     {
-      title: "Define a Metric",
+      titleOne: "Define a ",
+      titleTwo: "Metric",
       text:
         "Create a library of metrics to experiment against. You can always add more at any time, and even add them retroactively to past experiments.",
       cta: "Define a Metric",
       learnMoreLink: "Learn more about how to use metrics.",
       link: "https://docs.growthbook.io/app/metrics",
       completed: data?.metrics.length > 0 || dismissedSteps["Define a Metric"],
-      onClick: setMetricsOpen,
     },
     {
-      title: "Create an Experiment",
+      titleOne: "Create an ",
+      titleTwo: "Experiment",
       text:
         "Import an existing experiment from your data source or create a new draft from scratch.",
       cta: "Create Experiment",
@@ -206,219 +128,139 @@ export default function GuidedGetStarted({
       completed:
         experiments?.experiments.length > 0 ||
         dismissedSteps["Create an Experiment"],
-      onClick: setExperimentsModalOpen,
     },
   ];
-
-  const advancedTasks: Task[] = [
-    {
-      title: "Create a Dimension",
-      text:
-        "User Dimensions are attributes of your users - for example, 'subscription plan' or 'age group'. In Growth Book, you can use these to drill down into experiment results.",
-      completed: dimensions.length > 0 || dismissedSteps["Create a dimension"],
-      cta: "Create a Dimension",
-      learnMoreLink: "Learn more about dimensions.",
-      link: "https://docs.growthbook.io/app/dimensions",
-      onClick: setDimensionFormModalOpen,
-    },
-    {
-      title: "Create a Segment",
-      text:
-        "Segments define important groups of users - for example, 'annual subscribers' or 'left-handed people from France.'",
-      cta: "Create a Segment",
-      completed: segments.length > 0 || dismissedSteps["Create a segment"],
-      onClick: setSegmentFormModalOpen,
-    },
-    {
-      title: "Define an Attribute",
-      text:
-        "Attributes can be used when targeting feature flags. Attributes set here must also be passed in through the SDK.",
-      cta: "Create an Attribute",
-      onClick: setAttributesModalOpen,
-      completed:
-        attributeSchema.length > 0 || dismissedSteps["Define an attribute"],
-    },
-    {
-      title: "Invite teammates",
-      text:
-        "GrowthBook needs read access to where your experiment and metric data lives. We support Mixpanel, Snowflake, Redshift, BigQuery, Google Analytics, and more. If you don't see yours, let us know or open a GitHub issue.",
-      cta: "Invite a Teammate",
-      completed:
-        org.data?.organization.members.length +
-          org.data?.organization.invites.length >
-          1 || dismissedSteps["Invite teammates"],
-      onClick: () => {
-        router.push("/settings/team");
-      },
-    },
-    {
-      title: "Install our Chrome Plugin",
-      text:
-        "QA and debug feature flags and experiments from GrowthBook's js/react SDKs.",
-      cta: "Get the Plugin",
-      completed:
-        settings?.pluginInstructionsViewed ||
-        dismissedSteps["Install our Chrome Plugin"],
-      onClick: async () => {
-        await apiCall(`/organization`, {
-          method: "PUT",
-          body: JSON.stringify({
-            settings: {
-              pluginInstructionsViewed: true,
-            },
-          }),
-        });
-        window.open(
-          "https://chrome.google.com/webstore/detail/growthbook-devtools/opemhndcehfgipokneipaafbglcecjia",
-          "_blank",
-          "noopener,noreferrer"
-        );
-      },
-    },
-  ];
-
-  const calculatePercentComplete = (tasks) => {
-    let tasksCompleted = 0;
-    tasks.forEach((task) => {
-      if (task.completed === true) {
-        tasksCompleted++;
-      }
-    });
-
-    const percentComplete = (tasksCompleted / tasks.length) * 100;
-
-    return Math.round(percentComplete);
-  };
 
   useEffect(() => {
-    if (settings && features && experiments) {
-      setInitialTasksPercentComplete(calculatePercentComplete(initialTasks));
-      setAdvancedTasksPercentComplete(calculatePercentComplete(advancedTasks));
+    function setInitialStep() {
+      const initialStep = steps.findIndex((step) => step.completed === false);
+
+      if (initialStep >= 0) {
+        setCurrentStep(initialStep);
+      } else {
+        setCurrentStep(0);
+      }
     }
-  }, [initialTasks, advancedTasks, settings]);
+
+    setInitialStep();
+  }, []);
+
+  if (currentStep === null) return <LoadingOverlay />;
 
   return (
     <>
-      <GetStartedSteps />
-      {videoModalOpen && (
-        <GetStartedVideoModal close={() => setVideoModalOpen(false)} />
-      )}
-      {codeModalOpen && (
-        <CodeSnippetModal close={() => setCodeModalOpen(false)} />
-      )}
-      {featureModalOpen && (
-        <FeatureModal
-          close={() => setFeatureModalOpen(false)}
-          onSuccess={async (feature) => {
-            const url = `/features/${feature.id}${
-              features.length > 0 ? "" : "?first"
-            }`;
-            await router.push(url);
-          }}
-        />
-      )}
-      {dataSourceModalOpen && (
-        <DataSourceForm
-          data={{
-            name: "My Datasource",
-            settings: {},
-          }}
-          existing={false}
-          source="get-started"
-          onCancel={() => setDataSourceModalOpen(false)}
-          onSuccess={async () => {
-            await mutateDefinitions();
-            setDataSourceModalOpen(false);
-            setDataSourceQueriesOpen(true);
-          }}
-          importSampleData={
-            !hasDataSource &&
-            allowImport &&
-            !hasSampleExperiment &&
-            importSampleData("datasource-form")
-          }
-        />
-      )}
-      {metricsOpen && (
-        <MetricForm
-          current={{}}
-          edit={false}
-          source="get-started"
-          onClose={(refresh) => {
-            setMetricsOpen(false);
-            if (refresh) {
-              mutateDefinitions();
-            }
-          }}
-        />
-      )}
-      {experimentsModalOpen && (
-        <ImportExperimentModal
-          onClose={() => setExperimentsModalOpen(false)}
-          source={featureExperiment ? "feature-rule" : "get-started"}
-          initialValue={featureExperiment}
-          fromFeature={!!featureExperiment}
-        />
-      )}
-      {dataSourceQueriesOpen &&
-        datasources?.[0] &&
-        datasources[0].properties?.hasSettings && (
-          <EditDataSourceSettingsForm
-            firstTime={true}
-            data={datasources[0]}
-            onCancel={() => setDataSourceQueriesOpen(false)}
-            onSuccess={() => {
-              setDataSourceQueriesOpen(false);
-              mutateDefinitions();
-            }}
-            source="onboarding"
-          />
-        )}
-      {dimensionFormModalOpen && (
-        <DimensionForm
-          close={() => setDimensionFormModalOpen(false)}
-          current={dimensionForm}
-        />
-      )}
-      {segmentFormModalOpen && (
-        <SegmentForm
-          close={() => setSegmentFormModalOpen(false)}
-          current={segmentForm}
-        />
-      )}
-      {attributesModalOpen && (
-        <EditAttributesModal close={() => setAttributesModalOpen(false)} />
-      )}
-      <ExpandableDrawer
-        title="Quick Start Guide"
-        percentComplete={initialTasksPercentComplete}
-        open={
-          initialTasksPercentComplete && initialTasksPercentComplete === 100
-            ? false
-            : true
-        }
-      >
-        <SetupGuide
-          showSubHead
-          tasks={initialTasks}
-          dismissedSteps={dismissedSteps}
-          setDismissedSteps={setDismissedSteps}
-        />
-      </ExpandableDrawer>
-      <ExpandableDrawer
-        title="Setup Advanced Features"
-        percentComplete={advancedTasksPercentComplete}
-        open={false}
-      >
-        <SetupGuide
-          tasks={advancedTasks}
-          dismissedSteps={dismissedSteps}
-          setDismissedSteps={setDismissedSteps}
-        />
-      </ExpandableDrawer>
-      <ExpandableDrawer title="Need Help?" open={false}>
-        <GetHelpDiv />
-      </ExpandableDrawer>
+      <GetStartedSteps
+        setCurrentStep={setCurrentStep}
+        currentStep={currentStep}
+        steps={steps}
+      />
+      <div className="d-flex flex-column">
+        <div className="d-flex flex-column align-items-center p-2">
+          <h1>
+            <span>{steps[currentStep].titleOne}</span>
+            <span style={{ color: "#7C45E9", fontWeight: "bold" }}>
+              {steps[currentStep].titleTwo}
+            </span>
+          </h1>
+          <h5
+            className="m-0"
+            style={{ textAlign: "center", maxWidth: "800px" }}
+          >
+            {steps[currentStep].text}
+            {steps[currentStep].learnMoreLink && steps[currentStep].link && (
+              <span>
+                <Link href={steps[currentStep].link}>
+                  <a>{` ${steps[currentStep].learnMoreLink}`}</a>
+                </Link>
+              </span>
+            )}
+          </h5>
+          {steps[currentStep].titleOne === "Welcome to " && (
+            <Link href="/settings/team">
+              <a>Not an engineer? Invite a developer to get started.</a>
+            </Link>
+          )}
+        </div>
+        <div>
+          {steps[currentStep].titleOne === "Welcome to " && (
+            <>
+              <div className="d-flex flex-column align-items-center m-2">
+                <ReactPlayer
+                  className="m-2"
+                  url="https://www.youtube.com/watch?v=1ASe3K46BEw"
+                  light={true}
+                  playing={true}
+                  controls={true}
+                  style={{ boxShadow: "#9D9D9D 4px 4px 12px 0px" }}
+                />
+                <button
+                  onClick={() => setCurrentStep(currentStep + 1)}
+                  className="btn btn-primary w-25 m-2"
+                >
+                  Next: Install SDK
+                </button>
+                <button className="btn btn-outline-primary w-25 m-2">
+                  Skip Onboarding
+                </button>
+              </div>
+            </>
+          )}
+          {steps[currentStep].titleOne === "Install an " && (
+            <CodeSnippetModal
+              inline={true}
+              cta={"Next: Create Feature Flag"}
+              submit={() => {
+                setCurrentStep(currentStep + 1);
+              }}
+            />
+          )}
+          {steps[currentStep].titleOne === "Create a " && (
+            <FeatureModal
+              inline={true}
+              cta={"Next: Add a Data Source"}
+              onSuccess={async () => {
+                setCurrentStep(currentStep + 1);
+              }}
+            />
+          )}
+          {steps[currentStep].titleOne === "Add a " && (
+            <DataSourceForm
+              data={{
+                name: "My Datasource",
+                settings: {},
+              }}
+              existing={false}
+              source="get-started"
+              inline={true}
+              cta={"Next: Add a Data Source"}
+              onSuccess={async () => {
+                setCurrentStep(currentStep + 1);
+              }}
+            />
+          )}
+          {steps[currentStep].titleOne === "Define a " && (
+            <MetricForm
+              inline={true}
+              cta={"Next: Create Experiment"}
+              current={{}}
+              edit={false}
+              source="get-started"
+              onSuccess={() => {
+                setCurrentStep(currentStep + 1);
+              }}
+            />
+          )}
+          {steps[currentStep].titleOne === "Create an " && (
+            <ImportExperimentModal
+              inline={true}
+              source={featureExperiment ? "feature-rule" : "get-started"}
+              initialValue={featureExperiment}
+              fromFeature={!!featureExperiment}
+              showClose={false}
+            />
+          )}
+        </div>
+      </div>
     </>
   );
 }
