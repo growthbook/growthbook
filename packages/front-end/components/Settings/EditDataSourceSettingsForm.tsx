2f4fd218b2e0baf2545ebba947a8a14f64d1c7a6
tinahollygb <tina@growthbook.io>
2022-09-27 13:49:02 -0700

    restore old EditDataSourceSettingsForm to prevent regressions

--- packages/front-end/components/Settings/EditDataSourceSettingsForm.tsx
+++ packages/front-end/components/Settings/EditDataSourceSettingsForm.tsx
@@ -5,10 +5,12 @@ import track from "../../services/track";
 import Modal from "../Modal";
 import { DataSourceInterfaceWithParams } from "back-end/types/datasource";
 import Field from "../Forms/Field";
+import Code from "../Code";
 import DataSourceSchemaChooser from "./DataSourceSchemaChooser";
 import { useFieldArray, useForm } from "react-hook-form";
 import StringArrayField from "../Forms/StringArrayField";
 import uniqid from "uniqid";
+import MultiSelectField from "../Forms/MultiSelectField";
 import CodeTextArea from "../Forms/CodeTextArea";
 import Toggle from "../Forms/Toggle";
 import Tooltip from "../Tooltip";
@@ -61,6 +63,10 @@ const EditDataSourceSettingsForm: FC<{
   const identityJoins = useFieldArray({
     control: form.control,
     name: "settings.queries.identityJoins",
+  });
+  const userIdTypes = useFieldArray({
+    control: form.control,
+    name: "settings.userIdTypes",
   });
 
   const userIdTypeOptions = form.watch("settings.userIdTypes").map((t) => {
@@ -132,6 +138,81 @@ const EditDataSourceSettingsForm: FC<{
             />
           ) : (
             <>
+              <div className="mb-4">
+                <h4>Identifier Types</h4>
+                <div>
+                  Define all the different units you use to split traffic in an
+                  experiment. Some examples: user_id, device_id, ip_address.
+                </div>
+
+                {userIdTypes.fields.map((userIdType, i) => {
+                  return (
+                    <div
+                      key={userIdType.id}
+                      className="bg-light border my-2 p-3 ml-3"
+                    >
+                      <div className="row">
+                        <div className="col-auto">
+                          <h5>
+                            {i + 1}.{" "}
+                            {form.watch(`settings.userIdTypes.${i}.userIdType`)}
+                          </h5>
+                        </div>
+                        <div className="col-auto ml-auto">
+                          <a
+                            className="text-danger"
+                            href="#"
+                            title="Remove identifier type"
+                            onClick={(e) => {
+                              e.preventDefault();
+                              userIdTypes.remove(i);
+                            }}
+                          >
+                            delete
+                          </a>
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col-md-7 col-lg-8">
+                          <Field
+                            label="Identifier Type"
+                            pattern="^[a-z_]+$"
+                            title="Only lowercase letters and underscores allowed"
+                            required
+                            {...form.register(
+                              `settings.userIdTypes.${i}.userIdType`
+                            )}
+                            helpText="Only lowercase letters and underscores allowed. For example, 'user_id' or 'device_cookie'."
+                          />
+                          <Field
+                            label="Description (optional)"
+                            {...form.register(
+                              `settings.userIdTypes.${i}.description`
+                            )}
+                            minRows={1}
+                            maxRows={5}
+                            textarea
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+                <button
+                  className="btn btn-outline-primary ml-3"
+                  type="button"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    userIdTypes.append({
+                      userIdType: "",
+                      description: "",
+                    });
+                  }}
+                >
+                  Add New Identifier Type
+                </button>
+              </div>
+
               <div className="mb-4">
                 <h4>Experiment Assignment Tables</h4>
                 <div>
@@ -303,6 +384,171 @@ const EditDataSourceSettingsForm: FC<{
                 >
                   Add New Assignment Table
                 </button>
+              </div>
+
+              {userIdTypeOptions.length > 1 && (
+                <div className="mb-4">
+                  <h4>Identifier Join Tables</h4>
+                  <div>
+                    Queries that return a mapping between different identifier
+                    types
+                  </div>
+                  {identityJoins.fields.map((join, i) => {
+                    return (
+                      <div
+                        key={join.id}
+                        className="bg-light border my-2 p-3 ml-3"
+                      >
+                        <div className="row">
+                          <div className="col-auto">
+                            <h5>
+                              {i + 1}.{" "}
+                              {form
+                                .watch(
+                                  `settings.queries.identityJoins.${i}.ids`
+                                )
+                                ?.join(" + ")}
+                            </h5>
+                          </div>
+                          <div className="col-auto ml-auto">
+                            <a
+                              className="text-danger"
+                              href="#"
+                              title="Remove id join table"
+                              onClick={(e) => {
+                                e.preventDefault();
+                                identityJoins.remove(i);
+                              }}
+                            >
+                              delete
+                            </a>
+                          </div>
+                        </div>
+                        <div className="row">
+                          <div className="col-md-7 col-lg-8">
+                            <MultiSelectField
+                              label="Identifier Types"
+                              value={form.watch(
+                                `settings.queries.identityJoins.${i}.ids`
+                              )}
+                              onChange={(val) => {
+                                form.setValue(
+                                  `settings.queries.identityJoins.${i}.ids`,
+                                  val
+                                );
+                              }}
+                              options={userIdTypeOptions.map((u) => ({
+                                value: u.value,
+                                label: u.display,
+                              }))}
+                            />
+                          </div>
+                        </div>
+                        <div className="row">
+                          <div className="col">
+                            <CodeTextArea
+                              label="SQL Query"
+                              language="sql"
+                              value={form.watch(
+                                `settings.queries.identityJoins.${i}.query`
+                              )}
+                              setValue={(sql) =>
+                                form.setValue(
+                                  `settings.queries.identityJoins.${i}.query`,
+                                  sql
+                                )
+                              }
+                            />
+                          </div>
+                          <div className="col-md-5 col-lg-4">
+                            <div className="pt-md-4">
+                              <strong>Required columns</strong>
+                            </div>
+                            <ul>
+                              {form
+                                .watch(
+                                  `settings.queries.identityJoins.${i}.ids`
+                                )
+                                .map((id) => (
+                                  <li key={id}>
+                                    <code>{id}</code>
+                                  </li>
+                                ))}
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  })}
+                  <button
+                    className="btn btn-outline-primary ml-3"
+                    type="button"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      const userIds = userIdTypeOptions
+                        .map((u) => u.value)
+                        .slice(0, 2);
+
+                      identityJoins.append({
+                        ids: userIds,
+                        query: `SELECT\n  ${userIds[0]} as ${userIds[0]},\n  ${userIds[1]} as ${userIds[2]}\nFROM my_table`,
+                      });
+                    }}
+                  >
+                    Add New Identifier Join Table
+                  </button>
+                </div>
+              )}
+
+              <h4>Jupyter Notebook Query Runner (optional)</h4>
+              <div className="bg-light border my-2 p-3 ml-3">
+                <div className="row mb-3">
+                  <div className="col">
+                    <CodeTextArea
+                      label="Python runQuery definition"
+                      language="python"
+                      placeholder="def runQuery(sql):"
+                      value={form.watch(`settings.notebookRunQuery`)}
+                      setValue={(python) =>
+                        form.setValue(`settings.notebookRunQuery`, python)
+                      }
+                      helpText="Used when exporting experiment results to a Jupyter notebook"
+                    />
+                  </div>
+                  <div className="col-md-5 col-lg-4">
+                    <div className="pt-md-4">
+                      Function definition:
+                      <ul>
+                        <li>
+                          Function name: <code>runQuery</code>
+                        </li>
+                        <li>
+                          Arguments: <code>sql</code> (string)
+                        </li>
+                        <li>
+                          Return: <code>df</code> (pandas data frame)
+                        </li>
+                      </ul>
+                      <p>Example for postgres/redshift:</p>
+                      <Code
+                        language="python"
+                        theme="light"
+                        code={`import os
+import psycopg2
+import pandas as pd
+from sqlalchemy import create_engine, text
+
+# Use env variables or similar for passwords!
+password = os.getenv('POSTGRES_PW')
+connStr = f'postgresql+psycopg2://user:{password}@localhost'
+dbConnection = create_engine(connStr).connect();
+
+def runQuery(sql):
+  return pd.read_sql(text(sql), dbConnection)`}
+                      />
+                    </div>
+                  </div>
+                </div>
               </div>
             </>
           )}
