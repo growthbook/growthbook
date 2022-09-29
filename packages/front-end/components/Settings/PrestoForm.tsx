a7c29ed5fb617149779d1fc55199f247b1dd7171
Graham McNicoll <graham@growthbook.io>
2022-09-28 22:08:18 -0700

    Added SSL support to Trino/Presto

--- packages/front-end/components/Settings/PrestoForm.tsx
+++ packages/front-end/components/Settings/PrestoForm.tsx
@@ -1,13 +1,17 @@
-import { FC, ChangeEventHandler } from "react";
+import { FC, ChangeEventHandler, useState } from "react";
 import { PrestoConnectionParams } from "back-end/types/integrations/presto";
 import HostWarning from "./HostWarning";
+import Toggle from "../Forms/Toggle";
+import { FaCaretDown, FaCaretRight } from "react-icons/fa";
+import Field from "../Forms/Field";
 
 const PrestoForm: FC<{
   params: Partial<PrestoConnectionParams>;
   existing: boolean;
   onParamChange: ChangeEventHandler<HTMLInputElement | HTMLSelectElement>;
-  setParams: (params: { [key: string]: string }) => void;
+  setParams: (params: { [key: string]: string | boolean }) => void;
 }> = ({ params, existing, onParamChange, setParams }) => {
+  const [certs, setCerts] = useState(false);
   return (
     <div className="row">
       <div className="form-group col-md-12">
@@ -97,6 +101,67 @@ const PrestoForm: FC<{
           onChange={onParamChange}
         />
       </div>
+      <div className="col-md-12">
+        <div className="form-group">
+          <label htmlFor="require-ssl" className="mr-2">
+            Require SSL
+          </label>
+          <Toggle
+            id="require-ssl"
+            label="Require SSL"
+            value={params.ssl === true}
+            setValue={(value) => {
+              setParams({
+                ssl: value,
+              });
+            }}
+          />
+          {params.ssl && (
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                setCerts(!certs);
+              }}
+            >
+              Advanced SSL Settings {certs ? <FaCaretDown /> : <FaCaretRight />}
+            </a>
+          )}
+        </div>
+      </div>
+      {params.ssl && certs && (
+        <div className="col-md-12 mb-3">
+          <div className="p-2 bg-light border">
+            <Field
+              label="CA Cert (optional)"
+              textarea
+              placeholder={`-----BEGIN CERTIFICATE-----\nMIIE...`}
+              minRows={2}
+              value={params.caCert || ""}
+              name="caCert"
+              onChange={onParamChange}
+            />
+            <Field
+              label="Client Cert"
+              textarea
+              placeholder={`-----BEGIN CERTIFICATE-----\nMIIE...`}
+              minRows={2}
+              value={params.clientCert || ""}
+              name="clientCert"
+              onChange={onParamChange}
+            />
+            <Field
+              label="Client Key"
+              textarea
+              placeholder={`-----BEGIN CERTIFICATE-----\nMIIE...`}
+              minRows={2}
+              value={params.clientKey || ""}
+              name="clientKey"
+              onChange={onParamChange}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
