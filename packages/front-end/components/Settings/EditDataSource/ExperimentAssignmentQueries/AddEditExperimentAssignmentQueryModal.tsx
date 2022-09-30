916bbd9ce1047a7703f75d96a7fc128636a5cd72
Tina Holly <113377031+tinahollygb@users.noreply.github.com>
2022-09-30 09:53:17 -0700

    390 (4) - Data Source editing UI/UX -> Experiment queries, Experiment Events, header (#550)
    * make outline buttons bold & primary
    
    * add new collapsible view layout for identity joins
    
    * add toggles and tooltips
    
    * simplify layout for EditIdentifierType
    
    * remove old identity joins view section
    
    * add Add/Edit modal for ID join tables
    
    * remove tooltip and add clarifying copy instead
    
    * delete old Identity Joins editing in unified form
    
    * add validation for duplicate identity joins and truthy values
    
    * disable in edit mode
    
    * hide Required Columns when list is empty in  identity join add modal
    
    * use vars instead of form.watch
    
    * copy change
    
    * allow user to edit IDs in identifier join queries
    
    * make height default to 260px with configurable height
    
    * for mode=add, use userIdTypes as the default identityJoinQuery.ids
    
    * dont mutate array when sorting
    
    * hide identifier joins section if < 2 identity types and if no existing identifier joins
    
    * jupyter query changes: conditional Add button, change copy to not construe that its disabled
    
    * reorder identity joins & experiment queries to group joins w/ id types
    
    * design tweaks for suggested design
    
    * whitespace tweaks
    
    * add Experiment Assignment queries component & section in [did]
    
    * todos
    
    * add/edit experiment assignment query
    
    * clean up layout; rm old experiment assignment queries
    
    * fix empty state, Add modal
    
    * save & delete exposure queries
    
    * fix whitespace in button
    
    * add reusable DataSourceEvents
    
    * add edit modal for event properties
    
    * rename component
    
    * add new Experiment Assignment/Exposure query, validation
    
    * clean up jupyter section
    
    * clean up layouts, docs
    
    * restore old EditDataSourceSettingsForm to prevent regressions
    
    * layout header
    
    * cleanup todos
    
    * async/await all the things
    
    * support undefined dataSource.settings.events so users can recover
    
    * lint fix
    
    * delete unused EditDataSourceSettingsForm

---
+++ packages/front-end/components/Settings/EditDataSource/ExperimentAssignmentQueries/AddEditExperimentAssignmentQueryModal.tsx
@@ -0,0 +1,188 @@
+import React, { FC } from "react";
+import Modal from "../../../Modal";
+import {
+  DataSourceInterfaceWithParams,
+  ExposureQuery,
+} from "back-end/types/datasource";
+import { useForm } from "react-hook-form";
+import cloneDeep from "lodash/cloneDeep";
+import uniqId from "uniqid";
+import Field from "../../../Forms/Field";
+import CodeTextArea from "../../../Forms/CodeTextArea";
+import Tooltip from "../../../Tooltip";
+import Toggle from "../../../Forms/Toggle";
+import StringArrayField from "../../../Forms/StringArrayField";
+
+type EditExperimentAssignmentQueryProps = {
+  exposureQuery?: ExposureQuery;
+  dataSource: DataSourceInterfaceWithParams;
+  mode: "add" | "edit";
+  onSave: (exposureQuery: ExposureQuery) => void;
+  onCancel: () => void;
+};
+
+export const AddEditExperimentAssignmentQueryModal: FC<EditExperimentAssignmentQueryProps> = ({
+  exposureQuery,
+  dataSource,
+  mode,
+  onSave,
+  onCancel,
+}) => {
+  const modalTitle =
+    mode === "add"
+      ? "Add an Experiment Assignment query"
+      : `Edit ${exposureQuery.name}`;
+
+  const userIdTypeOptions = dataSource.settings.userIdTypes.map(
+    ({ userIdType }) => ({
+      display: userIdType,
+      value: userIdType,
+    })
+  );
+  const defaultUserId = userIdTypeOptions[0]?.value || "user_id";
+
+  const form = useForm<ExposureQuery>({
+    defaultValues:
+      mode === "edit"
+        ? cloneDeep<ExposureQuery>(exposureQuery)
+        : {
+            description: "",
+            id: uniqId("tbl_"),
+            name: "",
+            dimensions: [],
+            query: `SELECT\n  ${defaultUserId} as ${defaultUserId},\n  timestamp as timestamp,\n  experiment_id as experiment_id,\n  variation_id as variation_id\nFROM my_table`,
+            userIdType: userIdTypeOptions[0]?.value || "",
+          },
+  });
+
+  // User-entered values
+  const userEnteredUserIdType = form.watch("userIdType");
+  const userEnteredQuery = form.watch("query");
+  const userEnteredHasNameCol = form.watch("hasNameCol");
+  const userEnteredDimensions = form.watch("dimensions");
+
+  const handleSubmit = form.handleSubmit(async (value) => {
+    await onSave(value);
+
+    form.reset({
+      id: null,
+      query: "",
+      name: "",
+      dimensions: [],
+      description: "",
+      hasNameCol: false,
+      userIdType: null,
+    });
+  });
+
+  const identityTypes = dataSource.settings.userIdTypes || [];
+
+  const saveEnabled =
+    userEnteredUserIdType && userEnteredQuery && userEnteredHasNameCol;
+
+  if (!exposureQuery && mode === "edit") {
+    console.error(
+      "ImplementationError: exposureQuery is required for Edit mode"
+    );
+    return null;
+  }
+
+  return (
+    <Modal
+      open={true}
+      submit={handleSubmit}
+      close={onCancel}
+      size="max"
+      header={modalTitle}
+      cta="Save"
+      ctaEnabled={saveEnabled}
+      autoFocusSelector="#id-modal-identify-joins-heading"
+    >
+      <div className="my-2 ml-3">
+        <div className="row">
+          <div className="col-xs-12">
+            <Field label="Display Name" required {...form.register("name")} />
+            <Field
+              label="Description (optional)"
+              textarea
+              minRows={1}
+              {...form.register("description")}
+            />
+            <Field
+              label="Identifier Type"
+              options={identityTypes.map((i) => i.userIdType)}
+              required
+              {...form.register("userIdType")}
+            />
+
+            <div className="row">
+              <div className="col">
+                <CodeTextArea
+                  label="SQL Query"
+                  required
+                  language="sql"
+                  value={userEnteredQuery}
+                  setValue={(sql) => form.setValue("query", sql)}
+                />
+                <div className="form-group">
+                  <label className="mr-2">
+                    Use Name Columns
+                    <Tooltip body="Enable this if you store experiment/variation names as well as ids in your table" />
+                  </label>
+                  <Toggle
+                    id="exposure-query-toggle"
+                    value={userEnteredHasNameCol}
+                    setValue={(hasNameCol) => {
+                      form.setValue("hasNameCol", hasNameCol);
+                    }}
+                  />
+                </div>
+
+                <StringArrayField
+                  label="Dimension Columns"
+                  value={userEnteredDimensions}
+                  onChange={(dimensions) => {
+                    form.setValue("dimensions", dimensions);
+                  }}
+                />
+              </div>
+              <div className="col-md-5 col-lg-4">
+                <div className="pt-md-4">
+                  <strong>Required columns</strong>
+                </div>
+                <ul>
+                  <li>
+                    <code>{userEnteredUserIdType}</code>
+                  </li>
+                  <li>
+                    <code>timestamp</code>
+                  </li>
+                  <li>
+                    <code>experiment_id</code>
+                  </li>
+                  <li>
+                    <code>variation_id</code>
+                  </li>
+                  {userEnteredHasNameCol && (
+                    <>
+                      <li>
+                        <code>experiment_name</code>
+                      </li>
+                      <li>
+                        <code>variation_name</code>
+                      </li>
+                    </>
+                  )}
+                </ul>
+                <div>
+                  Any additional columns you select can be listed as dimensions
+                  to drill down into experiment results.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Modal>
+  );
+};
