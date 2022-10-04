b2ea90d5957756d052e67976a5e91903f2be8cd7
tinahollygb <tina@growthbook.io>
2022-10-04 16:53:02 -0700

    update icons

--- packages/front-end/components/Layout/ThemeToggler.tsx
+++ packages/front-end/components/Layout/ThemeToggler.tsx
@@ -1,5 +1,7 @@
 import { FC, useCallback, useMemo, useState } from "react";
-import { FaLightbulb, FaMoon } from "react-icons/fa";
+import { FaMoon } from "react-icons/fa";
+import { BsCircleHalf } from "react-icons/bs";
+import { ImSun } from "react-icons/im";
 import clsx from "clsx";
 import useGlobalMenu from "../../services/useGlobalMenu";
 import {
@@ -7,20 +9,10 @@ import {
   useAppearanceUITheme,
 } from "../../services/AppearanceUIThemeProvider";
 
-const HalfCircleIcon: FC = () => (
-  <svg
-    width="15"
-    height="15"
-    viewBox="0 0 24 24"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M12 22.5C14.7848 22.5 17.4555 21.3938 19.4246 19.4246C21.3938 17.4555 22.5 14.7848 22.5 12C22.5 9.21523 21.3938 6.54451 19.4246 4.57538C17.4555 2.60625 14.7848 1.5 12 1.5V22.5ZM12 24C8.8174 24 5.76516 22.7357 3.51472 20.4853C1.26428 18.2348 0 15.1826 0 12C0 8.8174 1.26428 5.76516 3.51472 3.51472C5.76516 1.26428 8.8174 0 12 0C15.1826 0 18.2348 1.26428 20.4853 3.51472C22.7357 5.76516 24 8.8174 24 12C24 15.1826 22.7357 18.2348 20.4853 20.4853C18.2348 22.7357 15.1826 24 12 24Z"
-      fill="currentColor"
-    />
-  </svg>
-);
+// Icons
+const SystemIcon = BsCircleHalf;
+const DarkIcon = FaMoon;
+const LightIcon = ImSun;
 
 export const ThemeToggler: FC = () => {
   const [themeDropdownOpen, setThemeDropdownOpen] = useState(false);
@@ -40,17 +32,13 @@ export const ThemeToggler: FC = () => {
   const activeIcon = useMemo(() => {
     switch (preferredTheme) {
       case "dark":
-        return <FaMoon className="text-secondary mr-2" />;
+        return <DarkIcon className="text-secondary mr-2" />;
 
       case "light":
-        return <FaLightbulb className="text-secondary mr-2" />;
+        return <LightIcon className="text-secondary mr-2" />;
 
       case "system":
-        return (
-          <span className="mr-2 text-secondary">
-            <HalfCircleIcon />
-          </span>
-        );
+        return <SystemIcon className="text-secondary mr-2" />;
     }
   }, [preferredTheme]);
 
@@ -76,10 +64,7 @@ export const ThemeToggler: FC = () => {
             "text-primary": preferredTheme === "system",
           })}
         >
-          <span className="mr-2">
-            <HalfCircleIcon />
-          </span>
-          System Default
+          <SystemIcon className="mr-1" /> System Default
         </button>
         <button
           onClick={() => handleThemeChange("light")}
@@ -87,7 +72,7 @@ export const ThemeToggler: FC = () => {
             "text-primary": preferredTheme === "light",
           })}
         >
-          <FaLightbulb className="mr-1" /> Light mode
+          <LightIcon className="mr-1" /> Light mode
         </button>
 
         <button
@@ -96,7 +81,7 @@ export const ThemeToggler: FC = () => {
             "text-primary": preferredTheme === "dark",
           })}
         >
-          <FaMoon className="mr-1" /> Dark mode
+          <DarkIcon className="mr-1" /> Dark mode
         </button>
       </div>
     </div>
