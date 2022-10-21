3f67531f196e77c64251614e3f168ca741710d46
Michael Knowlton <michael@growthbook.io>
2022-10-21 12:30:06 -0400

    Fixes a bug where we're setting the initial value of groupList to an array of strings, instead of a comma-separated strings by using String()

--- packages/front-end/components/SavedGroupForm.tsx
+++ packages/front-end/components/SavedGroupForm.tsx
@@ -25,7 +25,7 @@ const SavedGroupForm: FC<{
       groupName: current.groupName || "",
       owner: current.owner || "",
       attributeKey: current.attributeKey || "",
-      groupList: current.values || "",
+      groupList: String(current.values) || "",
       id: current.id || "",
     },
   });
