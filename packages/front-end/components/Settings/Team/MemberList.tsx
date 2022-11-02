302769aa39a5a4e16193ea76912fb9a9845985f2
Michael Knowlton <michael@growthbook.io>
2022-11-02 10:45:50 -0400

    Added support to display number of users and invites on the teams page.

--- packages/front-end/components/Settings/Team/MemberList.tsx
+++ packages/front-end/components/Settings/Team/MemberList.tsx
@@ -34,7 +34,7 @@ const MemberList: FC<{
 
   return (
     <div className="my-4">
-      <h5>Active Members</h5>
+      <h5>Active Members{` (${Array.from(users.values()).length})`}</h5>
       {inviting && (
         <InviteModal close={() => setInviting(false)} mutate={mutate} />
       )}
