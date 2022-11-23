3ae2e9cfa4efb24ac506d7d7c53c0a9a84cbe01a
tinahollygb <tina@growthbook.io>
2022-11-23 13:52:09 -0800

    use return type for handler

--- packages/back-end/src/events/handlers/slack/slackEventHandler.ts
+++ packages/back-end/src/events/handlers/slack/slackEventHandler.ts
@@ -2,6 +2,6 @@
  * handle Slack events. Can be handled individually or with a common handler, depending on needs.
  * @param eventId
  */
-export const slackEventHandler = async (eventId: string) => {
+export const slackEventHandler = async (eventId: string): Promise<void> => {
   console.log("slackEventHandler", eventId);
 };
