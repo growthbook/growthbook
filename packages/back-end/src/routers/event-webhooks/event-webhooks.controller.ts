3fe18a903d6a146ba593538d02c35f7ca780478d
tinahollygb <tina@growthbook.io>
2022-11-25 16:19:35 -0800

    get event webhook logs for an event web hook

--- packages/back-end/src/routers/event-webhooks/event-webhooks.controller.ts
+++ packages/back-end/src/routers/event-webhooks/event-webhooks.controller.ts
@@ -2,9 +2,11 @@ import type { Response } from "express";
 import { ApiErrorResponse } from "../../../types/api";
 import { EventWebHookInterface } from "../../../types/event-webhook";
 import * as EventWebHook from "../../models/EventWebhookModel";
+import * as EventWebHookLog from "../../models/EventWebHookLogModel";
 
 import { AuthRequest } from "../../types/AuthRequest";
 import { getOrgFromReq } from "../../services/organizations";
+import { EventWebHookLogInterface } from "../../../types/event-webhook-log";
 
 type GetEventWebHooksRequest = AuthRequest;
 
@@ -23,4 +25,29 @@ export const getEventWebHooks = async (
   const eventWebHooks = await EventWebHook.getAllEventWebHooks(org.id);
 
   return res.json({ eventWebHooks });
+};
+
+type GetEventWebHookLogsRequest = AuthRequest<
+  undefined,
+  { eventWebHookId: string }
+>;
+
+type GetEventWebHookLogsResponse = {
+  eventWebHookLogs: EventWebHookLogInterface[];
+};
+
+export const getEventWebHookLogs = async (
+  req: GetEventWebHookLogsRequest,
+  res: Response<GetEventWebHookLogsResponse | ApiErrorResponse>
+) => {
+  req.checkPermissions("manageWebhooks");
+
+  const { org } = getOrgFromReq(req);
+
+  const eventWebHookLogs = await EventWebHookLog.getLatestRunsForWebHook(
+    org.id,
+    req.params.eventWebHookId
+  );
+
+  return res.json({ eventWebHookLogs });
 };
