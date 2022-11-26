0c88afbdfac3d735c666920fc3beb1ceb63644d0
tinahollygb <tina@growthbook.io>
2022-11-25 16:56:18 -0800

    get all event webhooks for a relevant event

--- packages/back-end/src/events/handlers/webhooks/webHooksEventHandler.ts
+++ packages/back-end/src/events/handlers/webhooks/webHooksEventHandler.ts
@@ -1,6 +1,24 @@
+import { getEvent } from "../../../models/EventModel";
+import { getAllEventWebHooksForEvent } from "../../../models/EventWebhookModel";
+
 /**
  * Common handler that looks up the web hooks and makes a post request with the event.
  */
 export const webHooksEventHandler = async (eventId: string): Promise<void> => {
   console.log("webHooksEventHandler -> ", eventId);
+
+  const event = await getEvent(eventId);
+  if (!event) {
+    // We should never get here
+    throw new Error("No event for provided ID");
+  }
+
+  const eventWebHooks = await getAllEventWebHooksForEvent(
+    event.organizationId,
+    event.data.event
+  );
+
+  console.log("relevant web hooks", eventWebHooks);
+
+  // TODO: Enqueue each web hook in Agenda
 };
