f0a6fe098aedfdb40b210b0a6b92afbd05bd31dc
Michael Knowlton <michael@growthbook.io>
2022-11-29 11:24:35 -0500

    Added some visual updates to make it more like the condition inputs component.

--- packages/front-end/components/Features/ScheduleInputs.tsx
+++ packages/front-end/components/Features/ScheduleInputs.tsx
@@ -6,46 +6,48 @@ export default function ScheduleInputs({ form }: { form: UseFormReturn<any> }) {
   return (
     <div className="pb-2">
       <label>Scheduling Conditions (optional)</label>
-      <div className="pb-2">
-        <span className="pr-2">Start Date</span>
-        <input
-          type="datetime-local"
-          value={form.watch("validAfter") || ""}
-          onChange={(e) => form.setValue("validAfter", e.target.value)}
-        />
-        {form.watch("validAfter") && (
-          <button
-            className="btn btn-link text-danger"
-            type="button"
-            onClick={(e) => {
-              e.preventDefault();
-              form.setValue("validAfter", null);
-            }}
-          >
-            {" "}
-            remove
-          </button>
-        )}
-      </div>
-      <div className="pb-2">
-        <span className="pr-2">End Date</span>
-        <input
-          type="datetime-local"
-          value={form.watch("validBefore") || ""}
-          onChange={(e) => form.setValue("validBefore", e.target.value)}
-        />
-        {form.watch("validBefore") && (
-          <button
-            className="btn btn-link text-danger"
-            type="button"
-            onClick={(e) => {
-              e.preventDefault();
-              form.setValue("validBefore", null);
-            }}
-          >
-            remove
-          </button>
-        )}
+      <div className="bg-light p-3 border">
+        <div className="pb-2">
+          <span className="pr-2">Start Date</span>
+          <input
+            type="datetime-local"
+            value={form.watch("validAfter") || ""}
+            onChange={(e) => form.setValue("validAfter", e.target.value)}
+          />
+          {form.watch("validAfter") && (
+            <button
+              className="btn btn-link text-danger"
+              type="button"
+              onClick={(e) => {
+                e.preventDefault();
+                form.setValue("validAfter", null);
+              }}
+            >
+              {" "}
+              remove
+            </button>
+          )}
+        </div>
+        <div className="pb-2">
+          <span className="pr-2">End Date</span>
+          <input
+            type="datetime-local"
+            value={form.watch("validBefore") || ""}
+            onChange={(e) => form.setValue("validBefore", e.target.value)}
+          />
+          {form.watch("validBefore") && (
+            <button
+              className="btn btn-link text-danger"
+              type="button"
+              onClick={(e) => {
+                e.preventDefault();
+                form.setValue("validBefore", null);
+              }}
+            >
+              remove
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
