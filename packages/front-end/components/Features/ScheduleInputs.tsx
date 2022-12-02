e203234730244077b0ff511d9793eee653530de6
Michael Knowlton <michael@growthbook.io>
2022-12-02 14:04:41 -0500

    Cleaned up some styles for the schedule rules input.

--- packages/front-end/components/Features/ScheduleInputs.tsx
+++ packages/front-end/components/Features/ScheduleInputs.tsx
@@ -2,7 +2,7 @@ import { ScheduleRule } from "back-end/types/feature";
 import React, { useEffect, useState } from "react";
 import Field from "../Forms/Field";
 import SelectField from "../Forms/SelectField";
-import { GBAddCircle } from "../Icons";
+import styles from "./ScheduleInputs.module.scss";
 
 interface Props {
   defaultValue: ScheduleRule[];
@@ -79,116 +79,99 @@ export default function ScheduleInputs(props: Props) {
   }
 
   return (
-    <>
-      <label className="mb-0">Schedule</label>
-      <div className="bg-light p-3 border mt-2 mb-2">
-        <em className="text-muted mr-3">
+    <div className="form-group">
+      <label>Schedule</label>
+      <div className={`mb-3 bg-light p-3 ${styles.conditionbox}`}>
+        <span className="text-muted mr-3 mt-3">
           Automatically enable and disable an override rule.
-        </em>
-        {rules.map(({ timestamp, enableFeature }, i) => {
-          const onChange = (value, property, i) => {
-            const newRules = [...rules];
-            newRules[i][property] = value;
-            setRules(newRules);
-          };
+        </span>
+        <ul className={styles.conditionslist}>
+          {rules.map(({ timestamp, enableFeature }, i) => {
+            const onChange = (value, property, i) => {
+              const newRules = [...rules];
+              newRules[i][property] = value;
+              setRules(newRules);
+            };
 
-          return (
-            <div className="d-flex align-items-center mt-2" key={i}>
-              <div className="mb-2">
-                <span className="mb-2">
-                  {(i + 1) % 2 === 1 ? "Launch rule" : "Disable Rule"}
-                </span>
-              </div>
-              <div className="col-sm-12 col-md mb-2">
-                <SelectField
-                  name="date-operator"
-                  value={
-                    timestamp !== null
-                      ? "at a specific date and time"
-                      : enableFeature
-                      ? "immediately"
-                      : "manually"
-                  }
-                  options={(i + 1) % 2 === 1 ? launchOptions : endOptions}
-                  onChange={(value) => {
-                    if (value === "at a specific date and time") {
-                      onChange(new Date().toISOString(), "timestamp", i);
-                    } else {
-                      onChange(null, "timestamp", i);
-                    }
-                  }}
-                />
-              </div>
-              {timestamp !== null && (
-                <>
-                  <span className="mb-2">on</span>
-                  <div className="col-sm-12 col-md mb-2">
-                    <Field
-                      type="datetime-local"
-                      value={getLocalDateTime(timestamp)}
-                      onChange={(e) => {
-                        setDateErrors({ [i]: "" });
-                        if (
-                          i > 0 &&
-                          new Date(e.target.value).valueOf() <
-                            new Date(rules[i - 1].timestamp).valueOf()
-                        ) {
-                          setDateErrors({
-                            [i]:
-                              "Date must be greater than the previous rule date.",
-                          });
-                          return;
+            return (
+              <li key={i} className={styles.listitem}>
+                <div className="row align-items-center">
+                  <div className="mb-2 w-auto p-3">
+                    <span className="mb-2">
+                      {(i + 1) % 2 === 1 ? "Launch rule" : "Disable Rule"}
+                    </span>
+                  </div>
+                  <div className="mb-2 pl-2 pr-2">
+                    <SelectField
+                      name="date-operator"
+                      value={
+                        timestamp !== null
+                          ? "at a specific date and time"
+                          : enableFeature
+                          ? "immediately"
+                          : "manually"
+                      }
+                      options={(i + 1) % 2 === 1 ? launchOptions : endOptions}
+                      onChange={(value) => {
+                        if (value === "at a specific date and time") {
+                          onChange(new Date().toISOString(), "timestamp", i);
+                        } else {
+                          onChange(null, "timestamp", i);
                         }
-                        onChange(e.target.value, "timestamp", i);
                       }}
-                      name="timestamp"
-                      error={dateErrors[i] || ""}
                     />
                   </div>
-                  {getLocalDateTime(timestamp) && (
-                    <span
-                      className="font-italic font-weight-light mb-2"
-                      style={{ fontSize: "12px" }}
-                    >
-                      (
-                      {new Date(getLocalDateTime(timestamp))
-                        .toLocaleDateString(undefined, {
-                          day: "2-digit",
-                          timeZoneName: "short",
-                        })
-                        .substring(4)}
-                      )
-                    </span>
+                  {timestamp !== null && (
+                    <>
+                      <div className="w-auto mb-2 p-3">
+                        <span className="mb-2">ON</span>
+                      </div>
+                      <div className="col-sm-12 col-md mb-2 d-flex align-items-center">
+                        <Field
+                          type="datetime-local"
+                          value={getLocalDateTime(timestamp)}
+                          onChange={(e) => {
+                            setDateErrors({ [i]: "" });
+                            if (
+                              i > 0 &&
+                              new Date(e.target.value).valueOf() <
+                                new Date(rules[i - 1].timestamp).valueOf()
+                            ) {
+                              setDateErrors({
+                                [i]:
+                                  "Date must be greater than the previous rule date.",
+                              });
+                              return;
+                            }
+                            onChange(e.target.value, "timestamp", i);
+                          }}
+                          name="timestamp"
+                          error={dateErrors[i] || ""}
+                        />
+                        {getLocalDateTime(timestamp) && (
+                          <span
+                            className="font-italic font-weight-light ml-2"
+                            style={{ fontSize: "12px" }}
+                          >
+                            (
+                            {new Date(getLocalDateTime(timestamp))
+                              .toLocaleDateString(undefined, {
+                                day: "2-digit",
+                                timeZoneName: "short",
+                              })
+                              .substring(4)}
+                            )
+                          </span>
+                        )}
+                      </div>
+                    </>
                   )}
-                </>
-              )}
-            </div>
-          );
-        })}
-        {rules && rules.length < 2 && (
-          <div>
-            <a
-              className={`mr-3 btn btn-outline-primary mt-3`}
-              href="#"
-              onClick={(e) => {
-                e.preventDefault();
-                setRules([
-                  ...rules,
-                  {
-                    timestamp: null,
-                    enableFeature: true,
-                  },
-                ]);
-              }}
-            >
-              <span className={`h4 pr-2 m-0 d-inline-block align-top`}>
-                <GBAddCircle />
-              </span>
-              Add another schedule rule
-            </a>
-          </div>
-        )}
+                </div>
+              </li>
+            );
+          })}
+        </ul>
       </div>
-    </>
+    </div>
   );
 }
