b3b42773ab50f15d11c7270a49dab9686cfe121b
tinahollygb <tina@growthbook.io>
2022-12-02 11:22:19 -0800

    add sample component UI

--- packages/front-end/components/EventWebHookListItem/EventWebHookListItem.stories.tsx
+++ packages/front-end/components/EventWebHookListItem/EventWebHookListItem.stories.tsx
@@ -1,5 +1,7 @@
+import { select } from "@storybook/addon-knobs";
 import { EventWebHookListItem } from "./EventWebHookListItem";
 import { EventWebHookInterface } from "back-end/types/event-webhook";
+import { getValidDate } from "../../services/dates";
 
 export default {
   component: EventWebHookListItem,
@@ -7,20 +9,32 @@ export default {
 };
 
 export const Default = () => {
+  const lastRunAt = select<string | null>(
+    "Last Run",
+    [null, new Date().toString()],
+    null
+  );
   const eventWebHook: EventWebHookInterface = {
     events: ["feature.updated", "feature.created", "feature.deleted"],
-    id: "ewh-089e484a-f8e4-42de-8ad3-1ac708c88c8d",
+    id: "ewh-4af2033e-80e7-45af-a06d-01f3fccc8d1e",
     organizationId: "org_sktwi1id9l7z9xkjb",
+    name: "My 4th WebHook",
     dateCreated: new Date(),
     dateUpdated: new Date(),
-    error: null,
-    signingKey: "ewhk-fc580ba5875bb040e022aed14703d098",
-    url: "http://192.168.0.15:1115/events/webhooks?v=sb",
+    enabled: false,
+    url: "http://192.168.0.15:1115/events/webhooks?v=4",
+    signingKey: "ewhk_6220c5592f03244c43a929850816d644",
+    lastRunAt: lastRunAt ? getValidDate(lastRunAt) : null,
+    lastState: select("Last Run state", ["none", "error", "success"], "none"),
+    lastResponseBody: null,
   };
 
   return (
     <div>
-      <EventWebHookListItem eventWebHook={eventWebHook} />
+      <EventWebHookListItem
+        href="/event-webhooks/ewh-4af2033e-80e7-45af-a06d-01f3fccc8d1e"
+        eventWebHook={eventWebHook}
+      />
     </div>
   );
 };
--- packages/front-end/components/EventWebHookListItem/EventWebHookListItem.tsx
+++ packages/front-end/components/EventWebHookListItem/EventWebHookListItem.tsx
@@ -1,18 +1,50 @@
-import React, { FC } from "react";
+import React, { FC, useMemo } from "react";
 import { EventWebHookInterface } from "back-end/types/event-webhook";
+import { BsCheck, BsQuestion, BsX } from "react-icons/all";
+import { datetime } from "../../services/dates";
 
 type EventWebHookListItemProps = {
+  href: string;
   eventWebHook: EventWebHookInterface;
 };
 
 export const EventWebHookListItem: FC<EventWebHookListItemProps> = ({
+  href,
   eventWebHook,
 }) => {
-  return (
-    <div>
-      <h1>EventWebHookListItem</h1>
+  const { name, url, lastState, lastRunAt } = eventWebHook;
 
-      <pre>{JSON.stringify(eventWebHook, null, 2)}</pre>
-    </div>
+  const iconForState = useMemo(() => {
+    switch (lastState) {
+      case "none":
+        return <BsQuestion className="text-muted" />;
+      case "success":
+        return <BsCheck className="text-success" />;
+      case "error":
+        return <BsX className="text-danger" />;
+      default:
+        return null;
+    }
+  }, [lastState]);
+
+  return (
+    <a href={href} style={{ textDecoration: "none" }} className="card p-3">
+      <div className="d-flex justify-content-md-between align-items-center">
+        <div>
+          <h2 className="text-main">{name}</h2>
+          <h3 className="text-muted">{url}</h3>
+        </div>
+        <div className="d-flex justify-content-md-between align-items-center">
+          {!lastRunAt ? (
+            <div className="text-muted">No runs</div>
+          ) : (
+            <div className="text-main">Last run: {datetime(lastRunAt)}</div>
+          )}
+          <div className="ml-2" style={{ fontSize: "1.5rem" }}>
+            {iconForState}
+          </div>
+        </div>
+      </div>
+    </a>
   );
 };
