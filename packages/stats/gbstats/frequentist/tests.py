905d374bbd1dee20da2339caa0414829929f2688
Luke Sonnet <luke.sonnet@gmail.com>
2022-12-07 12:36:06 -0800

    Add simple validity check for ttest

--- packages/stats/gbstats/frequentist/tests.py
+++ packages/stats/gbstats/frequentist/tests.py
@@ -20,7 +20,6 @@ class TTest(BaseABTest):
         super().__init__(stat_a, stat_b)
         self.alpha = alpha
         self.test_value = test_value
-        # TODO: validate same type of statistic
 
     @property
     def variance(self) -> float:
@@ -56,8 +55,8 @@ class TTest(BaseABTest):
 
     def compute_result(self) -> FrequentistTestResult:
         return FrequentistTestResult(
-            expected=self.point_estimate,
-            # have to make CI about percent as well for x-axis
+            # have to make CI and midpoint percentages for x-axis
+            expected=self.point_estimate / self.stat_a.value,
             ci=[x / self.stat_a.value for x in self.confidence_interval],
             p_value=self.p_value,
             uplift=Uplift(
---
+++ packages/stats/tests/frequentist/test_tests.py
@@ -0,0 +1,39 @@
+from dataclasses import asdict
+from functools import partial
+from unittest import TestCase, main as unittest_main
+
+import numpy as np
+
+from gbstats.frequentist.tests import TwoSidedTTest
+from gbstats.shared.models import FrequentistTestResult, Statistic, Uplift
+
+DECIMALS = 5
+round_ = partial(np.round, decimals=DECIMALS)
+
+
+class TestTwoSidedTTest(TestCase):
+    def test_two_sided_ttest(self):
+        stat_a = Statistic(value=0.41, stddev=3.9, count=3407, n=3407)
+        stat_b = Statistic(value=0.7, stddev=6.2, count=3461, n=3461)
+        result_dict = asdict(TwoSidedTTest(stat_a, stat_b).compute_result())
+        expected_rounded_dict = asdict(
+            FrequentistTestResult(
+                expected=0.70732,
+                ci=[0.11068, 1.30396],
+                uplift=Uplift("normal", 0.70732, 0.30435),
+                p_value=0.02016,
+            )
+        )
+
+        # round result
+        for k, v in result_dict.items():
+            if k == "uplift":
+                v = {
+                    kk: round_(vv) if isinstance(vv, float) else vv
+                    for kk, vv in v.items()
+                }
+            else:
+                v = [round_(x) for x in v] if isinstance(v, list) else round_(v)
+            result_dict[k] = v
+
+        self.assertEqual(result_dict, expected_rounded_dict)
