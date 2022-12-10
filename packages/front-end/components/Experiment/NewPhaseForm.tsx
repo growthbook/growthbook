806199d17b9d52a0da1f50922ae5da877e020d6a
Jeremy Dorn <jeremy@jeremydorn.com>
2022-12-10 15:59:31 -0600

    Support higher precision when specifying variation weights
    Was using 3 digits (e.g. 33.4/33.3/33.3), which was enough rounding to cause SRM failures in some instances.
    Now using 4 digits by default (e.g. 33.34/33.33/33.33), plus an easy way to increase in the future if needed.
    
    Also added unit tests for all variation weighting logic on the front-end

--- packages/front-end/components/Experiment/NewPhaseForm.tsx
+++ packages/front-end/components/Experiment/NewPhaseForm.tsx
@@ -7,7 +7,7 @@ import { useForm } from "react-hook-form";
 import Modal from "../Modal";
 import { useAuth } from "../../services/auth";
 import { useWatching } from "../../services/WatchProvider";
-import { getEvenSplit } from "../../services/utils";
+import { getEqualWeights } from "../../services/utils";
 import GroupsInput from "../GroupsInput";
 import { useDefinitions } from "../../services/DefinitionsContext";
 import Field from "../Forms/Field";
@@ -32,7 +32,7 @@ const NewPhaseForm: FC<{
       coverage: prevPhase.coverage || 1,
       variationWeights:
         prevPhase.variationWeights ||
-        getEvenSplit(experiment.variations.length),
+        getEqualWeights(experiment.variations.length),
       reason: "",
       dateStarted: new Date().toISOString().substr(0, 16),
       groups: prevPhase.groups || [],
