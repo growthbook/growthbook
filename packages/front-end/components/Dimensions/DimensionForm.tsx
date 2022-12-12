6127b6067573c50b4177c4c7cefb6d80b0302d27
Michael Knowlton <michael@growthbook.io>
2022-12-12 10:12:36 -0500

    Wired up the query test service for dimensions.

--- packages/front-end/components/Dimensions/DimensionForm.tsx
+++ packages/front-end/components/Dimensions/DimensionForm.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useMemo, useState } from "react";
 import Modal from "../Modal";
 import { useForm } from "react-hook-form";
 import { useAuth } from "../../services/auth";
@@ -9,11 +9,25 @@ import SelectField from "../Forms/SelectField";
 import CodeTextArea from "../Forms/CodeTextArea";
 import useMembers from "../../hooks/useMembers";
 import { validateSQL } from "../../services/datasources";
+import { TestQueryRow } from "back-end/src/types/Integration";
+import { FaPlay } from "react-icons/fa";
+import DisplayTestQueryResults from "../Settings/DisplayTestQueryResults";
+
+type TestQueryResults = {
+  duration?: string;
+  error?: string;
+  results?: TestQueryRow[];
+  sql?: string;
+};
 
 const DimensionForm: FC<{
   close: () => void;
   current: Partial<DimensionInterface>;
 }> = ({ close, current }) => {
+  const [
+    testQueryResults,
+    setTestQueryResults,
+  ] = useState<TestQueryResults | null>(null);
   const { apiCall } = useAuth();
   const { memberUsernameOptions } = useMembers();
   const {
@@ -33,10 +47,34 @@ const DimensionForm: FC<{
 
   const datasource = form.watch("datasource");
   const userIdType = form.watch("userIdType");
+  const userEnteredQuery = form.watch("sql");
 
   const dsObj = getDatasourceById(datasource);
   const dsProps = dsObj?.properties;
   const sql = dsProps?.queryLanguage === "sql";
+
+  const requiredColumns = useMemo(() => {
+    return new Set(["user_id", "value"]);
+  }, []);
+
+  const handleTestQuery = async () => {
+    setTestQueryResults(null);
+    try {
+      validateSQL(userEnteredQuery, [...requiredColumns]);
+
+      const res: TestQueryResults = await apiCall("/query/test", {
+        method: "POST",
+        body: JSON.stringify({
+          query: userEnteredQuery,
+          datasourceId: dsObj.id,
+        }),
+      });
+
+      setTestQueryResults(res);
+    } catch (e) {
+      setTestQueryResults({ error: e.message });
+    }
+  };
 
   return (
     <Modal
@@ -91,20 +129,49 @@ const DimensionForm: FC<{
         />
       )}
       {sql ? (
-        <CodeTextArea
-          label="SQL"
-          required
-          language="sql"
-          value={form.watch("sql")}
-          setValue={(sql) => form.setValue("sql", sql)}
-          placeholder={`SELECT\n      ${userIdType}, browser as value\nFROM\n      users`}
-          helpText={
-            <>
-              Select two columns named <code>{userIdType}</code> and{" "}
-              <code>value</code>
-            </>
-          }
-        />
+        <div className="row">
+          <div className="col-lg-12">
+            <label className="font-weight-bold mb-1">SQL Query</label>
+            <div>
+              <div className="d-flex justify-content-between align-items-center p-1 border rounded">
+                <button
+                  className="btn btn-sm btn-primary m-1"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleTestQuery();
+                  }}
+                >
+                  <span className="pr-2">
+                    <FaPlay />
+                  </span>
+                  Test Query
+                </button>
+              </div>
+              <CodeTextArea
+                required
+                language="sql"
+                value={userEnteredQuery}
+                setValue={(sql) => form.setValue("sql", sql)}
+                placeholder={`SELECT\n      ${userIdType}, browser as value\nFROM\n      users`}
+                helpText={
+                  <>
+                    Select two columns named <code>{userIdType}</code> and{" "}
+                    <code>value</code>
+                  </>
+                }
+              />
+              {testQueryResults && (
+                <DisplayTestQueryResults
+                  duration={parseInt(testQueryResults.duration || "0")}
+                  requiredColumns={[...requiredColumns]}
+                  result={testQueryResults.results?.[0]}
+                  error={testQueryResults.error}
+                  sql={testQueryResults.sql}
+                />
+              )}
+            </div>
+          </div>
+        </div>
       ) : (
         <Field
           label="Event Condition"
