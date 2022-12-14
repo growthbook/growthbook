0b89abcf19355ece773ac4110728607d9dd75c93
Michael Knowlton <michael@growthbook.io>
2022-12-14 09:58:53 -0500

    Updated the segment.controller file to use the new exported methods as opposed to using the model itself.

--- packages/back-end/src/models/SegmentModel.ts
+++ packages/back-end/src/models/SegmentModel.ts
@@ -63,3 +63,25 @@ export async function findSegmentsByDataSource(
     toInterface
   );
 }
+
+export async function deleteSegmentById(id: string, organization: string) {
+  // If using config.yml, immediately throw error
+  if (usingFileConfig()) {
+    throw new Error("Cannot delete. Segments are being managed by config.yml");
+  }
+
+  await SegmentModel.deleteOne({ id, organization });
+}
+
+export async function updateSegment(
+  id: string,
+  organization: string,
+  updates: Partial<SegmentInterface>
+) {
+  // If using config.yml, immediately return the list from there
+  if (usingFileConfig()) {
+    throw new Error("Cannot update. Segments are being managed by config.yml");
+  }
+
+  await SegmentModel.updateOne({ id, organization }, { $set: updates });
+}
