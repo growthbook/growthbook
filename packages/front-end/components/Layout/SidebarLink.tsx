14e6b9aea90cb4cba561db0cd1bfd431da1c24b1
Bryce <bryce1@gmail.com>
2022-12-14 15:11:11 -0800

    limit upgrade modals to users with billing permissions

--- packages/front-end/components/Layout/SidebarLink.tsx
+++ packages/front-end/components/Layout/SidebarLink.tsx
@@ -1,14 +1,14 @@
 import { FC, useEffect, useState } from "react";
 import Link from "next/link";
 import { IconType } from "react-icons/lib";
-import { useUser } from "../../services/UserContext";
 import { useRouter } from "next/router";
 import clsx from "clsx";
-import styles from "./SidebarLink.module.scss";
 import { FiChevronRight } from "react-icons/fi";
-import { isCloud } from "../../services/env";
 import { AccountPlan, Permission } from "back-end/types/organization";
 import { useGrowthBook } from "@growthbook/growthbook-react";
+import { isCloud } from "../../services/env";
+import { useUser } from "../../services/UserContext";
+import styles from "./SidebarLink.module.scss";
 
 export type SidebarLinkProps = {
   name: string;
