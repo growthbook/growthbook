ca4608d8fea6a64a952f11842bdc0a87ab70c53c
Luke Sonnet <luke.sonnet@gmail.com>
2022-12-15 15:15:22 -0800

    Fix stats denominator name

--- packages/back-end/src/services/stats.ts
+++ packages/back-end/src/services/stats.ts
@@ -25,7 +25,7 @@ export interface StatsEngineDimensionResponse {
     cr: number;
     value: number;
     users: number;
-    count: number;
+    denominator: number;
     stats: MetricStats;
     expected?: number;
     chanceToWin?: number;
