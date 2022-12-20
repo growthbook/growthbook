e351719a54f9edfaaae7c619df55911e47329093
Adnan Chowdhury <2374625+bttf@users.noreply.github.com>
2022-12-20 10:19:04 -0500

    Frequentist - Refactor and clean up prior to UI changes (#847)
    This PR includes simple refactoring, type definition updates, and TODO placement in effort to prep for incoming UX changes for the Frequentist stats engine

---
+++ packages/back-end/types/stats.d.ts
@@ -0,0 +1,48 @@
+import type { MetricStats } from "./metric";
+
+interface BaseVariationResponse {
+  cr: number;
+  value: number;
+  users: number;
+  denominator?: number;
+  stats: MetricStats;
+  expected?: number;
+  uplift?: {
+    dist: string;
+    mean?: number;
+    stddev?: number;
+  };
+  ci?: [number, number];
+}
+
+interface BayesianVariationResponse extends BaseVariationResponse {
+  chanceToWin?: number;
+  risk?: [number, number];
+}
+
+interface FrequentistVariationResponse extends BaseVariationResponse {
+  pValue?: number;
+}
+
+interface BaseDimensionResponse {
+  dimension: string;
+  srm: number;
+}
+
+interface BayesianDimensionResponse extends BaseDimensionResponse {
+  variations: BayesianVariationResponse[];
+}
+
+interface FrequentistVariationResponse extends BaseDimensionResponse {
+  variations: FrequentistVariationResponse[];
+}
+
+type StatsEngineDimensionResponse =
+  | BayesianDimensionResponse
+  | FrequentistVariationResponse;
+
+export interface ExperimentMetricAnalysis {
+  unknownVariations: string[];
+  multipleExposures: number;
+  dimensions: StatsEngineDimensionResponse[];
+}
