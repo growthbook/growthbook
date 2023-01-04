9f65d806ea7497881cf701a0312996e2d5e3acc2
Graham McNicoll <graham@growthbook.io>
2023-01-04 12:05:25 -0500

    Added useEffect

--- packages/front-end/pages/404.tsx
+++ packages/front-end/pages/404.tsx
@@ -1,15 +1,19 @@
 import * as Sentry from "@sentry/react";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { isSentryEnabled } from "@/services/env";
 
 export default function Custom404() {
   const ind = Math.ceil(Math.random() * 2);
   const router = useRouter();
-  if (isSentryEnabled()) {
-    const badPath = router?.asPath || window?.location?.href || "";
-    const referrer = document?.referrer || "";
-    Sentry.captureMessage("404: " + badPath + " from " + referrer);
-  }
+  const badPath = router?.asPath || window?.location?.href || "";
+  useEffect(() => {
+    if (isSentryEnabled()) {
+      const referrer = document?.referrer || "";
+      Sentry.captureMessage("404: " + badPath + " from " + referrer);
+    }
+  }, [badPath]);
+
   return (
     <div className="container d-flex justify-content-center h-100 align-items-center flex-column">
       <img
