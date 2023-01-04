14e8825d31ff481145ca735867f0a30291685349
Graham McNicoll <graham@growthbook.io>
2023-01-04 11:11:53 -0500

    Added Sentry error tracking

--- packages/front-end/pages/404.tsx
+++ packages/front-end/pages/404.tsx
@@ -1,5 +1,15 @@
+import * as Sentry from "@sentry/react";
+import { useRouter } from "next/router";
+import { isCloud, isSentryEnabled } from "@/services/env";
+
 export default function Custom404() {
   const ind = Math.ceil(Math.random() * 2);
+  const router = useRouter();
+  if (isSentryEnabled() && isCloud()) {
+    const badPath = router?.asPath || window?.location?.href || "";
+    const referrer = document?.referrer || "";
+    Sentry.captureMessage("404: " + badPath + " from " + referrer);
+  }
   return (
     <div className="container d-flex justify-content-center h-100 align-items-center flex-column">
       <img
