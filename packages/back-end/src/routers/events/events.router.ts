c9d6a9afb0f0f50d6502fa42bc8ce1f630913782
tinahollygb <tina@growthbook.io>
2023-01-18 11:27:42 -0800

    add endpoint to get event by ID

--- packages/back-end/src/routers/events/events.router.ts
+++ packages/back-end/src/routers/events/events.router.ts
@@ -1,11 +1,25 @@
 import express from "express";
+import z from "zod";
 import { wrapController } from "../wrapController";
 import * as rawEventsController from "./events.controller";
+import { validateRequestMiddleware } from "../utils/validateRequestMiddleware";
 
 const router = express.Router();
 
 const eventsController = wrapController(rawEventsController);
 
 router.get("/", eventsController.getEvents);
+
+router.get(
+  "/:id",
+  validateRequestMiddleware({
+    params: z
+      .object({
+        id: z.string(),
+      })
+      .strict(),
+  }),
+  eventsController.getEventById
+);
 
 export { router as eventsRouter };
