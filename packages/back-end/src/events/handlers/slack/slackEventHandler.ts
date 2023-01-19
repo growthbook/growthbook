449cf79b8ba11dc78332ad880983b1ce2ae26580
tinahollygb <tina@growthbook.io>
2023-01-19 11:24:14 -0800

    get event in the entry jobHandler instead of each handler

--- packages/back-end/src/events/handlers/slack/slackEventHandler.ts
+++ packages/back-end/src/events/handlers/slack/slackEventHandler.ts
@@ -1,21 +1,11 @@
 import { NotificationEventHandler } from "../../notifiers/EventNotifier";
-import { getEvent } from "../../../models/EventModel";
 import { logger } from "../../../util/logger";
 import { handleFeatureEventForSlack } from "./handleFeatureEventForSlack";
 
 /**
  * handle Slack events. Can be handled individually or with a common handler, depending on needs.
  */
-export const slackEventHandler: NotificationEventHandler = async (eventId) => {
-  const event = await getEvent(eventId);
-  if (!event) {
-    // We should never get here
-    throw new Error(
-      "slackEventHandler -> ImplementationError: No event for provided ID"
-    );
-  }
-
-  // Get all Slack integrations for the organization and the events
+export const slackEventHandler: NotificationEventHandler = async (event) => {
   switch (event.event) {
     case "feature.created":
     case "feature.updated":
