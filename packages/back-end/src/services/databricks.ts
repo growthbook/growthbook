141663bcd3f35306cdbd7131fbdb773db5fa8683
Jeremy Dorn <jeremy@jeremydorn.com>
2023-01-26 15:22:59 -0600

    Workaround the badly designed Databricks SDK, add Databricks to the different event trackers that support it

--- packages/back-end/src/services/databricks.ts
+++ packages/back-end/src/services/databricks.ts
@@ -1,34 +1,65 @@
 import { DBSQLClient } from "@databricks/sql";
 import { DatabricksConnectionParams } from "../../types/integrations/databricks";
+import { logger } from "../util/logger";
+import { ENVIRONMENT } from "../util/secrets";
 
 export async function runDatabricksQuery<T>(
   conn: DatabricksConnectionParams,
   sql: string
 ): Promise<T[]> {
-  const client = new DBSQLClient();
+  // Because of how Databrick's SDK is written, it may reject or resolve multiple times
+  // So we have a quick boolean check to make sure we only do it the first time
+  let finished = false;
 
-  await client.connect({
-    token: conn.token,
-    host: conn.host,
-    port: conn.port || 443,
-    path: conn.path,
+  // Annoyingly, the `client.connect` method is async, but if there's an error,
+  // it just hangs and never rejects. Instead, it emits an "error" event.
+  // So we have to wrap everything in a `new Promise()` and handle errors manually
+  const result = await new Promise<T[]>((resolve, reject) => {
+    const client = new DBSQLClient({
+      logger: {
+        log(level, message) {
+          if (ENVIRONMENT !== "production") {
+            logger.info({ db: "Databricks", level }, message);
+          }
+        },
+      },
+    });
+    client
+      .on("error", (error) => {
+        if (finished) return;
+        finished = true;
+        reject(error);
+      })
+      .connect({
+        token: conn.token,
+        host: conn.host,
+        port: conn.port || 443,
+        path: conn.path,
+      })
+      .then(async () => {
+        const session = await client.openSession();
+        const queryOperation = await session.executeStatement(sql, {
+          runAsync: true,
+          // This is required to have the results returned immediately
+          maxRows: 1000,
+        });
+        const result = ((await queryOperation.fetchAll({
+          progress: false,
+        })) as unknown) as Promise<T[]>;
+
+        await queryOperation.close();
+        await session.close();
+        await client.close();
+
+        if (finished) return;
+        finished = true;
+        resolve(result);
+      })
+      .catch((e) => {
+        if (finished) return;
+        finished = true;
+        reject(e);
+      });
   });
-
-  const session = await client.openSession();
-
-  const queryOperation = await session.executeStatement(sql, {
-    runAsync: true,
-    // This is required to have the results returned immediately
-    maxRows: 1000,
-  });
-
-  const result = ((await queryOperation.fetchAll({
-    progress: false,
-  })) as unknown) as Promise<T[]>;
-
-  await queryOperation.close();
-  await session.close();
-  await client.close();
-
   return result;
 }
