3f5ea799ab4656eed762fb317eda599aa0e5a583
Luke Sonnet <luke.sonnet@gmail.com>
2023-01-30 15:42:16 -0800

    Change finite values in metric processing

--- packages/back-end/src/util/stats.ts
+++ packages/back-end/src/util/stats.ts
@@ -1,5 +1,4 @@
 import { jStat } from "jstat";
-import { values } from "lodash";
 
 export function checkSrm(users: number[], weights: number[]) {
   // Skip variations with weight=0 or users=0
@@ -27,14 +26,26 @@ export function checkSrm(users: number[], weights: number[]) {
   return 1 - jStat.chisquare.cdf(x, data.length - 1);
 }
 
-export function sumSquaresFromStats(sum: number, variance: number, n: number): number {
-  return variance * (n - 1) + Math.pow(sum, 2) / n;
-}
-
-export function meanVarianceFromSums(sum: number, sum_squares: number, n: number): number {
-  const variance = (sum_squares - Math.pow(sum, 2) / n) / (n - 1);
-  if (isFinite(variance)) {
-    return variance;
+function returnZeroIfNotFinite(x: number): number {
+  if (isFinite(x)) {
+    return x;
   }
   return 0;
+}
+
+export function sumSquaresFromStats(
+  sum: number,
+  variance: number,
+  n: number
+): number {
+  return returnZeroIfNotFinite(variance * (n - 1) + Math.pow(sum, 2) / n);
+}
+
+export function meanVarianceFromSums(
+  sum: number,
+  sum_squares: number,
+  n: number
+): number {
+  const variance = (sum_squares - Math.pow(sum, 2) / n) / (n - 1);
+  return returnZeroIfNotFinite(variance);
 }
