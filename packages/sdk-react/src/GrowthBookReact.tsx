86bb1b90885d64150c42910d35ab373781bd59b1
tinahollygb <tina@growthbook.io>
2023-02-28 20:02:48 -0800

    use any instead of never

--- packages/sdk-react/src/GrowthBookReact.tsx
+++ packages/sdk-react/src/GrowthBookReact.tsx
@@ -106,7 +106,7 @@ export function useFeature<T extends JSONValue = any>(
 }
 
 export function useFeatureIsOn<
-  AppFeatures extends Record<string, any> = Record<string, never>
+  AppFeatures extends Record<string, any> = Record<string, any>
 >(id: string & keyof AppFeatures): boolean {
   const growthbook = useGrowthBook<AppFeatures>();
   return growthbook ? growthbook.isOn(id) : false;
