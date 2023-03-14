8b68c9bf6730376a737942506d4a61e7eee2d383
Michael Knowlton <michael@growthbook.io>
2023-03-14 13:12:39 -0400

    Updates return type of initializeDatasourceInformationSchema to string as that'll be utilized by the front-end when that work is done.

--- packages/back-end/src/services/datasource.ts
+++ packages/back-end/src/services/datasource.ts
@@ -164,7 +164,7 @@ export async function testQuery(
 export async function initializeDatasourceInformationSchema(
   datasource: DataSourceInterface,
   organization: string
-): Promise<void> {
+): Promise<string> {
   // Create an empty informationSchema
   const informationSchema = await createInformationSchema(
     [],
@@ -186,4 +186,6 @@ export async function initializeDatasourceInformationSchema(
     databases: await generateInformationSchema(datasource),
     status: "COMPLETE",
   });
+
+  return informationSchema.id;
 }
