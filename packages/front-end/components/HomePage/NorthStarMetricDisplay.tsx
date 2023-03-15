cb8e00722b181ff92aac8b365e4dd469d5904ee3
Bryce <bryce@growthbook.io>
2023-03-15 14:02:33 -0700

    Allow multi hover on metric graphs (v2) (#1077)
    * rework multi-hover, use external state based on nearest date
    
    * cleanup
    
    * address feedback, cleanup, add smoothing to northstar
    
    * tsc fix

--- packages/front-end/components/HomePage/NorthStarMetricDisplay.tsx
+++ packages/front-end/components/HomePage/NorthStarMetricDisplay.tsx
@@ -15,11 +15,15 @@ const NorthStarMetricDisplay = ({
   metricId,
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   window,
-  resolution,
+  smoothBy,
+  hoverDate,
+  onHoverCallback,
 }: {
   metricId: string;
   window?: number | string;
-  resolution?: string;
+  smoothBy?: string;
+  hoverDate?: number | null;
+  onHoverCallback?: (ret: { d: number | null }) => void;
 }): React.ReactElement => {
   const { project } = useDefinitions();
   const permissions = usePermissions();
@@ -66,9 +70,11 @@ const NorthStarMetricDisplay = ({
               dates={analysis.dates}
               experiments={experiments}
               showStdDev={false}
-              smoothBy={resolution === "week" ? "week" : "day"}
+              smoothBy={smoothBy === "week" ? "week" : "day"}
               height={300}
               method={metric.type !== "binomial" ? "avg" : "sum"}
+              onHover={onHoverCallback}
+              hoverDate={hoverDate}
             />
           </div>
         ) : (
