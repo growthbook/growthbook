566dffb845275d7383bbb0f91cf10643bc9a4cb7
bttf <2374625+bttf@users.noreply.github.com>
2023-03-22 15:49:47 -0400

    Add hooks; Ensure experiment variation / visual change parity; Add delete visual changeset endpoint

--- packages/back-end/src/models/VisualChangesetModel.ts
+++ packages/back-end/src/models/VisualChangesetModel.ts
@@ -1,13 +1,17 @@
+import { isEqual, keyBy } from "lodash";
 import omit from "lodash/omit";
 import mongoose from "mongoose";
 import uniqid from "uniqid";
-import { ExperimentInterface } from "../../types/experiment";
+import { ExperimentInterface, Variation } from "../../types/experiment";
 import { ApiVisualChangeset } from "../../types/openapi";
+import { OrganizationInterface } from "../../types/organization";
 import {
   VisualChange,
   VisualChangesetInterface,
   VisualChangesetURLPattern,
 } from "../../types/visual-changeset";
+import { refreshSDKPayloadCache } from "../services/features";
+import { getExperimentById, getPayloadKeys } from "./ExperimentModel";
 
 /**
  * VisualChangeset is a collection of visual changes that are grouped together
@@ -145,11 +149,6 @@ export async function findVisualChangesets(
   ).map(toInterface);
 }
 
-// TODO
-// const onVisualChangeCreate = () => {};
-// const onVisualChangeUpdate = () => {};
-// const onVisualChangeDelete = () => {};
-
 export async function createVisualChange(
   id: string,
   organization: string,
@@ -190,10 +189,10 @@ export async function updateVisualChange({
   organization: string;
   payload: Partial<VisualChange>;
 }): Promise<{ nModified: number }> {
-  const visualChangeset = await VisualChangesetModel.findOne({
-    id: changesetId,
-    organization,
-  });
+  const visualChangeset = await findVisualChangesetById(
+    changesetId,
+    organization
+  );
 
   if (!visualChangeset) {
     throw new Error("Visual Changeset not found");
@@ -222,7 +221,14 @@ export async function updateVisualChange({
   return { nModified: res.nModified };
 }
 
-// TODO On creating a variation, we need to create a visual change for each
+const _genNewVisualChange = (variation: Variation): VisualChange => ({
+  id: uniqid("vc_"),
+  variation: variation.id,
+  description: "",
+  css: "",
+  domMutations: [],
+});
+
 export const createVisualChangeset = async ({
   experiment,
   organization,
@@ -230,26 +236,35 @@ export const createVisualChangeset = async ({
   editorUrl,
 }: {
   experiment: ExperimentInterface;
-  organization: string;
+  organization: OrganizationInterface;
   urlPatterns: VisualChangesetURLPattern[];
   editorUrl: VisualChangesetInterface["editorUrl"];
 }): Promise<VisualChangesetInterface> => {
-  const visualChangeset = await VisualChangesetModel.create({
-    id: uniqid("vcs_"),
-    experiment: experiment.id,
+  const visualChangeset = toInterface(
+    await VisualChangesetModel.create({
+      id: uniqid("vcs_"),
+      experiment: experiment.id,
+      organization: organization.id,
+      urlPatterns,
+      editorUrl,
+      visualChanges: experiment.variations.map(_genNewVisualChange),
+    })
+  );
+  await _onVisualChangesetCreate({
     organization,
-    urlPatterns,
-    editorUrl,
-    visualChanges: experiment.variations.map((variation) => ({
-      id: uniqid("vc_"),
-      variation: variation.id,
-      description: "",
-      css: "",
-      domMutations: [],
-    })),
+    visualChangeset,
+    experiment,
   });
-  return toInterface(visualChangeset);
+  return visualChangeset;
 };
+
+// type guard
+const _isUpdatingVisualChanges = (
+  updates: Partial<VisualChangesetInterface>
+): updates is {
+  visualChanges: VisualChange[];
+} & Partial<VisualChangesetInterface> =>
+  updates.visualChanges !== undefined && updates.visualChanges.length > 0;
 
 export const updateVisualChangeset = async ({
   changesetId,
@@ -257,28 +272,177 @@ export const updateVisualChangeset = async ({
   updates,
 }: {
   changesetId: string;
-  organization: string;
+  organization: OrganizationInterface;
   updates: Partial<VisualChangesetInterface>;
 }) => {
+  const visualChangeset = await findVisualChangesetById(
+    changesetId,
+    organization.id
+  );
+
+  if (!visualChangeset) {
+    throw new Error("Visual Changeset not found");
+  }
+
+  const isUpdatingVisualChanges = _isUpdatingVisualChanges(updates);
+
   // ensure new visual changes have ids assigned
-  const visualChanges =
-    updates.visualChanges?.map((vc) => ({
-      ...vc,
-      id: vc.id || uniqid("vc_"),
-    })) ?? [];
+  const visualChanges = isUpdatingVisualChanges
+    ? updates.visualChanges.map((vc) => ({
+        ...vc,
+        id: vc.id || uniqid("vc_"),
+      }))
+    : [];
 
   const res = await VisualChangesetModel.updateOne(
     {
       id: changesetId,
-      organization,
+      organization: organization.id,
     },
     {
       $set: {
         ...updates,
-        visualChanges,
+        ...(isUpdatingVisualChanges ? { visualChanges } : {}),
       },
     }
   );
 
+  await _onVisualChangesetUpdate({
+    oldVisualChangeset: visualChangeset,
+    newVisualChangeset: {
+      ...visualChangeset,
+      ...updates,
+      ...(isUpdatingVisualChanges ? { visualChanges } : {}),
+    },
+    organization,
+  });
+
   return { nModified: res.nModified, visualChanges };
+};
+
+const _hasVisualChanges = ({ visualChanges }: VisualChangesetInterface) =>
+  visualChanges.some((vc) => !!vc.css || !!vc.domMutations.length);
+
+const _onVisualChangesetCreate = async ({
+  organization,
+  visualChangeset,
+  experiment,
+}: {
+  organization: OrganizationInterface;
+  visualChangeset: VisualChangesetInterface;
+  experiment: ExperimentInterface;
+}) => {
+  if (!_hasVisualChanges(visualChangeset)) return;
+
+  const payloadKeys = getPayloadKeys(organization, experiment);
+
+  await refreshSDKPayloadCache(organization, payloadKeys);
+};
+
+const _onVisualChangesetUpdate = async ({
+  organization,
+  oldVisualChangeset,
+  newVisualChangeset,
+}: {
+  organization: OrganizationInterface;
+  oldVisualChangeset: VisualChangesetInterface;
+  newVisualChangeset: VisualChangesetInterface;
+}) => {
+  // if no effective delta between old and new, return early
+  const oldVisualChanges = oldVisualChangeset.visualChanges.map(
+    ({ css, domMutations }) => ({ css, domMutations })
+  );
+  const newVisualChanges = newVisualChangeset.visualChanges.map(
+    ({ css, domMutations }) => ({ css, domMutations })
+  );
+
+  if (isEqual(oldVisualChanges, newVisualChanges)) return;
+
+  const experiment = await getExperimentById(
+    organization.id,
+    newVisualChangeset.experiment
+  );
+
+  if (!experiment) return;
+
+  const payloadKeys = getPayloadKeys(organization, experiment);
+
+  await refreshSDKPayloadCache(organization, payloadKeys);
+};
+
+const _onVisualChangesetDelete = async ({
+  organization,
+  visualChangeset,
+}: {
+  organization: OrganizationInterface;
+  visualChangeset: VisualChangesetInterface;
+}) => {
+  // if there were no visual changes before deleting, return early
+  if (!_hasVisualChanges(visualChangeset)) return;
+
+  // get payload keys
+  const experiment = await getExperimentById(
+    organization.id,
+    visualChangeset.experiment
+  );
+
+  if (!experiment) return;
+
+  const payloadKeys = getPayloadKeys(organization, experiment);
+
+  await refreshSDKPayloadCache(organization, payloadKeys);
+};
+
+// when an experiment adds/removes variations, we need to update the analogous
+// visual changes to be in sync
+export const syncVisualChangesWithVariations = async ({
+  experiment,
+  organization,
+  visualChangeset,
+}: {
+  experiment: ExperimentInterface;
+  organization: OrganizationInterface;
+  visualChangeset: VisualChangesetInterface;
+}) => {
+  const { variations } = experiment;
+  const { visualChanges } = visualChangeset;
+  const visualChangesByVariationId = keyBy(visualChanges, "variation");
+  const newVisualChanges = variations.map((variation) => {
+    const visualChange = visualChangesByVariationId[variation.id];
+    return visualChange ? visualChange : _genNewVisualChange(variation);
+  });
+
+  await updateVisualChangeset({
+    organization,
+    changesetId: visualChangeset.id,
+    updates: { visualChanges: newVisualChanges },
+  });
+};
+
+// TODO implement in UI
+export const deleteVisualChangesetById = async ({
+  changesetId,
+  organization,
+}: {
+  changesetId: string;
+  organization: OrganizationInterface;
+}) => {
+  const visualChangeset = await findVisualChangesetById(
+    changesetId,
+    organization.id
+  );
+
+  if (!visualChangeset) {
+    throw new Error("Visual Changeset not found");
+  }
+
+  await VisualChangesetModel.deleteOne({
+    id: changesetId,
+    organization: organization.id,
+  });
+
+  await _onVisualChangesetDelete({
+    organization,
+    visualChangeset,
+  });
 };
