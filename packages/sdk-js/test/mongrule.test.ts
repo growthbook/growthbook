0eb82a841c1e2ce78918284464a1f44883416069
BarreiroT <tomasmbarreiroi@gmail.com>
2023-03-23 14:42:11 +0100

    include tests and types

---
+++ packages/sdk-js/test/mongrule.test.ts
@@ -0,0 +1,73 @@
+import { evalCondition } from "../src/mongrule";
+
+describe("Mongrule", () => {
+  describe("Null", () => {
+    it("Returns true when the value is null or not present", () => {
+      expect(
+        evalCondition(
+          { userId: null },
+          {
+            userId: null,
+          }
+        )
+      ).toBe(true);
+
+      expect(
+        evalCondition(
+          {},
+          {
+            userId: null,
+          }
+        )
+      ).toBe(true);
+    });
+
+    it("Returns true when the value is undefined", () => {
+      expect(
+        evalCondition(
+          { userId: undefined },
+          {
+            userId: null,
+          }
+        )
+      ).toBe(true);
+    });
+
+    it("Returns false when the value is present", () => {
+      expect(
+        evalCondition(
+          {
+            userId: "123",
+          },
+          {
+            userId: null,
+          }
+        )
+      ).toBe(false);
+    });
+
+    it("Returns false when the value is present but falsy", () => {
+      expect(
+        evalCondition(
+          {
+            userId: 0,
+          },
+          {
+            userId: null,
+          }
+        )
+      ).toBe(false);
+
+      expect(
+        evalCondition(
+          {
+            userId: "",
+          },
+          {
+            userId: null,
+          }
+        )
+      ).toBe(false);
+    });
+  });
+});
