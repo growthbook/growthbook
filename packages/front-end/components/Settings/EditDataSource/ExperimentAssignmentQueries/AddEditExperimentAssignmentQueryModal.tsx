a88d81a1f90d5df2b41abe833a263875fd6242ed
Michael Knowlton <michael@growthbook.io>
2023-03-28 13:41:20 -0400

    Reverts the add edit form back to what's on main

--- packages/front-end/components/Settings/EditDataSource/ExperimentAssignmentQueries/AddEditExperimentAssignmentQueryModal.tsx
+++ packages/front-end/components/Settings/EditDataSource/ExperimentAssignmentQueries/AddEditExperimentAssignmentQueryModal.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useMemo, useState } from "react";
+import React, { FC, useMemo } from "react";
 import {
   DataSourceInterfaceWithParams,
   ExposureQuery,
@@ -6,11 +6,7 @@ import {
 import { useForm } from "react-hook-form";
 import cloneDeep from "lodash/cloneDeep";
 import uniqId from "uniqid";
-import { FaExternalLinkAlt } from "react-icons/fa";
-import Code from "@/components/SyntaxHighlighting/Code";
-import EditSqlModal from "@/components/SchemaBrowser/EditSqlModal";
-import Tooltip from "@/components/Tooltip/Tooltip";
-import Toggle from "@/components/Forms/Toggle";
+import SQLInputField from "../../../SQLInputField";
 import Modal from "../../../Modal";
 import Field from "../../../Forms/Field";
 import StringArrayField from "../../../Forms/StringArrayField";
@@ -30,7 +26,6 @@ export const AddEditExperimentAssignmentQueryModal: FC<EditExperimentAssignmentQ
   onSave,
   onCancel,
 }) => {
-  const [sqlOpen, setSqlOpen] = useState(false);
   const modalTitle =
     mode === "add"
       ? "Add an Experiment Assignment query"
@@ -103,90 +98,50 @@ export const AddEditExperimentAssignmentQueryModal: FC<EditExperimentAssignmentQ
   }
 
   return (
-    <>
-      {sqlOpen && dataSource && (
-        <EditSqlModal
-          close={() => setSqlOpen(false)}
-          datasourceId={dataSource.id || ""}
-          placeholder=""
-          requiredColumns={Array.from(requiredColumns)}
-          value={userEnteredQuery}
-          save={async (sql) => form.setValue("query", sql)}
-        />
-      )}
-      <Modal
-        open={true}
-        submit={handleSubmit}
-        close={onCancel}
-        header={modalTitle}
-        cta="Save"
-        ctaEnabled={saveEnabled}
-        autoFocusSelector="#id-modal-identify-joins-heading"
-      >
-        <div className="my-2 ml-3 mr-3">
-          <div className="row">
-            <div className="col-12">
-              <Field label="Display Name" required {...form.register("name")} />
-              <Field
-                label="Description (optional)"
-                textarea
-                minRows={1}
-                {...form.register("description")}
-              />
-              <Field
-                label="Identifier Type"
-                options={identityTypes.map((i) => i.userIdType)}
-                required
-                {...form.register("userIdType")}
-              />
-              <StringArrayField
-                label="Dimension Columns"
-                value={userEnteredDimensions}
-                onChange={(dimensions) => {
-                  form.setValue("dimensions", dimensions);
-                }}
-              />
-              <div className="mb-1">
-                <Toggle
-                  id="require-ssl"
-                  label="Require SSL"
-                  {...form.register("hasNameCol")}
-                  value={form.watch("hasNameCol")}
-                  setValue={(value) => form.setValue("hasNameCol", value)}
-                  className="mr-2"
-                />
-                <label htmlFor="require-ssl" className="mr-1">
-                  Use Name Columns
-                </label>
-                <Tooltip body="Enable this if you store experiment/variation names as well as ids in your table" />
-              </div>
-              <div className="form-group">
-                <label>Query</label>
-                {userEnteredQuery && (
-                  <Code
-                    language="sql"
-                    code={userEnteredQuery}
-                    expandable={true}
-                  />
-                )}
-                <div>
-                  <button
-                    className="btn btn-outline-primary"
-                    type="button"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      setSqlOpen(true);
-                    }}
-                  >
-                    {userEnteredQuery ? "Edit" : "Add"} SQL{" "}
-                    <FaExternalLinkAlt />
-                  </button>
-                </div>
-              </div>
-            </div>
+    <Modal
+      open={true}
+      submit={handleSubmit}
+      close={onCancel}
+      size="max"
+      header={modalTitle}
+      cta="Save"
+      ctaEnabled={saveEnabled}
+      autoFocusSelector="#id-modal-identify-joins-heading"
+    >
+      <div className="my-2 ml-3 mr-3">
+        <div className="row">
+          <div className="col-12">
+            <Field label="Display Name" required {...form.register("name")} />
+            <Field
+              label="Description (optional)"
+              textarea
+              minRows={1}
+              {...form.register("description")}
+            />
+            <Field
+              label="Identifier Type"
+              options={identityTypes.map((i) => i.userIdType)}
+              required
+              {...form.register("userIdType")}
+            />
+            <StringArrayField
+              label="Dimension Columns"
+              value={userEnteredDimensions}
+              onChange={(dimensions) => {
+                form.setValue("dimensions", dimensions);
+              }}
+            />
+            <SQLInputField
+              userEnteredQuery={userEnteredQuery}
+              datasourceId={dataSource.id}
+              form={form}
+              requiredColumns={requiredColumns}
+              identityTypes={identityTypes}
+              queryType="experiment-assignment"
+            />
           </div>
         </div>
-      </Modal>
-    </>
+      </div>
+    </Modal>
   );
 };
