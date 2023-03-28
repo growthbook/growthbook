2ec02c58e188588581b2e01b9b1728c70870456f
Bryce <bryce1@gmail.com>
2023-03-28 09:32:38 -0700

    settings page cuped warning responsive

--- packages/front-end/pages/settings/index.tsx
+++ packages/front-end/pages/settings/index.tsx
@@ -732,9 +732,8 @@ const GeneralSettingsPage = (): React.ReactElement => {
                         }
                       />
                     </div>
-                    {!!form.watch("regressionAdjustmentEnabled") &&
-                      (!settings.statsEngine ||
-                        settings.statsEngine === "bayesian") && (
+                    {form.watch("regressionAdjustmentEnabled") &&
+                      form.watch("statsEngine") === "bayesian" && (
                         <div className="d-flex">
                           <small className="mb-1 text-warning-orange">
                             <FaExclamationTriangle /> Your organization uses
