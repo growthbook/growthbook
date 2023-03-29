4d87fa0408dfc91daad9950c23b71d58ec0d24d6
tinahollygb <tina@growthbook.io>
2023-03-29 13:41:09 -0700

    more validation tests

--- packages/back-end/test/services/experiments.test.ts
+++ packages/back-end/test/services/experiments.test.ts
@@ -4,6 +4,24 @@ import { postMetricValidator } from "../../src/validators/openapi";
 
 describe("experiments utils", () => {
   describe("postMetricApiPayloadIsValid", () => {
+    it("should return a successful result when providing the minimum number of fields", () => {
+      const input: z.infer<typeof postMetricValidator.bodySchema> = {
+        datasourceId: "ds_abc123",
+        sql: {
+          identifierTypes: ["user_id"],
+          conversionSQL: "select * from foo",
+        },
+        name: "My Cool Metric",
+        type: "count",
+      };
+
+      const result = postMetricApiPayloadIsValid(input) as {
+        valid: true;
+      };
+
+      expect(result.valid).toBe(true);
+    });
+
     it("should return a failed result when multiple query formats provided", () => {
       const input: z.infer<typeof postMetricValidator.bodySchema> = {
         datasourceId: "ds_abc123",
@@ -51,6 +69,32 @@ describe("experiments utils", () => {
       expect(result.valid).toBe(false);
       expect(result.error).toEqual(
         "Binomial metrics cannot have userAggregationSQL"
+      );
+    });
+
+    it("should return a failed result when conversionWindowEnd provided but not conversionWindowStart", () => {
+      const input: z.infer<typeof postMetricValidator.bodySchema> = {
+        datasourceId: "ds_abc123",
+        sql: {
+          identifierTypes: ["user_id"],
+          conversionSQL: "select * from foo",
+          userAggregationSQL: "sum(values)",
+        },
+        behavior: {
+          conversionWindowEnd: 1337,
+        },
+        name: "My Cool Metric",
+        type: "count",
+      };
+
+      const result = postMetricApiPayloadIsValid(input) as {
+        valid: false;
+        error: string;
+      };
+
+      expect(result.valid).toBe(false);
+      expect(result.error).toEqual(
+        "Must specify `behavior.conversionWindowStart` when providing `behavior.conversionWindowEnd`"
       );
     });
   });
