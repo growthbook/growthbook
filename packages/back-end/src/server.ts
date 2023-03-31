2bf3eb117a6a3f88a198072ebf542ace20326545
Bryce <bryce1@gmail.com>
2023-03-31 16:19:11 -0700

    app level error handling

--- packages/back-end/src/server.ts
+++ packages/back-end/src/server.ts
@@ -10,3 +10,17 @@ const server = app.listen(app.get("port"), () => {
 });
 
 export default server;
+
+// App-level error handling
+interface UnhandledRejectionError extends Error {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  promise: Promise<any>;
+}
+process.on("unhandledRejection", (reason, promise) => {
+  const err = new Error(reason as string) as UnhandledRejectionError;
+  err.promise = promise;
+  logger.error(err, "Unhandled Rejection");
+});
+process.on("uncaughtException", (err) => {
+  logger.error(err, "Uncaught Exception");
+});
