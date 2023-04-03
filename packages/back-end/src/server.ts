fbe3ef9607f9613dbc39a89e4fab2767ef9bb933
Bryce <bryce1@gmail.com>
2023-04-03 12:56:58 -0700

    simplify

--- packages/back-end/src/server.ts
+++ packages/back-end/src/server.ts
@@ -13,7 +13,7 @@ export default server;
 
 process.on("unhandledRejection", (rejection: unknown) => {
   if (["string", "number", "boolean"].includes(typeof rejection)) {
-    logger.error(new Error(rejection as string), "Unhandled Rejection");
+    logger.error(new Error(rejection + ""), "Unhandled Rejection");
     return;
   }
   logger.error(rejection, "Unhandled Rejection");
