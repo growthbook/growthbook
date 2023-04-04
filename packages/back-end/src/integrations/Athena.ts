3774c70152f8a140c153f266440418777401d962
Michael Knowlton <michael@growthbook.io>
2023-04-04 13:49:32 -0400

    Adds basic information schema support for AWS Athena.

--- packages/back-end/src/integrations/Athena.ts
+++ packages/back-end/src/integrations/Athena.ts
@@ -2,6 +2,9 @@ import { decryptDataSourceParams } from "../services/datasource";
 import { runAthenaQuery } from "../services/athena";
 import { AthenaConnectionParams } from "../../types/integrations/athena";
 import { FormatDialect } from "../util/sql";
+import { DataSourceProperties } from "../../types/datasource";
+import { formatInformationSchema } from "../util/informationSchemas";
+import { InformationSchema, RawInformationSchema } from "../types/Integration";
 import SqlIntegration from "./SqlIntegration";
 
 export default class Athena extends SqlIntegration {
@@ -12,6 +15,12 @@ export default class Athena extends SqlIntegration {
     this.params = decryptDataSourceParams<AthenaConnectionParams>(
       encryptedParams
     );
+  }
+  getSourceProperties(): DataSourceProperties {
+    return {
+      ...super.getSourceProperties(),
+      supportsInformationSchema: true,
+    };
   }
   getFormatDialect(): FormatDialect {
     return "trino";
@@ -44,5 +53,53 @@ export default class Athena extends SqlIntegration {
   }
   ensureFloat(col: string): string {
     return `1.0*${col}`;
+  }
+  async getInformationSchema(): Promise<InformationSchema[]> {
+    const sql = `SELECT
+        table_name,
+        table_catalog,
+        table_schema,
+        count(column_name) as column_count
+      FROM
+        information_schema.columns
+      WHERE
+        table_schema
+      NOT IN ('pg_catalog', 'information_schema', 'pg_toast')
+      GROUP BY (table_name, table_schema, table_catalog)`;
+
+    const results = await this.runQuery(sql);
+
+    if (!results.length) {
+      throw new Error(`No tables found.`);
+    }
+
+    return formatInformationSchema(results as RawInformationSchema[], "athena");
+  }
+
+  async getTableData(
+    databaseName: string,
+    tableSchema: string,
+    tableName: string
+  ): Promise<{ tableData: null | unknown[]; refreshMS: number }> {
+    const sql = `SELECT
+        data_type,
+        column_name
+      FROM
+        information_schema.columns
+      WHERE
+        table_catalog
+      IN ('${databaseName}')
+      AND
+        table_schema
+      IN ('${tableSchema}')
+      AND
+        table_name
+      IN ('${tableName}')`;
+
+    const queryStartTime = Date.now();
+    const tableData = await this.runQuery(sql);
+    const queryEndTime = Date.now();
+
+    return { tableData, refreshMS: queryEndTime - queryStartTime };
   }
 }
