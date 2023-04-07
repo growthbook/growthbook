26db1d5effb5545e10a05cb2413a5560ab9c9068
Luke Sonnet <luke.sonnet@gmail.com>
2023-04-06 17:01:11 -0700

    prefer CI to stddev for dateresults

--- packages/front-end/components/Experiment/DateResults.tsx
+++ packages/front-end/components/Experiment/DateResults.tsx
@@ -95,22 +95,27 @@ const DateResults: FC<{
                 let error: [number, number] | undefined = undefined;
                 // Since this is relative uplift, the baseline is a horizontal line at zero
                 let value = 0;
-                // For non-baseline variations and cumulative turned off, include error bars
+                // For non-baseline variations and cumulative turned off, include error bars and
+                // prefer CI to stddev it exists
                 if (i && !cumulative) {
                   const x = uplift?.mean || 0;
                   const sx = uplift?.stddev || 0;
                   const dist = uplift?.dist || "";
-                  if (dist === "lognormal") {
-                    // Uplift distribution is lognormal, so need to correct this
-                    // Add 2 standard deviations (~95% CI) for an error bar
-                    error = [
-                      Math.exp(x - 2 * sx) - 1,
-                      Math.exp(x + 2 * sx) - 1,
-                    ];
-                    value = Math.exp(x) - 1;
+                  if (stats.ci) {
+                    error = stats.ci;
                   } else {
-                    error = [x - 2 * sx, x + 2 * sx];
-                    value = x;
+                    if (dist === "lognormal") {
+                      // Uplift distribution is lognormal, so need to correct this
+                      // Add 2 standard deviations (~95% CI) for an error bar
+                      error = [
+                        Math.exp(x - 2 * sx) - 1,
+                        Math.exp(x + 2 * sx) - 1,
+                      ];
+                      value = Math.exp(x) - 1;
+                    } else {
+                      error = [x - 2 * sx, x + 2 * sx];
+                      value = x;
+                    }
                   }
                 }
                 // For non-baseline variations and cumulative turned ON, calculate uplift from cumulative data
@@ -119,7 +124,6 @@ const DateResults: FC<{
                   const crB = totalUsers[i] ? totalValue[i] / totalUsers[i] : 0;
                   value = crA ? (crB - crA) / crA : 0;
                 }
-
                 // Baseline should show the actual conversion rate
                 // Variations should show the relative uplift on top of this conversion rate
                 const label = i
