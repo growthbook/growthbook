959ef10cf9d11ba1cd15cda26ba9f454368a1992
bttf <2374625+bttf@users.noreply.github.com>
2023-04-13 14:44:52 -0400

    Introduce new Settings interface; Allow custom fieldnames for scope overrides

--- packages/back-end/test/services/settings.test.ts
+++ packages/back-end/test/services/settings.test.ts
@@ -1,4 +1,4 @@
-import { useOrgSettings } from "../../src/services/orgSettings";
+import { useSettings } from "../../src/services/settings";
 import { OrganizationInterface } from "../../types/organization";
 
 const baseOrganization: OrganizationInterface = {
@@ -11,15 +11,15 @@ const baseOrganization: OrganizationInterface = {
   url: "test-org",
 };
 
-describe("orgSettings", () => {
-  describe("useOrgSettings fn", () => {
+describe("settings", () => {
+  describe("useSettings fn", () => {
     it("returns org settings if no scopes are applied", () => {
       const settings = { pValueThreshold: 0.001 };
       const organization: OrganizationInterface = {
         ...baseOrganization,
         settings,
       };
-      const { settings: newSettings } = useOrgSettings(organization);
+      const { settings: newSettings } = useSettings(organization.settings);
       expect(newSettings.pValueThreshold.value).toEqual(
         settings.pValueThreshold
       );
