e44069165108596e45bdbc8a13a05620913b2e73
bttf <2374625+bttf@users.noreply.github.com>
2023-04-13 15:25:01 -0400

    Add test

--- packages/back-end/test/services/settings.test.ts
+++ packages/back-end/test/services/settings.test.ts
@@ -1,5 +1,9 @@
 import { useSettings } from "../../src/services/settings";
-import { OrganizationInterface } from "../../types/organization";
+import {
+  OrganizationInterface,
+  OrganizationSettings,
+} from "../../types/organization";
+import { ProjectInterface } from "../../types/project";
 
 const baseOrganization: OrganizationInterface = {
   dateCreated: new Date("2020-01-01"),
@@ -11,17 +15,47 @@ const baseOrganization: OrganizationInterface = {
   url: "test-org",
 };
 
+const mockProject: ProjectInterface = {
+  id: "1",
+  name: "Test Project",
+  organization: "1",
+  dateCreated: new Date("2020-01-01"),
+  dateUpdated: new Date("2020-01-01"),
+};
+
+const genOrgWithSettings = (settings: Partial<OrganizationSettings>) => ({
+  ...baseOrganization,
+  settings,
+});
+
 describe("settings", () => {
   describe("useSettings fn", () => {
     it("returns org settings if no scopes are applied", () => {
       const settings = { pValueThreshold: 0.001 };
-      const organization: OrganizationInterface = {
-        ...baseOrganization,
-        settings,
-      };
+      const organization = genOrgWithSettings(settings);
+
       const { settings: newSettings } = useSettings(organization.settings);
+
       expect(newSettings.pValueThreshold.value).toEqual(
         settings.pValueThreshold
+      );
+    });
+
+    it('applies project settings if "project" scope is applied', () => {
+      const settings = { pValueThreshold: 0.001 };
+      const organization = genOrgWithSettings(settings);
+
+      const projectWithPValueOverride = {
+        ...mockProject,
+        pValueThreshold: 0.06,
+      };
+
+      const { settings: newSettings } = useSettings(organization.settings, {
+        project: projectWithPValueOverride,
+      });
+
+      expect(newSettings.pValueThreshold.value).toEqual(
+        projectWithPValueOverride.pValueThreshold
       );
     });
   });
