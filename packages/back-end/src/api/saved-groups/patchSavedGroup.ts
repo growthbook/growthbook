46b0d4afea1d30f9e2b74c74b045e4ea3469e698
Michael Knowlton <michael@growthbook.io>
2023-04-17 07:12:51 -0400

    Adds a patch request for saved groups so a user can partially update a saved group, also adds logic that allows the user to reset the owner field rather than just falling back on the existing owner value.

--- packages/back-end/src/api/saved-groups/patchSavedGroup.ts
+++ packages/back-end/src/api/saved-groups/patchSavedGroup.ts
@@ -5,11 +5,15 @@ import {
   updateSavedGroup,
 } from "../../models/SavedGroupModel";
 import { createApiRequestHandler } from "../../util/handler";
-import { putSavedGroupValidator } from "../../validators/openapi";
+import { patchSavedGroupValidator } from "../../validators/openapi";
 
-export const patchSavedGroup = createApiRequestHandler(putSavedGroupValidator)(
+export const patchSavedGroup = createApiRequestHandler(
+  patchSavedGroupValidator
+)(
   async (req): Promise<PutSavedGroupResponse> => {
-    const { name, values, owner } = req.body;
+    const { name, values } = req.body;
+    let { owner } = req.body;
+
     const { id } = req.params;
 
     const savedGroup = await getSavedGroupById(id, req.organization.id);
@@ -18,10 +22,22 @@ export const patchSavedGroup = createApiRequestHandler(putSavedGroupValidator)(
       throw new Error(`Unable to locate the saved-group: ${id}`);
     }
 
+    if (!values && !name && typeof owner === "undefined") {
+      throw new Error(
+        'You must pass in at least one of the following: "values", "name", "owner".'
+      );
+    }
+
+    if (typeof owner === "string") {
+      owner = owner ? owner : "";
+    } else {
+      owner = savedGroup.owner;
+    }
+
     const updatedSavedGroup = await updateSavedGroup(id, req.organization.id, {
       values: values ? values : savedGroup.values,
-      groupName: name ? name : savedGroup?.groupName,
-      owner: owner ? owner : savedGroup?.owner,
+      groupName: name ? name : savedGroup.groupName,
+      owner,
     });
 
     return {
