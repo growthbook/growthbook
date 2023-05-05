93113962f216e5e07d3ffdcfe9c3bf7ccffec93b
tinahollygb <tina@growthbook.io>
2023-05-05 12:15:09 -0700

    wrap localStorage access in try/catch

--- packages/front-end/services/auth.tsx
+++ packages/front-end/services/auth.tsx
@@ -321,14 +321,18 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({
       }
 
       if (orgs.length > 0) {
-        const pickedOrg = localStorage.getItem("gb-last-picked-org");
-        if (pickedOrg && !router.query.org) {
-          try {
-            setOrgId(JSON.parse(pickedOrg));
-          } catch (e) {
+        try {
+          const pickedOrg = localStorage.getItem("gb-last-picked-org");
+          if (pickedOrg && !router.query.org) {
+            try {
+              setOrgId(JSON.parse(pickedOrg));
+            } catch (e) {
+              setOrgId(orgs[0].id);
+            }
+          } else {
             setOrgId(orgs[0].id);
           }
-        } else {
+        } catch (e) {
           setOrgId(orgs[0].id);
         }
       }
