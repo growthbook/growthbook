2a3736c9d8450b3de2f68ceb178271c2eeed50e9
Jeremy Dorn <jeremy@jeremydorn.com>
2023-05-12 11:00:39 -0400

    Experiment snapshot refactor - part 1
    This PR migrates the snapshot data structor to the new format. No user-facing changes, but required to unblock future work.
    
    TODO:
    - [ ] Define new snapshot data structure
    - [ ] JIT migration in the model
    - [ ] Fix type errors in front-end and back-end

--- packages/back-end/types/experiment-snapshot.d.ts
+++ packages/back-end/types/experiment-snapshot.d.ts
@@ -6,6 +6,7 @@ import {
   ExperimentReportResultDimension,
   MetricRegressionAdjustmentStatus,
 } from "./report";
+import { DimensionInterface } from "./dimension";
 
 export interface SnapshotMetric {
   value: number;
@@ -45,31 +46,99 @@ export interface ExperimentSnapshotSettings {
   sequentialTestingTuningParameter: number;
 }
 
-export interface ExperimentSnapshotInterface {
-  id: string;
-  organization: string;
-  experiment: string;
-  error?: string;
-  phase: number;
-  dateCreated: Date;
-  runStarted: Date | null;
-  manual: boolean;
-  query?: string;
-  queryLanguage?: QueryLanguage;
-  queries: Queries;
-  dimension: string | null;
-  unknownVariations?: string[];
-  multipleExposures?: number;
+export type LegacyExperimentSnapshotInterface = ExperimentSnapshotInterface & {
+  activationMetric?: string;
+  statsEngine?: StatsEngine;
+  hasRawQueries?: boolean;
   hasCorrectedStats?: boolean;
   results?: ExperimentReportResultDimension[];
-  hasRawQueries?: boolean;
-  queryFilter?: string;
-  segment?: string;
-  activationMetric?: string;
-  skipPartialData?: boolean;
-  statsEngine?: StatsEngine;
   regressionAdjustmentEnabled?: boolean;
   metricRegressionAdjustmentStatuses?: MetricRegressionAdjustmentStatus[];
   sequentialTestingEnabled?: boolean;
   sequentialTestingTuningParameter?: number;
+};
+
+export interface SnapshotAnalysis {
+  // Determines which analysis this is
+  settings: {
+    dimensions: string[];
+    statsEngine: StatsEngine;
+    frequentistSettings?: {
+      regressionAdjusted: boolean;
+      sequentialTesting: boolean;
+      sequentialTestingTuningParameter: number;
+      pValueCorrection: null | "holm-bonferroni" | "benjamini-hochberg";
+    };
+  };
+  dateCreated: Date;
+  status: "running" | "success" | "error";
+  error?: string;
+  results: ExperimentReportResultDimension[];
+}
+
+export interface MetricForSnapshot {
+  id: string;
+  // TODO: add more metric fields to see if it's "out-of-date"
+}
+
+export type DimensionForSnapshot = {
+  // The same format we use today that encodes both the type and id
+  // For example: `exp:country` or `pre:date`
+  id: string;
+  // Dimension settings at the time the snapshot was created
+  // Used to show an "out-of-date" warning on the front-end
+  settings?: Pick<
+    DimensionInterface,
+    "id" | "datasource" | "userIdType" | "sql"
+  >;
+};
+
+export interface ExperimentSnapshotInterface {
+  // Fields that uniquely define the snapshot
+  id: string;
+  organization: string;
+  experiment: string;
+  phase: number;
+  dimension: string | null;
+
+  // Status and meta info about the snapshot run
+  error?: string;
+  dateCreated: Date;
+  runStarted: Date | null;
+  status: "running" | "success" | "error";
+
+  // Settings that control which queries were run
+  // Used to determine which types of analyses are possible
+  querySettings: {
+    manual: boolean;
+    dimensions: DimensionForSnapshot[];
+    goalMetrics: MetricForSnapshot[];
+    guardrailMetrics: MetricForSnapshot[];
+    activationMetric: MetricForSnapshot | null;
+    regressionAdjustmentEnabled: boolean;
+    startDate: Date;
+    endDate: Date;
+  };
+
+  // Experiment/metric settings at the time the snapshot was run
+  // These are only used to show an "out-of-date" warning on the front-end
+  settings: {
+    experimentId: string;
+    queryFilter: string;
+    segment: string;
+    skipPartialData: boolean;
+    activationMetric: string;
+    datasourceId: string;
+    exposureQuery: string;
+  };
+
+  // What queries were run as part of this snapshot
+  query?: string;
+  queryLanguage?: QueryLanguage;
+  queries: Queries;
+
+  // Results
+  unknownVariations?: string[];
+  multipleExposures?: number;
+  analyses: SnapshotAnalysis[];
 }
