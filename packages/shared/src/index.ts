f5185a62699b1136812b1a0a662cad1b01fee0c5
Jeremy Dorn <jeremy@jeremydorn.com>
2023-05-14 10:10:38 -0400

    Fix named export

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,4 +1,4 @@
-export * as stats from "./constants";
+export * as constants from "./constants";
 export * as dates from "./dates";
 export * as settings from "./settings";
 export * as util from "./util";
