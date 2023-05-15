831a30ee5e4000b92cca3d3c7f7533e7db311075
Luis Luvia <lluvia@lusade.net>
2023-05-15 00:27:17 -0500

    Add GrowthBook context

---
+++ packages/sdk-svelte/src/lib/GrowthBookProvider.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type { GrowthBook } from "@growthbook/growthbook";
+  import { setContext } from "svelte";
+  import { ContextSymbol, type GrowthBookContext } from "./context";
+
+  export let growthbook: GrowthBook | undefined;
+
+  if (!growthbook) {
+    console.warn("GrowthBookProvider: GrowthBook instance not provided")
+  }
+
+  setContext<GrowthBookContext>(ContextSymbol, { growthbook });
+</script>
+
+<slot />
