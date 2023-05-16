633ba03d8c6dc4f6bb08478cf601ba2dbec40c39
bttf <2374625+bttf@users.noreply.github.com>
2023-05-16 10:30:40 -0400

    WIP

--- packages/sdk-react/rollup.config.js
+++ packages/sdk-react/rollup.config.js
@@ -2,37 +2,80 @@ import babel from "@rollup/plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
 import replace from "@rollup/plugin-replace";
 import commonjs from "@rollup/plugin-commonjs";
+import dts from "rollup-plugin-dts";
 
 const extensions = [".js", ".ts", ".tsx", ".jsx"];
 
-export default {
-  input: "src/index.ts",
-  external: (id) => {
-    return !id.match(/(sdk-react|^\.)/);
+export default [
+  {
+    input: "src/client/index.ts",
+    external: (id) => {
+      return !id.match(/(sdk-react|^\.)/);
+    },
+    output: [
+      {
+        dir: "dist/client/esm",
+        format: "esm",
+        sourcemap: true,
+      },
+      {
+        dir: "dist/client/cjs",
+        format: "cjs",
+        name: "growthbook",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve({ extensions, jsnext: true }),
+      commonjs(),
+      replace({
+        "process.env.NODE_ENV": JSON.stringify("production"),
+        preventAssignment: true,
+      }),
+      babel({
+        babelHelpers: "bundled",
+        extensions,
+      }),
+    ],
   },
-  output: [
-    {
-      file: "dist/esm/index.js",
-      format: "esm",
-      sourcemap: true,
+  {
+    input: "src/server/index.ts",
+    external: (id) => {
+      return !id.match(/(sdk-react|^\.)/);
     },
-    {
-      file: "dist/cjs/index.js",
-      format: "cjs",
-      name: "growthbook",
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    resolve({ extensions, jsnext: true }),
-    commonjs(),
-    replace({
-      "process.env.NODE_ENV": JSON.stringify("production"),
-      preventAssignment: true,
-    }),
-    babel({
-      babelHelpers: "bundled",
-      extensions,
-    }),
-  ],
-};
+    output: [
+      {
+        dir: "dist/server/esm",
+        format: "esm",
+        sourcemap: true,
+      },
+      {
+        dir: "dist/server/cjs",
+        format: "cjs",
+        name: "growthbook",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve({ extensions, jsnext: true }),
+      commonjs(),
+      replace({
+        "process.env.NODE_ENV": JSON.stringify("production"),
+        preventAssignment: true,
+      }),
+      babel({
+        babelHelpers: "bundled",
+        extensions,
+      }),
+    ],
+  },
+  {
+    input: [
+      "./dist/client/index.d.ts",
+      "./dist/server/index.d.ts",
+      "./dist/client/GrowthBookReact.d.ts",
+    ],
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    plugins: [dts()],
+  },
+];
---
+++ packages/sdk-react/src/server/index.ts
@@ -0,0 +1,25 @@
+export {
+  GrowthBook,
+  setPolyfills,
+  configureCache,
+  clearCache,
+} from "@growthbook/growthbook";
+
+export type {
+  Context,
+  Experiment,
+  Result,
+  ExperimentOverride,
+  Attributes,
+  ConditionInterface,
+  ExperimentStatus,
+  FeatureDefinition,
+  FeatureResult,
+  FeatureResultSource,
+  FeatureRule,
+  JSONValue,
+  SubscriptionFunction,
+  Filter,
+  VariationMeta,
+  VariationRange,
+} from "@growthbook/growthbook";
