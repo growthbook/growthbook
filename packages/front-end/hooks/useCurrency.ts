1bde3bdd5ba895442630fccb1576ba4073b2512c
Michael Knowlton <michael@growthbook.io>
2023-05-23 09:13:35 -0400

    Removes type casting in useCurrency hook.

--- packages/front-end/hooks/useCurrency.ts
+++ packages/front-end/hooks/useCurrency.ts
@@ -4,8 +4,8 @@ import { useUser } from "@/services/UserContext";
 export function useCurrency(): string {
   const { settings } = useUser();
   return (
-    (Object.keys(supportedCurrencies).find(
+    Object.keys(supportedCurrencies).find(
       (key) => key === settings.displayCurrency
-    ) as string) || "USD"
+    ) || "USD"
   );
 }
