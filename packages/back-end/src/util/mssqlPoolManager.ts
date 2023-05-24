4003782b3c999f55ef8316187ae5649d7e9042f8
Michael Knowlton <michael@growthbook.io>
2023-05-24 12:24:48 -0400

    Adds support for closing connections.

--- packages/back-end/src/util/mssqlPoolManager.ts
+++ packages/back-end/src/util/mssqlPoolManager.ts
@@ -8,6 +8,12 @@ export function findOrCreateConnection(
 ) {
   if (!pools.has(name)) {
     const pool: mssql.ConnectionPool = new mssql.ConnectionPool(config);
+    // automatically remove the pool from the cache if `pool.close()` is called
+    const close = pool.close.bind(pool);
+    pool.close = () => {
+      pools.delete(name);
+      return close();
+    };
     pools.set(name, pool.connect());
   }
   return pools.get(name);
