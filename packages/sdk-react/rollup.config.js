cac25d9d6d3efb67884c23bc81c2c066855b6427
bttf <2374625+bttf@users.noreply.github.com>
2023-05-31 15:55:37 -0400

    server submodule

--- packages/sdk-react/rollup.config.js
+++ packages/sdk-react/rollup.config.js
@@ -5,34 +5,67 @@ import commonjs from "@rollup/plugin-commonjs";
 
 const extensions = [".js", ".ts", ".tsx", ".jsx"];
 
-export default {
-  input: "src/index.ts",
-  external: (id) => {
-    return !id.match(/(sdk-react|^\.)/);
+export default [
+  {
+    input: "src/index.ts",
+    external: (id) => {
+      return !id.match(/(sdk-react|^\.)/);
+    },
+    output: [
+      {
+        file: "dist/esm/index.js",
+        format: "esm",
+        sourcemap: true,
+      },
+      {
+        file: "dist/cjs/index.js",
+        format: "cjs",
+        name: "growthbook",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve({ extensions, jsnext: true }),
+      commonjs(),
+      replace({
+        "process.env.NODE_ENV": JSON.stringify("production"),
+        preventAssignment: true,
+      }),
+      babel({
+        babelHelpers: "bundled",
+        extensions,
+      }),
+    ],
   },
-  output: [
-    {
-      file: "dist/esm/index.js",
-      format: "esm",
-      sourcemap: true,
+  {
+    input: "src/server.ts",
+    external: (id) => {
+      return !id.match(/(sdk-react|^\.)/);
     },
-    {
-      file: "dist/cjs/index.js",
-      format: "cjs",
-      name: "growthbook",
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    resolve({ extensions, jsnext: true }),
-    commonjs(),
-    replace({
-      "process.env.NODE_ENV": JSON.stringify("production"),
-      preventAssignment: true,
-    }),
-    babel({
-      babelHelpers: "bundled",
-      extensions,
-    }),
-  ],
-};
+    output: [
+      {
+        file: "dist/server/esm/index.js",
+        format: "esm",
+        sourcemap: true,
+      },
+      {
+        file: "dist/server/cjs/index.js",
+        format: "cjs",
+        name: "growthbook",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve({ extensions, jsnext: true }),
+      commonjs(),
+      replace({
+        "process.env.NODE_ENV": JSON.stringify("production"),
+        preventAssignment: true,
+      }),
+      babel({
+        babelHelpers: "bundled",
+        extensions,
+      }),
+    ],
+  },
+];
