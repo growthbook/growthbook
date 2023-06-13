75dbc5261fa792b8f40d052042ab4305128da230
Michael Knowlton <michael@growthbook.io>
2023-06-13 16:45:26 -0400

    Update Experiment Assignment Query form to use EditSqlModal.

--- packages/front-end/components/SchemaBrowser/EditSqlModal.tsx
+++ packages/front-end/components/SchemaBrowser/EditSqlModal.tsx
@@ -1,8 +1,9 @@
-import { useCallback, useState } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { FaPlay } from "react-icons/fa";
 import { TestQueryRow } from "back-end/src/types/Integration";
 import clsx from "clsx";
+import { UserIdType } from "@/../back-end/types/datasource";
 import { useAuth } from "@/services/auth";
 import { useDefinitions } from "@/services/DefinitionsContext";
 import { validateSQL } from "@/services/datasources";
@@ -19,8 +20,14 @@ export interface Props {
   datasourceId: string;
   save: (sql: string) => Promise<void>;
   close: () => void;
-  requiredColumns: string[];
-  placeholder: string;
+  requiredColumns: Set<string>;
+  placeholder?: string;
+  queryType?: "segment" | "dimension" | "metric" | "experiment-assignment";
+  setDimensions?: (dimensions: string[]) => void;
+  setHasNameCols?: (hasNameCols: boolean) => void;
+  identityTypes?: UserIdType[];
+  userEnteredHasNameCol?: boolean;
+  userEnteredDimensions?: string[];
 }
 
 type TestQueryResults = {
@@ -35,9 +42,16 @@ export default function EditSqlModal({
   save,
   close,
   requiredColumns,
-  placeholder,
+  placeholder = "",
   datasourceId,
+  queryType,
+  identityTypes,
+  setHasNameCols,
+  setDimensions,
+  userEnteredDimensions = [],
+  userEnteredHasNameCol,
 }: Props) {
+  const [suggestions, setSuggestions] = useState<ReactElement[]>([]);
   const form = useForm({
     defaultValues: {
       sql: value,
@@ -54,19 +68,109 @@ export default function EditSqlModal({
   const [cursorData, setCursorData] = useState<null | CursorData>(null);
   const [testingQuery, setTestingQuery] = useState(false);
 
-  const handleTestQuery = useCallback(async () => {
-    const sql = form.getValues("sql");
-    try {
-      // Just check for basic SQL syntax, not any required columns
-      // We can check for required columns after the results are returned
-      validateSQL(sql, []);
+  // We do some one-off logic for Experiment Assignment queries
+  useEffect(() => {
+    if (
+      queryType === "experiment-assignment" &&
+      setHasNameCols &&
+      setDimensions
+    ) {
+      const result = testQueryResults?.results?.[0];
+      if (!result) return;
 
+      const suggestions: ReactElement[] = [];
+
+      const namedCols = ["experiment_name", "variation_name"];
+      const userIdTypes = identityTypes?.map((type) => type.userIdType || []);
+
+      const returnedColumns = new Set<string>(Object.keys(result));
+      const optionalColumns = [...returnedColumns].filter(
+        (col) =>
+          !requiredColumns.has(col) &&
+          !namedCols.includes(col) &&
+          !userIdTypes?.includes(col)
+      );
+
+      // Check if `hasNameCol` should be enabled
+      if (!userEnteredHasNameCol) {
+        // Selected both required columns, turn on `hasNameCol` automatically
+        if (
+          returnedColumns.has("experiment_name") &&
+          returnedColumns.has("variation_name")
+        ) {
+          setHasNameCols(true);
+        }
+        // Only selected `experiment_name`, add warning
+        else if (returnedColumns.has("experiment_name")) {
+          suggestions.push(
+            <>
+              Add <code>variation_name</code> to your SELECT clause to enable
+              GrowthBook to populate names automatically.
+            </>
+          );
+        }
+        // Only selected `variation_name`, add warning
+        else if (returnedColumns.has("variation_name")) {
+          suggestions.push(
+            <>
+              Add <code>experiment_name</code> to your SELECT clause to enable
+              GrowthBook to populate names automatically.
+            </>
+          );
+        }
+      }
+
+      // Prompt to add optional columns as dimensions
+      if (optionalColumns.length > 0) {
+        suggestions.push(
+          <>
+            The following columns were returned, but will be ignored. Add them
+            as dimensions or disregard this message.
+            <ul className="mb-0 pb-0">
+              {optionalColumns.map((col) => (
+                <li key={col}>
+                  <code>{col}</code> -{" "}
+                  <a
+                    href="#"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setDimensions([...userEnteredDimensions, col]);
+                    }}
+                  >
+                    add as dimension
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </>
+        );
+      }
+
+      setSuggestions(suggestions);
+    }
+  }, [
+    requiredColumns,
+    testQueryResults,
+    form,
+    queryType,
+    userEnteredHasNameCol,
+    userEnteredDimensions,
+    identityTypes,
+    setHasNameCols,
+    setDimensions,
+  ]);
+
+  const handleTestQuery = async () => {
+    const sql = form.getValues("sql");
+    setTestQueryResults(null);
+    try {
+      validateSQL(sql, []);
       setTestingQuery(true);
-      const res = await apiCall<TestQueryResults>("/query/test", {
+      const res: TestQueryResults = await apiCall("/query/test", {
         method: "POST",
         body: JSON.stringify({
           query: sql,
-          datasourceId,
+          datasourceId: datasourceId,
         }),
       });
 
@@ -75,17 +179,19 @@ export default function EditSqlModal({
       setTestQueryResults({ error: e.message });
     }
     setTestingQuery(false);
-  }, [form, apiCall, datasourceId]);
-
+  };
   const datasource = getDatasourceById(datasourceId);
-  // @ts-expect-error TS(2531) If you come across this, please fix it!: Object is possibly 'null'.
-  const supportsSchemaBrowser = datasource.properties.supportsInformationSchema;
+  const supportsSchemaBrowser =
+    datasource?.properties?.supportsInformationSchema;
 
   return (
     <Modal
       open
       header="Edit SQL"
       submit={form.handleSubmit(async (value) => {
+        if (queryType === "experiment-assignment") {
+          await handleTestQuery();
+        }
         await save(value.sql);
       })}
       close={close}
@@ -119,10 +225,10 @@ export default function EditSqlModal({
                     Test Query
                   </Button>
                 </div>
-                {requiredColumns.length > 0 && (
+                {Array.from(requiredColumns).length > 0 && (
                   <div className="col-auto ml-auto pr-3">
                     <strong>Required Columns:</strong>
-                    {requiredColumns.map((col) => (
+                    {Array.from(requiredColumns).map((col) => (
                       <code className="mx-1 border p-1" key={col}>
                         {col}
                       </code>
@@ -149,12 +255,10 @@ export default function EditSqlModal({
                 <DisplayTestQueryResults
                   duration={parseInt(testQueryResults.duration || "0")}
                   requiredColumns={[...requiredColumns]}
-                  // @ts-expect-error TS(2322) If you come across this, please fix it!: Type 'TestQueryRow[] | undefined' is not assignabl... Remove this comment to see the full error message
-                  results={testQueryResults.results}
+                  results={testQueryResults.results || []}
                   error={testQueryResults.error}
-                  // @ts-expect-error TS(2322) If you come across this, please fix it!: Type 'string | undefined' is not assignable to typ... Remove this comment to see the full error message
-                  sql={testQueryResults.sql}
-                  suggestions={[]}
+                  sql={testQueryResults.sql || ""}
+                  suggestions={suggestions}
                 />
               </div>
             )}
@@ -166,10 +270,8 @@ export default function EditSqlModal({
               updateSqlInput={(sql: string) => {
                 form.setValue("sql", sql);
               }}
-              // @ts-expect-error TS(2322) If you come across this, please fix it!: Type 'DataSourceInterfaceWithParams | null' is not... Remove this comment to see the full error message
               datasource={datasource}
-              // @ts-expect-error TS(2322) If you come across this, please fix it!: Type 'CursorData | null' is not assignable to type... Remove this comment to see the full error message
-              cursorData={cursorData}
+              cursorData={cursorData || undefined}
             />
           </div>
         )}
--- packages/front-end/components/Settings/EditDataSource/ExperimentAssignmentQueries/AddEditExperimentAssignmentQueryModal.tsx
+++ packages/front-end/components/Settings/EditDataSource/ExperimentAssignmentQueries/AddEditExperimentAssignmentQueryModal.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useMemo } from "react";
+import React, { FC, useMemo, useState } from "react";
 import {
   DataSourceInterfaceWithParams,
   ExposureQuery,
@@ -6,10 +6,14 @@ import {
 import { useForm } from "react-hook-form";
 import cloneDeep from "lodash/cloneDeep";
 import uniqId from "uniqid";
-import SQLInputField from "../../../SQLInputField";
+import { FaExternalLinkAlt } from "react-icons/fa";
+import Code from "@/components/SyntaxHighlighting/Code";
+import StringArrayField from "@/components/Forms/StringArrayField";
+import Toggle from "@/components/Forms/Toggle";
+import Tooltip from "@/components/Tooltip/Tooltip";
 import Modal from "../../../Modal";
 import Field from "../../../Forms/Field";
-import StringArrayField from "../../../Forms/StringArrayField";
+import EditSqlModal from "../../../SchemaBrowser/EditSqlModal";
 
 type EditExperimentAssignmentQueryProps = {
   exposureQuery?: ExposureQuery;
@@ -26,55 +30,59 @@ export const AddEditExperimentAssignmentQueryModal: FC<EditExperimentAssignmentQ
   onSave,
   onCancel,
 }) => {
+  const [sqlOpen, setSqlOpen] = useState(false);
   const modalTitle =
     mode === "add"
       ? "Add an Experiment Assignment query"
-      : // @ts-expect-error TS(2532) If you come across this, please fix it!: Object is possibly 'undefined'.
-        `Edit ${exposureQuery.name}`;
+      : `Edit ${
+          exposureQuery ? exposureQuery.name : "Experiment Assignment"
+        } query`;
 
-  // @ts-expect-error TS(2532) If you come across this, please fix it!: Object is possibly 'undefined'.
-  const userIdTypeOptions = dataSource.settings.userIdTypes.map(
+  const userIdTypeOptions = dataSource?.settings?.userIdTypes?.map(
     ({ userIdType }) => ({
       display: userIdType,
       value: userIdType,
     })
   );
-  const defaultUserId = userIdTypeOptions[0]?.value || "user_id";
+  const defaultUserId = userIdTypeOptions
+    ? userIdTypeOptions[0]?.value
+    : "user_id";
 
   const form = useForm<ExposureQuery>({
     defaultValues:
-      mode === "edit"
-        ? // @ts-expect-error TS(2345) If you come across this, please fix it!: Argument of type 'ExposureQuery | undefined' is no... Remove this comment to see the full error message
-          cloneDeep<ExposureQuery>(exposureQuery)
+      mode === "edit" && exposureQuery
+        ? cloneDeep<ExposureQuery>(exposureQuery)
         : {
             description: "",
             id: uniqId("tbl_"),
             name: "",
             dimensions: [],
             query: `SELECT\n  ${defaultUserId} as ${defaultUserId},\n  timestamp as timestamp,\n  experiment_id as experiment_id,\n  variation_id as variation_id\nFROM my_table`,
-            userIdType: userIdTypeOptions[0]?.value || "",
+            userIdType: userIdTypeOptions ? userIdTypeOptions[0]?.value : "",
           },
   });
 
   // User-entered values
   const userEnteredUserIdType = form.watch("userIdType");
   const userEnteredQuery = form.watch("query");
-  const userEnteredHasNameCol = form.watch("hasNameCol");
   const userEnteredDimensions = form.watch("dimensions");
+  const userEnteredHasNameCol = form.watch("hasNameCol");
+
+  const [showAdvancedMode, setShowAdvancedMode] = useState(
+    !!userEnteredDimensions || !!userEnteredHasNameCol
+  );
 
   const handleSubmit = form.handleSubmit(async (value) => {
     await onSave(value);
 
     form.reset({
-      // @ts-expect-error TS(2322) If you come across this, please fix it!: Type 'null' is not assignable to type 'string | un... Remove this comment to see the full error message
-      id: null,
+      id: undefined,
       query: "",
       name: "",
       dimensions: [],
       description: "",
       hasNameCol: false,
-      // @ts-expect-error TS(2322) If you come across this, please fix it!: Type 'null' is not assignable to type 'string | un... Remove this comment to see the full error message
-      userIdType: null,
+      userIdType: undefined,
     });
   });
 
@@ -103,50 +111,121 @@ export const AddEditExperimentAssignmentQueryModal: FC<EditExperimentAssignmentQ
   }
 
   return (
-    <Modal
-      open={true}
-      submit={handleSubmit}
-      close={onCancel}
-      size="max"
-      header={modalTitle}
-      cta="Save"
-      ctaEnabled={saveEnabled}
-      autoFocusSelector="#id-modal-identify-joins-heading"
-    >
-      <div className="my-2 ml-3 mr-3">
-        <div className="row">
-          <div className="col-12">
-            <Field label="Display Name" required {...form.register("name")} />
-            <Field
-              label="Description (optional)"
-              textarea
-              minRows={1}
-              {...form.register("description")}
-            />
-            <Field
-              label="Identifier Type"
-              options={identityTypes.map((i) => i.userIdType)}
-              required
-              {...form.register("userIdType")}
-            />
-            <StringArrayField
-              label="Dimension Columns"
-              value={userEnteredDimensions}
-              onChange={(dimensions) => {
-                form.setValue("dimensions", dimensions);
-              }}
-            />
-            <SQLInputField
-              userEnteredQuery={userEnteredQuery}
-              datasourceId={dataSource.id}
-              form={form}
-              requiredColumns={requiredColumns}
-              identityTypes={identityTypes}
-              queryType="experiment-assignment"
-            />
+    <>
+      {sqlOpen && dataSource && (
+        <EditSqlModal
+          close={() => setSqlOpen(false)}
+          datasourceId={dataSource.id || ""}
+          requiredColumns={requiredColumns}
+          value={userEnteredQuery}
+          save={async (userEnteredQuery) =>
+            form.setValue("query", userEnteredQuery)
+          }
+          queryType="experiment-assignment"
+          setDimensions={(dimensions) =>
+            form.setValue("dimensions", dimensions)
+          }
+          setHasNameCols={(hasNameCol) => {
+            form.setValue("hasNameCol", hasNameCol);
+          }}
+          identityTypes={identityTypes}
+          userEnteredHasNameCol={userEnteredHasNameCol}
+          userEnteredDimensions={userEnteredDimensions}
+        />
+      )}
+      <Modal
+        open={true}
+        submit={handleSubmit}
+        close={onCancel}
+        size="max"
+        header={modalTitle}
+        cta="Save"
+        ctaEnabled={saveEnabled}
+        autoFocusSelector="#id-modal-identify-joins-heading"
+      >
+        <div className="my-2 ml-3 mr-3">
+          <div className="row">
+            <div className="col-12">
+              <Field label="Display Name" required {...form.register("name")} />
+              <Field
+                label="Description (optional)"
+                textarea
+                minRows={1}
+                {...form.register("description")}
+              />
+              <Field
+                label="Identifier Type"
+                options={identityTypes.map((i) => i.userIdType)}
+                required
+                {...form.register("userIdType")}
+              />
+              <div className="form-group">
+                <label>Query</label>
+                {userEnteredQuery && (
+                  <Code
+                    language="sql"
+                    code={userEnteredQuery}
+                    expandable={true}
+                  />
+                )}
+                <div>
+                  <button
+                    className="btn btn-outline-primary"
+                    type="button"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setSqlOpen(true);
+                    }}
+                  >
+                    {userEnteredQuery ? "Edit" : "Add"} SQL{" "}
+                    <FaExternalLinkAlt />
+                  </button>
+                </div>
+              </div>
+              <div className="form-group">
+                <a
+                  href="#"
+                  className="ml-auto"
+                  style={{ fontSize: "0.9em" }}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setShowAdvancedMode(!showAdvancedMode);
+                  }}
+                >
+                  {showAdvancedMode ? "Hide" : "Show"} Advanced Options
+                </a>
+                {showAdvancedMode && (
+                  <div>
+                    <div className="py-2">
+                      <Toggle
+                        id="userEnteredNameCol"
+                        value={form.watch("hasNameCol") || false}
+                        setValue={(value) => {
+                          form.setValue("hasNameCol", value);
+                        }}
+                      />
+                      <label
+                        className="mr-2 mb-0"
+                        htmlFor="exposure-query-toggle"
+                      >
+                        Use Name Columns
+                      </label>
+                      <Tooltip body="Enable this if you store experiment/variation names as well as ids in your table" />
+                    </div>
+                    <StringArrayField
+                      label="Dimension Columns"
+                      value={userEnteredDimensions}
+                      onChange={(dimensions) => {
+                        form.setValue("dimensions", dimensions);
+                      }}
+                    />
+                  </div>
+                )}
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </Modal>
+      </Modal>
+    </>
   );
 };
