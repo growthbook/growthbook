1d600e058c54b30788bb6ac2c8cb7c6c5d325d86
tinahollygb <tina@growthbook.io>
2023-06-27 13:21:16 -0700

    add test for containsEnvironment util

--- packages/back-end/test/util/environments.util.test.ts
+++ packages/back-end/test/util/environments.util.test.ts
@@ -1,41 +1,13 @@
 import { Environment } from "../../types/organization";
 import { deepFreeze } from "../test-helpers";
-import { addEnvironmentToOrganizationEnvironments } from "../../src/util/environments";
+import {
+  addEnvironmentToOrganizationEnvironments,
+  containsEnvironment,
+} from "../../src/util/environments";
 
-describe("addEnvironmentToOrganizationEnvironments", () => {
-  const existingEnvironments: Environment[] = [
-    {
-      id: "staging",
-      description: "",
-      toggleOnList: true,
-      defaultState: true,
-    },
-    {
-      id: "production",
-      description: "My existing production environment",
-      toggleOnList: false,
-      defaultState: true,
-    },
-  ];
-
-  beforeEach(() => {
-    deepFreeze(existingEnvironments);
-  });
-
-  it("should append the environment to the existing environments", () => {
-    const input: Environment = {
-      id: "development",
-      description: "local dev environment",
-      toggleOnList: true,
-      defaultState: true,
-    };
-
-    const result = addEnvironmentToOrganizationEnvironments(
-      input,
-      existingEnvironments
-    );
-
-    expect(result).toEqual([
+describe("environment utils", () => {
+  describe("addEnvironmentToOrganizationEnvironments", () => {
+    const existingEnvironments: Environment[] = [
       {
         id: "staging",
         description: "",
@@ -48,54 +20,23 @@ describe("addEnvironmentToOrganizationEnvironments", () => {
         toggleOnList: false,
         defaultState: true,
       },
-      {
+    ];
+
+    beforeEach(() => {
+      deepFreeze(existingEnvironments);
+    });
+
+    it("should append the environment to the existing environments", () => {
+      const input: Environment = {
         id: "development",
         description: "local dev environment",
         toggleOnList: true,
         defaultState: true,
-      },
-    ]);
-  });
+      };
 
-  describe("when the environment exists and should be replaced", () => {
-    const input: Environment = {
-      id: "production",
-      description: "production environment imported from another service",
-    };
-
-    it("should replace the existing environment", () => {
       const result = addEnvironmentToOrganizationEnvironments(
         input,
-        existingEnvironments,
-        true
-      );
-
-      expect(result).toEqual([
-        {
-          id: "staging",
-          description: "",
-          toggleOnList: true,
-          defaultState: true,
-        },
-        {
-          id: "production",
-          description: "production environment imported from another service",
-        },
-      ]);
-    });
-  });
-
-  describe("when the environment exists and should not be replaced", () => {
-    const input: Environment = {
-      id: "production",
-      description: "production environment imported from another service",
-    };
-
-    it("should not replace the existing environment", () => {
-      const result = addEnvironmentToOrganizationEnvironments(
-        input,
-        existingEnvironments,
-        false
+        existingEnvironments
       );
 
       expect(result).toEqual([
@@ -111,14 +52,54 @@ describe("addEnvironmentToOrganizationEnvironments", () => {
           toggleOnList: false,
           defaultState: true,
         },
+        {
+          id: "development",
+          description: "local dev environment",
+          toggleOnList: true,
+          defaultState: true,
+        },
       ]);
     });
 
-    describe("when replace arg omitted", () => {
+    describe("when the environment exists and should be replaced", () => {
+      const input: Environment = {
+        id: "production",
+        description: "production environment imported from another service",
+      };
+
+      it("should replace the existing environment", () => {
+        const result = addEnvironmentToOrganizationEnvironments(
+          input,
+          existingEnvironments,
+          true
+        );
+
+        expect(result).toEqual([
+          {
+            id: "staging",
+            description: "",
+            toggleOnList: true,
+            defaultState: true,
+          },
+          {
+            id: "production",
+            description: "production environment imported from another service",
+          },
+        ]);
+      });
+    });
+
+    describe("when the environment exists and should not be replaced", () => {
+      const input: Environment = {
+        id: "production",
+        description: "production environment imported from another service",
+      };
+
       it("should not replace the existing environment", () => {
         const result = addEnvironmentToOrganizationEnvironments(
           input,
-          existingEnvironments
+          existingEnvironments,
+          false
         );
 
         expect(result).toEqual([
@@ -136,6 +117,69 @@ describe("addEnvironmentToOrganizationEnvironments", () => {
           },
         ]);
       });
+
+      describe("when replace arg omitted", () => {
+        it("should not replace the existing environment", () => {
+          const result = addEnvironmentToOrganizationEnvironments(
+            input,
+            existingEnvironments
+          );
+
+          expect(result).toEqual([
+            {
+              id: "staging",
+              description: "",
+              toggleOnList: true,
+              defaultState: true,
+            },
+            {
+              id: "production",
+              description: "My existing production environment",
+              toggleOnList: false,
+              defaultState: true,
+            },
+          ]);
+        });
+      });
+    });
+  });
+
+  describe("containsEnvironment", () => {
+    const existingEnvironments: Environment[] = [
+      {
+        id: "production",
+        description: "My old production",
+      },
+      {
+        id: "development",
+        description: "My existing development environment",
+      },
+    ];
+
+    beforeEach(() => {
+      deepFreeze(existingEnvironments);
+    });
+
+    it("should return true when the environment by key already exists", () => {
+      const input: Environment = {
+        id: "production",
+        description: "My new production",
+      };
+
+      const result = containsEnvironment(existingEnvironments, input);
+
+      expect(result).toBe(true);
+    });
+
+    it("should return false when the environment does not exist", () => {
+      const input: Environment = {
+        id: "staging",
+        description: "My staging environment",
+      };
+
+      const result = containsEnvironment(existingEnvironments, input);
+
+      expect(result).toBe(false);
     });
   });
 });
