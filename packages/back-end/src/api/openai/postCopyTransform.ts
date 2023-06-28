cec5edd563a347ae3aec9acb8700b742c83798d2
bttf <2374625+bttf@users.noreply.github.com>
2023-06-27 21:27:19 -0700

    Ensure we pass along daily limit flag

--- packages/back-end/src/api/openai/postCopyTransform.ts
+++ packages/back-end/src/api/openai/postCopyTransform.ts
@@ -5,6 +5,7 @@ import { createApiRequestHandler } from "../../util/handler";
 interface PostCopyTransformResponse {
   original: string;
   transformed: string | undefined;
+  dailyLimitReached: boolean;
 }
 
 const transformModes = ["energetic", "concise", "humorous"] as const;
@@ -41,7 +42,11 @@ export const postCopyTransform = createApiRequestHandler(validation)(
     const { copy, mode } = req.body;
 
     if (await hasExceededUsageQuota(req.organization)) {
-      throw new Error("Usage quota exceeded");
+      return {
+        original: copy,
+        transformed: undefined,
+        dailyLimitReached: true,
+      };
     }
 
     const transformed = await simpleCompletion({
@@ -54,6 +59,7 @@ export const postCopyTransform = createApiRequestHandler(validation)(
     return {
       original: copy,
       transformed,
+      dailyLimitReached: false,
     };
   }
 );
