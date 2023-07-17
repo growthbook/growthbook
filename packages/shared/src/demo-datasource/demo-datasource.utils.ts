662a0036657f6464e563d996476983f7eb95115b
tinahollygb <tina@growthbook.io>
2023-07-17 15:31:45 -0700

    add utils to support demo datasource project

---
+++ packages/shared/src/demo-datasource/demo-datasource.utils.ts
@@ -0,0 +1,36 @@
+const DEMO_PROJECT_ID_SEPARATOR = "_";
+const DEMO_PROJECT_ID_SUFFIX = "demo-datasource-project";
+
+/**
+ * Returns the demo project ID to support the demo datasource project.
+ * e.g. prj_org-abc123_demo-datasource-project
+ * @param organizationId
+ */
+export function getDemoDatasourceProjectIdForOrganization(
+  organizationId: string
+): string {
+  return (
+    "prj" +
+    DEMO_PROJECT_ID_SEPARATOR +
+    organizationId +
+    DEMO_PROJECT_ID_SEPARATOR +
+    DEMO_PROJECT_ID_SUFFIX
+  );
+}
+
+/**
+ * Verifies if the provided project ID is the organization's demo datasource project ID
+ * @param projectId
+ * @param organizationId
+ */
+export function isDemoDatasourceProject({
+  projectId,
+  organizationId,
+}: {
+  projectId: string;
+  organizationId: string;
+}): boolean {
+  const demoId = getDemoDatasourceProjectIdForOrganization(organizationId);
+
+  return projectId === demoId;
+}
