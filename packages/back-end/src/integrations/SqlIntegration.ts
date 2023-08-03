451f284eab8d0b517f31711492f9096a6949086f
Cameron Moten <33769422+cameronmoten@users.noreply.github.com>
2023-08-03 16:03:30 -0700

    add end date and start date to segments

--- packages/back-end/src/integrations/SqlIntegration.ts
+++ packages/back-end/src/integrations/SqlIntegration.ts
@@ -1103,7 +1103,14 @@ export default abstract class SqlIntegration
               ? `JOIN __denominatorUsers du ON (du.${baseIdType} = e.${baseIdType})`
               : ""
           }
-        ${segment ? `WHERE s.date <= e.timestamp and s.date >= ${this.toTimestamp(startDate)}` : ""}
+        ${segment ? `WHERE s.date <= e.timestamp 
+          AND s.date >= ${this.toTimestamp(startDate)}
+          ${
+            endDate // This will provide a hint to the Query Planner for a speed improvement.
+              ? `AND s.date <= ${this.toTimestamp(endDate)}`
+              : ""
+          }` 
+          : ""}
         GROUP BY
           e.${baseIdType}
       )
