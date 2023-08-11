f5f064e1452bdff97c55f6a76f8128bc06df4ca9
tinahollygb <tina@growthbook.io>
2023-08-11 13:34:01 -0700

    add link to demo datasource project page from ExperimentsGetStarted (/dashboard, /experiments)

--- packages/front-end/components/HomePage/ExperimentsGetStarted.tsx
+++ packages/front-end/components/HomePage/ExperimentsGetStarted.tsx
@@ -2,31 +2,26 @@ import React, { useMemo, useState } from "react";
 import Link from "next/link";
 import { ExperimentInterfaceStringDates } from "back-end/types/experiment";
 import { useRouter } from "next/router";
-import { FaChevronRight, FaDatabase, FaQuestionCircle } from "react-icons/fa";
+import { FaDatabase } from "react-icons/fa";
 import { useDefinitions } from "@/services/DefinitionsContext";
-import { useAuth } from "@/services/auth";
-import track from "@/services/track";
 import { hasFileConfig } from "@/services/env";
 import usePermissions from "@/hooks/usePermissions";
 import NewDataSourceForm from "@/components/Settings/NewDataSourceForm";
 import MetricForm from "@/components/Metrics/MetricForm";
-import Button from "@/components/Button";
 import { DocLink } from "@/components/DocLink";
 import DocumentationLinksSidebar from "@/components/HomePage/DocumentationLinksSidebar";
 import GetStartedStep from "@/components/HomePage/GetStartedStep";
 import ImportExperimentModal from "@/components/Experiment/ImportExperimentModal";
-import Tooltip from "@/components/Tooltip/Tooltip";
+import { useDemoDataSourceProject } from "@/hooks/useDemoDataSourceProject";
 import AddExperimentModal from "../Experiment/AddExperimentModal";
 
 const ExperimentsGetStarted = ({
   experiments,
-  mutate,
 }: {
   experiments: ExperimentInterfaceStringDates[];
   mutate: () => void;
 }): React.ReactElement => {
   const { metrics, datasources, mutateDefinitions, project } = useDefinitions();
-  const { apiCall } = useAuth();
 
   const permissions = usePermissions();
 
@@ -53,10 +48,6 @@ const ExperimentsGetStarted = ({
     }
   }, [router?.query?.featureExperiment]);
 
-  const hasSampleExperiment = experiments.filter((m) =>
-    m.id.match(/^exp_sample/)
-  )[0];
-
   const hasDataSource = datasources.length > 0;
   const hasMetrics =
     metrics.filter((m) => !m.id.match(/^met_sample/)).length > 0;
@@ -69,21 +60,7 @@ const ExperimentsGetStarted = ({
     : hasDataSource
     ? 2
     : 1;
-  const allowImport = !(hasMetrics || hasExperiments) && !hasFileConfig();
-
-  const importSampleData = (source: string) => async () => {
-    const res = await apiCall<{
-      experiment: string;
-    }>(`/organization/sample-data`, {
-      method: "POST",
-    });
-    await mutateDefinitions();
-    await mutate();
-    track("Add Sample Data", {
-      source,
-    });
-    await router.push("/experiment/" + res.experiment);
-  };
+  const { exists: demoProjectExists } = useDemoDataSourceProject();
 
   return (
     <>
@@ -101,15 +78,10 @@ const ExperimentsGetStarted = ({
               await mutateDefinitions();
               setDataSourceOpen(false);
             }}
-            // @ts-expect-error TS(2322) If you come across this, please fix it!: Type 'boolean' is not assignable to type '(source:... Remove this comment to see the full error message
-            showImportSampleData={
-              !hasDataSource &&
-              allowImport &&
-              !hasSampleExperiment &&
-              importSampleData("datasource-form")
-            }
+            showImportSampleData={!demoProjectExists}
           />
         )}
+
         {metricsOpen && (
           <MetricForm
             current={{}}
@@ -151,36 +123,21 @@ const ExperimentsGetStarted = ({
                 can view results for your experiment.
               </div>
             ) : (
-              allowImport &&
-              (hasSampleExperiment ||
-                permissions.check("createAnalyses", project)) && (
+              !demoProjectExists &&
+              permissions.check("createAnalyses", project) && (
                 <div className="alert alert-info mb-3 d-none d-md-block">
                   <div className="d-flex align-items-center">
                     <strong className="mr-2">Just here to explore?</strong>
                     <div style={{ flex: 1 }}>
                       We have some sample data you can use.
                     </div>
-                    {hasSampleExperiment ? (
-                      <Link href={`/experiment/${hasSampleExperiment.id}`}>
-                        <a className="btn btn-sm btn-link ml-2">
-                          View Sample Experiment <FaChevronRight />
+                    <div>
+                      <Link href="/demo-datasource-project">
+                        <a color="info" className="btn-sm ml-3 mr-2">
+                          <FaDatabase /> Import Sample Data
                         </a>
                       </Link>
-                    ) : (
-                      <div>
-                        <Button
-                          color="info"
-                          className="btn-sm ml-3 mr-2"
-                          onClick={importSampleData("onboarding")}
-                        >
-                          <FaDatabase /> Import Sample Data
-                        </Button>
-
-                        <Tooltip body="Includes a sample experiment with results. Don't worry, it's easy to remove later.">
-                          <FaQuestionCircle className="text-dark" />
-                        </Tooltip>
-                      </div>
-                    )}
+                    </div>
                   </div>
                 </div>
               )
