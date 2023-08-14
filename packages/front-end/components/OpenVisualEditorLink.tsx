40d62d1f6a3b9e6aec7c03e885deae631e5f97d8
bttf <2374625+bttf@users.noreply.github.com>
2023-08-14 15:06:52 -0700

    Include exp url

--- packages/front-end/components/OpenVisualEditorLink.tsx
+++ packages/front-end/components/OpenVisualEditorLink.tsx
@@ -104,7 +104,6 @@ const OpenVisualEditorLink: FC<{
     url = appendQueryParamsToURL(url, {
       "vc-id": visualChangeset.id,
       "v-idx": 1,
-      "exp-url": encodeURIComponent(window.location.href),
     });
 
     return url;
@@ -129,6 +128,7 @@ const OpenVisualEditorLink: FC<{
             data: {
               apiHost,
               apiKey: key,
+              experimentUrl: window.location.href,
             },
           },
           window.location.origin
