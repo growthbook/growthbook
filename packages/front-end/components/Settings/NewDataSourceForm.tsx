5e004769c9df8bec94b79d7e911559ed9a91e2c4
Michael Knowlton <michael@growthbook.io>
2023-08-15 15:06:16 -0400

    Fixed a bug where the form wouldn't submit correctly.

--- packages/front-end/components/Settings/NewDataSourceForm.tsx
+++ packages/front-end/components/Settings/NewDataSourceForm.tsx
@@ -353,7 +353,7 @@ const NewDataSourceForm: FC<{
       ? null
       : form.handleSubmit(async (data) => {
           let newDataId = dataSourceId;
-          if (step === 1) {
+          if (step === 2) {
             // @ts-expect-error TS(2322) If you come across this, please fix it!: Type 'string | undefined' is not assignable to typ... Remove this comment to see the full error message
             newDataId = await saveDataConnection();
           }
