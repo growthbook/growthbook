49149d001326106b40eaeb2c972f3ff62ce01391
Michael Knowlton <michael@growthbook.io>
2023-08-15 11:04:27 -0400

    Updating the flow based on feedback - should be in a good spot CI wise.

--- packages/front-end/components/Settings/EventSourceList.tsx
+++ packages/front-end/components/Settings/EventSourceList.tsx
@@ -1,42 +1,25 @@
 import { SchemaFormat } from "back-end/types/datasource";
-import React, { useState } from "react";
-import { MdKeyboardArrowDown } from "react-icons/md";
+import React from "react";
 import { eventSchema, eventSchemas } from "@/services/eventSchema";
 import styles from "./EventSourceList.module.scss";
 
 export interface Props {
   selected?: SchemaFormat;
   onSelect: (schema: eventSchema) => void;
-  setSchema: any;
-  setDatasource: any;
-  project: any;
-  setStep: any;
-  setPossibleTypes: any;
-  form: any;
-  dataSourceConnections: any;
-  importSampleData: any;
+  selectCustomOption: (event: React.MouseEvent<HTMLAnchorElement>) => void;
 }
 
 export default function EventSourceList({
   onSelect,
   selected,
-  setSchema,
-  setDatasource,
-  project,
-  setStep,
-  setPossibleTypes,
-  form,
-  dataSourceConnections,
-  importSampleData,
+  selectCustomOption,
 }: Props) {
-  const [expand, setExpand] = useState(false);
-
   return (
     <>
       <div
         className="d-flex flex-wrap align-items-stretch align-middle row mb-3"
         style={{
-          maxHeight: expand ? 999 : 240,
+          maxHeight: 999,
           overflow: "hidden",
           position: "relative",
           transition: "max-height 0.5s",
@@ -79,96 +62,29 @@ export default function EventSourceList({
             </a>
           </div>
         ))}
-        <div>
+        <div className="d-flex flex-column col-12">
           <div className="my-2">
-            <h4>Don&apos;t See Yours?</h4>
-            <p>
-              If your organization uses an event tracker that isn&apos;t listed
-              above, or if you use a complex data pipeline with multiple event
-              trackers and custom data processing, we suggest a Custom Data
-              Source.
-            </p>
+            <strong style={{ fontSize: "1.2em" }}>
+              Don&apos;t see your event tracker?
+            </strong>
           </div>
-          <div className="form-group d-flex">
-            <div className="col-6">
+          <div className={`row`}>
+            <div className="col-4">
               <a
-                className={`btn btn-light-hover btn-outline-primary mb-3 py-3`}
-                onClick={(e) => {
-                  e.preventDefault();
-                  setSchema("custom");
-                  setDatasource({
-                    name: "My Datasource",
-                    settings: {},
-                    projects: project ? [project] : [],
-                  });
-                  // no options for custom:
-                  form.setValue(`settings.schemaOptions`, {});
-
-                  // set to all possible types:
-                  setPossibleTypes(dataSourceConnections.map((o) => o.type));
-                  // jump to next step
-                  setStep(1);
-                }}
+                className={`btn btn-light-hover btn-outline-primary
+               mb-3 py-3`}
+                onClick={(e) => selectCustomOption(e)}
               >
-                <h4>Start Custom Setup</h4>
+                <h4>Use Custom Source</h4>
                 <p className="mb-0 text-dark">
                   We&apos;ll guide you through how to manually configure a Data
                   Source.
                 </p>
               </a>
             </div>
-            {importSampleData && (
-              <div className="col-6">
-                <a
-                  className="btn btn-light-hover btn-outline-primary mb-3 py-3 ml-auto"
-                  onClick={async (e) => {
-                    e.preventDefault();
-                    await importSampleData("new data source form");
-                  }}
-                >
-                  <h4>Use Sample Dataset</h4>
-                  <p className="mb-0 text-dark">
-                    Explore GrowthBook with a pre-loaded sample dataset.
-                  </p>
-                </a>
-              </div>
-            )}
           </div>
         </div>
-        {!expand && (
-          <div
-            style={{
-              position: "absolute",
-              bottom: 0,
-              left: 0,
-              right: 0,
-              height: 50,
-              textAlign: "center",
-              background:
-                "linear-gradient(transparent, var(--surface-background-color))",
-              cursor: "pointer",
-            }}
-            className="text-primary"
-            onClick={(e) => {
-              e.preventDefault();
-              setExpand(true);
-            }}
-          />
-        )}
       </div>
-      {!expand && (
-        <div
-          className="text-center mb-3 cursor-pointer"
-          onClick={(e) => {
-            e.preventDefault();
-            setExpand(true);
-          }}
-        >
-          <a href="#" className="display-block">
-            Show All <MdKeyboardArrowDown />
-          </a>
-        </div>
-      )}
     </>
   );
 }
