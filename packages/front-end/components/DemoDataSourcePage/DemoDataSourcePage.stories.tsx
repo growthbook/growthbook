83cf46eb66d2c0dc947a538aff549f8c3b80c960
Tina Holly <113377031+tinahollygb@users.noreply.github.com>
2023-08-23 12:14:33 -0700

    Demo datasource page (#1499)
    * Add post endpoint for demo datasource
    
    * Update post endpoint to create all objects
    
    * Add snapshots; test button
    
    * Fix variation for checkout-layout
    
    * lint
    
    * generate AppFeature types to get new demo-datasource feature flag
    
    * demo datasource page
    
    * compute demo project exists; use definitions ready state;
    
    * Respond to comments
    
    * Add copy and screenshots
    
    * Update keys
    
    * Add fixed ID for denominator; other comments
    
    * return to fixed name; make more fault tolerant
    
    * Remove test button
    
    * expose Project model in REPL
    
    * add page /demo-datasource-project & wire up delete button
    
    * update DemoDataSourcePage stories
    
    * link up create button
    
    * reset error/success states when performing new action
    
    * add warning under create/edit metrics form
    
    * show warning when a user tries to create a feature under the demo project
    
    * show warning for new experiment modal when under demo datasource project
    
    * disable "add datasource" button for demo datasource project
    
    * filter out demo project from the add datasource modal
    
    * add credentials
    
    * Change ssl setting
    
    * replace link to old sample data in the Getting Started screen
    
    * refactor: DRY up code w/ useDemoDataSourceProject hook
    
    * add link to demo datasource project page from ExperimentsGetStarted (/dashboard, /experiments)
    
    * add link to the demo datasource page from the datasources page
    
    * copy change suggestions from luke
    
    * add link to demo datasource project page in the global demo banner
    
    * default datasource & project in the metric add/edit form when using demo datasource project
    
    * abstract the demo project feature ID "gbdemo-checkout-layout" into separate common util
    
    * link to created data source, experiment & feature from demo datasource page
    
    * add link to metrics from demo datasource page
    
    * update demo datasource backend to use experiment-ref instead of experiment
    
    * swap variationId and value fields
    
    * inline demo data
    
    ---------
    
    Co-authored-by: Luke Sonnet <luke.sonnet@gmail.com>
    Co-authored-by: Luke Sonnet <lukesonnet@users.noreply.github.com>

---
+++ packages/front-end/components/DemoDataSourcePage/DemoDataSourcePage.stories.tsx
@@ -0,0 +1,112 @@
+import { action } from "@storybook/addon-actions";
+import { DemoDataSourcePage } from "./DemoDataSourcePage";
+
+export default {
+  component: DemoDataSourcePage,
+  title: "Demo Datasource/DemoDataSourcePage",
+};
+
+export const DoesNotExist = () => {
+  const onDelete = action("onDelete");
+  const onCreate = action("onCreate");
+
+  return (
+    <>
+      <DemoDataSourcePage
+        onDelete={onDelete}
+        onCreate={onCreate}
+        ready={true}
+        exists={false}
+        success={null}
+        error={null}
+        demoDataSourceId={null}
+        demoExperimentId={null}
+        demoFeatureId={null}
+      />
+    </>
+  );
+};
+
+export const WasCreatedSuccessfully = () => {
+  const onDelete = action("onDelete");
+  const onCreate = action("onCreate");
+
+  return (
+    <>
+      <DemoDataSourcePage
+        onDelete={onDelete}
+        onCreate={onCreate}
+        ready={true}
+        exists={true}
+        success="Success!"
+        error={null}
+        demoDataSourceId={null}
+        demoExperimentId={null}
+        demoFeatureId={null}
+      />
+    </>
+  );
+};
+
+export const AlreadyExists = () => {
+  const onDelete = action("onDelete");
+  const onCreate = action("onCreate");
+
+  return (
+    <>
+      <DemoDataSourcePage
+        onDelete={onDelete}
+        onCreate={onCreate}
+        ready={true}
+        exists={true}
+        success={null}
+        error={null}
+        demoDataSourceId={null}
+        demoExperimentId={null}
+        demoFeatureId={null}
+      />
+    </>
+  );
+};
+
+export const Loading = () => {
+  const onDelete = action("onDelete");
+  const onCreate = action("onCreate");
+
+  return (
+    <>
+      <DemoDataSourcePage
+        onDelete={onDelete}
+        onCreate={onCreate}
+        ready={false}
+        exists={false}
+        success={null}
+        error={null}
+        demoDataSourceId={null}
+        demoExperimentId={null}
+        demoFeatureId={null}
+      />
+    </>
+  );
+};
+
+export const WithError = () => {
+  const onDelete = action("onDelete");
+  const onCreate = action("onCreate");
+
+  return (
+    <>
+      <DemoDataSourcePage
+        onDelete={onDelete}
+        onCreate={onCreate}
+        ready={true}
+        exists={false}
+        success={null}
+        error="Some kind of error occurred!"
+        demoDataSourceId={null}
+        demoExperimentId={null}
+        demoFeatureId={null}
+      />
+    </>
+  );
+};
