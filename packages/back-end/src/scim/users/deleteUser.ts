f3a8183540f893756765d128e2c44ca517235d2b
Michael Knowlton <michael@growthbook.io>
2023-10-04 10:38:56 -0400

    Cleaning up /delete endpoint

--- packages/back-end/src/scim/users/deleteUser.ts
+++ packages/back-end/src/scim/users/deleteUser.ts
@@ -1,31 +1,6 @@
 import { Response } from "express";
-import { updateOrganization } from "../../models/OrganizationModel";
-import { ScimGetRequest } from "../../../types/scim";
 
-export async function deleteUser(req: ScimGetRequest, res: Response) {
-  const requestBody = req.body.toString("utf-8");
-
-  const requestBodyObject = JSON.parse(requestBody);
-
-  const org = req.organization;
-
-  const requestToRemoveUser =
-    requestBodyObject.Operations[0].value.active === false;
-
-  if (!requestToRemoveUser) {
-    // throw error that this isn't supported or return something
-  }
-
-  // Look up the user in the org's member list
-  const userIndex = org.members.findIndex(
-    (member) => member.id === req.params.id
-  );
-
-  const updatedOrg = org;
-
-  updatedOrg.members.splice(userIndex, 1);
-
-  await updateOrganization(org.id, updatedOrg);
-
+export async function deleteUser(res: Response) {
+  // SCIM requires that we have a /user/delete endpoint, but given a user can belong to multiple orgs, we don't want to actually delete the user from the DB
   return res.status(204);
 }
