d202427ae0aed778fd501beaa9e6cef977d150d6
Michael Knowlton <michael@growthbook.io>
2023-10-11 15:22:35 -0400

    Removes updateUser functionality

--- packages/back-end/src/scim/users/users.router.ts
+++ packages/back-end/src/scim/users/users.router.ts
@@ -3,7 +3,6 @@ import { getUser } from "./getUser";
 import { createUser } from "./createUser";
 import { listUsers } from "./listUsers";
 import { patchUser } from "./patchUser";
-import { updateUser } from "./updateUser";
 
 const router = Router();
 
@@ -12,7 +11,6 @@ const router = Router();
 router.get("/", listUsers);
 router.get("/:id", getUser);
 router.post("/", createUser);
-router.put("/:id", updateUser);
 router.patch("/:id", patchUser); // For Okta: Only used for user activation & deactivation (and password sync but that shouldn't be relevant)
 
 export default router;
