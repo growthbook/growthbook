bfb54d202b742eb4285ad8aa18bc6056cc8bcead
Michael Knowlton <michael@growthbook.io>
2023-10-12 07:15:21 -0400

    Updates the createUser method to better handle an existing scim provisioned user who was removed and re-added.

--- packages/back-end/src/scim/users/createUser.ts
+++ packages/back-end/src/scim/users/createUser.ts
@@ -7,7 +7,10 @@ import {
 } from "../../services/organizations";
 import { OrganizationInterface } from "../../../types/organization";
 import { ScimUserPutOrPostRequest } from "../../../types/scim";
-import { createUser as createNewUser } from "../../services/users";
+import {
+  createUser as createNewUser,
+  getUserByEmail,
+} from "../../services/users";
 
 export async function createUser(req: ScimUserPutOrPostRequest, res: Response) {
   const { externalId, displayName, userName } = req.body;
@@ -15,7 +18,7 @@ export async function createUser(req: ScimUserPutOrPostRequest, res: Response) {
   const org: OrganizationInterface = req.organization;
 
   const expandedMembers = await expandOrgMembers(org.members);
-  const existingUser = expandedMembers.find(
+  const existingOrgMember = expandedMembers.find(
     (member) => member.email === userName
   );
 
@@ -23,7 +26,7 @@ export async function createUser(req: ScimUserPutOrPostRequest, res: Response) {
   const responseObj = cloneDeep(req.body);
 
   try {
-    if (existingUser && existingUser.managedByIdp) {
+    if (existingOrgMember && existingOrgMember.managedByIdp) {
       // Return an error if user exists and they're managed by an external IDP
       return res.status(409).json({
         schemas: ["urn:ietf:params:scim:api:messages:2.0:Error"],
@@ -31,36 +34,40 @@ export async function createUser(req: ScimUserPutOrPostRequest, res: Response) {
         detail: "User already exists in this organization",
         status: 409,
       });
-    } else if (existingUser && !existingUser.managedByIdp) {
+    } else if (existingOrgMember && !existingOrgMember.managedByIdp) {
       // If created through GrowthBook, update the user with the externalId and managedByIdp: true
       await convertMemberToManagedByIdp({
         organization: org,
-        userId: existingUser.id,
+        userId: existingOrgMember.id,
         externalId: externalId ? externalId : undefined,
       });
 
-      responseObj.id = existingUser.id;
-    } else {
-      const newUser = await createNewUser(
+      responseObj.id = existingOrgMember.id;
+    }
+
+    let user = await getUserByEmail(userName);
+
+    if (!user) {
+      user = await createNewUser(
         displayName,
         userName,
         "12345678", // TODO: SSO shouldn't need a password. figure out how to test this
         false
       );
-
-      await addMemberToOrg({
-        organization: org,
-        userId: newUser.id,
-        role,
-        limitAccessByEnvironment: false,
-        environments: [],
-        projectRoles: [],
-        externalId,
-        managedByIdp: true,
-      });
-
-      responseObj.id = newUser.id;
     }
+
+    await addMemberToOrg({
+      organization: org,
+      userId: user.id,
+      role,
+      limitAccessByEnvironment: false,
+      environments: [],
+      projectRoles: [],
+      externalId,
+      managedByIdp: true,
+    });
+
+    responseObj.id = user.id;
 
     return res.status(201).json(responseObj);
   } catch (e) {
