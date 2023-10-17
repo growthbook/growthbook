1c30a8f81687f8f767e78972ed61ae82ed0c70c8
James Irwin <tallnerd@gmail.com>
2023-10-17 17:01:34 +0200

    add missing upload router

---
+++ packages/back-end/src/routers/upload/upload.router.ts
@@ -0,0 +1,20 @@
+import bodyParser from "body-parser";
+import express from "express";
+import { wrapController } from "../wrapController";
+import * as uploadControllerRaw from "./upload.controller";
+
+const router = express.Router();
+
+const uploadController = wrapController(uploadControllerRaw);
+
+router.get("/:path*", uploadController.getImage);
+router.put(
+  "/",
+  bodyParser.raw({
+    type: "image/*",
+    limit: "10mb",
+  }),
+  uploadController.putUpload
+);
+
+export { router as uploadRouter };
