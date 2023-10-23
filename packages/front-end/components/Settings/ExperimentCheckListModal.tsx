926591af35f384f34cb08844148431d1c97a2492
Michael Knowlton <michael@growthbook.io>
2023-10-23 16:54:49 -0400

    Introduce react-select for creating new item.

--- packages/front-end/components/Settings/ExperimentCheckListModal.tsx
+++ packages/front-end/components/Settings/ExperimentCheckListModal.tsx
@@ -1,7 +1,47 @@
 import { useState } from "react";
 import { OrganizationInterface } from "back-end/types/organization";
+import CreatableSelect from "react-select/creatable";
 import { useAuth } from "@/services/auth";
 import Modal from "../Modal";
+import Button from "../Button";
+
+function ChecklistItem({
+  value,
+  index,
+  experimentLaunchChecklist,
+  setExperimentLaunchChecklist,
+}: {
+  value: string;
+  index: number;
+  experimentLaunchChecklist: string[];
+  setExperimentLaunchChecklist: (value: string[]) => void;
+}) {
+  console.log("value", value);
+  return (
+    <div className="d-flex align-items-center py-1">
+      <CreatableSelect
+        className="w-100"
+        isMulti={false}
+        placeholder="Select a checklist option or create your own"
+        onCreateOption={(inputValue) => {
+          console.log("inputValue", inputValue);
+        }}
+        value={value ? { label: value, value } : null}
+      />
+      <Button
+        color="link"
+        className="m-1"
+        onClick={async () => {
+          const newChecklist = [...experimentLaunchChecklist];
+          newChecklist.splice(index, 1);
+          setExperimentLaunchChecklist(newChecklist);
+        }}
+      >
+        Remove
+      </Button>
+    </div>
+  );
+}
 
 export default function ExperimentCheckListModal({
   org,
@@ -14,8 +54,6 @@ export default function ExperimentCheckListModal({
     string[]
   >(() => org.settings?.experimentLaunchChecklist || []);
   const { apiCall } = useAuth();
-
-  console.log("org", org);
 
   async function handleSubmit() {
     const settings = {
@@ -36,21 +74,27 @@ export default function ExperimentCheckListModal({
       cta="Save"
       submit={() => handleSubmit()}
     >
-      <h1>Hi</h1>
-      <p>Info about this goes here</p>
+      <p>
+        Ensure all experiments meet essential criteria before launch by
+        customizing your organizations pre-launch checklist.
+      </p>
       {experimentLaunchChecklist.map((item, i) => (
-        <div key={i}>{item}</div>
+        <ChecklistItem
+          key={i}
+          value={item}
+          index={i}
+          experimentLaunchChecklist={experimentLaunchChecklist}
+          setExperimentLaunchChecklist={setExperimentLaunchChecklist}
+        />
       ))}
       <button
-        onClick={(e) => {
+        className="btn btn-outline-primary"
+        onClick={async (e) => {
           e.preventDefault();
-          setExperimentLaunchChecklist([
-            ...experimentLaunchChecklist,
-            "Another one 1",
-          ]);
+          setExperimentLaunchChecklist([...experimentLaunchChecklist, ""]);
         }}
       >
-        Add another item
+        Add another checklist item
       </button>
     </Modal>
   );
