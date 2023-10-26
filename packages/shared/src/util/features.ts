f7d66b1dac4c007a305c1ff6a227c003b352b0c0
Jeremy Dorn <jeremy@jeremydorn.com>
2023-10-25 21:58:53 -0500

    Helper method to merge revisions and resolve conflicts

--- packages/shared/src/util/features.ts
+++ packages/shared/src/util/features.ts
@@ -1,9 +1,10 @@
 import Ajv from "ajv";
 import dJSON from "dirty-json";
 import stringify from "json-stringify-pretty-compact";
-import { FeatureInterface } from "back-end/types/feature";
+import { FeatureInterface, FeatureRule } from "back-end/types/feature";
 import { FeatureRevisionInterface } from "back-end/types/feature-revision";
 import cloneDeep from "lodash/cloneDeep";
+import isEqual from "lodash/isEqual";
 
 export function getValidation(feature: FeatureInterface) {
   try {
@@ -133,4 +134,138 @@ export function validateFeatureValue(
   }
 
   return value;
+}
+
+export interface MergeConflict {
+  field: string;
+  resolved: boolean;
+}
+export type MergeStrategy = "error" | "overwrite" | "discard";
+export type AutoMergeResult =
+  | {
+      success: true;
+      conflicts: MergeConflict[];
+      result: {
+        defaultValue?: string;
+        rules?: Record<string, FeatureRule[]>;
+      };
+    }
+  | {
+      success: false;
+      conflicts: MergeConflict[];
+    };
+
+export type RulesAndValues = Pick<
+  FeatureRevisionInterface,
+  "defaultValue" | "rules" | "version"
+>;
+
+export function autoMerge(
+  live: RulesAndValues,
+  base: RulesAndValues,
+  revision: RulesAndValues,
+  strategy: MergeStrategy
+): AutoMergeResult {
+  // If the base and feature have not diverged, no need to merge anything
+  if (live.version === base.version) {
+    return {
+      success: true,
+      result: {
+        defaultValue: revision.defaultValue,
+        rules: revision.rules,
+      },
+      conflicts: [],
+    };
+  }
+
+  const result: {
+    defaultValue?: string;
+    rules?: Record<string, FeatureRule[]>;
+  } = {};
+
+  const conflicts: MergeConflict[] = [];
+
+  // If the revision's defaultValue has been changed
+  if (revision.defaultValue !== base.defaultValue) {
+    // If there's a conflict with the live version
+    if (
+      live.defaultValue !== base.defaultValue &&
+      live.defaultValue !== revision.defaultValue
+    ) {
+      if (strategy === "overwrite") {
+        conflicts.push({
+          field: "defaultValue",
+          resolved: true,
+        });
+        result.defaultValue = revision.defaultValue;
+      } else if (strategy === "discard") {
+        conflicts.push({
+          field: "defaultValue",
+          resolved: true,
+        });
+        result.defaultValue = live.defaultValue;
+      } else if (strategy === "error") {
+        conflicts.push({
+          field: "defaultValue",
+          resolved: false,
+        });
+      }
+    }
+    // Otherwise, there's no conflict and it's safe to update
+    else {
+      result.defaultValue = revision.defaultValue;
+    }
+  }
+
+  // Check for conflicts in rules
+  Object.entries(revision.rules).forEach(([env, rules]) => {
+    // If the revision doesn't have changes in this environment, skip
+    if (isEqual(rules, base.rules[env])) return;
+
+    result.rules = result.rules || {};
+
+    // If there's a conflict
+    // TODO: be smarter about this - it's only really a conflict if the same rule is being changed in both
+    if (
+      env in live.rules &&
+      !isEqual(live.rules[env], base.rules[env]) &&
+      !isEqual(live.rules[env], rules)
+    ) {
+      if (strategy === "overwrite") {
+        conflicts.push({
+          field: `rules.${env}`,
+          resolved: true,
+        });
+        result.rules[env] = rules;
+      } else if (strategy === "discard") {
+        conflicts.push({
+          field: `rules.${env}`,
+          resolved: true,
+        });
+        result.rules[env] = live.rules[env];
+      } else if (strategy === "error") {
+        conflicts.push({
+          field: `rules.${env}`,
+          resolved: false,
+        });
+      }
+    }
+    // No conflict
+    else {
+      result.rules[env] = rules;
+    }
+  });
+
+  if (conflicts.some((c) => !c.resolved)) {
+    return {
+      success: false,
+      conflicts,
+    };
+  }
+
+  return {
+    success: true,
+    conflicts,
+    result,
+  };
 }
