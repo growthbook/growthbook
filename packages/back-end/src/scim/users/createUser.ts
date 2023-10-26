7f8eef1e99329705d507fb2cc46dbabd7c53da7f
Michael Knowlton <michael@growthbook.io>
2023-10-26 11:11:40 -0400

    Blocks users from provisioning or update a user with an invalid role.

--- packages/back-end/src/scim/users/createUser.ts
+++ packages/back-end/src/scim/users/createUser.ts
@@ -5,27 +5,40 @@ import {
   convertMemberToManagedByIdp,
   expandOrgMembers,
 } from "../../services/organizations";
-import { OrganizationInterface } from "../../../types/organization";
+import { OrganizationInterface, MemberRole } from "../../../types/organization";
 import { ScimError, ScimUser, ScimUserPostRequest } from "../../../types/scim";
 import {
   createUser as createNewUser,
   getUserByEmail,
 } from "../../services/users";
 
+export function isRoleValid(role: MemberRole) {
+  const validRoles: MemberRole[] = [
+    "readonly",
+    "collaborator",
+    "designer",
+    "analyst",
+    "developer",
+    "engineer",
+    "experimenter",
+    "admin",
+  ];
+  return validRoles.includes(role);
+}
+
 export async function createUser(
   req: ScimUserPostRequest,
   res: Response<ScimUser | ScimError>
 ) {
-  const { externalId, displayName, userName } = req.body;
+  const { externalId, displayName, userName, growthbookRole } = req.body;
 
   const org: OrganizationInterface = req.organization;
 
-  let role = org.settings?.defaultRole?.role || "readonly";
+  let role: MemberRole = org.settings?.defaultRole?.role || "readonly";
 
-  const scimDefinedRole = req.body.growthbookRole;
-
-  if (scimDefinedRole) {
-    role = scimDefinedRole;
+  if (growthbookRole && isRoleValid(growthbookRole)) {
+    // If a growthbookRole is provided, and it's a MemberRole, use that
+    role = growthbookRole;
   }
 
   const expandedMembers = await expandOrgMembers(org.members);
