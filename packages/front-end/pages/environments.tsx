0eb0d9281e032fa2bdf1910d4efef676645b3b09
bttf <2374625+bttf@users.noreply.github.com>
2023-11-30 16:20:48 -0500

    update images; trigger ci

--- packages/front-end/pages/environments.tsx
+++ packages/front-end/pages/environments.tsx
@@ -37,7 +37,7 @@ const EnvironmentsPage: FC = () => {
       )}
       <div className="row align-items-center mb-1">
         <div className="col-auto">
-          <h1 className="mb-0">Environments (test 123)</h1>
+          <h1 className="mb-0">Environments (test)</h1>
         </div>
         {canCreate && (
           <div className="col-auto ml-auto">
