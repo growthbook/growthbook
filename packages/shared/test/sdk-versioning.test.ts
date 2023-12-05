7380ddf3f4a0222b72bef8bbbf80c350263210c8
Bryce <bryce1@gmail.com>
2023-12-05 14:50:07 -0800

    clean up scrubbing, add tests

--- packages/shared/test/sdk-versioning.test.ts
+++ packages/shared/test/sdk-versioning.test.ts
@@ -1,5 +1,10 @@
 import { SDKConnectionInterface } from "back-end/types/sdk-connection";
-import { getConnectionSDKCapabilities } from "../src/sdk-versioning";
+import cloneDeep from "lodash/cloneDeep";
+import {
+  getConnectionSDKCapabilities,
+  scrubExperiments,
+  scrubFeatures,
+} from "../src/sdk-versioning";
 
 const baseConnection: SDKConnectionInterface = {
   id: "sdk-123",
@@ -60,5 +65,190 @@ describe("getConnectionSDKCapabilities", () => {
     };
     const capabilities = getConnectionSDKCapabilities(connection); // should be empty due to Python 0.0.0 having nothing
     expect(capabilities).toStrictEqual(["bucketingV2"]);
+  });
+});
+
+describe("payload scrubbing", () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const sdkPayload: any = {
+    features: {
+      exp1: {
+        defaultValue: "control",
+        rules: [
+          {
+            key: "feature-exp",
+            seed: "feature-exp",
+            hashAttribute: "id",
+            fallbackAttribute: "deviceId",
+            hashVersion: 2,
+            bucketVersion: 1,
+            condition: { country: "USA" },
+            variations: ["control", "red", "blue"],
+            coverage: 1,
+            weights: [0.3334, 0.3333, 0.3333],
+            phase: "0",
+          },
+        ],
+      },
+    },
+    experiments: [
+      {
+        key: "my-experiment",
+        seed: "s1",
+        hashAttribute: "id",
+        fallbackAttribute: "anonymousId",
+        hashVersion: 2,
+        bucketVersion: 1,
+        stickyBucketing: true,
+        manual: true,
+        variations: [
+          {},
+          {
+            domMutations: [
+              {
+                selector: "h1",
+                action: "set",
+                attribute: "html",
+                value: "red",
+              },
+            ],
+          },
+          {
+            domMutations: [
+              {
+                selector: "h1",
+                action: "set",
+                attribute: "html",
+                value: "blue",
+              },
+            ],
+          },
+        ],
+        weights: [0.3334, 0.3333, 0.3333],
+        coverage: 1,
+      },
+    ],
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const scrubbedPayload: any = {
+    features: {
+      exp1: {
+        defaultValue: "control",
+        rules: [
+          {
+            key: "feature-exp",
+            seed: "feature-exp",
+            hashAttribute: "id",
+            // fallbackAttribute: "deviceId",
+            hashVersion: 2,
+            // bucketVersion: 1,
+            condition: { country: "USA" },
+            variations: ["control", "red", "blue"],
+            coverage: 1,
+            weights: [0.3334, 0.3333, 0.3333],
+            phase: "0",
+          },
+        ],
+      },
+    },
+    experiments: [
+      {
+        key: "my-experiment",
+        seed: "s1",
+        hashAttribute: "id",
+        // fallbackAttribute: "anonymousId",
+        hashVersion: 2,
+        // bucketVersion: 1,
+        // stickyBucketing: true,
+        // manual: true,
+        variations: [
+          {},
+          {
+            domMutations: [
+              {
+                selector: "h1",
+                action: "set",
+                attribute: "html",
+                value: "red",
+              },
+            ],
+          },
+          {
+            domMutations: [
+              {
+                selector: "h1",
+                action: "set",
+                attribute: "html",
+                value: "blue",
+              },
+            ],
+          },
+        ],
+        weights: [0.3334, 0.3333, 0.3333],
+        coverage: 1,
+      },
+    ],
+  };
+
+  it("does not scrub the payload for a safe language version", () => {
+    const connection: SDKConnectionInterface = {
+      ...baseConnection,
+    };
+    const capabilities = getConnectionSDKCapabilities(connection);
+
+    const scrubbed = cloneDeep(sdkPayload);
+    const scrubbedFeatures = scrubFeatures(scrubbed.features, capabilities);
+    const scrubbedExperiments = scrubExperiments(
+      scrubbed.experiments,
+      capabilities
+    );
+    scrubbed.features = scrubbedFeatures;
+    scrubbed.experiments = scrubbedExperiments;
+
+    // no change to payload for default connection (javascript, 0.27.0)
+    expect(scrubbed).toStrictEqual(sdkPayload);
+  });
+
+  it("scrubs the payload for a risky language version", () => {
+    const connection: SDKConnectionInterface = {
+      ...baseConnection,
+      languages: ["python"],
+      sdkVersion: "0.0.0",
+    };
+    const capabilities = getConnectionSDKCapabilities(connection);
+
+    const scrubbed = cloneDeep(sdkPayload);
+    const scrubbedFeatures = scrubFeatures(scrubbed.features, capabilities);
+    const scrubbedExperiments = scrubExperiments(
+      scrubbed.experiments,
+      capabilities
+    );
+    scrubbed.features = scrubbedFeatures;
+    scrubbed.experiments = scrubbedExperiments;
+
+    // no change to payload for default connection (javascript, 0.27.0)
+    expect(scrubbed).toStrictEqual(scrubbedPayload);
+  });
+
+  it("scrubs as necessary for multi-language SDKs", () => {
+    const connection: SDKConnectionInterface = {
+      ...baseConnection,
+      languages: ["python", "php", "javascript"],
+      sdkVersion: undefined,
+    };
+    const capabilities = getConnectionSDKCapabilities(connection);
+
+    const scrubbed = cloneDeep(sdkPayload);
+    const scrubbedFeatures = scrubFeatures(scrubbed.features, capabilities);
+    const scrubbedExperiments = scrubExperiments(
+      scrubbed.experiments,
+      capabilities
+    );
+    scrubbed.features = scrubbedFeatures;
+    scrubbed.experiments = scrubbedExperiments;
+
+    // no change to payload for default connection (javascript, 0.27.0)
+    expect(scrubbed).toStrictEqual(scrubbedPayload);
   });
 });
