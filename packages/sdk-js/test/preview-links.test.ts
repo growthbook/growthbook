25737ef44876aa41bb130a20c737b794de499f47
bttf <2374625+bttf@users.noreply.github.com>
2023-12-13 14:51:44 -0800

    correct tests

--- packages/sdk-js/test/preview-links.test.ts
+++ packages/sdk-js/test/preview-links.test.ts
@@ -13,7 +13,7 @@ describe("preview links", () => {
     window.location.href = "http://app.customer.com";
   });
 
-  describe("when experiment query param override is set", () => {
+  describe("when experiment is inactive and query param override is set", () => {
     it("should consider the client in the experiment", () => {
       window.location.href = "http://app.customer.com?exp-01=1";
       const context: Context = { user: { id: "1" } };
@@ -21,8 +21,15 @@ describe("preview links", () => {
       expect(
         growthbook.run({
           key: "exp-01",
+          active: false,
           variations: [0, 1],
-          url: /^\/path/,
+          urlPatterns: [
+            {
+              include: true,
+              pattern: "http://app.customer.com",
+              type: "simple",
+            },
+          ],
         }).inExperiment
       ).toEqual(true);
 
@@ -30,11 +37,19 @@ describe("preview links", () => {
       expect(
         growthbook.run({
           key: "exp-01",
+          active: false,
           variations: [0, 1],
-          url: /^\/path/,
+          urlPatterns: [
+            {
+              include: true,
+              pattern: "http://app.customer.com",
+              type: "simple",
+            },
+          ],
         }).inExperiment
       ).toEqual(false);
     });
+
     it("should match the variation index with the index supplied via query param", () => {
       window.location.href = "http://app.customer.com?exp-01=1";
       const context: Context = { user: { id: "1" } };
@@ -42,8 +57,15 @@ describe("preview links", () => {
       expect(
         growthbook.run({
           key: "exp-01",
+          active: false,
           variations: [0, 1, 2, 3],
-          url: /^\/path/,
+          urlPatterns: [
+            {
+              include: true,
+              pattern: "http://app.customer.com",
+              type: "simple",
+            },
+          ],
         }).variationId
       ).toEqual(1);
 
@@ -51,8 +73,15 @@ describe("preview links", () => {
       expect(
         growthbook.run({
           key: "exp-01",
+          active: false,
           variations: [0, 1, 2, 3],
-          url: /^\/path/,
+          urlPatterns: [
+            {
+              include: true,
+              pattern: "http://app.customer.com",
+              type: "simple",
+            },
+          ],
         }).variationId
       ).toEqual(3);
     });
