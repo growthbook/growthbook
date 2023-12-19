aa0207fb3c8e3ae1f9e2486a929c14d32d7ed445
Jeremy Dorn <jeremy@jeremydorn.com>
2023-12-19 08:28:05 -0600

    Batch calls to the stats engine to improve performance (#1823)
    Co-authored-by: Luke Sonnet <luke.sonnet@gmail.com>

--- packages/back-end/types/stats.d.ts
+++ packages/back-end/types/stats.d.ts
@@ -48,8 +48,11 @@ type StatsEngineDimensionResponse =
   | BayesianDimensionResponse
   | FrequentistVariationResponse;
 
-export interface ExperimentMetricAnalysis {
-  unknownVariations: string[];
-  multipleExposures: number;
-  dimensions: StatsEngineDimensionResponse[];
-}
+export type ExperimentMetricAnalysis = {
+  metric: string;
+  analyses: {
+    unknownVariations: string[];
+    multipleExposures: number;
+    dimensions: StatsEngineDimensionResponse[];
+  }[];
+}[];
