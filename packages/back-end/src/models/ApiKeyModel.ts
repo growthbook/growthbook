3ae8d837271ed3487168d4d7b87b99af2791fac7
bttf <2374625+bttf@users.noreply.github.com>
2024-01-31 09:02:08 -0600

    Revert "No longer tie visual editor keys with a user"
    This reverts commit e74b30bb3780c71155de384ff5906325e44c6a0d.

--- packages/back-end/src/models/ApiKeyModel.ts
+++ packages/back-end/src/models/ApiKeyModel.ts
@@ -134,14 +134,17 @@ export async function createUserPersonalAccessApiKey({
 }
 
 export async function createUserVisualEditorApiKey({
+  userId,
   organizationId,
   description,
 }: {
+  userId: string;
   organizationId: string;
   description: string;
 }): Promise<ApiKeyInterface> {
   return await createApiKey({
     organization: organizationId,
+    userId,
     secret: true,
     environment: "",
     project: "",
