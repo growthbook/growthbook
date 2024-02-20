7a9ebecf4439a5ed5a0c61d1c502e8325eb0acb8
Michael Knowlton <michael@growthbook.io>
2024-02-20 09:13:32 -0500

    Fixes failing test and adds specific test case for new logic.

--- packages/front-end/services/features.ts
+++ packages/front-end/services/features.ts
@@ -600,7 +600,7 @@ export function jsonToConds(
         const v = value[operator];
 
         if (operator === "$in" || operator === "$nin") {
-          if (v.some((str) => str.includes(","))) {
+          if (v.some((str) => typeof str === "string" && str.includes(","))) {
             valid = false;
             return;
           }
