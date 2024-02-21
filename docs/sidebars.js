534346394b7fb11eb8a42adc929dcf21008bc4a2
Jeremy Dorn <jeremy@jeremydorn.com>
2024-02-20 20:52:24 -0600

    New docs page on self-hosting GrowthBook in production (#2145)

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -360,7 +360,11 @@ const sidebars = {
         type: "doc",
         id: "self-host/index",
       },
-      items: ["self-host/environment-variables", "self-host/config-yml"],
+      items: [
+        "self-host/environment-variables",
+        "self-host/config-yml",
+        "self-host/production",
+      ],
     },
     /*
         {
