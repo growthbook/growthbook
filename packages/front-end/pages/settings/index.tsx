37374900790ccea174cefaff3a9f6edb13b6a863
bttf <2374625+bttf@users.noreply.github.com>
2024-02-26 14:44:56 -0500

    Break out config.yml ettings

--- packages/front-end/pages/settings/index.tsx
+++ packages/front-end/pages/settings/index.tsx
@@ -23,14 +23,11 @@ import { useGrowthBook } from "@growthbook/growthbook-react";
 import { MdInfoOutline } from "react-icons/md";
 import { getConnectionsSDKCapabilities } from "shared/sdk-versioning";
 import { useAuth } from "@/services/auth";
-import BackupConfigYamlButton from "@/components/Settings/BackupConfigYamlButton";
-import RestoreConfigYamlButton from "@/components/Settings/RestoreConfigYamlButton";
 import { hasFileConfig, isCloud } from "@/services/env";
 import Field from "@/components/Forms/Field";
 import MetricsSelector from "@/components/Experiment/MetricsSelector";
 import TempMessage from "@/components/TempMessage";
 import Button from "@/components/Button";
-import { DocLink } from "@/components/DocLink";
 import {
   OrganizationSettingsWithMetricDefaults,
   useOrganizationMetricDefaults,
@@ -57,6 +54,7 @@ import useSDKConnections from "@/hooks/useSDKConnections";
 import Tooltip from "@/components/Tooltip/Tooltip";
 import { supportedCurrencies } from "@/services/settings";
 import OrganizationAndLicenseSettings from "@/components/GeneralSettings/OrganizationAndLicenseSettings";
+import ConfigYmlSettings from "@/components/GeneralSettings/ConfigYmlSettings";
 
 export const DEFAULT_SRM_THRESHOLD = 0.001;
 
@@ -429,57 +427,12 @@ const GeneralSettingsPage = (): React.ReactElement => {
             refreshOrg={refreshOrganization}
           />
 
-          {hasFileConfig() && (
-            <div className="alert alert-info my-3">
-              The below settings are controlled through your{" "}
-              <code>config.yml</code> file and cannot be changed through the web
-              UI.{" "}
-              <DocLink
-                docSection="config_organization_settings"
-                className="font-weight-bold"
-              >
-                View Documentation
-              </DocLink>
-              .
-            </div>
-          )}
-
-          {!hasFileConfig() && (
-            <div className="alert alert-info my-3">
-              <h3>Import/Export config.yml</h3>
-              <p>
-                {isCloud()
-                  ? "GrowthBook Cloud stores"
-                  : "You are currently storing"}{" "}
-                all organization settings, data sources, metrics, and dimensions
-                in a database.
-              </p>
-              <p>
-                You can import/export these settings to a{" "}
-                <code>config.yml</code> file to more easily move between
-                GrowthBook Cloud accounts and/or self-hosted environments.{" "}
-                <DocLink docSection="config_yml" className="font-weight-bold">
-                  Learn More
-                </DocLink>
-              </p>
-              <div className="row mb-3">
-                <div className="col-auto">
-                  <BackupConfigYamlButton settings={settings} />
-                </div>
-                <div className="col-auto">
-                  <RestoreConfigYamlButton
-                    settings={settings}
-                    mutate={refreshOrganization}
-                  />
-                </div>
-              </div>
-              <div className="text-muted">
-                <strong>Note:</strong> For security reasons, the exported file
-                does not include data source connection secrets such as
-                passwords. You must edit the file and add these yourself.
-              </div>
-            </div>
-          )}
+          <ConfigYmlSettings
+            hasFileConfig={hasFileConfig()}
+            isCloud={isCloud()}
+            settings={settings}
+            refreshOrg={refreshOrganization}
+          />
 
           {growthbook?.getFeatureValue("import-from-x", false) && (
             <div className="bg-white p-3 border position-relative my-3">
