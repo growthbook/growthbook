0c9205e03fc20869ae3ce337ebb00e3c01b28891
Bryce <bryce1@gmail.com>
2024-03-04 15:46:02 -0800

    csp and header update to disallow framing

--- packages/front-end/next.config.js
+++ packages/front-end/next.config.js
@@ -1,3 +1,7 @@
+const cspHeader = `
+    frame-ancestors 'none';
+`;
+
 module.exports = {
   // We already run eslint and typescript in CI/CD
   // Disable here to speed up production builds
@@ -7,4 +11,19 @@ module.exports = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  headers: () => ([
+    {
+      source: '/(.*)',
+      headers: [
+        {
+          key: 'Content-Security-Policy',
+          value: cspHeader.replace(/\n/g, ''),
+        },
+        {
+          key: 'X-Frame-Options',
+          value: 'deny',
+        },
+      ],
+    },
+  ]),
 };
