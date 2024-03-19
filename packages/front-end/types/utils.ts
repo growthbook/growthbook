00c3066294229932ec02d9dc26a5c16e037e5936
Romain Beauxis <romain@growthbook.io>
2024-03-19 14:29:06 -0500

    Cleanup risk code.

--- packages/front-end/types/utils.ts
+++ packages/front-end/types/utils.ts
@@ -3,3 +3,8 @@ export function ensure<T>(x: T): asserts x is NonNullable<T> {
     throw new TypeError("Internal error value should not be undefined");
   }
 }
+
+export function ensureAndReturn<T>(x: T): NonNullable<T> {
+  ensure(x);
+  return x;
+}
