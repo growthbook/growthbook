938ba87f7e1f7ee80f20de8635f71adfed80adaa
Guy Langford-Lee <guy@growthbook.io>
2024-03-26 16:11:13 -0700

    test

--- packages/back-end/src/api/features/updateFeature.ts
+++ packages/back-end/src/api/features/updateFeature.ts
@@ -146,7 +146,6 @@ export const updateFeature = createApiRequestHandler(updateFeatureValidator)(
           environments: orgEnvs,
           publish: true,
           changes: revisionChanges,
-          org,
         });
         updates.version = revision.version;
       }
