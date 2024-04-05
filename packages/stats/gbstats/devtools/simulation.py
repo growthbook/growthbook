53f4248396eece3ca7dd547e71f2da162d74e69a
Luke Sonnet <luke.sonnet@gmail.com>
2024-04-05 14:48:40 -0700

    Update sim study approach

--- packages/stats/gbstats/devtools/simulation.py
+++ packages/stats/gbstats/devtools/simulation.py
@@ -1,98 +1,95 @@
+from abc import ABC, abstractmethod
+from typing import Dict, Mapping, Tuple, Type
+
 import numpy as np
-from pydantic.dataclasses import dataclass
-from scipy.stats import norm
-from typing import cast
+
+from gbstats.models.statistics import TestStatistic
+from gbstats.models.tests import BaseABTest, BaseConfig
 
 ##############################################
 # this file is used for internal testing only.
 # no methods from this file should be exported.
 ###############################################
-# Doesnt it seem like you should pass in some estimator class
-# that always returns a point estimate and a standard error,
-# and more or less everything else is handled by this simulation engine?
-
-# You basically need to have fixed interfaces across classes
-# (input: Statistic output: se and mean ). Which we kind of have.
 
 
-@dataclass
-class Estimator:
-    n_sim: int
-    alpha: float = 0.05
-
-    def create_arrays(self):
-        array_shape = (self.n_sim,)
-        self.pt = np.empty(array_shape)
-        self.se = np.empty(array_shape)
-        self.theta = np.empty(array_shape)
-
-    @property
-    def multiplier(self):
-        return norm.ppf(1.0 - 0.5 * self.alpha, loc=0, scale=1)
-
-    @property
-    def lower_limit(self) -> np.ndarray:
-        return self.pt - self.se * self.multiplier
-
-    @property
-    def upper_limit(self) -> np.ndarray:
-        return self.pt + self.se * self.multiplier
-
-    @property
-    def coverage(self) -> float:
-        """computes coverage."""
-        return cast(
-            float,
-            np.mean(
-                (self.lower_limit <= self.theta) * (self.upper_limit >= self.theta)
-            ),
-        )
-
-    @property
-    def reject(self):
-        zero_in_interval = (self.lower_limit < 0.0) * (self.upper_limit > 0.0)
-        return 1.0 - np.mean(zero_in_interval)
-
-    @property
-    def mse(self):
-        return np.mean((self.pt - self.theta) ** 2, axis=0)
-
-    @property
-    def bias(self):
-        return np.mean(self.pt - self.theta)
-
-    @property
-    def variance(self):
-        return np.var(self.pt)
-
-
-@dataclass
-class SimulationStudy:
-    n_sim: int = 100
-    alpha: float = 0.05
-    seed: int = 20240401
+class SimulationStudy(ABC):
+    def __init__(
+        self,
+        test_dict: Mapping[str, Tuple[Type[BaseABTest], BaseConfig]],
+        data_params: Dict,
+        seed: int,
+        n_sim: int = 100,
+        alpha: float = 0.05,
+    ):
+        self.n_sim = n_sim
+        self.alpha = alpha
+        self.seed = seed
+        self.test_names = list(test_dict.keys())
+        self.tests = list([v[0] for v in test_dict.values()])
+        self.configs = list([v[1] for v in test_dict.values()])
+        self.data_params = data_params
+        self.create_storage_arrays()
 
     def run_sim(self):
-        self.create_storage_arrays()
-        self.run_all_iterations()
-
-    def run_all_iterations(self):
         for i in range(self.n_sim):
             self.run_iteration(i)
 
     def run_iteration(self, i):
         np.random.seed(self.seed + i)
-        self.generate_data()
+        stat_a, stat_b, estimand = self.generate_data()
+        for j, test in enumerate(self.tests):
+            t = test(stat_a, stat_b, self.configs[j])
+            test_result = t.compute_result()
+            self.pt[i, j] = test_result.uplift.mean
+            self.se[i, j] = test_result.uplift.stddev
+            self.lower_limit[i, j] = test_result.ci[0]
+            self.upper_limit[i, j] = test_result.ci[1]
+            self.theta[i, j] = estimand
 
     def create_storage_arrays(self):
-        pass
+        array_shape = (self.n_sim, len(self.tests))
+        self.pt = np.empty(array_shape)
+        self.se = np.empty(array_shape)
+        self.theta = np.empty(array_shape)
+        self.lower_limit = np.empty(array_shape)
+        self.upper_limit = np.empty(array_shape)
 
-    def generate_data(self):
+    @abstractmethod
+    def generate_data(self) -> Tuple[TestStatistic, TestStatistic, float]:
         pass
-
-    def get_standard_error_from_interval(self, lower, upper):
-        return (upper - lower) / (2 * self.multiplier)
 
     @property
-    def multiplier(self):
-        return norm.ppf(1.0 - 0.5 * self.alpha, loc=0, scale=1)
+    def coverage(self):
+        """computes coverage."""
+        return [
+            np.mean(
+                (self.lower_limit[:, j] <= self.theta[:, j])
+                * (self.upper_limit[:, j] >= self.theta[:, j])
+            )
+            for j in range(len(self.tests))
+        ]
+
+    @property
+    def reject(self):
+        return [
+            1.0
+            - np.mean((self.lower_limit[:, j] < 0.0) * (self.upper_limit[:, j] > 0.0))
+            for j in range(len(self.tests))
+        ]
+
+    @property
+    def mse(self):
+        return [
+            np.mean((self.pt[:, j] - self.theta[:, j]) ** 2)
+            for j in range(len(self.tests))
+        ]
+
+    @property
+    def bias(self):
+        return [
+            np.mean(self.pt[:, j] - self.theta[:, j]) for j in range(len(self.tests))
+        ]
+
+    @property
+    def variance(self):
+        return [np.var(self.pt[:, j]) for j in range(len(self.tests))]
