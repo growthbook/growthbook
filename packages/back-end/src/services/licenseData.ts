760a7242accb88fe769b7016219604080722f259
James Irwin <tallnerd@gmail.com>
2024-04-18 18:02:36 +0200

    Add and AgendaJob that will refresh the license once a day

--- packages/back-end/src/services/licenseData.ts
+++ packages/back-end/src/services/licenseData.ts
@@ -7,10 +7,7 @@ import { getInstallationId } from "../models/InstallationModel";
 import { IS_CLOUD } from "../util/secrets";
 import { getInstallationDatasources } from "../models/DataSourceModel";
 import { OrganizationInterface } from "../../types/organization";
-import {
-  getAllInviteEmailsInDb,
-  getOrganization,
-} from "../models/OrganizationModel";
+import { getAllInviteEmailsInDb } from "../models/OrganizationModel";
 import { UserModel } from "../models/UserModel";
 import { getUsersByIds } from "./users";
 
@@ -64,24 +61,9 @@ export async function getLicenseMetaData() {
 }
 
 export async function initializeLicenseForOrg(
-  reqOrg?: OrganizationInterface,
+  org: OrganizationInterface,
   forceRefresh = false
 ) {
-  let org = reqOrg;
-  if (!org) {
-    // When we initialize the app, we don't have a request with an org yet.
-    if (IS_CLOUD) {
-      // On cloud we will initialize license on an as need basis once we know the org of a request
-      return;
-    }
-
-    // For self-hosted there is either
-    // no orgs upon first initialization - in which case we get the key from the env var
-    // exactly one org normally - in which case we get the key from the org
-    // or multiple orgs if it is a MULTI_ORG site - in which case the org is not allowed to have a license key and we shall also fall back to the env var
-    org = (await getOrganization()) || undefined;
-  }
-
   const key = org?.licenseKey || process.env.LICENSE_KEY;
 
   if (!key) {
