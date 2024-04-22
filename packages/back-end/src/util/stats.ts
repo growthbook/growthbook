b13202d5bdd9fc3214a777d4833167f42d337cea
Romain Beauxis <romain@growthbook.io>
2024-04-22 11:17:46 -0500

    Bring this back.

---
+++ packages/back-end/src/util/stats.ts
@@ -0,0 +1,51 @@
+import chisquare from "@stdlib/stats/base/dists/chisquare";
+
+export function checkSrm(users: number[], weights: number[]) {
+  // Skip variations with weight=0 or users=0
+  const data: [number, number][] = [];
+  let totalUsers = 0;
+  let totalWeight = 0;
+  for (let i = 0; i < weights.length; i++) {
+    if (!weights[i] || !users[i]) continue;
+    data.push([users[i], weights[i]]);
+    totalUsers += users[i];
+    totalWeight += weights[i];
+  }
+
+  // Skip SRM calculation if there aren't enough valid variations
+  if (data.length < 2) {
+    return 1;
+  }
+
+  // Calculate and return SRM p-value using a ChiSquare test
+  let x = 0;
+  data.forEach(([o, e]) => {
+    e = (e / totalWeight) * totalUsers;
+    x += Math.pow(o - e, 2) / e;
+  });
+  return 1 - chisquare.cdf(x, data.length - 1);
+}
+
+function returnZeroIfNotFinite(x: number): number {
+  if (isFinite(x)) {
+    return x;
+  }
+  return 0;
+}
+
+export function sumSquaresFromStats(
+  sum: number,
+  variance: number,
+  n: number
+): number {
+  return returnZeroIfNotFinite(variance * (n - 1) + Math.pow(sum, 2) / n);
+}
+
+export function meanVarianceFromSums(
+  sum: number,
+  sum_squares: number,
+  n: number
+): number {
+  const variance = (sum_squares - Math.pow(sum, 2) / n) / (n - 1);
+  return returnZeroIfNotFinite(variance);
+}
