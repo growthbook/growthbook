dfdaca0044a089f3230490e56cb7747b333bc642
Mikl Kurkov <mkurkov@gmail.com>
2024-04-22 22:19:28 +0300

    Add Elixir SDK docs

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -371,6 +371,7 @@ const sidebars = {
         { type: "doc", id: "lib/kotlin", label: "Kotlin (Android)" },
         { type: "doc", id: "lib/flutter", label: "Flutter" },
         { type: "doc", id: "lib/swift", label: "Swift (iOS)" },
+        { type: "doc", id: "lib/elixir", label: "Elixir", className: "pill-new" },
         { type: "doc", id: "lib/build-your-own", label: "Build Your Own" },
       ],
     },
