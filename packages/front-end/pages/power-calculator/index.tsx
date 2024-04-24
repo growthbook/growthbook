84f7757b23784b5a63d0fbee8fff6ab3ca303137
Romain Beauxis <romain@growthbook.io>
2024-04-24 10:39:40 -0500

    Persist calculation in local storage.

--- packages/front-end/pages/power-calculator/index.tsx
+++ packages/front-end/pages/power-calculator/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useMemo, useState } from "react";
+import { useCallback, useMemo, useState, useEffect } from "react";
 import { DEFAULT_SEQUENTIAL_TESTING_TUNING_PARAMETER } from "shared/constants";
 import PowerCalculationSettingsModal from "@/components/PowerCalculation/PowerCalculationSettingsModal";
 import EmptyPowerCalculation from "@/components/PowerCalculation/EmptyPowerCalculation";
@@ -17,18 +17,66 @@ import {
 import { powerMetricWeeks } from "@/components/PowerCalculation/stats";
 
 const WEEKS = 9;
-
 const INITIAL_FORM_PARAMS = { metrics: {} } as const;
+const LOCAL_STORAGE_KEY = "GROWTHBOOK_POWER_CALCULATION";
+
+type PageSettings = {
+  powerCalculationParams?: FullModalPowerCalculationParams;
+  settingsModalParams: PartialPowerCalculationParams;
+  variations: number;
+  statsEngine?: StatsEngine;
+};
+
+const INITIAL_PAGE_SETTINGS: PageSettings = {
+  settingsModalParams: INITIAL_FORM_PARAMS,
+  variations: 2,
+};
 
 const PowerCalculationPage = (): React.ReactElement => {
+  const orgSettings = useOrgSettings();
+
+  const initialJSONParams = localStorage.getItem(LOCAL_STORAGE_KEY);
+
+  const initialParams: PageSettings = initialJSONParams
+    ? JSON.parse(initialJSONParams)
+    : INITIAL_PAGE_SETTINGS;
+
   const [showModal, setShowModal] = useState(false);
 
   const [powerCalculationParams, setProcessedPowerCalculationParams] = useState<
     FullModalPowerCalculationParams | undefined
-  >();
+  >(initialParams.powerCalculationParams);
 
-  const [settingsModalParams, setSettingsModalParams] =
-    useState<PartialPowerCalculationParams>(INITIAL_FORM_PARAMS);
+  const [
+    settingsModalParams,
+    setSettingsModalParams,
+  ] = useState<PartialPowerCalculationParams>(
+    initialParams.settingsModalParams
+  );
+
+  const [variations, setVariations] = useState(initialParams.variations);
+
+  const [statsEngine, setStatsEngine] = useState<StatsEngine>(
+    initialParams.statsEngine || {
+      type: "frequentist",
+      sequentialTesting: orgSettings.sequentialTestingEnabled
+        ? orgSettings.sequentialTestingTuningParameter ||
+          DEFAULT_SEQUENTIAL_TESTING_TUNING_PARAMETER
+        : false,
+    }
+  );
+
+  useEffect(() => {
+    localStorage.setItem(
+      LOCAL_STORAGE_KEY,
+      JSON.stringify({
+        powerCalculationParams,
+        settingsModalParams,
+        variations,
+        statsEngine,
+      })
+    );
+  }, [powerCalculationParams, settingsModalParams, variations, statsEngine]);
 
   const setPowerCalculationParams = useCallback(
     (p: FullModalPowerCalculationParams | undefined) => {
@@ -49,27 +97,15 @@ const PowerCalculationPage = (): React.ReactElement => {
                   ? p.metrics[key][entry] / 100
                   : p.metrics[key][entry],
               }),
-              {},
+              {}
             ),
           }),
-          {},
+          {}
         ),
       });
     },
-    [setProcessedPowerCalculationParams],
+    [setProcessedPowerCalculationParams]
   );
-
-  const [variations, setVariations] = useState(2);
-
-  const orgSettings = useOrgSettings();
-
-  const [statsEngine, setStatsEngine] = useState<StatsEngine>({
-    type: "frequentist",
-    sequentialTesting: orgSettings.sequentialTestingEnabled
-      ? orgSettings.sequentialTestingTuningParameter ||
-        DEFAULT_SEQUENTIAL_TESTING_TUNING_PARAMETER
-      : false,
-  });
 
   const finalParams: PowerCalculationParams | undefined = useMemo(() => {
     if (!powerCalculationParams) return;
@@ -91,7 +127,7 @@ const PowerCalculationPage = (): React.ReactElement => {
   }, [finalParams]);
 
   return (
-    <>
+    <div className="contents container-fluid pagecontents">
       {showModal && (
         <PowerCalculationSettingsModal
           close={() => setShowModal(false)}
@@ -122,7 +158,7 @@ const PowerCalculationPage = (): React.ReactElement => {
           }}
         />
       )}
-    </>
+    </div>
   );
 };
 
