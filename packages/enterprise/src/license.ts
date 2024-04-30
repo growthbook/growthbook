f3130d5fa22a9bdb21da015acee5daa7662db15a
James Irwin <tallnerd@gmail.com>
2024-04-30 16:42:37 +0200

    Throw an error if force refresh fails (#2451)

--- packages/enterprise/src/license.ts
+++ packages/enterprise/src/license.ts
@@ -655,6 +655,7 @@ async function updateLicenseFromServer(
     license.lastServerErrorMessage = e.message;
     license.usingMongoCache = true;
     verifyAndSetServerLicenseData(license);
+    throw e;
   }
   return license;
 }
