af60defb7400dd8c6253bbee9e92c939a5db9a4d
James Irwin <tallnerd@gmail.com>
2024-05-02 16:50:12 +0200

    With next.config.js

--- packages/front-end/next.config.js
+++ packages/front-end/next.config.js
@@ -1,3 +1,19 @@
+const fs = require("fs");
+const path = require("path");
+
+const rootPath = path.join(__dirname, "..", "..");
+
+let gitSha = "";
+let gitCommitDate = "";
+if (fs.existsSync(path.join(rootPath, "buildinfo", "SHA"))) {
+  gitSha = fs.readFileSync(path.join(rootPath, "buildinfo", "SHA")).toString();
+}
+if (fs.existsSync(path.join(rootPath, "buildinfo", "DATE"))) {
+  gitCommitDate = fs
+    .readFileSync(path.join(rootPath, "buildinfo", "DATE"))
+    .toString();
+}
+
 const cspHeader = `
     frame-ancestors 'none';
 `;
@@ -5,6 +21,9 @@ const cspHeader = `
 module.exports = {
   // We already run eslint and typescript in CI/CD
   // Disable here to speed up production builds
+  assetPrefix: process.env.IS_CLOUD
+    ? `https://growthbook-cloud-static-files.s3.amazonaws.com/${gitCommitDate}/${gitSha}/`
+    : "",
   eslint: {
     ignoreDuringBuilds: true,
   },
