13ee5397968732b36d538dba5bbf44fc2439b404
Michael Knowlton <michael@growthbook.io>
2024-05-07 08:13:28 -0400

    Pushes up prettier changes that passed locally

--- packages/front-end/pages/sdks/SdkWebhooks.tsx
+++ packages/front-end/pages/sdks/SdkWebhooks.tsx
@@ -20,7 +20,8 @@ export default function SdkWebhooks({ sdkid }) {
     `/webhooks/sdk/${sdkid}`
   );
   const [
-    createWebhookModalOpen, setCreateWebhookModalOpen
+    createWebhookModalOpen,
+    setCreateWebhookModalOpen,
   ] = useState<null | Partial<WebhookInterface>>(null);
   const { apiCall } = useAuth();
   const permissionsUtil = usePermissionsUtil();
