07770a8e7e34361481b61816f674bd1c2b7a47e8
Michael Knowlton <michael@growthbook.io>
2024-05-16 09:17:49 -0400

    Bug - Adds permission check to event webhook id page before we try to fetch the details

--- packages/front-end/pages/settings/webhooks/event/[eventwebhookid].tsx
+++ packages/front-end/pages/settings/webhooks/event/[eventwebhookid].tsx
@@ -2,8 +2,20 @@ import React from "react";
 import { NextPage } from "next";
 import { EventWebHookDetailContainer } from "@/components/EventWebHooks/EventWebHookDetail/EventWebHookDetail";
 import { EventWebHookLogsContainer } from "@/components/EventWebHooks/EventWebHookLogs/EventWebHookLogs";
+import usePermissionsUtil from "@/hooks/usePermissionsUtils";
 
 const EventWebHookDetailPage: NextPage = () => {
+  const permissionsUtil = usePermissionsUtil();
+
+  if (!permissionsUtil.canViewEventWebhook()) {
+    return (
+      <div className="container pagecontents">
+        <div className="alert alert-danger">
+          You do not have permission to view this page.
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="container pagecontents">
       <EventWebHookDetailContainer />
