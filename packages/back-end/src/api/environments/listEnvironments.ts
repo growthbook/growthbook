3325dd46001fd10ebb840b2cb6448594b98835aa
Romain Beauxis <romain@growthbook.io>
2024-05-24 09:52:11 -0500

    Use request org.

--- packages/back-end/src/api/environments/listEnvironments.ts
+++ packages/back-end/src/api/environments/listEnvironments.ts
@@ -1,21 +1,13 @@
 import { ListEnvironmentsResponse } from "../../../types/openapi";
 import { createApiRequestHandler } from "../../util/handler";
-import { findOrganizationById } from "../../models/OrganizationModel";
 import { listEnvironmentsValidator } from "../../validators/openapi";
 
 export const listEnvironments = createApiRequestHandler(
   listEnvironmentsValidator
 )(
   async (req): Promise<ListEnvironmentsResponse> => {
-    const id = req.params.id;
-
-    const org = await findOrganizationById(id);
-    if (!org) {
-      throw Error("Organization not found");
-    }
-
     const environments = (
-      org.settings?.environments || []
+      req.context.org.settings?.environments || []
     ).filter((environment) =>
       req.context.permissions.canReadMultiProjectResource(environment.projects)
     );
