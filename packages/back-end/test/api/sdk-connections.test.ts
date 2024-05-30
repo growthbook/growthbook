c7571776a0b91c19e903679318e09b7e3844798e
Romain Beauxis <romain@growthbook.io>
2024-05-30 09:38:58 -0500

    Use test api setup.

--- packages/back-end/test/api/sdk-connections.test.ts
+++ packages/back-end/test/api/sdk-connections.test.ts
@@ -1,79 +1,36 @@
-import mongoose from "mongoose";
-import { MongoMemoryServer } from "mongodb-memory-server";
 import request from "supertest";
-import { getAuthConnection } from "../../src/services/auth";
-import authenticateApiRequestMiddleware from "../../src/middleware/authenticateApiRequestMiddleware";
-import app from "../../src/app";
 import {
   toApiSDKConnectionInterface,
   findSDKConnectionsByOrganization,
   createSDKConnection,
 } from "../../src/models/SdkConnectionModel";
 import { sdkConnectionFactory } from "../factories/SdkConnection.factory";
+import { setupApp } from "./api.setup";
 
 jest.mock("../../src/models/SdkConnectionModel", () => ({
-  ...jest.requireActual("../../src/models/SdkConnectionModel"),
+  toApiSDKConnectionInterface: jest.fn(),
   createSDKConnection: jest.fn(),
   findSDKConnectionsByOrganization: jest.fn(),
 }));
 
-jest.mock("../../src/init/queue", () => ({
-  queueInit: () => undefined,
-}));
-
-jest.mock("../../src/services/auth", () => ({
-  ...jest.requireActual("../../src/services/auth"),
-  getAuthConnection: () => ({
-    middleware: jest.fn(),
-  }),
-}));
-
-jest.mock("../../src/middleware/authenticateApiRequestMiddleware", () => ({
-  ...jest.requireActual(
-    "../../src/middleware/authenticateApiRequestMiddleware"
-  ),
-  __esModule: true,
-  default: jest.fn(),
-}));
-
-let mongod;
-
 describe("sdk-connections API", () => {
-  const OLD_ENV = process.env;
+  const { app, setReqContext } = setupApp();
+  const mockApiSDKConnectionInterface = ({ id }) => `mock-${id}`;
 
-  beforeAll(async () => {
-    jest.resetModules();
-    mongod = await MongoMemoryServer.create();
-    const uri = mongod.getUri();
-    process.env.MONGO_URL = uri;
-    getAuthConnection().middleware.mockImplementation((req, res, next) => {
-      next();
-    });
+  beforeEach(() => {
+    toApiSDKConnectionInterface.mockImplementation(
+      mockApiSDKConnectionInterface
+    );
   });
 
-  afterAll(async () => {
-    await mongoose.connection.dropDatabase();
-    await mongoose.connection.close();
-    await mongod.stop();
-    process.env = OLD_ENV;
-  });
-
-  afterEach(async () => {
+  afterEach(() => {
     jest.clearAllMocks();
-    const collections = mongoose.connection.collections;
-    for (const key in collections) {
-      const collection = collections[key];
-      await collection.deleteMany();
-    }
   });
 
   const org = { id: "org", environments: [{ id: "production" }] };
 
   it("can list all sdk-connections", async () => {
-    authenticateApiRequestMiddleware.mockImplementation((req, res, next) => {
-      req.context = { org };
-      next();
-    });
+    setReqContext({ org });
 
     const connections = [...Array(10)].map(() =>
       sdkConnectionFactory.build({
@@ -90,7 +47,7 @@ describe("sdk-connections API", () => {
 
     expect(response.status).toBe(200);
     expect(response.body).toEqual({
-      connections: connections.map(toApiSDKConnectionInterface),
+      connections: connections.map(mockApiSDKConnectionInterface),
       count: 10,
       hasMore: false,
       limit: 10,
@@ -101,10 +58,7 @@ describe("sdk-connections API", () => {
   });
 
   it("can paginate sdk-connections", async () => {
-    authenticateApiRequestMiddleware.mockImplementation((req, res, next) => {
-      req.context = { org };
-      next();
-    });
+    setReqContext({ org });
 
     const connections = [...Array(10)].map(() =>
       sdkConnectionFactory.build({
@@ -121,7 +75,7 @@ describe("sdk-connections API", () => {
 
     expect(response.status).toBe(200);
     expect(response.body).toEqual({
-      connections: connections.slice(5, 11).map(toApiSDKConnectionInterface),
+      connections: connections.slice(5, 11).map(mockApiSDKConnectionInterface),
       count: 5,
       hasMore: false,
       limit: 5,
@@ -132,13 +86,7 @@ describe("sdk-connections API", () => {
   });
 
   it("can create new sdk-connections", async () => {
-    authenticateApiRequestMiddleware.mockImplementation((req, res, next) => {
-      req.context = {
-        org,
-        permissions: { canCreateSDKConnection: () => true },
-      };
-      next();
-    });
+    setReqContext({ org, permissions: { canCreateSDKConnection: () => true } });
 
     let created;
 
@@ -160,7 +108,7 @@ describe("sdk-connections API", () => {
 
     expect(response.status).toBe(200);
     expect(response.body).toEqual({
-      sdkConnection: toApiSDKConnectionInterface(created),
+      sdkConnection: mockApiSDKConnectionInterface(created),
     });
   });
 });
