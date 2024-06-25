114e8aa546bbe46116a04f5723efa863c942911f
Guy Langford-Lee <guy@langford-lee.com>
2024-06-24 17:26:11 -0700

    fixed up theme swapping with system default (#2678)
    * fixed up theme swapping with system default
    
    * reverted
    
    * cleaned up testing code
    
    ---------
    
    Co-authored-by: Guy Langford-Lee <guy@growthbook.io>

--- packages/front-end/services/RadixTheme.tsx
+++ packages/front-end/services/RadixTheme.tsx
@@ -4,7 +4,6 @@ import { useAppearanceUITheme } from "@/services/AppearanceUIThemeProvider";
 
 export const RadixTheme: FC<PropsWithChildren> = ({ children }) => {
   const { theme } = useAppearanceUITheme();
-
   return (
     <Theme accentColor="violet" panelBackground="solid" appearance={theme}>
       {children}
