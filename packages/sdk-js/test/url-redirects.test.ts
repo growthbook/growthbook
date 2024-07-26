0b03b793ef8254b97ec5b89039fb668fc2f7afe6
Bryce <bryce1@gmail.com>
2024-07-26 10:29:53 -0700

    fix test

--- packages/sdk-js/test/url-redirects.test.ts
+++ packages/sdk-js/test/url-redirects.test.ts
@@ -45,7 +45,7 @@ describe("urlRedirects", () => {
     await sleep();
 
     gb.setURL("http://www.example.com/home");
-    await sleep(100);
+    await sleep(350);
     expect(window.location.replace).toHaveBeenCalledWith(
       "http://www.example.com/home-new"
     );
@@ -98,7 +98,7 @@ describe("urlRedirects", () => {
     await sleep();
 
     gb.setURL("http://www.example.com/home");
-    await sleep(100);
+    await sleep(350);
     expect(window.location.replace).toHaveBeenCalledWith(
       "http://www.example.com/home-new"
     );
@@ -151,7 +151,7 @@ describe("urlRedirects", () => {
     await sleep();
 
     gb.setURL("http://www.example.com/home");
-    await sleep(100);
+    await sleep(350);
     // expect replace to not have been called
     expect(window.location.replace).not.toHaveBeenCalled();
 
