6f045c425505e695e51f8bbdfb6371186982ac4c
Romain Beauxis <romain@growthbook.io>
2024-07-29 12:25:52 -0500

    Fix test.

--- packages/back-end/test/models/EventWebhookModel.test.ts
+++ packages/back-end/test/models/EventWebhookModel.test.ts
@@ -1,76 +1,83 @@
-import {
-  EventWebHookModel,
-  getAllEventWebHooksForEvent,
-} from "../../src/models/EventWebHookModel";
+import { EventWebHookModel } from "../../src/models/EventWebHookModel";
+import { BaseModel } from "../../src/models/BaseModel";
+
+class TestModel extends EventWebHookModel {
+  public addIndexes() {}
+}
 
 describe("getAllEventWebHooksForEvent", () => {
   describe("when event has no projects", () => {
     it("implements the right logic", async () => {
-      jest.spyOn(EventWebHookModel, "find").mockImplementation(() => [
+      const model = new TestModel();
+
+      jest.spyOn(BaseModel.prototype, "_find").mockImplementation(() => [
         {
-          toJSON: () => ({ name: "webhook with no filter on projects" }),
+          name: "webhook with no filter on projects",
           projects: [],
         },
         {
-          toJSON: () => ({ name: "webhook with filter for event project" }),
+          name: "webhook with filter for event project",
           projects: ["event project"],
         },
         {
-          toJSON: () => ({ name: "webhook with filter for foo project" }),
+          name: "webhook with filter for foo project",
           projects: ["foo"],
         },
       ]);
 
-      const ret = await getAllEventWebHooksForEvent({
-        organizationId: "aabb",
+      const ret = await model.getAllForEvent({
         eventName: "feature.created",
         enabled: true,
         tags: [],
         projects: [],
       });
 
-      expect(EventWebHookModel.find).toHaveBeenCalledWith({
+      expect(BaseModel.prototype._find).toHaveBeenCalledWith({
         enabled: true,
         events: "feature.created",
-        organizationId: "aabb",
       });
-      expect(ret).toEqual([{ name: "webhook with no filter on projects" }]);
+      expect(ret).toEqual([
+        { name: "webhook with no filter on projects", projects: [] },
+      ]);
     });
   });
 
   describe("when event has projects", () => {
     it("implements the right logic", async () => {
-      jest.spyOn(EventWebHookModel, "find").mockImplementation(() => [
+      const model = new TestModel();
+
+      jest.spyOn(BaseModel.prototype, "_find").mockImplementation(() => [
         {
-          toJSON: () => ({ name: "webhook with no filter on projects" }),
+          name: "webhook with no filter on projects",
           projects: [],
         },
         {
-          toJSON: () => ({ name: "webhook with filter for event project" }),
+          name: "webhook with filter for event project",
           projects: ["event project"],
         },
         {
-          toJSON: () => ({ name: "webhook with filter for foo projects" }),
+          name: "webhook with filter for foo projects",
           projects: ["foo"],
         },
       ]);
 
-      const ret = await getAllEventWebHooksForEvent({
-        organizationId: "aabb",
+      const ret = await model.getAllForEvent({
         eventName: "feature.created",
         enabled: true,
         tags: [],
         projects: ["event project"],
       });
 
-      expect(EventWebHookModel.find).toHaveBeenCalledWith({
+      expect(BaseModel.prototype._find).toHaveBeenCalledWith({
         enabled: true,
         events: "feature.created",
-        organizationId: "aabb",
       });
       expect(ret).toEqual([
-        { name: "webhook with no filter on projects" },
-        { name: "webhook with filter for event project" },
+        { name: "webhook with no filter on projects", projects: [] },
+        {
+          name: "webhook with filter for event project",
+          projects: ["event project"],
+        },
       ]);
     });
   });
