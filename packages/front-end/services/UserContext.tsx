cfc69209684096724103ef9a26b00151c3e7aeca
Graham McNicoll <graham@growthbook.io>
2024-08-04 19:20:47 -0700

    Removed moved permission

--- packages/front-end/services/UserContext.tsx
+++ packages/front-end/services/UserContext.tsx
@@ -91,7 +91,6 @@ export const DEFAULT_PERMISSIONS: Record<GlobalPermission, boolean> = {
   manageNamespaces: false,
   manageNorthStarMetric: false,
   manageSavedGroups: false,
-  manageArchetype: false,
   manageTags: false,
   manageTeam: false,
   manageEventWebhooks: false,
