3f7b6a1e86e750b83114d348c43d610ed049136c
Michael Knowlton <michael@growthbook.io>
2024-08-07 10:26:12 -0400

    Adds logging to putMemberRole

--- packages/back-end/src/api/members/putMemberRole.ts
+++ packages/back-end/src/api/members/putMemberRole.ts
@@ -1,4 +1,5 @@
 import { isRoleValid } from "shared/permissions";
+import { auditDetailsUpdate } from "@back-end/src/services/audit";
 import { PutMemberRoleResponse } from "../../../types/openapi";
 import { createApiRequestHandler } from "../../util/handler";
 import { putMemberRoleValidator } from "../../validators/openapi";
@@ -24,13 +25,26 @@ export const putMemberRole = createApiRequestHandler(putMemberRoleValidator)(
       throw new Error("Could not find user with that ID");
     }
 
+    if (orgUser.role === globalRole) {
+      return {
+        globalRole,
+      };
+    }
+
     try {
       await updateUserRole(req.context.org, req.params.id, globalRole);
+
+      await req.audit({
+        event: "user.update",
+        entity: {
+          object: "user",
+          id: orgUser.id,
+        },
+        details: auditDetailsUpdate(orgUser, { ...orgUser, role: globalRole }),
+      });
     } catch (e) {
       throw new Error(`Unable to update the user's role: ${e.message}`);
     }
-
-    //TODO: Add logging
 
     return {
       globalRole,
