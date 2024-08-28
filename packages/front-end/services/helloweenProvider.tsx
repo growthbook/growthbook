5ecce9ea86a230801f8eac2786bd3f042210dad4
Guy Langford-Lee <guy@growthbook.io>
2024-08-28 13:24:46 -0700

    added some nasty hacks to get it to work better

--- packages/front-end/services/helloweenProvider.tsx
+++ packages/front-end/services/helloweenProvider.tsx
@@ -7,6 +7,7 @@ import {
   useState,
 } from "react";
 import { useLocalStorage } from "@/hooks/useLocalStorage";
+import { useAppearanceUITheme } from "@/services/AppearanceUIThemeProvider";
 
 const HELLOWEEN_STORAGE_KEY = "gb_hello_theme";
 
@@ -34,27 +35,52 @@ export const HelloweenThemeProvider: FC<PropsWithChildren> = ({ children }) => {
     HELLOWEEN_STORAGE_KEY,
     false
   );
+  const { setTheme, preferredTheme } = useAppearanceUITheme();
+  const [previousTheme] = useState(preferredTheme);
   const [spiders, setSpiders] = useState();
   useEffect(() => {
     if (showHelloweenTheme) {
-      setSpiders(
+      if (!spiders) {
+        try {
+          setSpiders(
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-expect-error
+            new SpiderController({
+              minBugs: 1,
+              maxBugs: 4,
+              mouseOver: "die",
+              imageSprite: "/images/helloween/spider-sprite.png",
+            })
+          );
+        } catch (e) {
+          setTimeout(() => {
+            setSpiders(
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-expect-error
+              new SpiderController({
+                minBugs: 3,
+                maxBugs: 10,
+                mouseOver: "die",
+                imageSprite: "/images/helloween/spider-sprite.png",
+              })
+            );
+          }, 1000);
+        }
+      } else {
+        setTheme("dark");
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-expect-error
-        new SpiderController({
-          minBugs: 1,
-          maxBugs: 4,
-          mouseOver: "die",
-          imageSprite: "/images/helloween/spider-sprite.png",
-        })
-      );
+        spiders.reset();
+      }
     } else {
+      setTheme(previousTheme);
       if (spiders) {
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-expect-error
         spiders.end();
       }
     }
-  }, [showHelloweenTheme, spiders]);
+  }, [showHelloweenTheme, spiders, previousTheme, setTheme]);
 
   return (
     <HelloweenThemeContext.Provider
