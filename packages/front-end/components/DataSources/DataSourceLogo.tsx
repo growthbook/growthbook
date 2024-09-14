38257313623f8c8987dc5b50c25921fc623c1246
Michael Samper <msamper@growthbook.io>
2024-09-13 17:09:25 -0700

    add in event tracker logos and simplify file

--- packages/front-end/components/DataSources/DataSourceLogo.tsx
+++ packages/front-end/components/DataSources/DataSourceLogo.tsx
@@ -1,181 +1,70 @@
-import { SDKLanguage } from "back-end/types/sdk-connection";
-import { IconBaseProps, IconType } from "react-icons";
-import { SiGoogleanalytics } from "react-icons/si";
-import React, { ReactElement } from "react";
-import { isSDKOutdated } from "shared/sdk-versioning";
-import { HiOutlineExclamationCircle } from "react-icons/hi";
+import React from "react";
 import { SchemaFormat } from "@back-end/types/datasource";
-import { DocSection } from "@/components/DocLink";
-import Tooltip from "@/components/Tooltip/Tooltip";
 
-export type LanguageFilter =
-  | "popular"
-  | "all"
-  | "browser"
-  | "server"
-  | "mobile"
-  | "edge"
-  | "essential";
-export type LanguageType =
-  | "frontend"
-  | "backend"
-  | "mobile"
-  | "edge"
-  | "nocode"
-  | "other";
 export type LanguageLogo = {
-  Icon: IconType;
-  color: string;
+  logo: string;
   label: string;
-  docLabel?: string;
-  docs: DocSection;
-  type: string;
-  filters: LanguageFilter[];
-  hideVersion?: boolean;
-  extra?: ReactElement | string;
 };
+
 export const languageMapping: Record<
   Exclude<SchemaFormat, "mixpanel" | "custom">,
   LanguageLogo
 > = {
   ga4: {
-    Icon: SiGoogleanalytics,
-    color: "#E37400",
+    logo: "/images/3rd-party-logos/datasource-logos/ga4.svg",
     label: "Google Analytics v4",
-    docs: "google_analytics",
-    type: "other",
-    filters: [],
-    hideVersion: true,
   },
   segment: {
-    Icon: SiGoogleanalytics,
-    color: "#E37400",
+    logo: "/images/3rd-party-logos/datasource-logos/segment.svg",
     label: "Segment",
-    docs: "mixpanel",
-    type: "",
-    filters: [],
-    hideVersion: undefined,
-    extra: undefined,
   },
   snowplow: {
-    Icon: SiGoogleanalytics,
-    color: "#E37400",
+    logo: "/images/3rd-party-logos/datasource-logos/snowplow.svg",
     label: "Snowplow",
-    docs: "mixpanel",
-    type: "",
-    filters: [],
-    hideVersion: undefined,
-    extra: undefined,
   },
   jitsu: {
-    Icon: SiGoogleanalytics,
-    color: "#E37400",
+    logo: "/images/3rd-party-logos/datasource-logos/jitsu.svg",
     label: "Jitsu",
-    docs: "mixpanel",
-    type: "",
-    filters: [],
-    hideVersion: undefined,
-    extra: undefined,
   },
   freshpaint: {
-    Icon: SiGoogleanalytics,
-    color: "#E37400",
+    logo: "/images/3rd-party-logos/datasource-logos/freshpaint.svg",
     label: "Freshpaint",
-    docs: "mixpanel",
-    type: "",
-    filters: [],
-    hideVersion: undefined,
-    extra: undefined,
   },
   fullstory: {
-    Icon: SiGoogleanalytics,
-    color: "#E37400",
+    logo: "/images/3rd-party-logos/datasource-logos/fullstory.png",
     label: "Fullstory",
-    docs: "mixpanel",
-    type: "",
-    filters: [],
-    hideVersion: undefined,
-    extra: undefined,
   },
   matomo: {
-    Icon: SiGoogleanalytics,
-    color: "#E37400",
+    logo: "/images/3rd-party-logos/datasource-logos/matomo.svg",
     label: "Matomo",
-    docs: "mixpanel",
-    type: "",
-    filters: [],
-    hideVersion: undefined,
-    extra: undefined,
   },
   heap: {
-    Icon: SiGoogleanalytics,
-    color: "#E37400",
+    logo: "/images/3rd-party-logos/datasource-logos/heap.png",
     label: "Heap",
-    docs: "mixpanel",
-    type: "",
-    filters: [],
-    hideVersion: undefined,
-    extra: undefined,
   },
   rudderstack: {
-    Icon: SiGoogleanalytics,
-    color: "#E37400",
+    logo: "/images/3rd-party-logos/datasource-logos/rudderstack.svg",
     label: "Rudderstack",
-    docs: "mixpanel",
-    type: "",
-    filters: [],
-    hideVersion: undefined,
-    extra: undefined,
   },
   amplitude: {
-    Icon: SiGoogleanalytics,
-    color: "#E37400",
+    logo: "/images/3rd-party-logos/datasource-logos/amplitude.svg",
     label: "Amplitude",
-    docs: "mixpanel",
-    type: "",
-    filters: [],
-    hideVersion: undefined,
-    extra: undefined,
   },
   mparticle: {
-    Icon: SiGoogleanalytics,
-    color: "#E37400",
+    logo: "/images/3rd-party-logos/datasource-logos/mparticle.svg",
     label: "mparticle",
-    docs: "mixpanel",
-    type: "",
-    filters: [],
-    hideVersion: undefined,
-    extra: undefined,
   },
   firebase: {
-    Icon: SiGoogleanalytics,
-    color: "#E37400",
+    logo: "/images/3rd-party-logos/datasource-logos/firebase.svg",
     label: "Firebase",
-    docs: "mixpanel",
-    type: "",
-    filters: [],
-    hideVersion: undefined,
-    extra: undefined,
   },
   keen: {
-    Icon: SiGoogleanalytics,
-    color: "#E37400",
+    logo: "/images/3rd-party-logos/datasource-logos/keen.svg",
     label: "keen io",
-    docs: "mixpanel",
-    type: "",
-    filters: [],
-    hideVersion: undefined,
-    extra: undefined,
   },
   clevertap: {
-    Icon: SiGoogleanalytics,
-    color: "#E37400",
+    logo: "/images/3rd-party-logos/datasource-logos/clevertap.png",
     label: "CleverTap",
-    docs: "mixpanel",
-    type: "",
-    filters: [],
-    hideVersion: undefined,
-    extra: undefined,
   },
 };
 
@@ -193,53 +82,17 @@ export default function DataSourceLogo({
   if (!Object.keys(languageMapping).includes(language)) {
     return null;
   }
-  const { Icon, color, label } = languageMapping[language];
+  const { logo, label } = languageMapping[language];
 
   return (
     <span className="d-inline-flex align-items-center position-relative">
-      <Icon
-        style={{ color, height: size, fontSize: size, lineHeight: size }}
+      <img
+        src={logo}
+        style={{ height: size, fontSize: size, lineHeight: size }}
         className="m-0"
         title={titlePrefix + label}
       />
       <span className="ml-2">{showLabel && label}</span>
     </span>
   );
-}
-
-export function getLanguagesByFilter(
-  languageFilter: LanguageFilter = "all"
-): SDKLanguage[] {
-  return Object.entries(languageMapping)
-    .filter(([_, language]) => {
-      if (languageFilter === "all") return true;
-      return language.filters.includes(languageFilter);
-    })
-    .map((o) => o[0]) as SDKLanguage[];
-}
-
-export function getConnectionLanguageFilter(
-  languages: SDKLanguage[]
-): LanguageFilter {
-  const languageFilters = new Set<LanguageFilter>();
-  languages.forEach((language) => {
-    const type = languageMapping?.[language]?.type;
-    if (type) {
-      languageFilters.add(
-        type
-          // todo: only use filters (not types) to make this consistent
-          .replace("frontend", "browser")
-          .replace("nocode", "browser")
-          .replace("backend", "server")
-          .replace("other", "all") as LanguageFilter
-      );
-    }
-  });
-  if (languageFilters.size === 0) {
-    return "popular";
-  }
-  if (languageFilters.size === 1) {
-    return [...languageFilters][0];
-  }
-  return "all";
 }
