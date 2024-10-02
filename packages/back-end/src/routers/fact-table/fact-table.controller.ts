f13bfafe4ae69d1a0acc5f1b741768492fbaee5e
Jeremy Dorn <jeremy@jeremydorn.com>
2024-10-01 20:00:45 -0500

    Fix import

--- packages/back-end/src/routers/fact-table/fact-table.controller.ts
+++ packages/back-end/src/routers/fact-table/fact-table.controller.ts
@@ -29,11 +29,9 @@ import { addTags, addTagsDiff } from "back-end/src/models/TagModel";
 import { getSourceIntegrationObject } from "back-end/src/services/datasource";
 import { getDataSourceById } from "back-end/src/models/DataSourceModel";
 import { DataSourceInterface } from "back-end/types/datasource";
-import {
-  runColumnTopValuesQuery,
-  runRefreshColumnsQuery,
-} from "back-end/src/jobs/refreshFactTableColumns";
+import { runRefreshColumnsQuery } from "back-end/src/jobs/refreshFactTableColumns";
 import { logger } from "back-end/src/util/logger";
+import { runColumnTopValuesQuery } from "back-end/src/jobs/refreshFactTableTopValues";
 
 export const getFactTables = async (
   req: AuthRequest,
