0c9dfc6f40f85ae3c52408e0c91da564196e31a7
Bryce <bryce1@gmail.com>
2024-10-04 12:46:26 -0700

    fix button group regression

--- packages/front-end/components/Forms/ButtonSelectField.tsx
+++ packages/front-end/components/Forms/ButtonSelectField.tsx
@@ -30,38 +30,40 @@ export default function ButtonSelectField<T extends string>({
       })}
     >
       {label && <label>{label}</label>}
-      <div
-        className={clsx(className, {
-          "btn-group": buttonType === "inline",
-          "btn-group-card d-flex justify-content-center align-items-stretch w-100":
-            buttonType === "card",
-        })}
-        style={buttonType === "card" ? { gap: "1rem" } : {}}
-      >
-        {options.map((option) => (
-          <button
-            key={option.value}
-            type="button"
-            className={clsx("btn", {
-              "flex-1": buttonType === "card",
-              ...(buttonType === "inline" && {
-                "active btn-primary": value === option.value,
-                "btn-outline-primary": value !== option.value,
-              }),
-              ...(buttonType === "card" && {
-                active: value === option.value,
-              }),
-              "cursor-disabled": option.disabled,
-            })}
-            onClick={(e) => {
-              e.preventDefault();
-              setValue(option.value);
-            }}
-            disabled={option.disabled ?? false}
-          >
-            {option.label}
-          </button>
-        ))}
+      <div>
+        <div
+          className={clsx(className, {
+            "btn-group": buttonType === "inline",
+            "btn-group-card d-flex justify-content-center align-items-stretch w-100":
+              buttonType === "card",
+          })}
+          style={buttonType === "card" ? { gap: "1rem" } : {}}
+        >
+          {options.map((option) => (
+            <button
+              key={option.value}
+              type="button"
+              className={clsx("btn", {
+                "flex-1": buttonType === "card",
+                ...(buttonType === "inline" && {
+                  "active btn-primary": value === option.value,
+                  "btn-outline-primary": value !== option.value,
+                }),
+                ...(buttonType === "card" && {
+                  active: value === option.value,
+                }),
+                "cursor-disabled": option.disabled,
+              })}
+              onClick={(e) => {
+                e.preventDefault();
+                setValue(option.value);
+              }}
+              disabled={option.disabled ?? false}
+            >
+              {option.label}
+            </button>
+          ))}
+        </div>
       </div>
     </div>
   );
