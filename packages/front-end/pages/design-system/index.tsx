b867476226301a8f90467c61318d8942a4b1ef06
Luke Sonnet <lukesonnet@users.noreply.github.com>
2024-10-07 13:20:12 -0700

    RadioGroup radix component (#3060)

--- packages/front-end/pages/design-system/index.tsx
+++ packages/front-end/pages/design-system/index.tsx
@@ -3,12 +3,14 @@ import { useState } from "react";
 import { PiInfoFill } from "react-icons/pi";
 import HelperText from "@/components/Radix/HelperText";
 import Checkbox from "@/components/Radix/Checkbox";
+import RadioGroup from "@/components/Radix/RadioGroup";
 import Badge from "@/components/Radix/Badge";
 import Callout from "@/components/Radix/Callout";
 import Avatar from "@/components/Radix/Avatar";
 
 export default function DesignSystemPage() {
   const [checked, setChecked] = useState(false);
+  const [radioSelected, setRadioSelected] = useState("k1");
 
   return (
     <div className="pagecontents container-fluid">
@@ -111,6 +113,58 @@ export default function DesignSystemPage() {
               setChecked(v);
             }}
             disabled
+          />
+        </Flex>
+      </div>
+      <div className="appbox p-3">
+        <h3>Radio Group</h3>
+        <Flex direction="column" gap="3">
+          <RadioGroup
+            value={radioSelected}
+            setValue={(v) => {
+              setRadioSelected(v);
+            }}
+            options={[
+              {
+                value: "k1",
+                label: "Radio 1",
+              },
+              {
+                value: "k2",
+                label: "Radio 2",
+              },
+              {
+                value: "k3",
+                label: "Radio 3, with description",
+                description: "This is a description",
+              },
+              {
+                value: "k4",
+                label: "Radio 4, with error",
+                error: "This is an error",
+                errorLevel: "error",
+              },
+              {
+                value: "k5",
+                label: "Radio 5, with warning",
+                error: "This is a warning",
+                errorLevel: "warning",
+              },
+              {
+                value: "k6",
+                label: "Radio 6, disabled",
+                description: "This is a description",
+                disabled: true,
+              },
+              {
+                value: "k7",
+                label: "Radio 7, disabled with error",
+                description: "This is a description",
+                disabled: true,
+                error: "This is an error",
+                errorLevel: "error",
+              },
+            ]}
           />
         </Flex>
       </div>
