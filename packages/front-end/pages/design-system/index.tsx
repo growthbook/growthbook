7d5acd3727aeafe1df89762c4ebe6170ed68b496
Bryce <bryce@growthbook.io>
2024-10-07 13:13:29 -0700

    Radix Avatar (#3067)

--- packages/front-end/pages/design-system/index.tsx
+++ packages/front-end/pages/design-system/index.tsx
@@ -1,9 +1,11 @@
 import { Flex } from "@radix-ui/themes";
 import { useState } from "react";
+import { PiInfoFill } from "react-icons/pi";
 import HelperText from "@/components/Radix/HelperText";
 import Checkbox from "@/components/Radix/Checkbox";
 import Badge from "@/components/Radix/Badge";
 import Callout from "@/components/Radix/Callout";
+import Avatar from "@/components/Radix/Avatar";
 
 export default function DesignSystemPage() {
   const [checked, setChecked] = useState(false);
@@ -17,6 +19,23 @@ export default function DesignSystemPage() {
       </p>
 
       <h2>Components</h2>
+
+      <div className="appbox p-3">
+        <h3>Avatar</h3>
+        <Flex direction="row" gap="3">
+          <Avatar>BF</Avatar>
+          <Avatar color="green">
+            <PiInfoFill size={25} />
+          </Avatar>
+          <Avatar size="lg" radius="small">
+            <img src="https://app.growthbook.io/logo/growth-book-logomark-white.svg" />
+          </Avatar>
+          <Avatar color="orange" variant="soft" size="sm">
+            sm
+          </Avatar>
+        </Flex>
+      </div>
+
       <div className="appbox p-3">
         <h3>Callout</h3>
         <Flex direction="column" gap="3">
