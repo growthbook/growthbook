eb59629622bfba3a37eaf7dd0bb867267a0636f7
Madhu Chavva <madhu.chavva@gmail.com>
2024-10-08 20:12:08 -0700

    slider variations

--- packages/front-end/pages/design-system/index.tsx
+++ packages/front-end/pages/design-system/index.tsx
@@ -1,4 +1,4 @@
-import { Flex } from "@radix-ui/themes";
+import { Flex, Slider } from "@radix-ui/themes";
 import { useState } from "react";
 import { PiInfoFill } from "react-icons/pi";
 import HelperText from "@/components/Radix/HelperText";
@@ -11,6 +11,7 @@ import Avatar from "@/components/Radix/Avatar";
 export default function DesignSystemPage() {
   const [checked, setChecked] = useState(false);
   const [radioSelected, setRadioSelected] = useState("k1");
+  const [sliderVal, setSliderVal] = useState(10);
 
   return (
     <div className="pagecontents container-fluid">
@@ -175,6 +176,42 @@ export default function DesignSystemPage() {
           <HelperText status="warning">This is a warning message</HelperText>
           <HelperText status="error">This is an error message</HelperText>
           <HelperText status="success">This is a success message</HelperText>
+        </Flex>
+      </div>
+      <div className="appbox p-3">
+        <h3>Slider</h3>
+        <Flex direction="column" gap="3" maxWidth="300px">
+          <div>
+            <label>Slider</label>
+            <Slider
+              value={[sliderVal]}
+              min={0}
+              max={100}
+              step={1}
+              onValueChange={(e) => {
+                setSliderVal(e[0]);
+              }}
+            />
+            <span className="col-auto" style={{ fontSize: "1.3em" }}>
+              {sliderVal}%
+            </span>
+          </div>
+          <div>
+            <label>Slider in cyan (high contrast) </label>
+            <Slider defaultValue={[35]} color="cyan" highContrast />
+          </div>
+          <div>
+            <label>Slider with no Radius</label>
+            <Slider defaultValue={[75]} radius="none" />
+          </div>
+          <div>
+            <label>Range Slider with Soft visual style</label>
+            <Slider defaultValue={[25, 75]} variant="soft" />
+          </div>
+          <div>
+            <label>Large Slider Disabled</label>
+            <Slider defaultValue={[25]} size="3" disabled={true} />
+          </div>
         </Flex>
       </div>
     </div>
