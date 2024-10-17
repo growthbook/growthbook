c7cd158ec10f37172a86971a8af348b094b7f6b1
Michael Samper <msamper@growthbook.io>
2024-10-17 14:06:39 -0700

    Redesign EnvironmentSelect

--- packages/front-end/components/Features/FeatureModal/EnvironmentSelect.tsx
+++ packages/front-end/components/Features/FeatureModal/EnvironmentSelect.tsx
@@ -1,9 +1,16 @@
 import { FC } from "react";
 import { Environment } from "back-end/types/organization";
 import { FeatureEnvironment } from "back-end/types/feature";
+import {
+  CheckboxGroup,
+  Container,
+  Text,
+  Checkbox as RadixCheckbox,
+  Flex,
+} from "@radix-ui/themes";
 import { useDefinitions } from "@/services/DefinitionsContext";
-import Toggle from "@/components/Forms/Toggle";
 import usePermissionsUtil from "@/hooks/usePermissionsUtils";
+import Checkbox from "@/components/Radix/Checkbox";
 
 const EnvironmentSelect: FC<{
   environmentSettings: Record<string, FeatureEnvironment>;
@@ -13,34 +20,45 @@ const EnvironmentSelect: FC<{
   const permissionsUtil = usePermissionsUtil();
   const { project } = useDefinitions();
   return (
-    <div className="form-group">
-      <label>Enabled Environments</label>
-      <div className="appbox px-3 pt-3 pb-2 bg-light">
-        <div className="row">
-          {environments.map((env) => (
-            <div className="col-auto" key={env.id}>
-              <div className="form-group mb-0">
-                <label htmlFor={`${env.id}_toggle_create`} className="mr-2">
-                  {env.id}:
-                </label>
-                <Toggle
-                  id={`${env.id}_toggle_create`}
-                  label={env.id}
-                  disabledMessage="You don't have permission to create features in this environment."
-                  disabled={
-                    !permissionsUtil.canPublishFeature({ project }, [env.id])
-                  }
-                  className="mr-3"
-                  value={environmentSettings[env.id].enabled}
-                  setValue={(enabled) => setValue(env, enabled)}
-                  type="environment"
-                />
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
+    // <div className="form-group">
+    <Container p="5" style={{ background: "#FAF8FF" }}>
+      <Text as="label" weight="bold">
+        Enabled Environments
+      </Text>
+      <CheckboxGroup.Root name="enabled-environments" mt="3">
+        <Text
+          as="label"
+          className={"rt-CheckboxItem"}
+          size="2"
+          onClick={() => {
+            environments.forEach((env) => {
+              setValue(env, true);
+            });
+          }}
+        >
+          <Flex gap="2" mb="2">
+            <RadixCheckbox checked={"indeterminate"} color={"violet"} />
+            <Flex direction="column" gap="1">
+              <Text weight="bold" className="main-text">
+                Select All
+              </Text>
+            </Flex>
+          </Flex>
+        </Text>
+        {environments.map((env) => (
+          <Checkbox
+            disabled={!permissionsUtil.canPublishFeature({ project }, [env.id])}
+            disabledMessage="You don't have permission to create features in this environment."
+            value={environmentSettings[env.id].enabled}
+            setValue={(enabled) => setValue(env, enabled)}
+            label={env.id}
+            key={env.id}
+            weight="regular"
+          />
+        ))}
+      </CheckboxGroup.Root>
+    </Container>
+    // </div>
   );
 };
 
