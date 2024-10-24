fd5df746216d1f0527a00702138d2da4d4c77b02
Bryce <bryce1@gmail.com>
2024-10-24 12:11:45 -0700

    console

--- packages/front-end/services/track.ts
+++ packages/front-end/services/track.ts
@@ -51,7 +51,6 @@ export default function track(
   event: string,
   props: TrackEventProps = {}
 ): void {
-  console.log("track", { event, props });
   // Only run client-side, not during SSR
   if (typeof window === "undefined") return;
 
