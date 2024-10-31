557df36d3b1da052c03265d45446405bad4df556
Michael Samper <msamper@growthbook.io>
2024-10-30 19:07:18 -0700

    Use CIELAB color space to find closest radix color

--- packages/front-end/components/Tags/Tag.tsx
+++ packages/front-end/components/Tags/Tag.tsx
@@ -92,31 +92,72 @@ function hexToRgb(hex) {
   return { r, g, b };
 }
 
-// Function to calculate the Euclidean distance between two colors
-function colorDistance(color1, color2) {
+// Convert RGB to XYZ
+function rgbToXyz({ r, g, b }) {
+  // Normalize the RGB values
+  r = r / 255;
+  g = g / 255;
+  b = b / 255;
+
+  // Apply the sRGB companding
+  r = r > 0.04045 ? Math.pow((r + 0.055) / 1.055, 2.4) : r / 12.92;
+  g = g > 0.04045 ? Math.pow((g + 0.055) / 1.055, 2.4) : g / 12.92;
+  b = b > 0.04045 ? Math.pow((b + 0.055) / 1.055, 2.4) : b / 12.92;
+
+  // Convert to XYZ using the D65 reference white
+  const x = (r * 0.4124564 + g * 0.3575761 + b * 0.1804375) * 100;
+  const y = (r * 0.2126729 + g * 0.7151522 + b * 0.072175) * 100;
+  const z = (r * 0.0193339 + g * 0.119192 + b * 0.9503041) * 100;
+
+  return { x, y, z };
+}
+
+// Convert XYZ to CIELAB
+function xyzToLab({ x, y, z }) {
+  // D65 reference white point
+  const refX = 95.047;
+  const refY = 100.0;
+  const refZ = 108.883;
+
+  x = x / refX;
+  y = y / refY;
+  z = z / refZ;
+
+  x = x > 0.008856 ? Math.cbrt(x) : 7.787 * x + 16 / 116;
+  y = y > 0.008856 ? Math.cbrt(y) : 7.787 * y + 16 / 116;
+  z = z > 0.008856 ? Math.cbrt(z) : 7.787 * z + 16 / 116;
+
+  const l = 116 * y - 16;
+  const a = 500 * (x - y);
+  const b = 200 * (y - z);
+
+  return { l, a, b };
+}
+
+// Calculate Euclidean distance in CIELAB
+function labDistance(lab1, lab2) {
   return Math.sqrt(
-    Math.pow(color1.r - color2.r, 2) +
-      Math.pow(color1.g - color2.g, 2) +
-      Math.pow(color1.b - color2.b, 2)
+    Math.pow(lab1.l - lab2.l, 2) +
+      Math.pow(lab1.a - lab2.a, 2) +
+      Math.pow(lab1.b - lab2.b, 2)
   );
 }
 
-// Function to find the closest color in the object
+// Find the closest color in CIELAB space
 function findClosestColorName(targetHex, colorsObject) {
-  const targetRgb = hexToRgb(targetHex);
+  const targetLab = xyzToLab(rgbToXyz(hexToRgb(targetHex)));
   let closestColorName;
   let smallestDistance = Infinity;
 
   for (const [colorName, hexColor] of Object.entries(colorsObject)) {
-    const currentRgb = hexToRgb(hexColor);
-    const distance = colorDistance(targetRgb, currentRgb);
+    const currentLab = xyzToLab(rgbToXyz(hexToRgb(hexColor)));
+    const distance = labDistance(targetLab, currentLab);
 
     if (distance < smallestDistance) {
       smallestDistance = distance;
       closestColorName = colorName;
     }
   }
-
   console.log({ targetHex, closestColorName });
   return closestColorName;
 }
@@ -128,7 +169,7 @@ export default function Tag({ tag, color, description, skipMargin }: Props) {
   const displayTitle = description ?? fullTag?.description ?? "";
 
   const tagColor = color ?? fullTag?.color ?? "blue";
-  // If a tag is still using a hex code color, we'll default to blue
+  // If a tag is still using a hex code color, we'll find the closest radix color
   const displayColor = isTagColor(tagColor)
     ? tagColor
     : findClosestColorName(tagColor, TAG_COLORS_MAP);
