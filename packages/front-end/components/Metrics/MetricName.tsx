9412715ec845245c49b63f3da08063fccfa2ea64
Bryce <bryce1@gmail.com>
2024-10-31 14:22:56 -0700

    fix color

--- packages/front-end/components/Metrics/MetricName.tsx
+++ packages/front-end/components/Metrics/MetricName.tsx
@@ -125,7 +125,7 @@ export default function MetricName({
         <VscListTree className="mr-1" />
         {metricGroup.name}
         <Tooltip
-          className="text-danger px-1"
+          className={clsx("px-1", { "text-danger": allJoinable })}
           body={
             <>
               {!allJoinable && (
