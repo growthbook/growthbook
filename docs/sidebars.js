da1285206c8512b978d918da5af44b705d3cf8db
Luke Sonnet <luke.sonnet@gmail.com>
2024-11-01 14:53:36 -0700

    Add bandit docs and re-organize docs left nav

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -87,48 +87,7 @@ const sidebars = {
     },
     {
       type: "category",
-      label: "Running Experiments",
-      collapsed: true,
-      link: {
-        type: "doc",
-        id: "experiments",
-      },
-      items: [
-        {
-          type: "doc",
-          id: "feature-flag-experiments",
-          label: "In Code (Feature Flags)",
-        },
-        {
-          type: "doc",
-          id: "visual-editor",
-          label: "Visual Editor",
-        },
-        {
-          type: "doc",
-          id: "running-experiments/url-redirects",
-          label: "URL Redirects",
-        },
-        {
-          type: "doc",
-          id: "running-experiments/making-changes",
-          label: "Making Changes",
-        },
-        {
-          type: "doc",
-          id: "sticky-bucketing",
-          label: "Sticky Bucketing",
-        },
-        {
-          type: "doc",
-          id: "running-experiments/pre-launch-checklist",
-          label: "Pre-Launch Checklist",
-        },
-      ],
-    },
-    {
-      type: "category",
-      label: "Experiment Analysis",
+      label: "Data & Metrics",
       collapsed: true,
       items: [
         {
@@ -307,21 +266,98 @@ const sidebars = {
           type: "doc",
           id: "experimentation-analysis/metrics",
           label: "Metrics",
+        }
+      ],
+    },
+    {
+      type: "category",
+      label: "Experimentation",
+      collapsed: true,
+      link: {
+        type: "doc",
+        id: "experiments",
+      },
+      items: [
+        {
+          type: "category",
+          label: "Ways to Run Experiments",
+          collapsed: true,
+          items: [
+            {
+              type: "doc",
+              id: "feature-flag-experiments",
+              label: "In Code (Feature Flags)",
+            },
+            {
+              type: "doc",
+              id: "visual-editor",
+              label: "Visual Editor",
+            },
+            {
+              type: "doc",
+              id: "running-experiments/url-redirects",
+              label: "URL Redirects",
+            },
+          ]
+        },
+        {
+          type: "category",
+          label: "Experiments",
+          collapsed: true,
+          items: [
+            {
+              type: "doc",
+              id: "experimentation-analysis/experiment-configuration",
+              label: "Configuring Experiments",
+            },
+            {
+              type: "doc",
+              id: "experimentation-analysis/experiment-results",
+              label: "Understanding Results",
+            },
+            {
+              type: "doc",
+              id: "running-experiments/making-changes",
+              label: "Making Changes",
+            },
+          ]
+        },
+        {
+          type: "category",
+          label: "Bandits",
+          collapsed: true,
+          items: [
+            {
+              type: "doc",
+              id: "bandits/overview",
+              label: "Overview",
+            },
+            {
+              type: "doc",
+              id: "bandits/config",
+              label: "Configuring Bandits",
+            },
+            {
+              type: "doc",
+              id: "bandits/results",
+              label: "Understanding Results",
+            },
+          ]
         },
         {
           type: "doc",
-          id: "experimentation-analysis/experiment-configuration",
-          label: "Experiments (Setup)",
-        },
-        {
-          type: "doc",
-          id: "experimentation-analysis/experiment-results",
-          label: "Experiments (Results)",
+          id: "sticky-bucketing",
+          label: "Sticky Bucketing",
         },
         {
           type: "doc",
           id: "experimentation-analysis/dimensions",
           label: "Dimensions",
+        },
+        {
+          type: "doc",
+          id: "running-experiments/pre-launch-checklist",
+          label: "Pre-Launch Checklists",
         },
         {
           type: "category",
