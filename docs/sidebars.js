9d8d5705d49807aa3c7d61317fbd15ba2a178782
Jeremy Dorn <jeremy@jeremydorn.com>
2024-11-11 13:38:48 -0600

    Nest metric pages in docs sidebar

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -299,20 +299,33 @@ const sidebars = {
           label: "Data Pipeline",
         },
         {
-          type: "doc",
-          id: "metrics/metrics",
+          type: "category",
           label: "Metrics and Fact Tables",
+          collapsed: true,
+
+          link: {
+            type: "doc",
+            id: "metrics/metrics",
+          },
+          items: [
+            {
+              type: "doc",
+              id: "metrics/metrics",
+              label: "Metrics Overview",
+            },
+            {
+              type: "doc",
+              id: "metrics/metric-examples",
+              label: "Metric Examples and Use Cases",
+            },
+            {
+              type: "doc",
+              id: "metrics/legacy-metrics",
+              label: "Legacy Metrics",
+            },
+          ],
         },
-        {
-          type: "doc",
-          id: "metrics/metric-examples",
-          label: "Metric Examples and Use Cases",
-        },
-        {
-          type: "doc",
-          id: "metrics/legacy-metrics",
-          label: "Legacy Metrics",
-        },
+
         {
           type: "doc",
           id: "experimentation-analysis/experiment-configuration",
