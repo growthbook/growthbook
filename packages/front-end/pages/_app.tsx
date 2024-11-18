5f54e11813986f6ffa57510fdfe36a4fe64eefc1
Adriel Vieira <ahdriel@gmail.com>
2024-11-18 12:26:47 -0800

    wip

--- packages/front-end/pages/_app.tsx
+++ packages/front-end/pages/_app.tsx
@@ -3,7 +3,6 @@ import { AppProps } from "next/app";
 import "@/styles/global.scss";
 import "@/styles/global-radix-overrides.scss";
 import "@radix-ui/themes/styles.css";
-import "@/styles/theme-config.css";
 import Head from "next/head";
 import { useEffect, useState } from "react";
 import {
