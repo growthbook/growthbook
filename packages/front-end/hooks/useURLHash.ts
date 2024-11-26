1fa9cfe060eb1cb40f87288764b7ce5e35468708
Adriel Vieira <ahdriel@gmail.com>
2024-11-25 16:20:14 -0800

    Updates

--- packages/front-end/hooks/useURLHash.ts
+++ packages/front-end/hooks/useURLHash.ts
@@ -1,5 +1,27 @@
 import { useEffect, useState } from "react";
 
+/**
+ * Hook to sync a component's state with the URL hash
+ *
+ * @param validIds - Array of valid hash values that this component can handle
+ * @returns [currentHash, setHash] - Current hash value and function to update it
+ *
+ * @example
+ * ```tsx
+ * const tabs = ['info', 'settings', 'advanced'] as const;
+ * const [activeTab, setActiveTab] = useURLHash(tabs);
+ *
+ * // activeTab will automatically update when URL hash changes
+ * // setActiveTab will update both state and URL hash
+ * return (
+ *   <Tabs active={activeTab} onChange={setActiveTab}>
+ *     <Tab id="info">Info</Tab>
+ *     <Tab id="settings">Settings</Tab>
+ *     <Tab id="advanced">Advanced</Tab>
+ *   </Tabs>
+ * );
+ * ```
+ */
 export default function useURLHash<Id extends string>(validIds: Id[]) {
   const [hash, setHashState] = useState(() => {
     // Get initial hash from URL, defaulting to first valid slug
