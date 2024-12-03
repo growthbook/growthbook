793558266f5dce36b436794c7e6962ae6bd484ac
Adriel Vieira <ahdriel@gmail.com>
2024-12-02 17:26:20 -0800

    Update eslint + use our Tabs instead of Radix directly

--- packages/front-end/components/Radix/Tabs.tsx
+++ packages/front-end/components/Radix/Tabs.tsx
@@ -1,5 +1,21 @@
 import { Tabs as RadixTabs } from "@radix-ui/themes";
 
+/**
+ * See more examples in design-system/index.tsx
+ *
+ * Simple usage:
+ * ```tsx
+ * <Tabs defaultValue="tab1">
+ *   <TabsList>
+ *     <TabsTrigger value="tab1">Tab 1</TabsTrigger>
+ *     <TabsTrigger value="tab2">Tab 2</TabsTrigger>
+ *   </TabsList>
+ *   <TabsContent value="tab1">Content for tab 1</TabsContent>
+ *   <TabsContent value="tab2">Content for tab 2</TabsContent>
+ * </Tabs>
+ * ```
+ */
+
 export function Tabs({
   children,
   ...props
