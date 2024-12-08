33a76af9d2c5b05fb08771737dce4cb0ccd5ad0d
Graham McNicoll <graham@growthbook.io>
2024-12-07 18:17:33 -0600

    Added new style for upgrade message

--- packages/front-end/pages/settings/customfields.tsx
+++ packages/front-end/pages/settings/customfields.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { useUser } from "@/services/UserContext";
 import UpgradeModal from "@/components/Settings/UpgradeModal";
-import UpgradeMessage from "@/components/Marketing/UpgradeMessage";
 import CustomFields from "@/components/CustomFields/CustomFields";
+import LinkButton from "@/components/Radix/LinkButton";
+import Button from "@/components/Radix/Button";
 
 const CustomFieldsPage = (): React.ReactElement => {
   const { hasCommercialFeature } = useUser();
@@ -11,41 +12,46 @@ const CustomFieldsPage = (): React.ReactElement => {
 
   if (!hasCustomFieldAccess) {
     return (
-      <>
-        <div className="contents container-fluid pagecontents">
-          <div className="mb-5">
-            <div className="row mb-3 align-items-center">
-              <div className="col-auto">
-                <h3>Custom Fields</h3>
-                <p className="text-gray"></p>
-              </div>
-            </div>
-            <div className="row mb-3">
-              <div className="col-12 mb-2 py-2">
+      <div className="contents container-fluid pagecontents">
+        <div className="mb-5">
+          <div className="mb-3">
+            <div className="appbox p-5 text-center">
+              <div className="py-2">
+                <h2>Custom Fields</h2>
                 <p>
-                  Custom experiment fields are available with an Enterprise
-                  plan.
+                  Custom fields allow you to add additional meta data to
+                  experiments and feature flags that can be required or
+                  optional. Custom fields are part of our enterprise plan.
                 </p>
-                <UpgradeMessage
-                  showUpgradeModal={() => setUpgradeModal(true)}
-                  commercialFeature="override-metrics"
-                  upgradeMessage="set custom experiment fields"
-                  isEnterprise={true}
-                />
+                <div className="mt-3">
+                  <LinkButton
+                    href="https://docs.growthbook.io/using/growthbook-best-practices#custom-fields"
+                    variant="outline"
+                    mr="3"
+                    external={true}
+                  >
+                    View docs
+                  </LinkButton>
+                  <Button
+                    onClick={() => {
+                      setUpgradeModal(true);
+                    }}
+                  >
+                    Upgrade Plan
+                  </Button>
+                </div>
               </div>
             </div>
+            {upgradeModal && (
+              <UpgradeModal
+                close={() => setUpgradeModal(false)}
+                reason="Add custom metadata,"
+                source="custom-fields"
+              />
+            )}
           </div>
         </div>
-        {upgradeModal && (
-          <>
-            <UpgradeModal
-              close={() => setUpgradeModal(false)}
-              reason="To add custom experiment metadata,"
-              source="custom-fields"
-            />
-          </>
-        )}
-      </>
+      </div>
     );
   }
 
