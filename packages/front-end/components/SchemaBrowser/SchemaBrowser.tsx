a40b1f8b17b72d67b72ca906c65d97f301380750
Michael Knowlton <michael@growthbook.io>
2024-12-10 10:33:14 -0500

    Updates modal scroll on SchemaBrowser on [did].tsx (#3413)
    * Updates modal scroll on SchemaBrowser on [did].tsx

--- packages/front-end/components/SchemaBrowser/SchemaBrowser.tsx
+++ packages/front-end/components/SchemaBrowser/SchemaBrowser.tsx
@@ -152,7 +152,7 @@ export default function SchemaBrowser({
   if (!data) return <LoadingSpinner />;
 
   return (
-    <div className="d-flex flex-column h-100">
+    <div className="d-flex flex-column">
       <SchemaBrowserWrapper
         datasourceName={datasource.name}
         datasourceId={datasource.id}
