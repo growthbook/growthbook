0fd271d3c9314b17974f4a43487d8114c34e5931
bwkussmaul <bwkussmaul@gmail.com>
2024-12-13 16:27:04 -0500

    Benk/jump to start of json diff (#8)
    * jump to start of json diff
    
    * also do line add
    
    * swap to instant

--- packages/front-end/components/Features/JsonDiff.tsx
+++ packages/front-end/components/Features/JsonDiff.tsx
@@ -1,4 +1,4 @@
-import { CSSProperties } from "react";
+import React, { CSSProperties, useEffect, useRef } from "react";
 import ReactJsonViewCompare from "react-json-view-compare";
 
 function fixJsonRecursive(obj) {
@@ -35,8 +35,35 @@ export default function JsonDiff({
   let newData = JSON.parse(value);
   newData = fixJsonRecursive(newData);
 
+  const viewerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const scrollToFirstDiff = () => {
+      if (viewerRef.current) {
+        const firstDiff = viewerRef.current.querySelector(
+          ".c-json-p.c-line-del, .c-json-p.c-line-add"
+        );
+        if (firstDiff) {
+          // calculate offset relative to the scrollable container
+          const containerRect = viewerRef.current.getBoundingClientRect();
+          const diffRect = firstDiff.getBoundingClientRect();
+          const offsetTop =
+            diffRect.top - containerRect.top + viewerRef.current.scrollTop - 30;
+
+          // scroll the container to the calculated offset
+          viewerRef.current.scrollTo({ top: offsetTop, behavior: "instant" });
+        }
+      }
+    };
+
+    // add a slight delay to ensure rendering is complete
+    const timeout = setTimeout(scrollToFirstDiff, 10);
+
+    return () => clearTimeout(timeout); // Cleanup timeout if component unmounts
+  }, []);
+
   return (
-    <div style={fullStyle}>
+    <div ref={viewerRef} style={fullStyle}>
       <ReactJsonViewCompare oldData={oldData} newData={newData} />
     </div>
   );
