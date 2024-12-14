cc062cf52d959feeff7fab64ec7bf572a0c1553c
Jeremy Dorn <jeremy@growthbook.io>
2024-12-14 08:16:59 -0600

    Fix user filters missing field (#3434)

--- packages/front-end/components/FactTables/FactMetricModal.tsx
+++ packages/front-end/components/FactTables/FactMetricModal.tsx
@@ -304,8 +304,6 @@ function ColumnRefSelector({
     includeStringColumns:
       datasource.properties?.hasCountDistinctHLL && aggregationType === "unit",
   });
-  console.log(datasource.properties?.hasCountDistinctHLL);
-  console.log(aggregationType);
 
   const selectedColumnDatatype = getSelectedColumnDatatype({
     factTable,
@@ -687,26 +685,44 @@ function ColumnRefSelector({
                 />
               </label>
               {value.aggregateFilterColumn || addUserFilter ? (
-                <SelectField
-                  value={value.aggregateFilterColumn || ""}
-                  onChange={(v) =>
-                    setValue({
-                      ...value,
-                      aggregateFilterColumn: v,
-                    })
-                  }
-                  options={getColumnOptions({
-                    factTable: factTable,
-                    includeCount: true,
-                    includeCountDistinct: false,
-                    includeStringColumns: false,
-                    showColumnsAsSums: true,
-                    groupPrefix: "Filter by ",
-                  })}
-                  initialOption="Any User"
-                  autoFocus
-                  onBlur={() => setAddUserFilter(false)}
-                />
+                <div className="d-flex align-items-center">
+                  <SelectField
+                    value={value.aggregateFilterColumn || ""}
+                    onChange={(v) =>
+                      setValue({
+                        ...value,
+                        aggregateFilterColumn: v,
+                      })
+                    }
+                    options={getColumnOptions({
+                      factTable: factTable,
+                      includeCount: true,
+                      includeCountDistinct: false,
+                      includeStringColumns: false,
+                      showColumnsAsSums: true,
+                      groupPrefix: "Filter by ",
+                    })}
+                    initialOption="Any User"
+                    autoFocus
+                    onBlur={() => setAddUserFilter(false)}
+                  />
+                  {value.aggregateFilterColumn ? (
+                    <div className="ml-1">
+                      <Field
+                        value={value.aggregateFilter || ""}
+                        onChange={(v) =>
+                          setValue({
+                            ...value,
+                            aggregateFilter: v.target.value,
+                          })
+                        }
+                        placeholder=">= 10"
+                        style={{ maxWidth: 100 }}
+                        required
+                      />
+                    </div>
+                  ) : null}
+                </div>
               ) : (
                 <div className="py-2">
                   <a
