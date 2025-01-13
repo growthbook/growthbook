eda1470a281250e6fd1cc6554d92cf32689dd0a7
Michael Knowlton <michael@growthbook.io>
2025-01-13 09:44:11 -0500

    Removes now defunct code in favor of whats on main and updates padding on experiment header

--- packages/front-end/components/Experiment/TabbedPage/ProjectTagBar.tsx
+++ packages/front-end/components/Experiment/TabbedPage/ProjectTagBar.tsx
@@ -136,7 +136,7 @@ export default function ProjectTagBar({
     );
   };
   return (
-    <div className="pb-3">
+    <div className="pb-3 container-fluid">
       <Flex gap="3" mt="2" mb="1">
         {renderProject()}
         <Metadata label="Experiment Key" value={trackingKey || "None"} />
