84fed7f81c4086baffae909d256ad18a1e83a7ba
Kevin-Chant <chant.kevin@yahoo.com>
2025-01-15 12:27:02 -0800

    Improve saved group reference checks (#3528)
    * Move checks for saved group references to utils
    
    * Add checks for experiments using saved groups

--- packages/front-end/pages/saved-groups/[sgid].tsx
+++ packages/front-end/pages/saved-groups/[sgid].tsx
@@ -4,8 +4,15 @@ import { SavedGroupInterface } from "shared/src/types";
 import { ago } from "shared/dates";
 import { FaPlusCircle } from "react-icons/fa";
 import { PiArrowsDownUp, PiWarningFill } from "react-icons/pi";
-import { getMatchingRules, isIdListSupportedDatatype } from "shared/util";
+import {
+  experimentsReferencingSavedGroups,
+  featuresReferencingSavedGroups,
+  isIdListSupportedDatatype,
+} from "shared/util";
 import Link from "next/link";
+import { FeatureInterface } from "back-end/types/feature";
+import { ExperimentInterfaceStringDates } from "back-end/types/experiment";
+import { isEmpty } from "lodash";
 import Field from "@/components/Forms/Field";
 import PageHead from "@/components/Layout/PageHead";
 import Pagination from "@/components/Pagination";
@@ -28,6 +35,7 @@ import { useDefinitions } from "@/services/DefinitionsContext";
 import ProjectBadges from "@/components/ProjectBadges";
 import { DocLink } from "@/components/DocLink";
 import Callout from "@/components/Radix/Callout";
+import { useExperiments } from "@/hooks/useExperiments";
 
 const NUM_PER_PAGE = 10;
 
@@ -39,6 +47,7 @@ export default function EditSavedGroupPage() {
   );
   const savedGroup = data?.savedGroup;
   const { features } = useFeaturesList(false);
+  const { experiments } = useExperiments();
   const environments = useEnvironments();
   const [sortNewestFirst, setSortNewestFirst] = useState<boolean>(true);
   const [addItems, setAddItems] = useState<boolean>(false);
@@ -90,29 +99,26 @@ export default function EditSavedGroupPage() {
     [mutate, savedGroup]
   );
 
-  const featuresReferencingSavedGroup = useMemo(() => {
-    const featuresReferencingSavedGroup: Set<string> = new Set();
-    if (!savedGroup) return featuresReferencingSavedGroup;
-    features.forEach((feature) => {
-      const matches = getMatchingRules(
-        feature,
-        (rule) =>
-          rule.condition?.includes(savedGroup.id) ||
-          rule.savedGroups?.some((g) => g.ids.includes(savedGroup.id)) ||
-          false,
-        environments.map((e) => e.id)
-      );
-
-      if (matches.length > 0) {
-        featuresReferencingSavedGroup.add(feature.id);
-      }
-    });
-    return featuresReferencingSavedGroup;
+  const referencingFeatures = useMemo(() => {
+    if (!savedGroup) return [] as FeatureInterface[];
+    return featuresReferencingSavedGroups({
+      savedGroups: [savedGroup],
+      features,
+      environments,
+    })[savedGroup.id];
   }, [savedGroup, features, environments]);
 
+  const referencingExperiments = useMemo(() => {
+    if (!savedGroup) return [] as ExperimentInterfaceStringDates[];
+    return experimentsReferencingSavedGroups({
+      savedGroups: [savedGroup],
+      experiments,
+    })[savedGroup.id];
+  }, [savedGroup, experiments]);
+
   const getConfirmationContent = useMemo(() => {
-    return getSavedGroupMessage(featuresReferencingSavedGroup);
-  }, [featuresReferencingSavedGroup]);
+    return getSavedGroupMessage(referencingFeatures, referencingExperiments);
+  }, [referencingFeatures, referencingExperiments]);
 
   const attr = (attributeSchema || []).find(
     (attr) => attr.property === savedGroup?.attributeKey
@@ -257,7 +263,9 @@ export default function EditSavedGroupPage() {
               text="Delete"
               title="Delete this Saved Group"
               getConfirmationContent={getConfirmationContent}
-              canDelete={(featuresReferencingSavedGroup?.size || 0) === 0}
+              canDelete={
+                isEmpty(referencingFeatures) && isEmpty(referencingExperiments)
+              }
               onClick={async () => {
                 await apiCall(`/saved-groups/${savedGroup.id}`, {
                   method: "DELETE",
