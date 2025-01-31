f239f405bbd2b257a81a90eaed779238b5698a9c
Luke Sonnet <luke.sonnet@gmail.com>
2025-01-31 15:01:45 -0800

    safer analysis retrieval

--- packages/front-end/components/PowerCalculation/PowerCalculationSettingsModal/index.tsx
+++ packages/front-end/components/PowerCalculation/PowerCalculationSettingsModal/index.tsx
@@ -162,6 +162,7 @@ export default function PowerCalculationSettingsModal({
                 }
               }
             } catch (e) {
+              console.log(e);
               form.setValue(
                 "metricValuesData.error",
                 `Unable to compute metric values: ${e.message}`
