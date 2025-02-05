dfa286305a3d4bf1662206520511d4cd1db59916
Ryan Feigenbaum <48868107+royalfig@users.noreply.github.com>
2025-02-05 15:02:32 -0500

    add feedback widget

---
+++ docs/src/theme/EditMetaRow/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+// eslint-disable-next-line import/no-unresolved
+import EditThisPage from "@theme/EditThisPage";
+import type { Props } from "@theme/EditMetaRow";
+
+import FeedbackWidget from "../../components/FeedbackWidget";
+import styles from "./styles.module.css";
+
+export default function EditMetaRow({ editUrl }: Props): JSX.Element {
+  return (
+    <div className={styles.docFooter}>
+      <div>{editUrl && <EditThisPage editUrl={editUrl} />}</div>
+      <FeedbackWidget />
+    </div>
+  );
+}
