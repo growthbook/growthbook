f905a8243ad0db73e5f2ebd65159ff39922e1c2c
Luke Sonnet <luke.sonnet@gmail.com>
2025-02-19 14:18:32 -0800

    Fix low powered to not show if shipping is ready

--- packages/front-end/components/Experiment/TabbedPage/ExperimentStatusIndicator.tsx
+++ packages/front-end/components/Experiment/TabbedPage/ExperimentStatusIndicator.tsx
@@ -153,17 +153,7 @@ function getStatusIndicatorData(
         powerStatus = getPowerStatus({
           power: healthSummary.power,
         });
-
-        if (
-          powerStatus?.isLowPowered &&
-          !experimentData.dismissedWarnings?.includes("low-power") &&
-          !beforeMinDuration
-        ) {
-          unhealthyStatuses.push("Low powered");
-          // If we have a override status from powerStatus, use it
-        } else if (powerStatus?.indicatorData) {
-          powerIndicatorData = powerStatus.indicatorData;
-        }
+        powerIndicatorData = powerStatus.indicatorData;
       }
 
       if (metricStatus) {
@@ -173,6 +163,15 @@ function getStatusIndicatorData(
           guardrailMetrics: experimentData.guardrailMetrics,
           powerStatus: powerStatus,
         });
+        // override low powered status if shipping criteria are ready
+        if (
+          !shippingIndicatorData &&
+          powerStatus?.isLowPowered &&
+          !experimentData.dismissedWarnings?.includes("low-power") &&
+          !beforeMinDuration
+        ) {
+          unhealthyStatuses.push("Low powered");
+        }
       }
     }
 
