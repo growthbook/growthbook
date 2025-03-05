5301443e7fa7524d40ebbb7a646abaf5cb806e3b
James Irwin <tallnerd@gmail.com>
2025-03-05 18:34:14 +0100

    fix import

--- packages/back-end/src/services/licenseData.ts
+++ packages/back-end/src/services/licenseData.ts
@@ -1,7 +1,7 @@
 import path from "path";
 import fs from "fs";
 import md5 from "md5";
-import { LicenseUserCodes } from "enterprise";
+import { LicenseUserCodes } from "shared/enterprise";
 import { findAllSDKConnectionsAcrossAllOrgs } from "back-end/src/models/SdkConnectionModel";
 import { getInstallationId } from "back-end/src/models/InstallationModel";
 import { IS_CLOUD } from "back-end/src/util/secrets";
