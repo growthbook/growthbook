38bfbadf2a8988e3e930c4336f96f2f9c7ad4342
Jeremy Dorn <jeremy@jeremydorn.com>
2025-03-05 21:13:42 -0600

    Change to React.JSX.Element

--- packages/sdk-react/src/GrowthBookReact.tsx
+++ packages/sdk-react/src/GrowthBookReact.tsx
@@ -154,9 +154,9 @@ export const withRunExperiment = <P extends WithRunExperimentProps>(
   Component: React.ComponentType<P>
 ): React.ComponentType<Omit<P, keyof WithRunExperimentProps>> => {
   // eslint-disable-next-line
-  const withRunExperimentWrapper = (props: any): React.ReactNode => (
+  const withRunExperimentWrapper = (props: any): React.JSX.Element => (
     <GrowthBookContext.Consumer>
-      {({ growthbook }): React.ReactNode => {
+      {({ growthbook }): React.JSX.Element => {
         return (
           <Component
             {...(props as P)}
@@ -170,14 +170,13 @@ export const withRunExperiment = <P extends WithRunExperimentProps>(
 };
 withRunExperiment.displayName = "WithRunExperiment";
 
-export type GBProviderType = (
-  props: React.PropsWithChildren<{ growthbook: GrowthBook }>
-) => React.ReactNode;
-
-export const GrowthBookProvider: GBProviderType = ({
+export function GrowthBookProvider({
   children,
   growthbook,
-}) => {
+}: {
+  children: React.ReactNode | React.JSX.Element;
+  growthbook: GrowthBook;
+}): React.JSX.Element {
   // Tell growthbook how to re-render our app (for dev mode integration)
   // eslint-disable-next-line
   const [_, setRenderCount] = React.useState(0);
@@ -203,4 +202,4 @@ export const GrowthBookProvider: GBProviderType = ({
       {children}
     </GrowthBookContext.Provider>
   );
-};
+}
