5d4a9c8b323e8f7e266b5360b722c889184c3485
Adriel Vieira <ahdriel@gmail.com>
2025-03-19 12:47:19 -0700

    Fixes to experimentTimeSeries (#3825)
    * Fixes to experimentTimeSeries
    
    Two things:
    - We might have invalid references pointing to metric settings that don't exist, so we are handling that now
    - `goalMetrics` and `guardrailMetrics` for `resultsStatus` are not saved in the db, so it is not a guarantee that the objects exist. Handling that as well by updating the types & fixing how we iterate over the metrics
      - Additional fix to include `goalMetrics` here that I forgot before.
    
    * Update type to be optional to reflect how it gets saved to DB
    
    * Fix

--- packages/back-end/src/services/experimentTimeSeries.ts
+++ packages/back-end/src/services/experimentTimeSeries.ts
@@ -141,7 +141,7 @@ export async function updateExperimentTimeSeries({
         metricId,
         experimentSnapshot.settings.metricSettings.find(
           (it) => it.id === metricId
-        )!,
+        ),
         factMetrics,
         factTableMap
       ),
@@ -214,13 +214,13 @@ function getExperimentSettingsHash(
 
 function getMetricSettingsHash(
   metricId: string,
-  metricSettings: MetricForSnapshot,
+  metricSettings?: MetricForSnapshot,
   factMetrics?: FactMetricInterface[],
   factTableMap?: Map<string, FactTableInterface>
 ): string {
   const factMetric = factMetrics?.find((metric) => metric.id === metricId);
   if (!factMetric) {
-    return hashObject(metricSettings);
+    return hashObject(metricSettings ?? { id: metricId });
   } else {
     const numeratorFactTableId = factMetric.numerator.factTableId;
     const numeratorFactTable = numeratorFactTableId
@@ -275,12 +275,22 @@ function getHasSignificantDifference(
 
   const parseToMap = (results: ExperimentAnalysisSummaryResultsStatus) => {
     return new Map(
-      results.variations.flatMap((variation) =>
-        Object.entries(variation.guardrailMetrics).map(([metricId, metric]) => [
-          `${variation.variationId}-${metricId}`,
-          metric.status,
-        ])
-      )
+      results.variations.flatMap((variation) => ({
+        ...(variation?.goalMetrics
+          ? Object.entries(variation.goalMetrics).map(([metricId, metric]) => [
+              `${variation.variationId}-${metricId}`,
+              metric.status,
+            ])
+          : []),
+        ...(variation?.guardrailMetrics
+          ? Object.entries(
+              variation.guardrailMetrics
+            ).map(([metricId, metric]) => [
+              `${variation.variationId}-${metricId}`,
+              metric.status,
+            ])
+          : []),
+      }))
     );
   };
 
