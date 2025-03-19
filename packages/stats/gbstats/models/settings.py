c77560016885dd1bc269733af3c87e4df902cd7e
Luke Smith <lukebrawleysmith@gmail.com>
2025-03-19 14:02:24 -0700

    updating AnalysisSettingsForStatsEngine

--- packages/stats/gbstats/models/settings.py
+++ packages/stats/gbstats/models/settings.py
@@ -28,6 +28,7 @@ class AnalysisSettingsForStatsEngine:
     max_dimensions: int = 20
     traffic_percentage: float = 1
     num_goal_metrics: int = 1
+    one_sided_intervals: bool = False
 
 
 @dataclass
