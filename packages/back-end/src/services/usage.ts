2f885015585a5eddba52cfb65da02e4a2ba1d89c
Romain Beauxis <romain@growthbook.io>
2025-03-21 10:00:24 -0500

    Reconcile.

--- packages/back-end/src/services/usage.ts
+++ packages/back-end/src/services/usage.ts
@@ -4,16 +4,40 @@ import { getEffectiveAccountPlan } from "shared/enterprise";
 import { MONGODB_URI } from "back-end/src/util/secrets";
 import { UsageLimits } from "back-end/types/organization";
 
+type OrganizationLimits = {
+  requests: number | "unlimited";
+  bandwidth: number | "unlimited";
+};
+
 type OrganizationUsage = {
-  limits: {
-    requests: number | "unlimited";
-    bandwidth: number | "unlimited";
+  limits: OrganizationLimits;
+  cdn: {
+    lastUpdated: Date;
+    status: "under" | "approaching" | "over";
   };
+};
+
+type OrganizationUsageDocument = {
+  organization: string;
+  usage: OrganizationUsage;
 };
 
 const backendLimits = ({
   limits: { requests: cdnRequests, bandwidth: cdnBandwidth },
 }: OrganizationUsage) => ({ cdnRequests, cdnBandwidth });
+
+export const STARTER_PLAN_LIMITS = {
+  requests: 1_000_000,
+  bandwidth: 5_000_000_000,
+} as const;
+export const PRO_PLAN_LIMITS = {
+  requests: 2_000_000,
+  bandwidth: 20_000_000_000,
+} as const;
+export const ENTERPRISE_PLAN_LIMITS = {
+  requests: "unlimited",
+  bandwidth: "unlimited",
+} as const;
 
 let cachedClient: MongoClient | undefined;
 let cachedDb: Db | undefined;
@@ -39,25 +63,44 @@ export const closeMongoUsageDb = async () => {
 export const getOrgUsageLimits = async (
   organization: string
 ): Promise<UsageLimits> => {
-  const collection = (await getMongoUsageDb()).collection<OrganizationUsage>(
-    "organizations_usage"
-  );
+  const collection = (
+    await getMongoUsageDb()
+  ).collection<OrganizationUsageDocument>("organizations_usage");
 
-  const storedLimits = await collection.findOne({
+  const storedUsage = await collection.findOne({
     organization,
   });
 
-  if (storedLimits) return backendLimits(storedLimits);
-
   const plan = getEffectiveAccountPlan({ id: organization });
 
-  if (plan === "starter") {
-    return { cdnRequests: 1_000_000, cdnBandwidth: 5_000_000_000 };
+  const planLimits = (() => {
+    if (plan === "starter")
+      return storedUsage?.usage.limits || STARTER_PLAN_LIMITS;
+
+    if (plan === "pro" || plan === "pro_sso") return PRO_PLAN_LIMITS;
+
+    return ENTERPRISE_PLAN_LIMITS;
+  })();
+
+  const planUsage: OrganizationUsage = {
+    cdn: {
+      lastUpdated: new Date(),
+      status: "under",
+    },
+    ...storedUsage?.usage,
+    limits: planLimits,
+  };
+
+  if (
+    !storedUsage ||
+    planUsage.limits.requests !== storedUsage.usage.limits.requests ||
+    planUsage.limits.bandwidth !== storedUsage.usage.limits.bandwidth
+  ) {
+    await collection.updateOne(
+      { organization },
+      { $set: { usage: planUsage } }
+    );
   }
 
-  if (plan === "pro" || plan === "pro_sso") {
-    return { cdnRequests: 2_000_000, cdnBandwidth: 20_000_000_000 };
-  }
-
-  return { cdnRequests: "unlimited", cdnBandwidth: "unlimited" };
+  return backendLimits(planUsage);
 };
