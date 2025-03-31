e9f54f7a002feaf12cbd2a7f33087800777bb5c0
Luke Smith <lukebrawleysmith@gmail.com>
2025-03-31 09:41:23 -0700

    adding ratio regression class

--- packages/stats/gbstats/frequentist/post_strat.py
+++ packages/stats/gbstats/frequentist/post_strat.py
@@ -9,7 +9,7 @@ from gbstats.models.statistics import (
     ProportionStatistic,
     RegressionAdjustedStatistic,
     RatioStatistic,
-    # RegressionAdjustedRatioStatistic,
+    RegressionAdjustedRatioStatistic,
     compute_theta,
 )
 
@@ -209,89 +209,82 @@ class PostStratificationRatio(BasePostStratification):
         return np.array([[0, 0, 1, 0], [1, 0, -1, 0], [0, 0, 0, 1], [0, 1, 0, -1]])
 
 
-# class PostStratificationRegressionAdjustedRatio(BasePostStratification):
-#     def __init__(
-#         self,
-#         stat_a: RegressionAdjustedRatioStatistic,
-#         stat_b: RegressionAdjustedRatioStatistic,
-#         config: FrequentistConfig(),
-#     ):
-#         self.stat_a = stat_a
-#         self.stat_b = stat_b
-#         self.config = config
+class PostStratificationRegressionAdjustedRatio(BasePostStratification):
+    def __init__(
+        self,
+        stat_a: RegressionAdjustedRatioStatistic,
+        stat_b: RegressionAdjustedRatioStatistic,
+        config: FrequentistConfig = FrequentistConfig(),
+    ):
+        self.stat_a = stat_a
+        self.stat_b = stat_b
+        self.config = config
 
+    @property
+    def strata_means(self) -> np.ndarray:
+        return np.array(
+            [
+                self.stat_b.m_statistic_post.mean,
+                self.stat_b.d_statistic_post.mean,
+                self.stat_b.m_statistic_pre.mean,
+                self.stat_b.d_statistic_pre.mean,
+                self.stat_a.m_statistic_post.mean,
+                self.stat_a.d_statistic_post.mean,
+                self.stat_a.m_statistic_pre.mean,
+                self.stat_a.d_statistic_pre.mean,
+            ]
+        )
 
-#     @property
-#     def strata_means(self) -> List[float]:
-#         return [
-#             self.stat_b.m_statistic_post.mean,
-#             self.stat_b.d_statistic_post.mean,
-#             self.stat_b.m_statistic_pre.mean,
-#             self.stat_b.d_statistic_pre.mean,
-#             self.stat_a.m_statistic_post.mean,
-#             self.stat_a.d_statistic_post.mean,
-#             self.stat_a.m_statistic_pre.mean,
-#             self.stat_a.d_statistic_pre.mean,
-#         ]
+    @property
+    def strata_covariance(self) -> np.ndarray:
+        v = np.zeros((8, 8))
+        v[0:4, 0:4] = self.stat_b.lambda_matrix
+        v[4:8, 4:8] = self.stat_b.lambda_matrix
+        return v
 
-#     @property
-#     def strata_covariance(self) -> np.ndarray:
-#         v = np.zeros((8, 8))
-#         v[0:4, 0:4] = self.stat_b.lambda_matrix
-#         v[4:8, 4:8] = self.stat_b.lambda_matrix
-#         return v
+    @property
+    def theta_numerator(self) -> float:
+        a = RegressionAdjustedStatistic(
+            n=self.stat_a.n,
+            post_statistic=self.stat_a.m_statistic_post,
+            pre_statistic=self.stat_a.m_statistic_pre,
+            post_pre_sum_of_products=self.stat_a.m_post_m_pre_sum_of_products,
+            theta=0,
+        )
+        b = RegressionAdjustedStatistic(
+            n=self.stat_b.n,
+            post_statistic=self.stat_b.m_statistic_post,
+            pre_statistic=self.stat_b.m_statistic_pre,
+            post_pre_sum_of_products=self.stat_b.m_post_m_pre_sum_of_products,
+            theta=0,
+        )
+        return compute_theta(a, b)
 
-#     @property
-#     def transformation_matrix(self) -> np.ndarray:
-#         return np.ones(5)
+    @property
+    def theta_denominator(self) -> float:
+        a = RegressionAdjustedStatistic(
+            n=self.stat_a.n,
+            post_statistic=self.stat_a.d_statistic_post,
+            pre_statistic=self.stat_a.d_statistic_pre,
+            post_pre_sum_of_products=self.stat_a.d_post_d_pre_sum_of_products,
+            theta=0,
+        )
+        b = RegressionAdjustedStatistic(
+            n=self.stat_b.n,
+            post_statistic=self.stat_b.d_statistic_post,
+            pre_statistic=self.stat_b.d_statistic_pre,
+            post_pre_sum_of_products=self.stat_b.d_post_d_pre_sum_of_products,
+            theta=0,
+        )
+        return compute_theta(a, b)
 
-#     @property
-#     def theta_numerator(self) -> float:
-#         a = RegressionAdjustedStatistic(
-#             n=self.stat_a.n,
-#             post_statistic=self.stat_a.m_statistic_post,
-#             pre_statistic=self.stat_a.m_statistic_pre,
-#             post_pre_sum_of_products=self.stat_a.m_post_m_pre_sum_of_products,
-#             theta=0,
-#         )
-#         y = compute_theta(a, a)
-#         b = RegressionAdjustedStatistic(
-#             n=self.stat_b.n,
-#             post_statistic=self.stat_b.m_statistic_post,
-#             pre_statistic=self.stat_b.m_statistic_pre,
-#             post_pre_sum_of_products=self.stat_b.m_post_m_pre_sum_of_products,
-#             theta=y,
-#         )
-#         return float(b.n)
-#         #return compute_theta(a, b)
-
-# #     @property
-#     def theta_denominator(self) -> float:
-#         a = RegressionAdjustedStatistic(
-#             n=self.stat_a.n,
-#             post_statistic=self.stat_a.d_statistic_post,
-#             pre_statistic=self.stat_a.d_statistic_pre,
-#             post_pre_sum_of_products=self.stat_a.d_post_d_pre_sum_of_products,
-#             theta=0,
-#         )
-#         b = RegressionAdjustedStatistic(
-#             n=self.stat_b.n,
-#             post_statistic=self.stat_b.d_statistic_post,
-#             pre_statistic=self.stat_b.d_statistic_pre,
-#             post_pre_sum_of_products=self.stat_b.d_post_d_pre_sum_of_products,
-#             theta=0,
-#         )
-#         return compute_theta(a, b)
-
-#     @property
-#     def transformation_matrix(self) -> np.ndarray:
-#         theta_numerator = self.theta_numerator if self.theta_numerator is not None else 0
-#         theta_denominator = self.theta_denominator if self.theta_denominator is not None else 0
-#         return np.array(
-#             [
-#                 [0, 0, 0, 0, 0, 0, 1, 0],
-#                 [1, 0, -theta_numerator, 0, -1, 0, theta_numerator, 0],
-#                 [0, 0, 0, 0, 0, 1, 0, 0],
-#                 [0, 1, 0, -theta_denominator, 0, -1, 0, theta_denominator],
-#             ]
-#         )
+    @property
+    def transformation_matrix(self) -> np.ndarray:
+        return np.array(
+            [
+                [0, 0, 0, 0, 0, 0, 1, 0],
+                [1, 0, -self.theta_numerator, 0, -1, 0, self.theta_numerator, 0],
+                [0, 0, 0, 0, 0, 1, 0, 0],
+                [0, 1, 0, -self.theta_denominator, 0, -1, 0, self.theta_denominator],
+            ]
+        )
