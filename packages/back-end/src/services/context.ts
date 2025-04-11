4565434d7207827680bf77af81af58ebface6732
Adriel Vieira <ahdriel@gmail.com>
2025-04-11 13:49:42 -0700

    fix

--- packages/back-end/src/services/context.ts
+++ packages/back-end/src/services/context.ts
@@ -77,7 +77,7 @@ export class ReqContextClass {
       segments: new SegmentModel(this),
       experimentTemplates: new ExperimentTemplatesModel(this),
       safeRolloutSnapshots: new SafeRolloutSnapshotModel(this),
-      safeRollouts: new SafeRolloutModel(this),
+      safeRollout: new SafeRolloutModel(this),
       decisionCriteria: new DecisionCriteriaModel(this),
       metricTimeSeries: new MetricTimeSeriesModel(this),
     };
