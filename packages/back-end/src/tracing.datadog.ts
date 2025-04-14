6d7ef91959a0793749db34f8872506ba61ca5823
Romain Beauxis <romain@growthbook.io>
2025-04-14 16:15:42 -0500

    Fix DD counter.

--- packages/back-end/src/tracing.datadog.ts
+++ packages/back-end/src/tracing.datadog.ts
@@ -6,12 +6,31 @@ tracer.init({
   logInjection: true,
 });
 
+const COLLECTION_INTERVAL = 15;
+
+class Counter {
+  name: string;
+  value: number;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  attributes?: any;
+
+  constructor(name: string) {
+    this.name = name;
+    this.value = 0;
+    setInterval(this.collect, COLLECTION_INTERVAL);
+  }
+
+  add(v: number) {
+    this.value += v;
+  }
+
+  collect() {
+    tracer.dogstatsd.gauge(this.name, this.value, this.attributes);
+  }
+}
+
 setMetrics({
-  getCounter: (name: string) => ({
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    add: (incr: number, attributes?: any) =>
-      tracer.dogstatsd.gauge(name, incr, attributes),
-  }),
+  getCounter: (name: string) => new Counter(name),
   getHistogram: (name: string) => ({
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     record: (value: number, attributes?: any) =>
