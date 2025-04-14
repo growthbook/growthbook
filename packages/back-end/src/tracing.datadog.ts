36aae0af926a13d7649115c44d0c70a84d20c99e
Romain Beauxis <romain@growthbook.io>
2025-04-14 16:18:41 -0500

    Fix histogram.

--- packages/back-end/src/tracing.datadog.ts
+++ packages/back-end/src/tracing.datadog.ts
@@ -20,8 +20,10 @@ class Counter {
     setInterval(this.collect, COLLECTION_INTERVAL);
   }
 
-  add(v: number) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  add(v: number, attributes?: any) {
     this.value += v;
+    this.attributes = attributes;
   }
 
   collect() {
@@ -29,11 +31,38 @@ class Counter {
   }
 }
 
+class Histogram {
+  name: string;
+  value: number;
+  count: number;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  attributes?: any;
+
+  constructor(name: string) {
+    this.name = name;
+    this.value = 0;
+    this.count = 0;
+    setInterval(this.collect, COLLECTION_INTERVAL);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  record(v: number, attributes?: any) {
+    this.value += v;
+    this.count++;
+    this.attributes = attributes;
+  }
+
+  collect() {
+    if (this.count)
+      tracer.dogstatsd.gauge(
+        this.name,
+        this.value / this.count,
+        this.attributes
+      );
+  }
+}
+
 setMetrics({
   getCounter: (name: string) => new Counter(name),
-  getHistogram: (name: string) => ({
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    record: (value: number, attributes?: any) =>
-      tracer.dogstatsd.histogram(name, value, attributes),
-  }),
+  getHistogram: (name: string) => new Histogram(name),
 });
