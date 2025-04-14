25b9215ebdbbe6b3717063466b7bc0de8a9a85bc
Romain Beauxis <romain@growthbook.io>
2025-04-14 13:49:19 -0500

    Revert

--- packages/back-end/src/tracing.datadog.ts
+++ packages/back-end/src/tracing.datadog.ts
@@ -10,7 +10,7 @@ setMetrics({
   getCounter: (name: string) => ({
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     add: (incr: number, attributes?: any) =>
-      tracer.dogstatsd.count(name, incr, attributes),
+      tracer.dogstatsd.increment(name, incr, attributes),
   }),
   getHistogram: (name: string) => ({
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
