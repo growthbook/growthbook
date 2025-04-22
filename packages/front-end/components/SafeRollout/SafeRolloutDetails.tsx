5c085bcc369442254aca8e2ab2435c25971a341b
Michael Samper <msamper@growthbook.io>
2025-04-22 09:30:35 -0700

    always show health section if showing details

--- packages/front-end/components/SafeRollout/SafeRolloutDetails.tsx
+++ packages/front-end/components/SafeRollout/SafeRolloutDetails.tsx
@@ -88,43 +88,41 @@ export default function SafeRolloutDetails({ safeRollout, rule }: Props) {
           <SafeRolloutResults safeRollout={safeRollout} />
         </Box>
 
-        {traffic && totalUsers ? (
-          <>
-            <Flex align="center" justify="between" mb="3">
-              <Text weight="medium" size="3">
-                Health
-              </Text>
-              <Link
-                weight="medium"
-                onClick={() => setIsHealthExpanded(!isHealthExpanded)}
-              >
-                Show {isHealthExpanded ? "less" : "more"}
-              </Link>
-            </Flex>
-            {isHealthExpanded ? (
-              <>
-                <SRMCard
-                  newDesign={true}
-                  traffic={traffic}
-                  variations={variations}
-                  totalUsers={totalUsers}
-                  onNotify={() => {}}
-                  dataSource={datasource}
-                  exposureQuery={exposureQuery}
-                  canConfigHealthTab={false}
-                />
-                <MultipleExposuresCard
-                  totalUsers={totalUsers}
-                  snapshot={snapshot}
-                  onNotify={() => {}}
-                />
-              </>
-            ) : null}
-          </>
-        ) : snapshot ? (
-          <Callout status="info" mt="3">
-            Please run a query to see health data.
-          </Callout>
+        <Flex align="center" justify="between" mb="3">
+          <Text weight="medium" size="3">
+            Health
+          </Text>
+          <Link
+            weight="medium"
+            onClick={() => setIsHealthExpanded(!isHealthExpanded)}
+          >
+            Show {isHealthExpanded ? "less" : "more"}
+          </Link>
+        </Flex>
+        {isHealthExpanded ? (
+          traffic && totalUsers ? (
+            <>
+              <SRMCard
+                newDesign={true}
+                traffic={traffic}
+                variations={variations}
+                totalUsers={totalUsers}
+                onNotify={() => {}}
+                dataSource={datasource}
+                exposureQuery={exposureQuery}
+                canConfigHealthTab={false}
+              />
+              <MultipleExposuresCard
+                totalUsers={totalUsers}
+                snapshot={snapshot}
+                onNotify={() => {}}
+              />
+            </>
+          ) : (
+            <Callout status="info" mt="3">
+              Please run a query to see health data.
+            </Callout>
+          )
         ) : null}
       </div>
     </div>
