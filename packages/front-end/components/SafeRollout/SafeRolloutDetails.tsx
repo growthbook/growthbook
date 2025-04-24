623750b27276e113c2f78c9fac85604bf636608d
Jeremy Dorn <jeremy@jeremydorn.com>
2025-04-24 11:08:58 -0500

    Safe Rollout Status Badge, Help Text, and CTA

--- packages/front-end/components/SafeRollout/SafeRolloutDetails.tsx
+++ packages/front-end/components/SafeRollout/SafeRolloutDetails.tsx
@@ -1,25 +1,14 @@
 import { Box } from "@radix-ui/themes";
 import { SafeRolloutInterface } from "back-end/src/validators/safe-rollout";
-import { SafeRolloutRule } from "back-end/src/validators/features";
 import SafeRolloutResults from "./SafeRolloutResults";
-import DecisionBanner from "./DecisionBanner";
 
 interface Props {
   safeRollout: SafeRolloutInterface;
-  rule: SafeRolloutRule;
-  openStatusModal: () => void;
 }
 
-export default function SafeRolloutDetails({
-  safeRollout,
-  rule,
-  openStatusModal,
-}: Props) {
+export default function SafeRolloutDetails({ safeRollout }: Props) {
   return (
     <div>
-      {rule.enabled && (
-        <DecisionBanner openStatusModal={openStatusModal} rule={rule} />
-      )}
       <div className="container-fluid pagecontents px-0 mt-4">
         <Box mb="6">
           <SafeRolloutResults safeRollout={safeRollout} />
