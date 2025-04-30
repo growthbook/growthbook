eb212c367aaa38dde3fe17bc7ebe3ae1b73ba585
Jeremy Dorn <jeremy@jeremydorn.com>
2025-04-29 21:54:31 -0500

    Fix weird type error

--- packages/sdk-js/src/plugins/index.ts
+++ packages/sdk-js/src/plugins/index.ts
@@ -7,6 +7,10 @@ export {
   devtoolsExpressPlugin,
   getDebugScriptContents,
   getDebugEvent,
+} from "./devtools";
+
+// Types must be exported separately, otherwise rollup includes them in the javascript output which breaks things
+export type {
   DevtoolsState,
   ExpressRequestCompat,
   NextjsReadonlyRequestCookiesCompat,
