f9016a0f942268b0906d9d03ea7b3e04f5e4fa43
gazzdingo <guy@langford-lee.com>
2025-05-13 09:20:08 -0700

    revert

--- packages/back-end/test/snapshots/feature.snapshot.ts
+++ packages/back-end/test/snapshots/feature.snapshot.ts
@@ -1,18 +1,12 @@
 export const featureSnapshot = {
-  id: "id",
-  organization: "org",
-  owner: "owner",
-  dateCreated: new Date(),
-  dateUpdated: new Date(),
-  valueType: "string" as const,
   defaultValue: "defaultValue",
-  version: 1,
+  valueType: "string",
+  owner: "owner",
   description: "description",
   project: "project",
+  id: "id",
   archived: true,
   tags: ["tag"],
-  environmentSettings: {
-    dev: { defaultValue: "defaultValue", enabled: false, rules: [] },
-    production: { defaultValue: "defaultValue", enabled: false, rules: [] },
-  },
+  dateCreated: new Date(),
+  dateUpdated: new Date(),
 };
