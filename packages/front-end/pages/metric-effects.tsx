a650755cea6da5adc811f65a60749bdb7a4e1b67
Luke Sonnet <luke.sonnet@gmail.com>
2025-06-09 13:39:12 -0400

    Simple text

--- packages/front-end/pages/metric-effects.tsx
+++ packages/front-end/pages/metric-effects.tsx
@@ -1,10 +1,15 @@
-import { Box, Heading } from "@radix-ui/themes";
+import { Box, Heading, Text } from "@radix-ui/themes";
 import MetricEffects from "@/enterprise/components/Insights/MetricEffects";
 
 const MetricEffectsPage = (): React.ReactElement => {
   return (
     <Box className="contents container-fluid pagecontents my-3">
       <Heading>Metric Effects</Heading>
+      <Box mb="2">
+        <Text>
+          View the distribution of experiment impacts on the selected metric.
+        </Text>
+      </Box>
       <MetricEffects />
     </Box>
   );
