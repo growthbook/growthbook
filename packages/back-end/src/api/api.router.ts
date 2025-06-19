3e66100c007c0f6b334c03957a67e96c821a0385
Dev Dalia <devdalia9@gmail.com>
2025-06-19 10:47:51 +0530

    feat(RESTAPI): metric group management enabled

--- packages/back-end/src/api/api.router.ts
+++ packages/back-end/src/api/api.router.ts
@@ -32,6 +32,7 @@ import ingestionRouter from "./ingestion/ingestion.router";
 import archetypesRouter from "./archetypes/archetypes.router";
 import { getExperimentNames } from "./experiments/getExperimentNames";
 import queryRouter from "./queries/queries.router";
+import metricGroupsRouter from "./metric-groups/metric-groups.router";
 
 const router = Router();
 let openapiSpec: string;
@@ -108,6 +109,7 @@ router.use("/members", membersRouter);
 router.use("/ingestion", ingestionRouter);
 router.use("/archetypes", archetypesRouter);
 router.use("/queries", queryRouter);
+router.use("/metric-groups", metricGroupsRouter);
 
 router.post("/transform-copy", postCopyTransform);
 
