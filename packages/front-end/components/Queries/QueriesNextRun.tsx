05d7a835edc579923bd37e2df5f6a7ea72aa684b
Dev Dalia <devdalia9@gmail.com>
2025-06-28 13:10:32 +0530

    Add QueriesNextRun component to show next schedule
    Introduced a new QueriesNextRun component to display the next scheduled snapshot attempt in the AnalysisSettingsSummary. This provides users with visibility into when the next experiment snapshot is planned.

---
+++ packages/front-end/components/Queries/QueriesNextRun.tsx
@@ -0,0 +1,23 @@
+import { FC } from "react";
+import { ago, datetime } from "shared/dates";
+
+const QueriesNextRun: FC<{
+  scheduledDate: Date | undefined;
+}> = ({ scheduledDate }) => {
+  return (
+    <div
+      className="text-muted text-left"
+      style={{ maxWidth: 130, fontSize: "0.8em" }}
+    >
+      <div className="font-weight-bold" style={{ lineHeight: 1.2 }}>
+        next schedule
+      </div>
+      <div className="d-flex align-items-center">
+        <div style={{ lineHeight: 1 }} title={datetime(scheduledDate ?? "")}>
+          {ago(scheduledDate ?? "")}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default QueriesNextRun;
