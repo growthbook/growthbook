7dbcbe1f2a7fec4bc163d7de6b618b9661f5a55b
gazzdingo <guy@langford-lee.com>
2025-07-01 11:23:18 -0700

    fixed code ref removal

--- packages/back-end/src/api/code-refs/postCodeRefs.ts
+++ packages/back-end/src/api/code-refs/postCodeRefs.ts
@@ -4,7 +4,8 @@ import { createApiRequestHandler } from "back-end/src/util/handler";
 import { postCodeRefsValidator } from "back-end/src/validators/openapi";
 import {
   getFeatureCodeRefsByFeatures,
-  upsertFeatureCodeRefs,
+  upsertFeatureCodeRefsWithRemoval,
+  getAllCodeRefsForOrg,
 } from "back-end/src/models/FeatureCodeRefs";
 
 export const postCodeRefs = createApiRequestHandler(postCodeRefsValidator)(
@@ -12,9 +13,45 @@ export const postCodeRefs = createApiRequestHandler(postCodeRefsValidator)(
     const { branch, repoName: repo } = req.body;
     const refsByFeature = groupBy(req.body.refs, "flagKey");
 
+    // Get all existing code references for this repo/branch combination
+    const allExistingCodeRefs = await getAllCodeRefsForOrg({
+      context: req.context,
+    });
+
+    const existingCodeRefsForRepoBranch = allExistingCodeRefs.filter(
+      (codeRef) => codeRef.repo === repo && codeRef.branch === branch
+    );
+
+    // Get the set of features that have existing references
+    const existingFeatures = new Set(
+      existingCodeRefsForRepoBranch.map((codeRef) => codeRef.feature)
+    );
+
+    // Get the set of features in the current request
+    const requestedFeatures = new Set(Object.keys(refsByFeature));
+
+    // Find features that exist but are not in the request (these should be removed)
+    const featuresToRemove = Array.from(existingFeatures).filter(
+      (feature) => !requestedFeatures.has(feature)
+    );
+
+    // Remove references for features not in the request
+    await Promise.all(
+      featuresToRemove.map(async (feature) => {
+        await upsertFeatureCodeRefsWithRemoval({
+          feature,
+          repo,
+          branch,
+          codeRefs: [], // Empty array will trigger removal
+          organization: req.context.org,
+        });
+      })
+    );
+
+    // Update references for features in the request
     await Promise.all(
       values(refsByFeature).map(async (refs) => {
-        await upsertFeatureCodeRefs({
+        await upsertFeatureCodeRefsWithRemoval({
           feature: refs[0].flagKey,
           repo,
           branch,
@@ -24,12 +61,15 @@ export const postCodeRefs = createApiRequestHandler(postCodeRefsValidator)(
       })
     );
 
+    // Get all features that were updated (both added/updated and removed)
+    const allAffectedFeatures = [...requestedFeatures, ...featuresToRemove];
+
     return {
       featuresUpdated: (
         await getFeatureCodeRefsByFeatures({
           repo,
           branch,
-          features: Object.keys(refsByFeature),
+          features: allAffectedFeatures,
           organization: req.context.org,
         })
       ).map((f) => f.feature),
