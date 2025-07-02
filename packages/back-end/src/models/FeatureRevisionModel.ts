f26c8d09693acf1b6853c0a81152a8047e29af25
James Irwin <tallnerd@gmail.com>
2025-07-02 12:49:11 +0200

    removed unecessary type declaration

--- packages/back-end/src/models/FeatureRevisionModel.ts
+++ packages/back-end/src/models/FeatureRevisionModel.ts
@@ -101,16 +101,13 @@ export async function getMinimalRevisions(
     .sort({ version: -1 })
     .limit(25);
 
-  return docs.map(
-    (m) =>
-      ({
-        version: m.version,
-        datePublished: m.datePublished,
-        dateUpdated: m.dateUpdated,
-        createdBy: m.createdBy,
-        status: m.status,
-      } as MinimalFeatureRevisionInterface)
-  );
+  return docs.map((m) => ({
+    version: m.version,
+    datePublished: m.datePublished,
+    dateUpdated: m.dateUpdated,
+    createdBy: m.createdBy,
+    status: m.status,
+  }));
 }
 
 export async function getLatestRevisions(
