437afdbdec6150197c264485788a0fa2c5cf1241
Michael Samper <msamper@growthbook.io>
2025-07-23 14:31:19 -0700

    remove console log

--- packages/back-end/src/routers/holdout/holdout.controller.ts
+++ packages/back-end/src/routers/holdout/holdout.controller.ts
@@ -24,7 +24,6 @@ import { FeatureInterface } from "back-end/types/feature";
 import { logger } from "back-end/src/util/logger";
 import {
   createExperimentSnapshot,
-  deleteExperiment,
   SNAPSHOT_TIMEOUT,
   validateVariationIds,
 } from "back-end/src/controllers/experiments";
@@ -382,7 +381,7 @@ export const deleteHoldout = async (
   res: Response<{ status: 200 | 404 | 403; message?: string }>
 ) => {
   const context = getContextFromReq(req);
-  console.log("id", req.params.id);
+
   const holdout = await context.models.holdout.getById(req.params.id);
 
   // TODO: Add holdout permissions check
