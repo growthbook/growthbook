69551da755f8a220c9931cd8b7c9a4463bd04e4c
Michael Samper <msamper@growthbook.io>
2025-08-25 10:47:18 -0700

    misc copy

--- packages/front-end/components/Experiment/TabbedPage/startHoldoutAnalysisModal.tsx
+++ packages/front-end/components/Experiment/TabbedPage/startHoldoutAnalysisModal.tsx
@@ -21,9 +21,10 @@ export default function StartAnalysisModal({ close, startAnalysis }: Props) {
     >
       <div className="p-2">
         <div>
-          Once you start Analysis Period you will not be able to add any
-          features or experiments to the holdout. The holdout will continue to
-          run and collect data until you stop the holdout
+          Once you start the Analysis Period you will not be able to add any
+          features or experiments to the holdout. Units will continue to be held
+          out from existing features and experiments and data will continue
+          collecting until you stop the holdout.
         </div>
       </div>
     </Modal>
