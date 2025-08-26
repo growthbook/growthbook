326391f4783cca4b4f7ee4e330e7f79a37bf33f5
Michael Samper <msamper@growthbook.io>
2025-08-26 10:49:57 -0700

    hide HoldoutTimeline if theres not at least one none draft holdout experiment

--- packages/front-end/components/Experiment/TabbedPage/SetupTabOverview.tsx
+++ packages/front-end/components/Experiment/TabbedPage/SetupTabOverview.tsx
@@ -79,6 +79,15 @@ export default function SetupTabOverview({
 
   const isBandit = experiment.type === "multi-armed-bandit";
   const isHoldout = experiment.type === "holdout";
+  const showHoldoutTimeline =
+    isHoldout &&
+    holdout &&
+    experiment.status !== "draft" &&
+    holdoutExperiments &&
+    experiment.phases[0]?.dateStarted &&
+    new Date(experiment.phases[0].dateStarted) &&
+    holdoutExperiments.length > 0 &&
+    holdoutExperiments.some((e) => e.status !== "draft");
   const { hasCommercialFeature } = useUser();
   const hasAISuggestions = hasCommercialFeature("ai-suggestions");
 
@@ -199,25 +208,23 @@ export default function SetupTabOverview({
           </Collapsible>
         </Frame>
 
-        {isHoldout &&
-          holdout &&
-          experiment.status !== "draft" &&
-          holdoutExperiments &&
-          experiment.phases[0]?.dateStarted &&
-          new Date(experiment.phases[0].dateStarted) &&
-          holdoutExperiments.length > 0 && (
-            <div className="box p-4 my-4">
-              <HoldoutTimeline
-                experiments={holdoutExperiments}
-                startDate={new Date(experiment.phases[0].dateStarted)}
-                holdoutEndDate={
-                  experiment.phases[0]?.dateEnded
-                    ? new Date(experiment.phases[0].dateEnded)
-                    : undefined
-                }
-              />
-            </div>
-          )}
+        {showHoldoutTimeline && (
+          <div className="box p-4 my-4">
+            <HoldoutTimeline
+              experiments={holdoutExperiments}
+              startDate={
+                experiment.phases[0]?.dateStarted
+                  ? new Date(experiment.phases[0].dateStarted)
+                  : new Date()
+              }
+              holdoutEndDate={
+                experiment.phases[0]?.dateEnded
+                  ? new Date(experiment.phases[0].dateEnded)
+                  : undefined
+              }
+            />
+          </div>
+        )}
 
         {!isBandit && !isHoldout && (
           <Frame>
