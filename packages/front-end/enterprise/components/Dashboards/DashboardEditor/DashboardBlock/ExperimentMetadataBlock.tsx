5d1bdf325e7764421a3627ad39d218c35ecb9153
Jeremy Dorn <jeremy@jeremydorn.com>
2025-08-28 17:57:59 -0500

    Design updates for traffic and metadata blocks

--- packages/front-end/enterprise/components/Dashboards/DashboardEditor/DashboardBlock/ExperimentMetadataBlock.tsx
+++ packages/front-end/enterprise/components/Dashboards/DashboardEditor/DashboardBlock/ExperimentMetadataBlock.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { Fragment, ReactElement } from "react";
 import { ExperimentMetadataBlockInterface } from "back-end/src/enterprise/validators/dashboard-block";
-import { ScrollArea, Text } from "@radix-ui/themes";
+import { ScrollArea, Separator, Text } from "@radix-ui/themes";
 import { Box } from "spectacle";
 import Markdown from "@/components/Markdown/Markdown";
 import VariationsTable from "@/components/Experiment/VariationsTable";
@@ -15,53 +15,77 @@ export default function ExperimentMetadataBlock({
       ? experiment.variations.map(({ id }) => id)
       : variationIds;
 
-  return (
-    <>
-      {showDescription && (
-        <>
-          <Text weight="medium">Description</Text>
-          {experiment.description ? (
-            <Box as="div" py="2">
-              <ScrollArea
-                style={{
-                  maxHeight: "491px",
-                }}
-              >
-                <Markdown>{experiment.description}</Markdown>
-              </ScrollArea>
-            </Box>
-          ) : (
-            <Box as="div" className="font-italic text-muted" py="2">
-              This experiment doesn&apos;t have a description yet.
-            </Box>
-          )}
-        </>
-      )}
-      {showHypothesis && (
-        <>
-          <Text weight="medium">Hypothesis</Text>
+  const blockParts: ReactElement[] = [];
 
-          {experiment.hypothesis ? (
-            <Box as="div" py="2">
-              {experiment.hypothesis}
-            </Box>
-          ) : (
-            <Box as="div" className="font-italic text-muted" py="2">
-              This experiment doesn&apos;t have a hypothesis yet.
-            </Box>
-          )}
-        </>
-      )}
-      {showVariationImages && (
+  if (showDescription) {
+    blockParts.push(
+      <>
+        <Text weight="medium" size="3">
+          Description
+        </Text>
+        {experiment.description ? (
+          <Box as="div" py="2" style={{ opacity: 0.8 }}>
+            <ScrollArea
+              style={{
+                maxHeight: "491px",
+              }}
+            >
+              <Markdown>{experiment.description}</Markdown>
+            </ScrollArea>
+          </Box>
+        ) : (
+          <Box as="div" className="font-italic text-muted" py="2">
+            This experiment doesn&apos;t have a description yet.
+          </Box>
+        )}
+      </>,
+    );
+  }
+  if (showHypothesis) {
+    blockParts.push(
+      <>
+        <Text weight="medium" size="3">
+          Hypothesis
+        </Text>
+        {experiment.hypothesis ? (
+          <Box as="div" py="2" style={{ opacity: 0.8 }}>
+            {experiment.hypothesis}
+          </Box>
+        ) : (
+          <Box as="div" className="font-italic text-muted" py="2">
+            This experiment doesn&apos;t have a hypothesis yet.
+          </Box>
+        )}
+      </>,
+    );
+  }
+  if (showVariationImages) {
+    blockParts.push(
+      <>
+        <Text weight="medium" size="3" mb="2">
+          Variations
+        </Text>
         <div className="variation-image-block">
           <VariationsTable
             experiment={experiment}
             variationsList={variationsList}
             canEditExperiment={false}
             allowImages={true}
+            noMargin={true}
           />
         </div>
-      )}
+      </>,
+    );
+  }
+
+  return (
+    <>
+      {blockParts.map((block, i) => (
+        <Fragment key={i}>
+          {block}
+          {i < blockParts.length - 1 && <Separator size="4" my="3" />}
+        </Fragment>
+      ))}
     </>
   );
 }
