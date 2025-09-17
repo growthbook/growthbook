181f31d05ddbe16936af8177e7dd1ef21dce1ff5
Michael Samper <msamper@growthbook.io>
2025-09-17 14:14:52 -0700

    address comments

--- packages/front-end/components/FactTables/NewMetricModal.tsx
+++ packages/front-end/components/FactTables/NewMetricModal.tsx
@@ -76,17 +76,11 @@ export function NewMetricModal({ close, source, datasource }: NewMetricProps) {
     .filter((f) => isProjectListValidForProject(f.projects, project));
 
   // Determine the most appropriate default type based on what the org has already created
-  // - If there are no metrics, default to fact
-  // - If there are fact tables, default to fact
   // - If there are no fact tables and there is at least one legacy metric, default to legacy
   // - Otherwise, default to fact
   // TODO: add an org setting to explicitly override this default
   let defaultType: "fact" | "legacy" = "fact";
-  if (filteredMetrics.length === 0) {
-    defaultType = "fact";
-  } else if (filteredFactTables.length > 0) {
-    defaultType = "fact";
-  } else if (filteredFactTables.length === 0 && filteredMetrics.length > 0) {
+  if (filteredFactTables.length === 0 && filteredMetrics.length > 0) {
     defaultType = "legacy";
   }
 
