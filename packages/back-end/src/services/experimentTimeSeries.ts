a0481b0ee842d2364db2d7d88023a3621a6ec048
Bryce <bryce1@gmail.com>
2025-10-03 16:13:38 -0700

    remove todo

--- packages/back-end/src/services/experimentTimeSeries.ts
+++ packages/back-end/src/services/experimentTimeSeries.ts
@@ -43,8 +43,6 @@ export async function updateExperimentTimeSeries({
   experimentSnapshot: ExperimentSnapshotInterface;
   notificationsTriggered: string[];
 }) {
-  // todo: make sure custom dimension metrics are querying the API properly
-
   // Only update time series for dimensionless snapshots, but if we want to
   // support dimensions for time series, we should revisit this
   if (
