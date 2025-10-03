04781dcf390f84f0152562fa2050a2a58e0121c2
Michael <mknowlton89@gmail.com>
2025-10-03 10:24:51 -0400

    Moves checkbox out of sub-tabs and removes comments

--- packages/back-end/src/models/FactMetricModel.ts
+++ packages/back-end/src/models/FactMetricModel.ts
@@ -174,7 +174,6 @@ export class FactMetricModel extends BaseClass {
   }
 
   protected async beforeUpdate(existing: FactMetricInterface) {
-    // Check the admin permission here?
     if (existing.managedBy === "api" && !this.context.isApiRequest) {
       throw new Error(
         "Cannot update fact metric managed by API if the request isn't from the API.",
@@ -183,7 +182,6 @@ export class FactMetricModel extends BaseClass {
   }
 
   protected async beforeDelete(existing: FactMetricInterface) {
-    // Check the admin permission here?
     if (existing.managedBy === "api" && !this.context.isApiRequest) {
       throw new Error(
         "Cannot delete fact metric managed by API if the request isn't from the API.",
