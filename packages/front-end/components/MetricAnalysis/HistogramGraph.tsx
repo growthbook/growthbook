dc45b899f5b9e3e76a5cd15d2135a7ddf9562b39
Luke Sonnet <luke.sonnet@gmail.com>
2025-10-09 15:00:41 -0700

    fix(graph): histogram x-axis should not be hard coded

--- packages/front-end/components/MetricAnalysis/HistogramGraph.tsx
+++ packages/front-end/components/MetricAnalysis/HistogramGraph.tsx
@@ -23,6 +23,7 @@ type TooltipData = { x: number; y: number; d: Datapoint };
 
 interface HistogramGraphProps {
   data: Datapoint[];
+  xAxisLabel?: string;
   mean?: number;
   formatter: (value: number, options?: Intl.NumberFormatOptions) => string;
   height?: number;
@@ -74,6 +75,7 @@ const HistogramGraph: FC<HistogramGraphProps> = ({
   data,
   mean,
   formatter,
+  xAxisLabel,
   height = 220,
   margin = [15, 15, 30, 80],
   highlightPositiveNegative = false,
@@ -367,7 +369,7 @@ const HistogramGraph: FC<HistogramGraphProps> = ({
                     fontSize: 10,
                     textAnchor: "middle",
                   })}
-                  label="Lift"
+                  label={xAxisLabel}
                   labelClassName="h5"
                   labelOffset={25}
                   labelProps={{
@@ -393,7 +395,7 @@ const HistogramGraph: FC<HistogramGraphProps> = ({
                     style: { fill: "var(--slate-11)", translate: "0 15px" },
                   }}
                   labelClassName="h5"
-                  labelOffset={55}
+                  labelOffset={75}
                 />
                 <line
                   x1={contentXScale(0)}
