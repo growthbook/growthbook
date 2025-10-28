0c3b31bf395095e101ef6d4443ca3930cc983b0e
Luke Sonnet <luke.sonnet@gmail.com>
2025-10-28 13:34:43 -0700

    Tests pass and fix type issues

--- packages/back-end/test/sqlintegration.test.ts
+++ packages/back-end/test/sqlintegration.test.ts
@@ -88,6 +88,8 @@ describe("bigquery integration", () => {
         metric: normalSqlMetric,
         overrideConversionWindows: false,
         endDate: new Date(),
+        metricTimestampCol: "m.timestamp",
+        exposureTimestampCol: "d.timestamp",
       }).replace(/\s+/g, " "),
     ).toEqual(
       "(CASE WHEN m.timestamp >= d.timestamp AND m.timestamp <= DATETIME_ADD(d.timestamp, INTERVAL 72 HOUR) THEN val ELSE NULL END)",
@@ -103,6 +105,8 @@ describe("bigquery integration", () => {
         metric: normalSqlMetric,
         overrideConversionWindows: true,
         endDate: date,
+        metricTimestampCol: "m.timestamp",
+        exposureTimestampCol: "d.timestamp",
       }).replace(/\s+/g, " "),
     ).toEqual(
       `(CASE WHEN m.timestamp >= d.timestamp ${endDateFilter} THEN val ELSE NULL END)`,
