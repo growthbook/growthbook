d688581b020423b9cc307feafb01b0f60a629de2
David Hernando <david.hernando@lingokids.com>
2025-11-13 16:29:28 +0100

    Filter experimetns by udpated at column

--- packages/back-end/src/models/ExperimentModel.ts
+++ packages/back-end/src/models/ExperimentModel.ts
@@ -403,10 +403,12 @@ export async function getAllExperiments(
     project,
     includeArchived = false,
     type,
+    onlyUpdatedSince,
   }: {
     project?: string;
     includeArchived?: boolean;
     type?: ExperimentType;
+    onlyUpdatedSince?: Date;
   } = {},
 ): Promise<ExperimentInterface[]> {
   const query: FilterQuery<ExperimentDocument> = {
@@ -429,6 +431,10 @@ export async function getAllExperiments(
     query.type = "holdout";
   } else if (!type) {
     query.type = { $ne: "holdout" };
+  }
+
+  if (onlyUpdatedSince) {
+    query.dateUpdated = { $gte: new Date(onlyUpdatedSince) };
   }
 
   return await findExperiments(context, query);
