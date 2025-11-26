e42b9b5392f504e9f6b51023bca8baf096ff394a
Kevin Chant <chant.kevin@yahoo.com>
2025-11-26 12:15:01 +0900

    Add missing redirect on duplication

--- packages/front-end/enterprise/components/Dashboards/DashboardsTab.tsx
+++ packages/front-end/enterprise/components/Dashboards/DashboardsTab.tsx
@@ -219,6 +219,9 @@ function DashboardsTab({
         if (dashboardId === "new") {
           setTemporaryDashboard(res.dashboard);
         }
+        if (method === "POST" && dashboardId !== "new") {
+          setDashboardId(res.dashboard.id);
+        }
       } else {
         console.error(res);
       }
