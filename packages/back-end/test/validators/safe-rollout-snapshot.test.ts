979ba9d4834564a12f3c573ce8391eea48386345
Adriel Vieira <ahdriel@gmail.com>
2025-12-03 11:58:02 -0800

    Remove unnecessary casting in test

--- packages/back-end/test/validators/safe-rollout-snapshot.test.ts
+++ packages/back-end/test/validators/safe-rollout-snapshot.test.ts
@@ -10,8 +10,8 @@ describe("safe-rollout-snapshot validators", () => {
   it("accepts Infinity and -Infinity values in ci fields", () => {
     const metricWithInfinity = {
       ...baseMetric,
-      ci: [-Infinity, Infinity] as [number, number],
-      ciAdjusted: [-Infinity, Infinity] as [number, number],
+      ci: [-Infinity, Infinity],
+      ciAdjusted: [-Infinity, Infinity],
     };
 
     const result =
@@ -22,8 +22,8 @@ describe("safe-rollout-snapshot validators", () => {
   it("accepts valid number values in ci fields", () => {
     const metricWithNumbers = {
       ...baseMetric,
-      ci: [-0.5, 0.5] as [number, number],
-      ciAdjusted: [-0.3, 0.7] as [number, number],
+      ci: [-0.5, 0.5],
+      ciAdjusted: [-0.3, 0.7],
     };
 
     const result = safeRolloutSnapshotMetricObject.safeParse(metricWithNumbers);
