ac32269e1f77e29e29d4722f0cd386301c14e4e6
Ryan Feigenbaum <48868107+royalfig@users.noreply.github.com>
2025-12-05 18:20:40 -0500

    Revert schema to match type signature

--- packages/back-end/types/openapi.d.ts
+++ packages/back-end/types/openapi.d.ts
@@ -8000,7 +8000,7 @@ export interface operations {
                  * @description The reason why the feature is stale (only present if stale is true) 
                  * @enum {string}
                  */
-                reason?: "no-rules" | "rules-one-sided";
+                reason?: "error" | "no-rules" | "rules-one-sided";
               })[];
           }) & {
             limit: number;
