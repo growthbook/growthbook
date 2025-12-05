7caa0fc2e1c6064e6132b8eda67828686d0898e4
Michael <mknowlton89@gmail.com>
2025-12-05 13:57:04 -0500

    Small qol improvement - auto collapses the configuration tab when a user opens the edit block IF there is already a metric selected

--- packages/front-end/enterprise/components/Dashboards/DashboardEditor/DashboardEditorSidebar/MetricExplorerSettings.tsx
+++ packages/front-end/enterprise/components/Dashboards/DashboardEditor/DashboardEditorSidebar/MetricExplorerSettings.tsx
@@ -86,7 +86,7 @@ export default function MetricExplorerSettings({ block, setBlock }: Props) {
         }}
       >
         <Collapsible
-          open={true}
+          open={!metric}
           transitionTime={100}
           trigger={
             <div
