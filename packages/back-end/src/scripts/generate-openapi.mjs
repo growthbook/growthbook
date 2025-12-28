95036ddded7f9ab6d8dd4a648ff8e699db459433
James Irwin <tallnerd@gmail.com>
2025-12-28 22:16:27 +0100

    fix import in openapi.d.ts

--- packages/back-end/src/scripts/generate-openapi.mjs
+++ packages/back-end/src/scripts/generate-openapi.mjs
@@ -36,7 +36,7 @@ async function run() {
   // Step 3: Add additional named types for easier access
   // Export each schema as a named type
   output += `import { z } from "zod";\n`;
-  output += `import * as openApiValidators from "shared/src/validators/openapi";\n`;
+  output += `import * as openApiValidators from "shared/validators";\n`;
   output += "\n// Schemas\n";
   Object.keys(api.components.schemas).forEach((k) => {
     // Zod validator for response body
