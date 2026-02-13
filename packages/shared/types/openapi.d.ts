ecaf1d87127b1bcf6319bb682b9078be6fae4a33
Franco Sarachu <fsarachu@gmail.com>
2026-02-13 10:58:23 -0300

    Add support for allowDuplicateTrackingKey parameter on both POST and PUT requests

--- packages/shared/types/openapi.d.ts
+++ packages/shared/types/openapi.d.ts
@@ -9500,6 +9500,8 @@ export interface operations {
           /** @description The ID property of one of the assignment query objects associated with the datasource */
           assignmentQueryId: string;
           trackingKey: string;
+          /** @description If true, allow creating an experiment even if another experiment with the same tracking key already exists */
+          allowDuplicateTrackingKey?: boolean;
           /** @description Name of the experiment */
           name: string;
           /** @enum {string} */
@@ -9985,6 +9987,8 @@ export interface operations {
           datasourceId?: string;
           assignmentQueryId?: string;
           trackingKey?: string;
+          /** @description If true, allow updating the tracking key even if another experiment with the same tracking key already exists */
+          allowDuplicateTrackingKey?: boolean;
           /** @description Name of the experiment */
           name?: string;
           /** @enum {string} */
