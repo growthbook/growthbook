6f6e1b90f62fbdeef078028aa7b0cd9ee2880073
James Irwin <tallnerd@gmail.com>
2026-02-16 15:25:51 +0100

    fix type check

--- packages/shared/src/util/constants.ts
+++ packages/shared/src/util/constants.ts
@@ -1,8 +1,3 @@
 // Isolated constants to avoid circular deps when util is loaded via jest.requireActual
 
-export const DEFAULT_ENVIRONMENT_IDS = [
-  "production",
-  "dev",
-  "staging",
-  "test",
-] as const;
+export const DEFAULT_ENVIRONMENT_IDS = ["production", "dev", "staging", "test"];
