32371a6bfc3ac09732a5037354f77ffb591bb2bc
James Irwin <tallnerd@gmail.com>
2026-02-16 14:48:17 +0100

    renamed backend jest config file

--- packages/back-end/jest.config.cjs
+++ packages/back-end/jest.config.cjs
@@ -2,12 +2,20 @@ module.exports = {
   moduleFileExtensions: ["ts", "js", "node", "json"],
   transform: {
     "^.+\\.(ts|tsx)$": "@swc/jest",
+    "shared/.+\\.(ts|tsx|js)$": "@swc/jest",
   },
+  transformIgnorePatterns: [
+    "node_modules/(?!shared/)",
+  ],
   testMatch: ["**/test/**/*.test.(ts|js)"],
   moduleNameMapper: {
     "^axios$": "axios/dist/axios.js",
     "^@typespec/ts-http-runtime/internal/(.*)$":
       "<rootDir>/../../node_modules/.pnpm/@typespec+ts-http-runtime@0.3.1/node_modules/@typespec/ts-http-runtime/dist/commonjs/$1/internal.js",
+    // Resolve shared package to src (required when shared uses "type": "module" and ESM .js imports)
+    "^shared/types/(.*)$": "<rootDir>/../shared/types/$1",
+    "^shared/(.*)$": "<rootDir>/../shared/src/$1",
+    "^(\\.\\.?/.*)\\.js$": "$1",
   },
   setupFilesAfterEnv: ["<rootDir>/test/jest.setup.ts"],
 };
