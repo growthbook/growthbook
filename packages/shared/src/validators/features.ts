6fdd055be31428fdf7700f5d04e39a779113da56
Luke Smith <lukebrawleysmith@gmail.com>
2026-02-20 08:50:37 -0800

    fixing typecheck issue in ExperimentRule

--- packages/shared/src/validators/features.ts
+++ packages/shared/src/validators/features.ts
@@ -121,6 +121,8 @@ const experimentRule = baseRule
     banditScheduleUnit: z.enum(["hours", "days"]).optional(),
     banditBurnInValue: z.number().optional(),
     banditBurnInUnit: z.enum(["hours", "days"]).optional(),
+    banditConversionWindowValue: z.number().optional(),
+    banditConversionWindowUnit: z.enum(["hours", "days"]).optional(),
     templateId: z.string().optional(),
     customFields: z.record(z.string(), z.any()).optional(),
   })
