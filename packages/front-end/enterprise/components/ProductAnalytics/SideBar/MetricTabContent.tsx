35eee41c167d03727ed05c8fc65dc17bdc809aa7
Michael <mknowlton89@gmail.com>
2026-02-23 11:05:34 -0500

    Updates metric tab to use MetricName formatter and also updates styles to wrap metric names so they're not hidden.

--- packages/front-end/enterprise/components/ProductAnalytics/SideBar/MetricTabContent.tsx
+++ packages/front-end/enterprise/components/ProductAnalytics/SideBar/MetricTabContent.tsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Flex } from "@radix-ui/themes";
 import { PiPlus } from "react-icons/pi";
 import type { MetricValue } from "shared/validators";
-import SelectField from "@/components/Forms/SelectField";
+import SelectField, {
+  GroupedValue,
+  SingleValue,
+} from "@/components/Forms/SelectField";
+import MetricName from "@/components/Metrics/MetricName";
 import Button from "@/ui/Button";
 import { useDefinitions } from "@/services/DefinitionsContext";
 import { generateUniqueValueName } from "@/enterprise/components/ProductAnalytics/util";
 import { useExplorerContext } from "@/enterprise/components/ProductAnalytics/ExplorerContext";
 import Text from "@/ui/Text";
 import ValueCard from "./ValueCard";
+import styles from "./ValueCard.module.scss";
 
 export default function MetricTabContent() {
   const { draftExploreState, addValueToDataset, updateValueInDataset } =
@@ -19,6 +24,29 @@ export default function MetricTabContent() {
     draftExploreState.dataset?.type === "metric"
       ? draftExploreState.dataset.values
       : [];
+
+  const metricOptions = useMemo(() => {
+    const groupedOptions: GroupedValue[] = [];
+    const managedMetrics: SingleValue[] = [];
+    const unManagedMetrics: SingleValue[] = [];
+    factMetrics.forEach((m) => {
+      if (m.managedBy) {
+        managedMetrics.push({ label: m.name, value: m.id });
+      } else {
+        unManagedMetrics.push({ label: m.name, value: m.id });
+      }
+    });
+    if (managedMetrics.length > 0) {
+      groupedOptions.push({
+        label: "Official Metrics",
+        options: managedMetrics,
+      });
+    }
+    if (unManagedMetrics.length > 0) {
+      groupedOptions.push({ label: "", options: unManagedMetrics });
+    }
+    return groupedOptions;
+  }, [factMetrics]);
 
   return (
     <Flex direction="column" gap="4">
@@ -33,6 +61,7 @@ export default function MetricTabContent() {
           <ValueCard key={idx} index={idx}>
             <Flex direction="column">
               <SelectField
+                className={styles.metricSelect}
                 value={v.metricId}
                 onChange={(val) => {
                   const newMetric = factMetrics.find((m) => m.id === val);
@@ -59,10 +88,15 @@ export default function MetricTabContent() {
 
                   updateValueInDataset(idx, updates as MetricValue);
                 }}
-                options={factMetrics.map((m) => ({
-                  label: m.name,
-                  value: m.id,
-                }))}
+                options={metricOptions}
+                formatOptionLabel={({ value }) => {
+                  const metric = factMetrics.find((m) => m.id === value);
+                  return metric ? <MetricName metric={metric} /> : value;
+                }}
+                formatGroupLabel={({ label }) => {
+                  return label;
+                }}
+                sort={false}
                 placeholder="Select metric..."
                 forceUndefinedValueToNull
               />
