bafe4da7841dd2e45490a26c9045ce2e8a3da808
Clement <clement@anthropic.com>
2026-04-03 08:54:39 -0600

    Support event quantile metrics in incremental refresh via KLL sketches (BigQuery) (#5532)

--- packages/front-end/components/Experiment/EditMetricsForm.tsx
+++ packages/front-end/components/Experiment/EditMetricsForm.tsx
@@ -207,7 +207,6 @@ const EditMetricsForm: FC<{
     >
       <ExperimentMetricsSelector
         noLegacyMetrics={isExperimentIncludedInIncrementalRefresh}
-        excludeQuantiles={isExperimentIncludedInIncrementalRefresh}
         datasource={experiment.datasource}
         exposureQueryId={experiment.exposureQueryId}
         project={experiment.project}
