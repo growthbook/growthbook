29932f4b80c9ff322c4952964c5af95c07e1ff0f
lukesonnet <luke.sonnet@gmail.com>
2026-05-05 11:02:00 -0700

    ci fix: use ModalStandard on did audit log

--- packages/front-end/pages/datasources/[did].tsx
+++ packages/front-end/pages/datasources/[did].tsx
@@ -40,7 +40,7 @@ import { useCombinedMetrics } from "@/components/Metrics/MetricsList";
 import { FeatureEvaluationQueries } from "@/components/Settings/EditDataSource/FeatureEvaluationQueries/FeatureEvaluationQueries";
 import Heading from "@/ui/Heading";
 import Text from "@/ui/Text";
-import Modal from "@/components/Modal";
+import ModalStandard from "@/ui/Modal/Patterns/ModalStandard";
 import HistoryTable from "@/components/HistoryTable";
 
 function quotePropertyName(name: string) {
@@ -536,7 +536,7 @@ mixpanel.init('YOUR PROJECT TOKEN', {
         />
       )}
       {auditModal && (
-        <Modal
+        <ModalStandard
           trackingEventModalType=""
           open={true}
           header="Audit Log"
@@ -545,7 +545,7 @@ mixpanel.init('YOUR PROJECT TOKEN', {
           closeCta="Close"
         >
           <HistoryTable type={"datasource"} id={d.id} />
-        </Modal>
+        </ModalStandard>
       )}
     </div>
   );
