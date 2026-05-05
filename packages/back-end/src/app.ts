a9153864bc0569ffb851c9545b18ef57816fff31
nhat-growthbook <nhat@growthbook.io>
2026-05-05 00:56:03 -0700

    Disable attribute types when forwarder is enabled (#5799)
    * Disable attribute types when forwarder is enabled
    
    * Stupid AI
    
    * Remove unused stuffs

--- packages/back-end/src/app.ts
+++ packages/back-end/src/app.ts
@@ -951,8 +951,8 @@ app.post(
 app.get("/datasources", datasourcesController.getDataSources);
 app.get("/datasource/:id", datasourcesController.getDataSource);
 app.get(
-  "/event-forwarder/has-ready",
-  datasourcesController.getHasReadyEventForwarder,
+  "/event-forwarder/exists",
+  datasourcesController.getEventForwarderExists,
 );
 app.post("/datasources", datasourcesController.postDataSources);
 app.post(
