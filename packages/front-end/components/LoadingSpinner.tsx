dd038ab6782dbd8ac33ff8ddb910012ca37388d1
Nhat Ho <nhat@growthbook.io>
2026-06-04 00:54:18 -0700

    NO

--- packages/front-end/components/LoadingSpinner.tsx
+++ packages/front-end/components/LoadingSpinner.tsx
@@ -3,9 +3,7 @@ import { FaSpinner } from "react-icons/fa";
 import clsx from "clsx";
 import styles from "./LoadingSpinner.module.scss";
 
-export default function LoadingSpinner({
-  className,
-  ...otherProps
-}: React.ComponentProps<typeof FaSpinner>): React.ReactElement {
-  return <FaSpinner className={clsx(styles.spin, className)} {...otherProps} />;
+export default function LoadingSpinner({ ...otherProps }): React.ReactElement {
+  const className = clsx(styles.spin, otherProps.className);
+  return <FaSpinner className={className} />;
 }
