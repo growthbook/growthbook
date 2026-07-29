8a15a1bbd768a13219220ae921278aae79aac128
Adriel Vieira <ahdriel@gmail.com>
2026-07-29 12:16:05 -0700

    add dataTypeFromWarehouse to fact table columns

--- packages/shared/types/fact-table.d.ts
+++ packages/shared/types/fact-table.d.ts
@@ -39,6 +39,7 @@ export interface ColumnInterface {
   description: string;
   column: string;
   datatype: FactTableColumnType;
+  dataTypeFromWarehouse?: FactTableColumnType;
   numberFormat: NumberFormat;
   alwaysInlineFilter?: boolean;
   topValues?: string[];
