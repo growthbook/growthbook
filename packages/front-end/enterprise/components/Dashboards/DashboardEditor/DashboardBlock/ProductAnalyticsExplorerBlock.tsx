88bbd30ee2267ba7474af5ef971f57920006b7bf
Michael <mknowlton89@gmail.com>
2026-07-29 11:40:00 -0400

    Another type error resolved

--- packages/front-end/enterprise/components/Dashboards/DashboardEditor/DashboardBlock/ProductAnalyticsExplorerBlock.tsx
+++ packages/front-end/enterprise/components/Dashboards/DashboardEditor/DashboardBlock/ProductAnalyticsExplorerBlock.tsx
@@ -64,6 +64,7 @@ export function ProductAnalyticsExplorerVisualization({
     | FactTableExplorationBlockInterface
     | DataSourceExplorationBlockInterface
     | SqlExplorationBlockInterface
+    | FunnelExplorationBlockInterface
   >;
   dashboardGlobalControls?: BlockProps<SqlExplorationBlockInterface>["dashboardGlobalControls"];
 }) {
