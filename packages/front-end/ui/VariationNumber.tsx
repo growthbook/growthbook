06c2439b5032d2700ad4a8240e1bcac4b33f3c90
Adriel Vieira <ahdriel@gmail.com>
2026-07-31 12:57:34 -0700

    robot magic

--- packages/front-end/ui/VariationNumber.tsx
+++ packages/front-end/ui/VariationNumber.tsx
@@ -6,8 +6,39 @@ type VariationNumberProps = { number: number } & ComponentPropsWithoutRef<
   typeof Box
 >;
 
+type DigitEdgeMetrics = {
+  actualLeft: number;
+  trailingSpace: number;
+};
+
+// Canvas text metrics for Inter 500 at 11px, matching VariationNumber.module.scss.
+const interDigitEdgeMetrics = new Map<string, DigitEdgeMetrics>([
+  ["0", { actualLeft: -0.605, trailingSpace: 0.605 }],
+  ["1", { actualLeft: -0.51, trailingSpace: 0.886 }],
+  ["2", { actualLeft: -0.764, trailingSpace: 0.722 }],
+  ["3", { actualLeft: -0.624, trailingSpace: 0.605 }],
+  ["4", { actualLeft: -0.61, trailingSpace: 0.587 }],
+  ["5", { actualLeft: -0.626, trailingSpace: 0.605 }],
+  ["6", { actualLeft: -0.605, trailingSpace: 0.605 }],
+  ["7", { actualLeft: -0.486, trailingSpace: 0.486 }],
+  ["8", { actualLeft: -0.605, trailingSpace: 0.605 }],
+  ["9", { actualLeft: -0.605, trailingSpace: 0.605 }],
+]);
+
+function getOpticalOffset(number: number): number {
+  const digits = String(number);
+  const firstDigit = interDigitEdgeMetrics.get(digits.charAt(0));
+  const lastDigit = interDigitEdgeMetrics.get(digits.charAt(digits.length - 1));
+
+  if (!firstDigit || !lastDigit) return 0;
+
+  return (firstDigit.actualLeft + lastDigit.trailingSpace) / 2;
+}
+
 export default forwardRef<HTMLDivElement, VariationNumberProps>(
   function VariationNumber({ number, className, ...rest }, ref) {
+    const opticalOffset = getOpticalOffset(number);
+
     return (
       <Box
         ref={ref}
@@ -17,7 +48,12 @@ export default forwardRef<HTMLDivElement, VariationNumberProps>(
         }`}
       >
         <Box as="span" className={styles.label}>
-          {number}
+          <span
+            className={styles.number}
+            style={{ transform: `translateX(${opticalOffset}px)` }}
+          >
+            {number}
+          </span>
         </Box>
       </Box>
     );
