233e467554bb6f1522421c0606c932f913a40e7c
Adriel Vieira <ahdriel@gmail.com>
2026-07-31 13:25:43 -0700

    more things

--- packages/front-end/ui/VariationNumber.tsx
+++ packages/front-end/ui/VariationNumber.tsx
@@ -6,24 +6,8 @@ type VariationNumberProps = { number: number } & ComponentPropsWithoutRef<
   typeof Box
 >;
 
-// The `1`'s flag makes it look left-heavy when its text box is centered.
-const digitOpticalOffsets = new Map<string, number>([["1", 0.5]]);
-
-function getOpticalOffset(number: number): number {
-  const digits = String(number);
-
-  return (
-    [...digits].reduce(
-      (offset, digit) => offset + (digitOpticalOffsets.get(digit) ?? 0),
-      0,
-    ) / digits.length
-  );
-}
-
 export default forwardRef<HTMLDivElement, VariationNumberProps>(
   function VariationNumber({ number, className, ...rest }, ref) {
-    const opticalOffset = getOpticalOffset(number);
-
     return (
       <Box
         ref={ref}
@@ -33,12 +17,7 @@ export default forwardRef<HTMLDivElement, VariationNumberProps>(
         }`}
       >
         <Box as="span" className={styles.label}>
-          <span
-            className={styles.number}
-            style={{ transform: `translateX(${opticalOffset}px)` }}
-          >
-            {number}
-          </span>
+          <span className={styles.number}>{number}</span>
         </Box>
       </Box>
     );
