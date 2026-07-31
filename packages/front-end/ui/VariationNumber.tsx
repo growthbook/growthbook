f24c33d43bc85773a314726f4ee0a2ba34abf136
Adriel Vieira <ahdriel@gmail.com>
2026-07-31 13:15:36 -0700

    more robot magic

--- packages/front-end/ui/VariationNumber.tsx
+++ packages/front-end/ui/VariationNumber.tsx
@@ -6,33 +6,18 @@ type VariationNumberProps = { number: number } & ComponentPropsWithoutRef<
   typeof Box
 >;
 
-type DigitEdgeMetrics = {
-  actualLeft: number;
-  trailingSpace: number;
-};
-
-// Canvas text metrics for Inter 500 at 11px, matching VariationNumber.module.scss.
-const interDigitEdgeMetrics = new Map<string, DigitEdgeMetrics>([
-  ["0", { actualLeft: -0.605, trailingSpace: 0.605 }],
-  ["1", { actualLeft: -0.51, trailingSpace: 0.886 }],
-  ["2", { actualLeft: -0.764, trailingSpace: 0.722 }],
-  ["3", { actualLeft: -0.624, trailingSpace: 0.605 }],
-  ["4", { actualLeft: -0.61, trailingSpace: 0.587 }],
-  ["5", { actualLeft: -0.626, trailingSpace: 0.605 }],
-  ["6", { actualLeft: -0.605, trailingSpace: 0.605 }],
-  ["7", { actualLeft: -0.486, trailingSpace: 0.486 }],
-  ["8", { actualLeft: -0.605, trailingSpace: 0.605 }],
-  ["9", { actualLeft: -0.605, trailingSpace: 0.605 }],
-]);
+// The `1`'s flag makes it look left-heavy when its text box is centered.
+const digitOpticalOffsets = new Map<string, number>([["1", 0.5]]);
 
 function getOpticalOffset(number: number): number {
   const digits = String(number);
-  const firstDigit = interDigitEdgeMetrics.get(digits.charAt(0));
-  const lastDigit = interDigitEdgeMetrics.get(digits.charAt(digits.length - 1));
 
-  if (!firstDigit || !lastDigit) return 0;
-
-  return (firstDigit.actualLeft + lastDigit.trailingSpace) / 2;
+  return (
+    [...digits].reduce(
+      (offset, digit) => offset + (digitOpticalOffsets.get(digit) ?? 0),
+      0,
+    ) / digits.length
+  );
 }
 
 export default forwardRef<HTMLDivElement, VariationNumberProps>(
