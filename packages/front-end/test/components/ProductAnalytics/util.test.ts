c7ca525158657213a85d9ab881674972a92d550f
Nobuhide Ajito <najito@growthbook.io>
2026-08-10 14:25:27 -0700

    Fix leftover factTable reference broken by factTableId rename

--- packages/front-end/test/components/ProductAnalytics/util.test.ts
+++ packages/front-end/test/components/ProductAnalytics/util.test.ts
@@ -549,13 +549,13 @@ describe("getRelevantFactTableIds", () => {
       steps: [
         {
           name: "s1",
-          factTable: "step1_ft",
+          factTableId: "step1_ft",
           rowFilters: [],
           optional: false,
         },
         {
           name: "s2",
-          factTable: "step2_ft",
+          factTableId: "step2_ft",
           rowFilters: [],
           optional: false,
         },
