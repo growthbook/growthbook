f84bdb51e9128ee5fc9c2b45ba566b1b8dd08e5a
Graham McNicoll <graham@growthbook.io>
2026-09-07 11:33:13 -0700

    Keep Slack workspace metadata exclusively on connections

--- packages/back-end/src/models/EventWebhookModel.ts
+++ packages/back-end/src/models/EventWebhookModel.ts
@@ -41,18 +41,10 @@ const eventWebHookSchema = new mongoose.Schema({
     required: false,
   },
   slack: {
-    appId: String,
     teamId: String,
-    teamName: String,
-    enterpriseId: String,
-    enterpriseName: String,
     channelName: String,
     channelId: String,
     configurationUrl: String,
-    botUserId: String,
-    authedUserId: String,
-    scope: String,
-    isEnterpriseInstall: Boolean,
   },
   method: {
     type: String,
@@ -194,9 +186,6 @@ type EventWebHookDocument = mongoose.Document & EventWebHookInterface;
  */
 const toInterface = (doc: EventWebHookDocument): EventWebHookInterface => {
   const payload = omit(doc.toJSON<EventWebHookDocument>(), ["__v", "_id"]);
-  if (payload.slack && "botAccessToken" in payload.slack) {
-    delete (payload.slack as Record<string, unknown>).botAccessToken;
-  }
 
   // Add defaults values
   const defaults = {
