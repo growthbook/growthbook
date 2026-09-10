e6f353d56b21bbfd68c8f466872c3e5702db7d6c
Bryce <bryce1@gmail.com>
2026-09-09 23:45:11 -0700

    Match the test-query button size to its row

--- packages/front-end/components/SchemaBrowser/EditSqlModal.tsx
+++ packages/front-end/components/SchemaBrowser/EditSqlModal.tsx
@@ -312,7 +312,7 @@ export default function EditSqlModal({
                           shouldDisplay={!canRunQueries}
                         >
                           <RadixButton
-                            size="sm"
+                            size="md"
                             onClick={handleTestQuery}
                             loading={testingQuery}
                             disabled={!canRunQueries}
