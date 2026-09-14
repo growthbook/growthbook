378f3c5c9f701ecfa2052ea17ad67fb2ffe422c4
Nobuhide Ajito <najito@growthbook.io>
2026-09-14 12:35:05 -0700

    Clean up preview integration after rebasing onto main

--- packages/back-end/src/routers/fact-table/fact-table.controller.ts
+++ packages/back-end/src/routers/fact-table/fact-table.controller.ts
@@ -1,7 +1,6 @@
 import type { Response } from "express";
 import {
   canInlineFilterColumn,
-  expandVirtualColumnsInSql,
   getColumnRefWhereClause,
   getFactTableTimestampColumn,
 } from "shared/experiments";
