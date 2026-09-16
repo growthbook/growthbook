7d954a7e780787407d822534aa94f092198a0c68
Bryce <bryce1@gmail.com>
2026-09-15 21:26:16 -0700

    Rename the Members nav item to Members and Teams

--- packages/front-end/components/Layout/sidebarNav.ts
+++ packages/front-end/components/Layout/sidebarNav.ts
@@ -270,7 +270,7 @@ export const navlinks: SidebarLinkProps[] = [
           permissionsUtils.canViewProjectsPage(),
       },
       {
-        name: "Members",
+        name: "Members and Teams",
         href: "/settings/team",
         path: /^settings\/team/,
         filter: ({ permissionsUtils }) => permissionsUtils.canManageTeam(),
