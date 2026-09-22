f3e4813ba87d5ea14ab0bd4b1ca439c1aa2dabf1
Michael <mknowlton89@gmail.com>
2026-09-22 14:38:15 -0400

    Adds SCIM support for OneLogin

--- packages/back-end/src/scim/users/users.router.ts
+++ packages/back-end/src/scim/users/users.router.ts
@@ -4,6 +4,7 @@ import { createUser } from "./createUser";
 import { listUsers } from "./listUsers";
 import { patchUser } from "./patchUser";
 import { putUser } from "./putUser";
+import { deleteUser } from "./deleteUser";
 
 const router = Router();
 
@@ -14,5 +15,6 @@ router.get("/:id", getUser as unknown as RequestHandler);
 router.post("/", createUser as unknown as RequestHandler);
 router.patch("/:id", patchUser as unknown as RequestHandler); // For Okta: Only used for user activation & deactivation (and password sync but that shouldn't be relevant)
 router.put("/:id", putUser as unknown as RequestHandler); // Only supports updating user's global role
+router.delete("/:id", deleteUser as unknown as RequestHandler); // OneLogin (and others) DELETE deactivates instead of hard-deleting
 
 export default router;
