23393a06b7fe8ae8475a3e2349deb442100ca503
Adriel Vieira <ahdriel@gmail.com>
2026-09-24 13:28:29 -0700

    datasources(bigquery): Add option to use custom endpoint (#6995)

--- packages/front-end/components/Settings/ConnectionSettings.tsx
+++ packages/front-end/components/Settings/ConnectionSettings.tsx
@@ -1,7 +1,9 @@
 import { DataSourceInterfaceWithParams } from "shared/types/datasource";
-import { ChangeEventHandler, ReactNode } from "react";
+import { ChangeEventHandler, ReactNode, useState } from "react";
+import { PiCaretRightFill } from "react-icons/pi";
+import Collapsible from "react-collapsible";
 import AthenaForm from "./AthenaForm";
-import BigQueryForm from "./BigQueryForm";
+import BigQueryForm, { BigQueryAdvancedSettings } from "./BigQueryForm";
 import ClickHouseForm from "./ClickHouseForm";
 import GoogleAnalyticsForm from "./GoogleAnalyticsForm";
 import MixpanelForm from "./MixpanelForm";
@@ -31,6 +33,12 @@ export default function ConnectionSettings({
   hasError,
   beforeAdvancedSettings,
 }: Props) {
+  const [advancedOpen, setAdvancedOpen] = useState(
+    !!datasource.settings?.maxConcurrentQueries ||
+      !!datasource.settings?.queryCacheTTLMins ||
+      (datasource.type === "bigquery" && !!datasource.params?.apiEndpoint),
+  );
+
   // Set the new params (specific per-datasource) and optionally settings (shared between datasources)
   const setParams = (
     params: { [key: string]: string | boolean },
@@ -191,6 +199,7 @@ export default function ConnectionSettings({
         <BigQueryForm
           existing={storedCredentials}
           datasourceId={storedCredentials ? datasource.id : undefined}
+          projects={datasource.projects}
           setParams={setParams}
           params={datasource?.params || {}}
           onParamChange={onParamChange}
@@ -224,11 +233,33 @@ export default function ConnectionSettings({
     <>
       {datasourceComponent}
       {beforeAdvancedSettings}
-      <SharedConnectionSettings
-        type={datasource.type}
-        onSettingChange={onSettingChange}
-        settings={datasource?.settings || {}}
-      />
+      <div className="mb-3">
+        <Collapsible
+          trigger={
+            <div className="link-purple font-weight-bold mb-2">
+              <PiCaretRightFill className="chevron mr-1" />
+              Advanced Settings
+            </div>
+          }
+          open={advancedOpen}
+          onClose={() => setAdvancedOpen(false)}
+          transitionTime={100}
+        >
+          <div className="rounded px-3 pt-3 pb-1 bg-highlight">
+            {datasource.type === "bigquery" && (
+              <BigQueryAdvancedSettings
+                params={datasource.params || {}}
+                onParamChange={onParamChange}
+              />
+            )}
+            <SharedConnectionSettings
+              type={datasource.type}
+              onSettingChange={onSettingChange}
+              settings={datasource?.settings || {}}
+            />
+          </div>
+        </Collapsible>
+      </div>
     </>
   );
 }
